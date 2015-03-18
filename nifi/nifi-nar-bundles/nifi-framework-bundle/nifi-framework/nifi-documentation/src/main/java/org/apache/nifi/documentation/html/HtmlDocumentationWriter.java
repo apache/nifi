fd89849f5f081c4a0c67c0dec63130800ba6f03a
danbress <dbress@onyxconsults.com>
2015-03-18 15:30:50 -0400

    NIFI-309 making sure dynamic property exists

--- nifi/nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-documentation/src/main/java/org/apache/nifi/documentation/html/HtmlDocumentationWriter.java
+++ nifi/nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-documentation/src/main/java/org/apache/nifi/documentation/html/HtmlDocumentationWriter.java
@@ -409,7 +409,9 @@ public class HtmlDocumentationWriter implements DocumentationWriter {
         }
         
         final DynamicProperty dynProp = configurableComponent.getClass().getAnnotation(DynamicProperty.class);
-        dynamicProperties.add(dynProp);
+        if (dynProp != null) {
+            dynamicProperties.add(dynProp);
+        }
         
         return dynamicProperties;
     }
