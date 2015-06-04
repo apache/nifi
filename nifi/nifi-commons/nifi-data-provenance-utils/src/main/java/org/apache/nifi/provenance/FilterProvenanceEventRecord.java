1383e8c43a89f2bddb809b5df7e093996694efd8
BELUGABEHR <BELUGABEHR@users.noreply.github.com>
2015-06-03 21:09:17 -0400

    Added license information Added class JavaDoc
    Updated coumentation
    Updated scope of constructor to protected
    
    Updated documentation
    Added license information
    Changed visibility of constructor of FilterProvenanceEventRecord

--- nifi/nifi-commons/nifi-data-provenance-utils/src/main/java/org/apache/nifi/provenance/FilterProvenanceEventRecord.java
+++ nifi/nifi-commons/nifi-data-provenance-utils/src/main/java/org/apache/nifi/provenance/FilterProvenanceEventRecord.java
@@ -1,14 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.nifi.provenance;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * A ProvenanceEventRecord that contains some other ProvenanceEventRecord, which
+ * it uses as its basic source of data, possibly transforming the data along the
+ * way or providing additional functionality. The class
+ * FilterProvenanceEventRecord itself simply overrides all methods of
+ * ProvenanceEventRecord with versions that pass all requests to the contained
+ * ProvenanceEventRecord. Subclasses of FilterProvenanceEventRecord may further
+ * override some of these methods and may also provide additional methods and
+ * fields.
+ */
 public abstract class FilterProvenanceEventRecord implements ProvenanceEventRecord {
 
+    /** The ProvenanceEventRecord to be filtered. */
     final protected ProvenanceEventRecord record;
 
-    public FilterProvenanceEventRecord(ProvenanceEventRecord record) {
+    protected FilterProvenanceEventRecord(final ProvenanceEventRecord record) {
         this.record = record;
     }
 
--- nifi/nifi-nar-bundles/nifi-provenance-repository-bundle/nifi-volatile-provenance-repository/src/main/java/org/apache/nifi/provenance/IdEnrichedProvenanceEvent.java
+++ nifi/nifi-nar-bundles/nifi-provenance-repository-bundle/nifi-volatile-provenance-repository/src/main/java/org/apache/nifi/provenance/IdEnrichedProvenanceEvent.java
@@ -1,5 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.nifi.provenance;
 
+/**
+ * Simple filter class to assign an arbitrary ID to a provenance event
+ */
 final class IdEnrichedProvenanceEvent extends FilterProvenanceEventRecord {
 
     private long eventId;
@@ -15,12 +35,6 @@ final class IdEnrichedProvenanceEvent extends FilterProvenanceEventRecord {
 
     public void setEventId(final long eventId) {
         this.eventId = eventId;
-    }
-
-    @Override
-    public String toString() {
-        return "IdEnrichedProvenanceEvent [eventId=" + this.eventId + ", record=" + super.record
-                + "]";
     }
 
     @Override
@@ -48,4 +62,9 @@ final class IdEnrichedProvenanceEvent extends FilterProvenanceEventRecord {
         return true;
     }
 
+    @Override
+    public String toString() {
+        return "IdEnrichedProvenanceEvent [eventId=" + this.eventId + ", record=" + super.record
+                + "]";
+    }
 }
