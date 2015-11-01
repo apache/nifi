ad849c77dff7b379116f4d57510c7b9136c7f4c0
Mark Payne <markap14@hotmail.com>
2015-11-01 14:37:01 -0500

    NIFI-1090: Fixed log message that was at info level but should have been debug level

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/main/java/org/apache/nifi/controller/repository/FileSystemRepository.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/main/java/org/apache/nifi/controller/repository/FileSystemRepository.java
@@ -1208,7 +1208,15 @@ public class FileSystemRepository implements ContentRepository {
         final long startNanos = System.nanoTime();
         final long toFree = minRequiredSpace - usableSpace;
         final BlockingQueue<ArchiveInfo> fileQueue = archivedFiles.get(containerName);
-        archiveExpirationLog.info("Currently {} bytes free for Container {}; requirement is {} byte free, so need to free {} bytes", usableSpace, containerName, minRequiredSpace, toFree);
+        if (archiveExpirationLog.isDebugEnabled()) {
+            if (toFree < 0) {
+                archiveExpirationLog.debug("Currently {} bytes free for Container {}; requirement is {} byte free, so no need to free space until an additional {} bytes are used",
+                    usableSpace, containerName, minRequiredSpace, Math.abs(toFree));
+            } else {
+                archiveExpirationLog.debug("Currently {} bytes free for Container {}; requirement is {} byte free, so need to free {} bytes",
+                    usableSpace, containerName, minRequiredSpace, toFree);
+            }
+        }
 
         ArchiveInfo toDelete;
         int deleteCount = 0;
