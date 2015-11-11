70cc61bb53d0fc46468f706465f34790d714fff2
Oleg Zhurakousky <oleg@suitcase.io>
2015-11-10 19:57:01 -0500

    NIFI-1124, NIFI-1062 addressed PR comments
    Signed-off-by: Bryan Bende <bbende@apache.org>

--- nifi-nar-bundles/nifi-hadoop-bundle/nifi-hdfs-processors/src/main/java/org/apache/nifi/processors/hadoop/PutHDFS.java
+++ nifi-nar-bundles/nifi-hadoop-bundle/nifi-hdfs-processors/src/main/java/org/apache/nifi/processors/hadoop/PutHDFS.java
@@ -345,7 +345,7 @@ public class PutHDFS extends AbstractHadoopProcessor {
                 }
             }
             getLogger().error("Failed to write to HDFS due to {}", t);
-            session.transfer(flowFile, REL_FAILURE);
+            session.transfer(session.penalize(flowFile), REL_FAILURE);
             context.yield();
         }
     }
