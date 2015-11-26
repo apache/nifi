0609a84fa2b84085cea48cfb60095fd75a3bf07a
joewitt <joewitt@apache.org>
2015-11-26 01:10:11 -0500

    NIFI-1059 Amending patch to fix identified logging output defect

--- nifi-bootstrap/src/main/java/org/apache/nifi/bootstrap/RunNiFi.java
+++ nifi-bootstrap/src/main/java/org/apache/nifi/bootstrap/RunNiFi.java
@@ -705,9 +705,9 @@ public class RunNiFi {
         } catch (final IOException ioe) {
             if (pid == null) {
                 logger.error("Failed to send shutdown command to port {} due to {}. No PID found for the NiFi process, so unable to kill process; "
-                    + "the process should be killed manually.", new Object[] {port, ioe.toString(), ioe});
+                    + "the process should be killed manually.", new Object[] {port, ioe.toString()});
             } else {
-                logger.error("Failed to send shutdown command to port {} due to {}. Will kill the NiFi Process with PID {}.", new Object[] {port, ioe.toString(), ioe, pid});
+                logger.error("Failed to send shutdown command to port {} due to {}. Will kill the NiFi Process with PID {}.", new Object[] {port, ioe.toString(), pid});
                 killProcessTree(pid, logger);
             }
         } finally {
