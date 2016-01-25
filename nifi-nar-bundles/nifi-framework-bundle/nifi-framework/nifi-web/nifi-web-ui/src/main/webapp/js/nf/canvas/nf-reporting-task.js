d71266502c80a58fa51e642d7a88cf5825a60e65
Matt Gilman <matt.c.gilman@gmail.com>
2016-01-25 10:20:13 -0500

    NIFI-1435: - Addressing typo when initializing and showing the reporting task property table which prevented dynamic properties to be created.

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-ui/src/main/webapp/js/nf/canvas/nf-reporting-task.js
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-ui/src/main/webapp/js/nf/canvas/nf-reporting-task.js
@@ -356,7 +356,7 @@ nf.ReportingTask = (function () {
             $('#reporting-task-properties').propertytable({
                 readOnly: false,
                 dialogContainer: '#new-reporting-task-property-container',
-                deferredDescriptor: getReportingTaskPropertyDescriptor,
+                descriptorDeferred: getReportingTaskPropertyDescriptor,
                 goToServiceDeferred: goToServiceFromProperty
             });
         },
@@ -377,7 +377,7 @@ nf.ReportingTask = (function () {
                 $('#reporting-task-properties').propertytable('destroy').propertytable({
                     readOnly: false,
                     dialogContainer: '#new-reporting-task-property-container',
-                    deferredDescriptor: getReportingTaskPropertyDescriptor,
+                    descriptorDeferred: getReportingTaskPropertyDescriptor,
                     goToServiceDeferred: goToServiceFromProperty
                 });
                 
