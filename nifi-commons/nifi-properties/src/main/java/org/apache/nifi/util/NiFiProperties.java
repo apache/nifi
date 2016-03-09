d8b23571d7a02b63a46151082574d7bce8f2f31f
Oleg Zhurakousky <oleg@suitcase.io>
2016-03-09 12:30:11 -0500

    NIFI-1464 ensured that OnUnscheduled is treated the same as OnScheduled
    NIFI-1464 polished javadocs, error messages and docs

--- nifi-commons/nifi-properties/src/main/java/org/apache/nifi/util/NiFiProperties.java
+++ nifi-commons/nifi-properties/src/main/java/org/apache/nifi/util/NiFiProperties.java
@@ -71,7 +71,7 @@ public class NiFiProperties extends Properties {
     public static final String ADMINISTRATIVE_YIELD_DURATION = "nifi.administrative.yield.duration";
     public static final String PERSISTENT_STATE_DIRECTORY = "nifi.persistent.state.directory";
     public static final String BORED_YIELD_DURATION = "nifi.bored.yield.duration";
-    public static final String PROCESSOR_START_TIMEOUT = "nifi.processor.start.timeout";
+    public static final String PROCESSOR_SCHEDULING_TIMEOUT = "nifi.processor.scheduling.timeout";
 
     // content repository properties
     public static final String REPOSITORY_CONTENT_PREFIX = "nifi.content.repository.directory.";
