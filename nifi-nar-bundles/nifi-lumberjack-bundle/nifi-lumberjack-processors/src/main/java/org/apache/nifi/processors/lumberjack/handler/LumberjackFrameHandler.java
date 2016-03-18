5a8979150ccef1bee9f90270dd1e37efa1d020ea
Andre F de Miranda <trixpan@users.noreply.github.com>
2016-03-19 01:41:35 +1100

    NIFI-856 - Implements ListenLumberjack Processor
    This closes #290.
    
    Signed-off-by: Aldrin Piri <aldrin@apache.org>

---
+++ nifi-nar-bundles/nifi-lumberjack-bundle/nifi-lumberjack-processors/src/main/java/org/apache/nifi/processors/lumberjack/handler/LumberjackFrameHandler.java
@@ -0,0 +1,114 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.processors.lumberjack.handler;
+
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+
+import org.apache.nifi.logging.ComponentLog;
+import org.apache.nifi.processor.util.listen.dispatcher.AsyncChannelDispatcher;
+import org.apache.nifi.processor.util.listen.event.Event;
+import org.apache.nifi.processor.util.listen.event.EventFactory;
+import org.apache.nifi.processor.util.listen.event.EventFactoryUtil;
+import org.apache.nifi.processor.util.listen.event.EventQueue;
+import org.apache.nifi.processor.util.listen.response.ChannelResponder;
+import org.apache.nifi.processors.lumberjack.event.LumberjackMetadata;
+import org.apache.nifi.processors.lumberjack.frame.LumberjackEncoder;
+import org.apache.nifi.processors.lumberjack.frame.LumberjackFrame;
+
+import com.google.gson.Gson;
+
+/**
+ * Encapsulates the logic to handle a LumberjackFrame once it has been read from the channel.
+ */
+public class LumberjackFrameHandler<E extends Event<SocketChannel>> {
+
+    private final Charset charset;
+    private final EventFactory<E> eventFactory;
+    private final EventQueue<E> events;
+    private final SelectionKey key;
+    private final AsyncChannelDispatcher dispatcher;
+    private final LumberjackEncoder encoder;
+    private final ComponentLog logger;
+
+    public LumberjackFrameHandler(final SelectionKey selectionKey,
+                                  final Charset charset,
+                                  final EventFactory<E> eventFactory,
+                                  final BlockingQueue<E> events,
+                                  final AsyncChannelDispatcher dispatcher,
+                                  final ComponentLog logger) {
+        this.key = selectionKey;
+        this.charset = charset;
+        this.eventFactory = eventFactory;
+        this.dispatcher = dispatcher;
+        this.logger = logger;
+        this.events = new EventQueue<>(events, logger);
+        this.encoder = new LumberjackEncoder();
+    }
+
+    public void handle(final LumberjackFrame frame, final ChannelResponder<SocketChannel> responder, final String sender)
+            throws IOException, InterruptedException {
+
+        final Map<String, String> metadata = EventFactoryUtil.createMapWithSender(sender.toString());
+        metadata.put(LumberjackMetadata.SEQNUMBER_KEY, String.valueOf(frame.getSeqNumber()));
+        String line = "";
+
+        /* If frameType is a data Frame, Handle the Lumberjack data payload, iterating over it and extracting
+        keys and values into metadata.
+
+        All keys are inserted into metadata with the exception of line that gets added into the body of the event
+        */
+        if (frame.getFrameType() == 0x44) {
+            ByteBuffer currentData = ByteBuffer.wrap(frame.getPayload());
+            long pairCount = currentData.getInt() & 0x00000000ffffffffL;
+            Map<String,String> fields = new HashMap<>();
+            for (int i = 0; i < pairCount; i++) {
+                long keyLength = currentData.getInt() & 0x00000000ffffffffL;
+                byte[] bytes = new byte[(int) keyLength];
+                currentData.get(bytes);
+                String key = new String(bytes);
+                long valueLength = currentData.getInt() & 0x00000000ffffffffL;
+                bytes = new byte[(int) valueLength];
+                currentData.get(bytes);
+                String value = new String(bytes);
+
+                if (key.equals("line")) {
+                    line = value;
+                } else {
+                    fields.put(key, value);
+                }
+            }
+            // Serialize the fields into a String to push it via metdate
+            Gson serialFields = new Gson();
+
+            metadata.put("lumberjack.fields", serialFields.toJson(fields).toString());
+
+            // queue the raw event blocking until space is available, reset the buffer
+            final E event = eventFactory.create(line.getBytes(), metadata, responder);
+            events.offer(event);
+        } else if (frame.getFrameType() == 0x4A ) {
+            logger.error("Data type was JSON. JSON payload aren't yet supported, pending the documentation of Lumberjack protocol v2");
+        }
+    }
+  }
---
+++ nifi-nar-bundles/nifi-lumberjack-bundle/nifi-lumberjack-processors/src/test/java/org/apache/nifi/processors/lumberjack/frame/TestLumberjackDecoder.java
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.processors.lumberjack.frame;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.xml.bind.DatatypeConverter;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestLumberjackDecoder {
+
+    // Because no encoder for type 43 was coded, added Static hex
+    // representation of compressed data
+    //
+    private static final String singleFrameData = "3143000000aa785e4c8e4daac3300c8413c8cbfeddc017681da7b48540775df51245103936f54fb" +
+        "04c4a6e5f6917d03020e91bc93c9ba669597faccefa80ec0fed72440dd1174833e819370c798d98aa0e79a10ae44e36972f94198b26886bc" +
+        "0774422589024c865aaecff07f24c6e1b0c37fb6c2da18cdb4176834f72747c4152e6aa46330db7e9725707567db0240c93aace93e212464" +
+        "95857f755e89e76e2d77e000000ffff010000ffff05b43bb8";
+    private static final String multiFrameData = "3143000000d7785ec48fcf6ac4201087b3bbe9defb06be40ab669b1602bdf5d49728031957a97f82" +
+        "232979fbaaa7c0924b2e018701f537f37df2ab699a53aea75cad321673ffe43a38e4e04c043f021f71461b26873e711bee9480f48b0af10fe28" +
+        "89113b8c9e28f4322b82395413a50cafd79957c253d0b992faf4129c2f27c12e5af35be2cedbec133d9b34e0ee27db87db05596fd62f468079" +
+        "6b421964fc9b032ac4dcb54d2575a28a3559df3413ae7be12edf6e9367c2e07f95ca4a848bb856e1b42ed61427d45da2df4f628f40f0000ffff" +
+        "010000ffff35e0eff0";
+    private static final String payload = "";
+
+    private LumberjackDecoder decoder;
+
+    @Before
+    public void setup() {
+        this.decoder = new LumberjackDecoder(StandardCharsets.UTF_8);
+    }
+
+    @Test
+    public void testDecodeSingleFrame() {
+        final byte[] input = DatatypeConverter.parseHexBinary(singleFrameData);
+
+        List<LumberjackFrame> frames = null;
+        LumberjackFrame frame = null;
+
+        for (byte b : input) {
+            if (decoder.process(b)) {
+                frames = decoder.getFrames();
+                break;
+            }
+        }
+
+        frame = frames.get(frames.size() - 1);
+
+        Assert.assertNotNull(frame);
+        Assert.assertEquals(0x31, frame.getVersion());
+        Assert.assertEquals(0x44, frame.getFrameType());
+        Assert.assertEquals(1, frame.getSeqNumber());
+        // Look for a predefined number of bytes for matching of the inner payload
+        Assert.assertArrayEquals(DatatypeConverter.parseHexBinary("000000050000000466696c65000000"), Arrays.copyOfRange(frame.getPayload(), 0, 15));
+    }
+
+    @Test
+    public void testDecodeMultipleFrame() {
+        final byte[] input = DatatypeConverter.parseHexBinary(multiFrameData);
+
+        List<LumberjackFrame> frames = null;
+        LumberjackFrame frame = null;
+
+        for (byte b : input) {
+            if (decoder.process(b)) {
+                frames = decoder.getFrames();
+                break;
+            }
+        }
+
+        frame = frames.get(1);
+
+        Assert.assertNotNull(frame);
+        Assert.assertEquals(0x31, frame.getVersion());
+        Assert.assertEquals(0x44, frame.getFrameType());
+        // Load the second frame therefore seqNumber = 2
+        Assert.assertEquals(2, frame.getSeqNumber());
+        // Look for a predefined number of bytes for matching of the inner payload
+        Assert.assertArrayEquals(DatatypeConverter.parseHexBinary("000000050000000466696c65000000"), Arrays.copyOfRange(frame.getPayload(), 0, 15));
+    }
+}
