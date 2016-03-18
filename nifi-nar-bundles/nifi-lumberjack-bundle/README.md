c92d5f618feb9115f71293c302f8efc1849ce3dd
Andre F de Miranda <trixpan@users.noreply.github.com>
2016-03-19 00:21:45 +1100

    NIFI-856 - Implements initial and experimental ListenLumberjack Processor, allowing NiFi to            replace Logstash as a pipeline for logstash-forwarder and Logstash's Lumberjack          output

---
+++ nifi-nar-bundles/nifi-lumberjack-bundle/README.md
@@ -0,0 +1,1 @@
+This is an experimental Lumberjack Processor for NiFi. It requires 0.5.0 or greater
---
+++ nifi-nar-bundles/nifi-lumberjack-bundle/nifi-lumberjack-processors/src/test/java/org/apache/nifi/processors/lumberjack/frame/TestLumberjackEncoder.java
@@ -0,0 +1,8 @@
+package org.apache.nifi.processors.lumberjack.frame;
+
+import static org.junit.Assert.*;
+
+
+public class TestLumberjackEncoder {
+// TODO: Implement
+}
