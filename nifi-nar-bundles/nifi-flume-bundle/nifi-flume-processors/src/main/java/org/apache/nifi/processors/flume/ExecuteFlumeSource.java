d6674fbb8e20ce6daeeac8cd7b07271963744bde
Oleg Zhurakousky <oleg@suitcase.io>
2016-05-19 10:10:21 -0400

    NIFI-1898 Fixed Flume processor @OnScheduled methods to accept ProcessContext
    This closes #455.
    
    Signed-off-by: Aldrin Piri <aldrin@apache.org>

--- nifi-nar-bundles/nifi-flume-bundle/nifi-flume-processors/src/main/java/org/apache/nifi/processors/flume/ExecuteFlumeSource.java
+++ nifi-nar-bundles/nifi-flume-bundle/nifi-flume-processors/src/main/java/org/apache/nifi/processors/flume/ExecuteFlumeSource.java
@@ -41,7 +41,6 @@ import org.apache.nifi.processor.ProcessSession;
 import org.apache.nifi.processor.ProcessSessionFactory;
 import org.apache.nifi.processor.ProcessorInitializationContext;
 import org.apache.nifi.processor.Relationship;
-import org.apache.nifi.processor.SchedulingContext;
 import org.apache.nifi.processor.exception.ProcessException;
 import org.apache.nifi.processor.util.StandardValidators;
 
@@ -117,7 +116,7 @@ public class ExecuteFlumeSource extends AbstractFlumeProcessor {
     }
 
     @OnScheduled
-    public void onScheduled(final SchedulingContext context) {
+    public void onScheduled(final ProcessContext context) {
         try {
             source = SOURCE_FACTORY.create(
                     context.getProperty(SOURCE_NAME).getValue(),
