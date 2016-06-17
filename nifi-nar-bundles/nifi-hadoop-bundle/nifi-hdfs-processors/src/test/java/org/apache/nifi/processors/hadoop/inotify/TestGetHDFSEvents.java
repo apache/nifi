76f8c9481fe0a491c3a5d7612486fb77b9c11fcf
JJ <jjmeyer0@gmail.com>
2016-06-16 19:01:17 -0500

    NIFI-1037 Ported processor for HDFS' inotify events.
    This closes #536.

---
+++ nifi-nar-bundles/nifi-hadoop-bundle/nifi-hdfs-processors/src/test/java/org/apache/nifi/processors/hadoop/inotify/TestGetHDFSEvents.java
@@ -0,0 +1,267 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.processors.hadoop.inotify;
+
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.hdfs.DFSInotifyEventInputStream;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
+import org.apache.hadoop.hdfs.client.HdfsAdmin;
+import org.apache.hadoop.hdfs.inotify.Event;
+import org.apache.hadoop.hdfs.inotify.EventBatch;
+import org.apache.nifi.components.state.Scope;
+import org.apache.nifi.hadoop.KerberosProperties;
+import org.apache.nifi.processors.hadoop.inotify.util.EventTestUtils;
+import org.apache.nifi.util.MockFlowFile;
+import org.apache.nifi.util.NiFiProperties;
+import org.apache.nifi.util.TestRunner;
+import org.apache.nifi.util.TestRunners;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class TestGetHDFSEvents {
+    NiFiProperties mockNiFiProperties;
+    KerberosProperties kerberosProperties;
+    DFSInotifyEventInputStream inotifyEventInputStream;
+    HdfsAdmin hdfsAdmin;
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Before
+    public void setup() {
+        mockNiFiProperties = mock(NiFiProperties.class);
+        when(mockNiFiProperties.getKerberosConfigurationFile()).thenReturn(null);
+        kerberosProperties = KerberosProperties.create(mockNiFiProperties);
+        inotifyEventInputStream = mock(DFSInotifyEventInputStream.class);
+        hdfsAdmin = mock(HdfsAdmin.class);
+    }
+
+    @Test
+    public void notSettingHdfsPathToWatchShouldThrowError() throws Exception {
+        exception.expect(AssertionError.class);
+        exception.expectMessage("'HDFS Path to Watch' is invalid because HDFS Path to Watch is required");
+
+        GetHDFSEvents processor = new TestableGetHDFSEvents(kerberosProperties, hdfsAdmin);
+        TestRunner runner = TestRunners.newTestRunner(processor);
+
+        runner.setProperty(GetHDFSEvents.POLL_DURATION, "1 second");
+        runner.run();
+    }
+
+    @Test
+    public void onTriggerShouldProperlyHandleAnEmptyEventBatch() throws Exception {
+        EventBatch eventBatch = mock(EventBatch.class);
+        when(eventBatch.getEvents()).thenReturn(new Event[]{});
+
+        when(inotifyEventInputStream.poll(1000000L, TimeUnit.MICROSECONDS)).thenReturn(eventBatch);
+        when(hdfsAdmin.getInotifyEventStream()).thenReturn(inotifyEventInputStream);
+        when(eventBatch.getTxid()).thenReturn(100L);
+
+        GetHDFSEvents processor = new TestableGetHDFSEvents(kerberosProperties, hdfsAdmin);
+        TestRunner runner = TestRunners.newTestRunner(processor);
+
+        runner.setProperty(GetHDFSEvents.POLL_DURATION, "1 second");
+        runner.setProperty(GetHDFSEvents.HDFS_PATH_TO_WATCH, "/some/path");
+        runner.setProperty(GetHDFSEvents.NUMBER_OF_RETRIES_FOR_POLL, "5");
+        runner.run();
+
+        List<MockFlowFile> successfulFlowFiles = runner.getFlowFilesForRelationship(GetHDFSEvents.REL_SUCCESS);
+        assertEquals(0, successfulFlowFiles.size());
+        verify(eventBatch).getTxid();
+        assertEquals("100", runner.getProcessContext().getStateManager().getState(Scope.CLUSTER).get("last.tx.id"));
+    }
+
+    @Test
+    public void onTriggerShouldProperlyHandleANullEventBatch() throws Exception {
+        when(inotifyEventInputStream.poll(1000000L, TimeUnit.MICROSECONDS)).thenReturn(null);
+        when(hdfsAdmin.getInotifyEventStream()).thenReturn(inotifyEventInputStream);
+
+        GetHDFSEvents processor = new TestableGetHDFSEvents(kerberosProperties, hdfsAdmin);
+        TestRunner runner = TestRunners.newTestRunner(processor);
+
+        runner.setProperty(GetHDFSEvents.POLL_DURATION, "1 second");
+        runner.setProperty(GetHDFSEvents.HDFS_PATH_TO_WATCH, "/some/path${now()}");
+        runner.run();
+
+        List<MockFlowFile> successfulFlowFiles = runner.getFlowFilesForRelationship(GetHDFSEvents.REL_SUCCESS);
+        assertEquals(0, successfulFlowFiles.size());
+        assertEquals("-1", runner.getProcessContext().getStateManager().getState(Scope.CLUSTER).get("last.tx.id"));
+    }
+
+    @Test
+    public void makeSureHappyPathForProcessingEventsSendsFlowFilesToCorrectRelationship() throws Exception {
+        Event[] events = getEvents();
+
+        EventBatch eventBatch = mock(EventBatch.class);
+        when(eventBatch.getEvents()).thenReturn(events);
+
+        when(inotifyEventInputStream.poll(1000000L, TimeUnit.MICROSECONDS)).thenReturn(eventBatch);
+        when(hdfsAdmin.getInotifyEventStream()).thenReturn(inotifyEventInputStream);
+        when(eventBatch.getTxid()).thenReturn(100L);
+
+        GetHDFSEvents processor = new TestableGetHDFSEvents(kerberosProperties, hdfsAdmin);
+        TestRunner runner = TestRunners.newTestRunner(processor);
+
+        runner.setProperty(GetHDFSEvents.POLL_DURATION, "1 second");
+        runner.setProperty(GetHDFSEvents.HDFS_PATH_TO_WATCH, "/some/path(/)?.*");
+        runner.run();
+
+        List<MockFlowFile> successfulFlowFiles = runner.getFlowFilesForRelationship(GetHDFSEvents.REL_SUCCESS);
+        assertEquals(6, successfulFlowFiles.size());
+        verify(eventBatch).getTxid();
+        assertEquals("100", runner.getProcessContext().getStateManager().getState(Scope.CLUSTER).get("last.tx.id"));
+    }
+
+    @Test
+    public void onTriggerShouldOnlyProcessEventsWithSpecificPath() throws Exception {
+        Event[] events = getEvents();
+
+        EventBatch eventBatch = mock(EventBatch.class);
+        when(eventBatch.getEvents()).thenReturn(events);
+
+        when(inotifyEventInputStream.poll(1000000L, TimeUnit.MICROSECONDS)).thenReturn(eventBatch);
+        when(hdfsAdmin.getInotifyEventStream()).thenReturn(inotifyEventInputStream);
+        when(eventBatch.getTxid()).thenReturn(100L);
+
+        GetHDFSEvents processor = new TestableGetHDFSEvents(kerberosProperties, hdfsAdmin);
+        TestRunner runner = TestRunners.newTestRunner(processor);
+
+        runner.setProperty(GetHDFSEvents.HDFS_PATH_TO_WATCH, "/some/path/create(/)?");
+        runner.run();
+
+        List<MockFlowFile> successfulFlowFiles = runner.getFlowFilesForRelationship(GetHDFSEvents.REL_SUCCESS);
+        assertEquals(1, successfulFlowFiles.size());
+        verify(eventBatch).getTxid();
+        assertEquals("100", runner.getProcessContext().getStateManager().getState(Scope.CLUSTER).get("last.tx.id"));
+    }
+
+    @Test
+    public void eventsProcessorShouldProperlyFilterEventTypes() throws Exception {
+        Event[] events = getEvents();
+
+        EventBatch eventBatch = mock(EventBatch.class);
+        when(eventBatch.getEvents()).thenReturn(events);
+
+        when(inotifyEventInputStream.poll(1000000L, TimeUnit.MICROSECONDS)).thenReturn(eventBatch);
+        when(hdfsAdmin.getInotifyEventStream()).thenReturn(inotifyEventInputStream);
+        when(eventBatch.getTxid()).thenReturn(100L);
+
+        GetHDFSEvents processor = new TestableGetHDFSEvents(kerberosProperties, hdfsAdmin);
+        TestRunner runner = TestRunners.newTestRunner(processor);
+
+        runner.setProperty(GetHDFSEvents.HDFS_PATH_TO_WATCH, "/some/path(/.*)?");
+        runner.setProperty(GetHDFSEvents.EVENT_TYPES, "create, metadata, rename");
+        runner.run();
+
+        List<MockFlowFile> successfulFlowFiles = runner.getFlowFilesForRelationship(GetHDFSEvents.REL_SUCCESS);
+        assertEquals(3, successfulFlowFiles.size());
+
+        List<String> expectedEventTypes = Arrays.asList("CREATE", "METADATA", "RENAME");
+        for (MockFlowFile f : successfulFlowFiles) {
+            String eventType = f.getAttribute(EventAttributes.EVENT_TYPE);
+            assertTrue(expectedEventTypes.contains(eventType));
+        }
+
+        verify(eventBatch).getTxid();
+        assertEquals("100", runner.getProcessContext().getStateManager().getState(Scope.CLUSTER).get("last.tx.id"));
+    }
+
+    @Test
+    public void makeSureExpressionLanguageIsWorkingProperlyWithinTheHdfsPathToWatch() throws Exception {
+        Event[] events = new Event[] {
+                new Event.AppendEvent("/some/path/1/2/3/t.txt"),
+                new Event.AppendEvent("/some/path/1/2/4/t.txt"),
+                new Event.AppendEvent("/some/path/1/2/3/.t.txt")
+        };
+
+        EventBatch eventBatch = mock(EventBatch.class);
+        when(eventBatch.getEvents()).thenReturn(events);
+
+        when(inotifyEventInputStream.poll(1000000L, TimeUnit.MICROSECONDS)).thenReturn(eventBatch);
+        when(hdfsAdmin.getInotifyEventStream()).thenReturn(inotifyEventInputStream);
+        when(eventBatch.getTxid()).thenReturn(100L);
+
+        GetHDFSEvents processor = new TestableGetHDFSEvents(kerberosProperties, hdfsAdmin);
+        TestRunner runner = TestRunners.newTestRunner(processor);
+
+        runner.setProperty(GetHDFSEvents.HDFS_PATH_TO_WATCH, "/some/path/${literal(1)}/${literal(2)}/${literal(3)}/.*.txt");
+        runner.setProperty(GetHDFSEvents.EVENT_TYPES, "append");
+        runner.setProperty(GetHDFSEvents.IGNORE_HIDDEN_FILES, "true");
+        runner.run();
+
+        List<MockFlowFile> successfulFlowFiles = runner.getFlowFilesForRelationship(GetHDFSEvents.REL_SUCCESS);
+        assertEquals(1, successfulFlowFiles.size());
+
+        for (MockFlowFile f : successfulFlowFiles) {
+            String eventType = f.getAttribute(EventAttributes.EVENT_TYPE);
+            assertTrue(eventType.equals("APPEND"));
+        }
+
+        verify(eventBatch).getTxid();
+        assertEquals("100", runner.getProcessContext().getStateManager().getState(Scope.CLUSTER).get("last.tx.id"));
+    }
+
+    private Event[] getEvents() {
+        return new Event[]{
+                    EventTestUtils.createCreateEvent(),
+                    EventTestUtils.createCloseEvent(),
+                    EventTestUtils.createAppendEvent(),
+                    EventTestUtils.createRenameEvent(),
+                    EventTestUtils.createMetadataUpdateEvent(),
+                    EventTestUtils.createUnlinkEvent()
+            };
+    }
+
+    private class TestableGetHDFSEvents extends GetHDFSEvents {
+
+        private final KerberosProperties testKerberosProperties;
+        private final FileSystem fileSystem = new DistributedFileSystem();
+        private final HdfsAdmin hdfsAdmin;
+
+        TestableGetHDFSEvents(KerberosProperties testKerberosProperties, HdfsAdmin hdfsAdmin) {
+            this.testKerberosProperties = testKerberosProperties;
+            this.hdfsAdmin = hdfsAdmin;
+        }
+
+        @Override
+        protected FileSystem getFileSystem() {
+            return fileSystem;
+        }
+
+        @Override
+        protected KerberosProperties getKerberosProperties() {
+            return testKerberosProperties;
+        }
+
+        @Override
+        protected HdfsAdmin getHdfsAdmin() {
+            return hdfsAdmin;
+        }
+    }
+}
---
+++ nifi-nar-bundles/nifi-hadoop-bundle/nifi-hdfs-processors/src/test/java/org/apache/nifi/processors/hadoop/inotify/util/EventTestUtils.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.processors.hadoop.inotify.util;
+
+
+import org.apache.hadoop.fs.XAttr;
+import org.apache.hadoop.fs.permission.AclEntry;
+import org.apache.hadoop.fs.permission.AclEntryScope;
+import org.apache.hadoop.fs.permission.AclEntryType;
+import org.apache.hadoop.fs.permission.FsAction;
+import org.apache.hadoop.fs.permission.FsPermission;
+import org.apache.hadoop.hdfs.inotify.Event;
+
+import java.util.Collections;
+import java.util.Date;
+
+public class EventTestUtils {
+    public static Event.CreateEvent createCreateEvent() {
+        return new Event.CreateEvent.Builder()
+                .ctime(new Date().getTime())
+                .groupName("group_name")
+                .iNodeType(Event.CreateEvent.INodeType.DIRECTORY)
+                .overwrite(false)
+                .ownerName("ownerName")
+                .path("/some/path/create")
+                .perms(new FsPermission(FsAction.ALL, FsAction.NONE, FsAction.NONE))
+                .replication(1)
+                .symlinkTarget("/some/symlink/target")
+                .build();
+    }
+
+    public static Event.CloseEvent createCloseEvent() {
+        return new Event.CloseEvent("/some/path/close", 1L, 2L);
+    }
+
+    public static Event.AppendEvent createAppendEvent() {
+        return new Event.AppendEvent("/some/path/append");
+    }
+
+    public static Event.RenameEvent createRenameEvent() {
+        return new Event.RenameEvent("/some/path/rename/src", "/some/path/rename/dest", 200L);
+    }
+
+    public static Event.MetadataUpdateEvent createMetadataUpdateEvent() {
+        return new Event.MetadataUpdateEvent.Builder()
+                .replication(0)
+                .perms(new FsPermission(FsAction.ALL, FsAction.NONE, FsAction.NONE))
+                .path("/some/path/metadata")
+                .ownerName("owner")
+                .acls(Collections.singletonList(new AclEntry.Builder().setName("schema").setPermission(FsAction.ALL).setScope(AclEntryScope.ACCESS).setType(AclEntryType.GROUP).build()))
+                .atime(new Date().getTime())
+                .groupName("groupName")
+                .metadataType(Event.MetadataUpdateEvent.MetadataType.ACLS)
+                .mtime(1L)
+                .xAttrs(Collections.singletonList(new XAttr.Builder().setName("name").setNameSpace(XAttr.NameSpace.USER).setValue(new byte[0]).build()))
+                .xAttrsRemoved(false)
+                .build();
+    }
+
+    public static Event.UnlinkEvent createUnlinkEvent() {
+        return new Event.UnlinkEvent("/some/path/unlink", 300L);
+    }
+}
