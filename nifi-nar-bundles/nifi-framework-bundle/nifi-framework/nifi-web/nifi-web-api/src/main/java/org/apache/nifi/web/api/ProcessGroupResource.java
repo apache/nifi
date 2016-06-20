cda4310ad898904a13d9aeb2c6092ad1c934a919
Mark Payne <markap14@hotmail.com>
2016-06-20 14:55:08 -0400

    NIFI-2035: Verify existence of source and destination when creating a connection
    Signed-off-by: Yolanda M. Davis <ymdavis@apache.org>
    
    This closes #690

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-api/src/main/java/org/apache/nifi/web/api/ProcessGroupResource.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-api/src/main/java/org/apache/nifi/web/api/ProcessGroupResource.java
@@ -35,6 +35,7 @@ import org.apache.nifi.authorization.user.NiFiUserUtils;
 import org.apache.nifi.cluster.protocol.NodeIdentifier;
 import org.apache.nifi.controller.Snippet;
 import org.apache.nifi.web.NiFiServiceFacade;
+import org.apache.nifi.web.ResourceNotFoundException;
 import org.apache.nifi.web.Revision;
 import org.apache.nifi.web.api.dto.ConnectionDTO;
 import org.apache.nifi.web.api.dto.ProcessGroupDTO;
@@ -108,8 +109,6 @@ import java.util.Set;
 public class ProcessGroupResource extends ApplicationResource {
 
     private static final Logger logger = LoggerFactory.getLogger(ProcessGroupResource.class);
-
-    private static final String VERBOSE = "false";
 
     @Context
     private ResourceContext resourceContext;
@@ -1530,10 +1529,17 @@ public class ProcessGroupResource extends ApplicationResource {
 
                 // ensure write access to the source
                 final Authorizable source = lookup.getConnectable(connection.getSource().getId());
+                if (source == null) {
+                    throw new ResourceNotFoundException("Cannot find source component with ID [" + connection.getSource().getId() + "]");
+                }
                 source.authorize(authorizer, RequestAction.WRITE, NiFiUserUtils.getNiFiUser());
 
                 // ensure write access to the destination
                 final Authorizable destination = lookup.getConnectable(connection.getDestination().getId());
+                if (destination == null) {
+                    throw new ResourceNotFoundException("Cannot find destination component with ID [" + connection.getDestination().getId() + "]");
+                }
+
                 destination.authorize(authorizer, RequestAction.WRITE, NiFiUserUtils.getNiFiUser());
             });
         }
