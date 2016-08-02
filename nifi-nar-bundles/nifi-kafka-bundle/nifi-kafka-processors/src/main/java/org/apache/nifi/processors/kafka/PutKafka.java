2a6580992bdbcd4994ddaac29c125a874e39f614
jpercivall <joepercivall@yahoo.com>
2016-08-01 20:38:28 -0400

    NIFI-2455 Removing 'Queue Buffering Max Time' as a required property of PutKafka. This closes #765

--- nifi-nar-bundles/nifi-kafka-bundle/nifi-kafka-processors/src/main/java/org/apache/nifi/processors/kafka/PutKafka.java
+++ nifi-nar-bundles/nifi-kafka-bundle/nifi-kafka-processors/src/main/java/org/apache/nifi/processors/kafka/PutKafka.java
@@ -207,9 +207,7 @@ public class PutKafka extends AbstractKafkaProcessor<KafkaPublisher> {
             .description("Maximum time to buffer data before sending to Kafka. For example a setting of 100 ms"
                     + " will try to batch together 100 milliseconds' worth of messages to send at once. This will improve"
                     + " throughput but adds message delivery latency due to the buffering.")
-            .required(true)
             .addValidator(StandardValidators.TIME_PERIOD_VALIDATOR)
-            .defaultValue("5 secs")
             .build();
     public static final PropertyDescriptor COMPRESSION_CODEC = new PropertyDescriptor.Builder()
             .name("Compression Codec")
