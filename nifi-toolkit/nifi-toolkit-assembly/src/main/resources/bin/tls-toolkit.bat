87d66c4724f5a01adcb30abc18e76bd6f49a09e5
Bryan Rosander <bryanrosander@gmail.com>
2016-08-16 12:58:20 -0400

    NIFI-2569 - Updating main class in windows bat file

--- nifi-toolkit/nifi-toolkit-assembly/src/main/resources/bin/tls-toolkit.bat
+++ nifi-toolkit/nifi-toolkit-assembly/src/main/resources/bin/tls-toolkit.bat
@@ -33,7 +33,7 @@ goto startConfig
 :startConfig
 set LIB_DIR=%~dp0..\classpath;%~dp0..\lib
 
-SET JAVA_PARAMS=-cp %LIB_DIR%\* -Xms12m -Xmx24m %JAVA_ARGS% org.apache.nifi.toolkit.tls.service.server.TlsCertificateAuthorityService
+SET JAVA_PARAMS=-cp %LIB_DIR%\* -Xms12m -Xmx24m %JAVA_ARGS% org.apache.nifi.toolkit.tls.TlsToolkitMain
 
 cmd.exe /C "%JAVA_EXE%" %JAVA_PARAMS% %*
 
