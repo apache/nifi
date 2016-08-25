9e4e68bec679b8a163646dabe2d452e8d126d269
Oleg Zhurakousky <oleg@suitcase.io>
2016-08-25 17:41:44 -0400

    NIFI-2675 fixed NPE in template generation

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-client-dto/src/main/java/org/apache/nifi/web/api/dto/FlowSnippetDTO.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-client-dto/src/main/java/org/apache/nifi/web/api/dto/FlowSnippetDTO.java
@@ -214,7 +214,7 @@ public class FlowSnippetDTO {
                     Map<String, PropertyDescriptorDTO> map = csDTO.getDescriptors();
                     Map<String, String> props = csDTO.getProperties();
                     for (Entry<String, PropertyDescriptorDTO> entry : map.entrySet()) {
-                        if (entry.getValue().getIdentifiesControllerService() != null) {
+                        if (entry.getValue().getIdentifiesControllerService() != null && props.get(entry.getKey()) != null) {
                             String key = entry.getKey();
                             String value = props.get(key);
                             id = UUID.fromString(value);
