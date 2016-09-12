eec4781c1298b5109b6c76d48bf3337f511b4a77
patricker <patricker@gmail.com>
2016-09-12 07:30:33 -0600

    NIFI-2754

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/main/java/org/apache/nifi/controller/StandardFlowFileQueue.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/main/java/org/apache/nifi/controller/StandardFlowFileQueue.java
@@ -548,13 +548,9 @@ public class StandardFlowFileQueue implements FlowFileQueue {
             return;
         }
 
-        int numSwapFiles = swapQueue.size() / SWAP_RECORD_POLL_SIZE;
+        migrateSwapToActive();
 
-        //if the Active Queue is empty, and the number of files just added to the swap queue is evenly splitable into swap files
-        // then reduce swap file count by 1, otherwise we won't have any active records.
-        if(activeQueue.size() == 0 && numSwapFiles > 0 && swapQueue.size() % SWAP_RECORD_POLL_SIZE == 0){
-            numSwapFiles -=1;
-        }
+        final int numSwapFiles = swapQueue.size() / SWAP_RECORD_POLL_SIZE;
 
         int originalSwapQueueCount = swapQueue.size();
         long originalSwapQueueBytes = 0L;
