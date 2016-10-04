99d3c397481a98a5b3d2d4e44b84516fc0e3675e
Michal Klempa <michal.klempa@gmail.com>
2016-10-04 14:25:49 +0200

    NIFI-1069 - improve init script exit codes so that results are LSB compliant
    This closes: #1093
    
    Signed-off-by: Andre F de Miranda <trixpan@users.noreply.github.com>

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-resources/src/main/resources/bin/nifi.sh
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-resources/src/main/resources/bin/nifi.sh
@@ -295,12 +295,14 @@ run() {
     else
         (eval $RUN_NIFI_CMD $@)
     fi
+    EXIT_STATUS=$?
 
     # Wait just a bit (3 secs) to wait for the logging to finish and then echo a new-line.
     # We do this to avoid having logs spewed on the console after running the command and then not giving
     # control back to the user
     sleep 3
     echo
+    exit $EXIT_STATUS
 }
 
 main() {
