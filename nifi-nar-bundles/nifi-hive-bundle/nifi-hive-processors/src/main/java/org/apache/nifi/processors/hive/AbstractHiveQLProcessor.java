bbc714e73ba245de7bc32fd9958667c847101f7d
David W. Streever <david@streever.com>
2016-11-04 11:03:17 -0400

    PutHiveQL and SelectHiveQL Processor enhancements. Added support for multiple statements in a script. Options for delimiters, quotes, escaping, include header and alternate header.
    Add support in SelectHiveQL to get script content from the Flow File to bring consistency with patterns used for PutHiveQL and support extra query management.
    
    Changed behavior of using Flowfile to match ExecuteSQL.  Handle query delimiter when embedded.  Added test case for embedded delimiter
    
    Formatting and License Header
    
    PutHiveQL and SelectHiveQL Processor enhancements. Added support for multiple statements in a script.  Options for delimiters, quotes, escaping, include header and alternate header.
    
    Add support in SelectHiveQL to get script content from the Flow File to bring consistency with patterns used for PutHiveQL and support extra query management.
    
    Changed behavior of using Flowfile to match ExecuteSQL.  Handle query delimiter when embedded.  Added test case for embedded delimiter
    
    Removing dead code.
    
    Signed-off-by: Matt Burgess <mattyb149@apache.org>
    
    Comments to Clarify test case.
    
    Signed-off-by: Matt Burgess <mattyb149@apache.org>
    
    Final whitespace/formatting/typo changes
    
    This closes #1316

--- nifi-nar-bundles/nifi-hive-bundle/nifi-hive-processors/src/main/java/org/apache/nifi/processors/hive/AbstractHiveQLProcessor.java
+++ nifi-nar-bundles/nifi-hive-bundle/nifi-hive-processors/src/main/java/org/apache/nifi/processors/hive/AbstractHiveQLProcessor.java
@@ -18,12 +18,37 @@ package org.apache.nifi.processors.hive;
 
 import org.apache.nifi.components.PropertyDescriptor;
 import org.apache.nifi.dbcp.hive.HiveDBCPService;
+import org.apache.nifi.flowfile.FlowFile;
 import org.apache.nifi.processor.AbstractProcessor;
+import org.apache.nifi.processor.ProcessSession;
+import org.apache.nifi.processor.exception.ProcessException;
+import org.apache.nifi.processor.io.InputStreamCallback;
+import org.apache.nifi.processor.util.StandardValidators;
+import org.apache.nifi.stream.io.StreamUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigDecimal;
+import java.nio.charset.Charset;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * An abstract base class for HiveQL processors to share common data, methods, etc.
  */
 public abstract class AbstractHiveQLProcessor extends AbstractProcessor {
+
+    protected static final Pattern HIVEQL_TYPE_ATTRIBUTE_PATTERN = Pattern.compile("hiveql\\.args\\.(\\d+)\\.type");
+    protected static final Pattern NUMBER_PATTERN = Pattern.compile("-?\\d+");
 
     public static final PropertyDescriptor HIVE_DBCP_SERVICE = new PropertyDescriptor.Builder()
             .name("Hive Database Connection Pooling Service")
@@ -31,5 +56,164 @@ public abstract class AbstractHiveQLProcessor extends AbstractProcessor {
             .required(true)
             .identifiesControllerService(HiveDBCPService.class)
             .build();
+
+    public static final PropertyDescriptor CHARSET = new PropertyDescriptor.Builder()
+            .name("hive-charset")
+            .displayName("Character Set")
+            .description("Specifies the character set of the record data.")
+            .required(true)
+            .defaultValue("UTF-8")
+            .addValidator(StandardValidators.CHARACTER_SET_VALIDATOR)
+            .build();
+
+    /**
+     * Determines the HiveQL statement that should be executed for the given FlowFile
+     *
+     * @param session  the session that can be used to access the given FlowFile
+     * @param flowFile the FlowFile whose HiveQL statement should be executed
+     * @return the HiveQL that is associated with the given FlowFile
+     */
+    protected String getHiveQL(final ProcessSession session, final FlowFile flowFile, final Charset charset) {
+        // Read the HiveQL from the FlowFile's content
+        final byte[] buffer = new byte[(int) flowFile.getSize()];
+        session.read(flowFile, new InputStreamCallback() {
+            @Override
+            public void process(final InputStream in) throws IOException {
+                StreamUtils.fillBuffer(in, buffer);
+            }
+        });
+
+        // Create the PreparedStatement to use for this FlowFile.
+        return new String(buffer, charset);
+    }
+
+    private class ParameterHolder {
+        String attributeName;
+        int jdbcType;
+        String value;
+    }
+
+    /**
+     * Sets all of the appropriate parameters on the given PreparedStatement, based on the given FlowFile attributes.
+     *
+     * @param stmt       the statement to set the parameters on
+     * @param attributes the attributes from which to derive parameter indices, values, and types
+     * @throws SQLException if the PreparedStatement throws a SQLException when the appropriate setter is called
+     */
+    protected int setParameters(int base, final PreparedStatement stmt, int paramCount, final Map<String, String> attributes) throws SQLException {
+
+        Map<Integer, ParameterHolder> parmMap = new TreeMap<Integer, ParameterHolder>();
+
+        for (final Map.Entry<String, String> entry : attributes.entrySet()) {
+                final String key = entry.getKey();
+                final Matcher matcher = HIVEQL_TYPE_ATTRIBUTE_PATTERN.matcher(key);
+                if (matcher.matches()) {
+                    final int parameterIndex = Integer.parseInt(matcher.group(1));
+                    if (parameterIndex >= base && parameterIndex < base + paramCount) {
+                        final boolean isNumeric = NUMBER_PATTERN.matcher(entry.getValue()).matches();
+                        if (!isNumeric) {
+                            throw new ProcessException("Value of the " + key + " attribute is '" + entry.getValue() + "', which is not a valid JDBC numeral jdbcType");
+                        }
+
+                        final String valueAttrName = "hiveql.args." + parameterIndex + ".value";
+
+                        ParameterHolder ph = new ParameterHolder();
+                        int realIndexLoc = parameterIndex - base +1;
+
+                        ph.jdbcType = Integer.parseInt(entry.getValue());
+                        ph.value = attributes.get(valueAttrName);
+                        ph.attributeName = valueAttrName;
+
+                        parmMap.put(realIndexLoc, ph);
+
+                    }
+                }
+        }
+
+
+        // Now that's we've retrieved the correct number of parameters and it's sorted, let's set them.
+        for (final Map.Entry<Integer, ParameterHolder> entry : parmMap.entrySet()) {
+            final Integer index = entry.getKey();
+            final ParameterHolder ph = entry.getValue();
+
+            try {
+                setParameter(stmt, ph.attributeName, index, ph.value, ph.jdbcType);
+            } catch (final NumberFormatException nfe) {
+                throw new ProcessException("The value of the " + ph.attributeName + " is '" + ph.value + "', which cannot be converted into the necessary data jdbcType", nfe);
+            }
+        }
+        return base + paramCount;
+    }
+
+    /**
+     * Determines how to map the given value to the appropriate JDBC data jdbcType and sets the parameter on the
+     * provided PreparedStatement
+     *
+     * @param stmt           the PreparedStatement to set the parameter on
+     * @param attrName       the name of the attribute that the parameter is coming from - for logging purposes
+     * @param parameterIndex the index of the HiveQL parameter to set
+     * @param parameterValue the value of the HiveQL parameter to set
+     * @param jdbcType       the JDBC Type of the HiveQL parameter to set
+     * @throws SQLException if the PreparedStatement throws a SQLException when calling the appropriate setter
+     */
+    protected void setParameter(final PreparedStatement stmt, final String attrName, final int parameterIndex, final String parameterValue, final int jdbcType) throws SQLException {
+        if (parameterValue == null) {
+            stmt.setNull(parameterIndex, jdbcType);
+        } else {
+            try {
+                switch (jdbcType) {
+                    case Types.BIT:
+                    case Types.BOOLEAN:
+                        stmt.setBoolean(parameterIndex, Boolean.parseBoolean(parameterValue));
+                        break;
+                    case Types.TINYINT:
+                        stmt.setByte(parameterIndex, Byte.parseByte(parameterValue));
+                        break;
+                    case Types.SMALLINT:
+                        stmt.setShort(parameterIndex, Short.parseShort(parameterValue));
+                        break;
+                    case Types.INTEGER:
+                        stmt.setInt(parameterIndex, Integer.parseInt(parameterValue));
+                        break;
+                    case Types.BIGINT:
+                        stmt.setLong(parameterIndex, Long.parseLong(parameterValue));
+                        break;
+                    case Types.REAL:
+                        stmt.setFloat(parameterIndex, Float.parseFloat(parameterValue));
+                        break;
+                    case Types.FLOAT:
+                    case Types.DOUBLE:
+                        stmt.setDouble(parameterIndex, Double.parseDouble(parameterValue));
+                        break;
+                    case Types.DECIMAL:
+                    case Types.NUMERIC:
+                        stmt.setBigDecimal(parameterIndex, new BigDecimal(parameterValue));
+                        break;
+                    case Types.DATE:
+                        stmt.setDate(parameterIndex, new Date(Long.parseLong(parameterValue)));
+                        break;
+                    case Types.TIME:
+                        stmt.setTime(parameterIndex, new Time(Long.parseLong(parameterValue)));
+                        break;
+                    case Types.TIMESTAMP:
+                        stmt.setTimestamp(parameterIndex, new Timestamp(Long.parseLong(parameterValue)));
+                        break;
+                    case Types.CHAR:
+                    case Types.VARCHAR:
+                    case Types.LONGNVARCHAR:
+                    case Types.LONGVARCHAR:
+                        stmt.setString(parameterIndex, parameterValue);
+                        break;
+                    default:
+                        stmt.setObject(parameterIndex, parameterValue, jdbcType);
+                        break;
+                }
+            } catch (SQLException e) {
+                // Log which attribute/parameter had an error, then rethrow to be handled at the top level
+                getLogger().error("Error setting parameter {} to value from {} ({})", new Object[]{parameterIndex, attrName, parameterValue}, e);
+                throw e;
+            }
+        }
+    }
 
 }
--- nifi-nar-bundles/nifi-hive-bundle/nifi-hive-processors/src/main/java/org/apache/nifi/processors/hive/PutHiveQL.java
+++ nifi-nar-bundles/nifi-hive-bundle/nifi-hive-processors/src/main/java/org/apache/nifi/processors/hive/PutHiveQL.java
@@ -16,6 +16,7 @@
  */
 package org.apache.nifi.processors.hive;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.nifi.annotation.behavior.InputRequirement;
 import org.apache.nifi.annotation.behavior.InputRequirement.Requirement;
 import org.apache.nifi.annotation.behavior.ReadsAttribute;
@@ -30,30 +31,19 @@ import org.apache.nifi.processor.ProcessContext;
 import org.apache.nifi.processor.ProcessSession;
 import org.apache.nifi.processor.Relationship;
 import org.apache.nifi.processor.exception.ProcessException;
-import org.apache.nifi.processor.io.InputStreamCallback;
 import org.apache.nifi.processor.util.StandardValidators;
-import org.apache.nifi.stream.io.StreamUtils;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.math.BigDecimal;
 import java.nio.charset.Charset;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.SQLNonTransientException;
-import java.sql.Time;
-import java.sql.Timestamp;
-import java.sql.Types;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @SeeAlso(SelectHiveQL.class)
@@ -80,13 +70,14 @@ public class PutHiveQL extends AbstractHiveQLProcessor {
             .defaultValue("100")
             .build();
 
-    public static final PropertyDescriptor CHARSET = new PropertyDescriptor.Builder()
-            .name("hive-charset")
-            .displayName("Character Set")
-            .description("Specifies the character set of the record data.")
+    public static final PropertyDescriptor STATEMENT_DELIMITER = new PropertyDescriptor.Builder()
+            .name("statement-delimiter")
+            .displayName("Statement Delimiter")
+            .description("Statement Delimiter used to separate SQL statements in a multiple statement script")
             .required(true)
-            .defaultValue("UTF-8")
-            .addValidator(StandardValidators.CHARACTER_SET_VALIDATOR)
+            .defaultValue(";")
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .expressionLanguageSupported(false)
             .build();
 
     public static final Relationship REL_SUCCESS = new Relationship.Builder()
@@ -103,14 +94,12 @@ public class PutHiveQL extends AbstractHiveQLProcessor {
                     + "such as an invalid query or an integrity constraint violation")
             .build();
 
-    private static final Pattern HIVEQL_TYPE_ATTRIBUTE_PATTERN = Pattern.compile("hiveql\\.args\\.(\\d+)\\.type");
-    private static final Pattern NUMBER_PATTERN = Pattern.compile("-?\\d+");
 
     private final static List<PropertyDescriptor> propertyDescriptors;
     private final static Set<Relationship> relationships;
 
     /*
-     * Will ensure that the list of property descriptors is build only once.
+     * Will ensure that the list of property descriptors is built only once.
      * Will also create a Set of relationships
      */
     static {
@@ -118,6 +107,7 @@ public class PutHiveQL extends AbstractHiveQLProcessor {
         _propertyDescriptors.add(HIVE_DBCP_SERVICE);
         _propertyDescriptors.add(BATCH_SIZE);
         _propertyDescriptors.add(CHARSET);
+        _propertyDescriptors.add(STATEMENT_DELIMITER);
         propertyDescriptors = Collections.unmodifiableList(_propertyDescriptors);
 
         Set<Relationship> _relationships = new HashSet<>();
@@ -149,19 +139,42 @@ public class PutHiveQL extends AbstractHiveQLProcessor {
         final long startNanos = System.nanoTime();
         final Charset charset = Charset.forName(context.getProperty(CHARSET).getValue());
         final HiveDBCPService dbcpService = context.getProperty(HIVE_DBCP_SERVICE).asControllerService(HiveDBCPService.class);
+        final String statementDelimiter =   context.getProperty(STATEMENT_DELIMITER).getValue();
+
         try (final Connection conn = dbcpService.getConnection()) {
 
             for (FlowFile flowFile : flowFiles) {
                 try {
-                    final String hiveQL = getHiveQL(session, flowFile, charset);
-                    final PreparedStatement stmt = conn.prepareStatement(hiveQL);
-                    setParameters(stmt, flowFile.getAttributes());
+                    final String script = getHiveQL(session, flowFile, charset);
+                    String regex = "(?<!\\\\)" + Pattern.quote(statementDelimiter);
 
-                    // Execute the statement
-                    stmt.execute();
+                    String[] hiveQLs = script.split(regex);
 
+                    int loc = 1;
+                    for (String hiveQL: hiveQLs) {
+                        getLogger().debug("HiveQL: {}", new Object[]{hiveQL});
+
+                        if (!StringUtils.isEmpty(hiveQL.trim())) {
+                            final PreparedStatement stmt = conn.prepareStatement(hiveQL.trim());
+
+                            // Get ParameterMetadata
+                            // Hive JDBC Doesn't support this yet:
+                            // ParameterMetaData pmd = stmt.getParameterMetaData();
+                            // int paramCount = pmd.getParameterCount();
+
+                            int paramCount = StringUtils.countMatches(hiveQL, "?");
+
+                            if (paramCount > 0) {
+                                loc = setParameters(loc, stmt, paramCount, flowFile.getAttributes());
+                            }
+
+                            // Execute the statement
+                            stmt.execute();
+                        }
+                    }
                     // Emit a Provenance SEND event
                     final long transmissionMillis = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNanos);
+
                     session.getProvenanceReporter().send(flowFile, dbcpService.getConnectionURL(), transmissionMillis, true);
                     session.transfer(flowFile, REL_SUCCESS);
 
@@ -185,130 +198,4 @@ public class PutHiveQL extends AbstractHiveQLProcessor {
             context.yield();
         }
     }
-
-    /**
-     * Determines the HiveQL statement that should be executed for the given FlowFile
-     *
-     * @param session  the session that can be used to access the given FlowFile
-     * @param flowFile the FlowFile whose HiveQL statement should be executed
-     * @return the HiveQL that is associated with the given FlowFile
-     */
-    private String getHiveQL(final ProcessSession session, final FlowFile flowFile, final Charset charset) {
-        // Read the HiveQL from the FlowFile's content
-        final byte[] buffer = new byte[(int) flowFile.getSize()];
-        session.read(flowFile, new InputStreamCallback() {
-            @Override
-            public void process(final InputStream in) throws IOException {
-                StreamUtils.fillBuffer(in, buffer);
-            }
-        });
-
-        // Create the PreparedStatement to use for this FlowFile.
-        return new String(buffer, charset);
-    }
-
-
-    /**
-     * Sets all of the appropriate parameters on the given PreparedStatement, based on the given FlowFile attributes.
-     *
-     * @param stmt       the statement to set the parameters on
-     * @param attributes the attributes from which to derive parameter indices, values, and types
-     * @throws SQLException if the PreparedStatement throws a SQLException when the appropriate setter is called
-     */
-    private void setParameters(final PreparedStatement stmt, final Map<String, String> attributes) throws SQLException {
-        for (final Map.Entry<String, String> entry : attributes.entrySet()) {
-            final String key = entry.getKey();
-            final Matcher matcher = HIVEQL_TYPE_ATTRIBUTE_PATTERN.matcher(key);
-            if (matcher.matches()) {
-                final int parameterIndex = Integer.parseInt(matcher.group(1));
-
-                final boolean isNumeric = NUMBER_PATTERN.matcher(entry.getValue()).matches();
-                if (!isNumeric) {
-                    throw new ProcessException("Value of the " + key + " attribute is '" + entry.getValue() + "', which is not a valid JDBC numeral type");
-                }
-
-                final int jdbcType = Integer.parseInt(entry.getValue());
-                final String valueAttrName = "hiveql.args." + parameterIndex + ".value";
-                final String parameterValue = attributes.get(valueAttrName);
-
-                try {
-                    setParameter(stmt, valueAttrName, parameterIndex, parameterValue, jdbcType);
-                } catch (final NumberFormatException nfe) {
-                    throw new ProcessException("The value of the " + valueAttrName + " is '" + parameterValue + "', which cannot be converted into the necessary data type", nfe);
-                }
-            }
-        }
-    }
-
-    /**
-     * Determines how to map the given value to the appropriate JDBC data type and sets the parameter on the
-     * provided PreparedStatement
-     *
-     * @param stmt           the PreparedStatement to set the parameter on
-     * @param attrName       the name of the attribute that the parameter is coming from - for logging purposes
-     * @param parameterIndex the index of the HiveQL parameter to set
-     * @param parameterValue the value of the HiveQL parameter to set
-     * @param jdbcType       the JDBC Type of the HiveQL parameter to set
-     * @throws SQLException if the PreparedStatement throws a SQLException when calling the appropriate setter
-     */
-    private void setParameter(final PreparedStatement stmt, final String attrName, final int parameterIndex, final String parameterValue, final int jdbcType) throws SQLException {
-        if (parameterValue == null) {
-            stmt.setNull(parameterIndex, jdbcType);
-        } else {
-            try {
-                switch (jdbcType) {
-                    case Types.BIT:
-                    case Types.BOOLEAN:
-                        stmt.setBoolean(parameterIndex, Boolean.parseBoolean(parameterValue));
-                        break;
-                    case Types.TINYINT:
-                        stmt.setByte(parameterIndex, Byte.parseByte(parameterValue));
-                        break;
-                    case Types.SMALLINT:
-                        stmt.setShort(parameterIndex, Short.parseShort(parameterValue));
-                        break;
-                    case Types.INTEGER:
-                        stmt.setInt(parameterIndex, Integer.parseInt(parameterValue));
-                        break;
-                    case Types.BIGINT:
-                        stmt.setLong(parameterIndex, Long.parseLong(parameterValue));
-                        break;
-                    case Types.REAL:
-                        stmt.setFloat(parameterIndex, Float.parseFloat(parameterValue));
-                        break;
-                    case Types.FLOAT:
-                    case Types.DOUBLE:
-                        stmt.setDouble(parameterIndex, Double.parseDouble(parameterValue));
-                        break;
-                    case Types.DECIMAL:
-                    case Types.NUMERIC:
-                        stmt.setBigDecimal(parameterIndex, new BigDecimal(parameterValue));
-                        break;
-                    case Types.DATE:
-                        stmt.setDate(parameterIndex, new Date(Long.parseLong(parameterValue)));
-                        break;
-                    case Types.TIME:
-                        stmt.setTime(parameterIndex, new Time(Long.parseLong(parameterValue)));
-                        break;
-                    case Types.TIMESTAMP:
-                        stmt.setTimestamp(parameterIndex, new Timestamp(Long.parseLong(parameterValue)));
-                        break;
-                    case Types.CHAR:
-                    case Types.VARCHAR:
-                    case Types.LONGNVARCHAR:
-                    case Types.LONGVARCHAR:
-                        stmt.setString(parameterIndex, parameterValue);
-                        break;
-                    default:
-                        stmt.setObject(parameterIndex, parameterValue, jdbcType);
-                        break;
-                }
-            } catch (SQLException e) {
-                // Log which attribute/parameter had an error, then rethrow to be handled at the top level
-                getLogger().error("Error setting parameter {} to value from {} ({})", new Object[]{parameterIndex, attrName, parameterValue}, e);
-                throw e;
-            }
-        }
-    }
-
 }
