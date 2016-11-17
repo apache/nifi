c1797746999edc3c5f1d9ed0ecb26eb65ff41f0a
joewitt <joewitt@apache.org>
2016-11-17 17:09:49 -0500

    NIFI-2954 Moved StandardPropertyValidator to nifi-utils, documented scope/purpose of a few util libs, removed deps from nifi-utils.

--- nifi-nar-bundles/nifi-standard-services/nifi-hbase-client-service-api/src/main/java/org/apache/nifi/hbase/put/PutFlowFile.java
+++ nifi-nar-bundles/nifi-standard-services/nifi-hbase-client-service-api/src/main/java/org/apache/nifi/hbase/put/PutFlowFile.java
@@ -16,7 +16,6 @@
  */
 package org.apache.nifi.hbase.put;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.nifi.flowfile.FlowFile;
 
 import java.util.Collection;
@@ -55,7 +54,7 @@ public class PutFlowFile {
     }
 
     public boolean isValid() {
-        if (StringUtils.isBlank(tableName) || null == row || flowFile == null || columns == null || columns.isEmpty()) {
+        if (tableName == null || tableName.trim().isEmpty() || null == row || flowFile == null || columns == null || columns.isEmpty()) {
             return false;
         }
 
