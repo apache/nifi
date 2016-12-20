954201a4d74b33964dd361732255d086032901ad
Mike Moser <mosermw@apache.org>
2016-12-20 17:26:39 +0000

    NIFI-3230 fix ActiveMQ SSL support for simple connect URIs in Get/Put JMS processors
    This closes #1344.

--- nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/util/JmsFactory.java
+++ nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/util/JmsFactory.java
@@ -378,6 +378,9 @@ public class JmsFactory {
     private static boolean isSSL(URI uri) {
         try {
             CompositeData compositeData = URISupport.parseComposite(uri);
+            if ("ssl".equals(compositeData.getScheme())) {
+                return true;
+            }
             for(URI component : compositeData.getComponents()){
                 if ("ssl".equals(component.getScheme())) {
                     return true;
