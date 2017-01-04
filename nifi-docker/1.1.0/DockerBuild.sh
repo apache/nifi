c809165ff1f375e771c0cf613af87c75c3474bd6
Jeremy Dyer <jeremydyer@apache.org>
2017-01-04 09:11:06 -0500

    NIFI-3260
    Added licensing and non root user fixes per @brosander comments.
    
    I know discussion about the final resting place of this logic is on
    going but want to at least continue forward motion on the core
    Dockerfile here for the time being. Things like the DockerBuild.sh and
    DockerRun.sh will almost certainly be of a different format once we
    either move to another repo or incorporate into the build process.

--- nifi-docker/1.1.0/DockerBuild.sh
+++ nifi-docker/1.1.0/DockerBuild.sh
@@ -1,3 +1,29 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #!/bin/bash
-DOCKER_IMAGE="$(cat DockerImage.txt)"
-docker build -t $DOCKER_IMAGE .
+
+DOCKER_UID=1000
+if [ -n "$1" ]; then
+  DOCKER_UID="$1"
+fi
+
+DOCKER_GID=50
+if [ -n "$2" ]; then
+  DOCKER_GID="$2"
+fi
+
+DOCKER_IMAGE="$(egrep -v '(^#|^\s*$|^\s*\t*#)' DockerImage.txt)"
+docker build --build-arg uid="$DOCKER_UID" --build-arg gid="$DOCKER_GID" -t $DOCKER_IMAGE .
