dcedd3470249c9a3ed0dc2b25564927e2e2074de
Mark Payne <markap14@hotmail.com>
2017-04-10 10:05:17 -0400

    NIFI-1280: Addressed feedback from PR Review

--- nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/test/java/org/apache/nifi/processors/standard/TestQueryFlowFile.java
+++ nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/test/java/org/apache/nifi/processors/standard/TestQueryFlowFile.java
@@ -24,7 +24,6 @@ import java.util.Collections;
 import java.util.List;
 
 import org.apache.nifi.controller.AbstractControllerService;
-import org.apache.nifi.flowfile.FlowFile;
 import org.apache.nifi.logging.ComponentLog;
 import org.apache.nifi.processors.standard.util.record.MockRecordParser;
 import org.apache.nifi.processors.standard.util.record.MockRecordWriter;
@@ -242,8 +241,6 @@ public class TestQueryFlowFile {
                     // Iterate over the rest of the records to ensure that we read the entire stream. If we don't
                     // do this, we won't consume all of the data and as a result we will not close the stream properly
                     Record record;
-<<<<<<< 103cb3695cf9f5cce67390997c97c72b7f3c8bf4
-=======
                     while ((record = rs.next()) != null) {
                         System.out.println(record);
                     }
@@ -263,110 +260,6 @@ public class TestQueryFlowFile {
             };
         }
 
-    }
-
-    private static class MockRecordWriter extends AbstractControllerService implements RecordSetWriterFactory {
-        private final String header;
-
-        public MockRecordWriter(final String header) {
-            this.header = header;
-        }
-
-        @Override
-        public RecordSetWriter createWriter(final ComponentLog logger) {
-            return new RecordSetWriter() {
-                @Override
-                public WriteResult write(final RecordSet rs, final OutputStream out) throws IOException {
-                    out.write(header.getBytes());
-                    out.write("\n".getBytes());
-
-                    int recordCount = 0;
-                    final int numCols = rs.getSchema().getFieldCount();
-                    Record record = null;
->>>>>>> NIFI-1280: Updated SimpleKeyValueSchemaRegistry to make use of new CHOICE RecordFieldType
-                    while ((record = rs.next()) != null) {
-                        System.out.println(record);
-                    }
-
-                    return WriteResult.of(0, Collections.emptyMap());
-                }
-
-                @Override
-                public String getMimeType() {
-                    return "text/plain";
-                }
-
-                @Override
-                public WriteResult write(Record record, OutputStream out) throws IOException {
-                    return null;
-                }
-            };
-        }
-
-<<<<<<< 103cb3695cf9f5cce67390997c97c72b7f3c8bf4
-=======
-        public MockRecordParser() {
-            this(-1);
-        }
-
-        public MockRecordParser(final int failAfterN) {
-            this.failAfterN = failAfterN;
-        }
-
-
-        public void addSchemaField(final String fieldName, final RecordFieldType type) {
-            fields.add(new RecordField(fieldName, type.getDataType()));
-        }
-
-        public void addRecord(Object... values) {
-            records.add(values);
-        }
-
-        @Override
-        public RecordReader createRecordReader(FlowFile flowFile, InputStream in, ComponentLog logger) throws IOException {
-            final Iterator<Object[]> itr = records.iterator();
-
-            return new RecordReader() {
-                private int recordCount = 0;
-
-                @Override
-                public void close() throws IOException {
-                }
-
-                @Override
-                public Record nextRecord() throws IOException, MalformedRecordException {
-                    if (failAfterN >= recordCount) {
-                        throw new MalformedRecordException("Intentional Unit Test Exception because " + recordCount + " records have been read");
-                    }
-                    recordCount++;
-
-                    if (!itr.hasNext()) {
-                        return null;
-                    }
-
-                    final Object[] values = itr.next();
-                    final Map<String, Object> valueMap = new HashMap<>();
-                    int i = 0;
-                    for (final RecordField field : fields) {
-                        final String fieldName = field.getFieldName();
-                        valueMap.put(fieldName, values[i++]);
-                    }
-
-                    return new MapRecord(new SimpleRecordSchema(fields), valueMap);
-                }
-
-                @Override
-                public RecordSchema getSchema() {
-                    return new SimpleRecordSchema(fields);
-                }
-            };
-        }
-
-        @Override
-        public RecordSchema getSchema(FlowFile flowFile) throws MalformedRecordException, IOException {
-            return null;
-        }
->>>>>>> NIFI-1280: Updated SimpleKeyValueSchemaRegistry to make use of new CHOICE RecordFieldType
     }
 
 }
