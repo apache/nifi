06f5b4ddf919bd0d38510d585c4480129b343a1b
Yolanda M. Davis <yolanda.m.davis@gmail.com>
2017-04-19 13:40:57 -0400

    NIFI-3695 - refactored to generate key/trust stores for tests

--- nifi-toolkit/nifi-toolkit-admin/src/test/groovy/org/apache/nifi/toolkit/admin/client/NiFiClientFactorySpec.groovy
+++ nifi-toolkit/nifi-toolkit-admin/src/test/groovy/org/apache/nifi/toolkit/admin/client/NiFiClientFactorySpec.groovy
@@ -17,9 +17,15 @@
 
 package org.apache.nifi.toolkit.admin.client
 
+import org.apache.commons.lang3.SystemUtils
 import org.apache.nifi.properties.NiFiPropertiesLoader
+import org.apache.nifi.toolkit.tls.standalone.TlsToolkitStandalone
+import org.apache.nifi.toolkit.tls.standalone.TlsToolkitStandaloneCommandLine
 import org.apache.nifi.util.NiFiProperties
 import spock.lang.Specification
+
+import java.nio.file.Files
+import java.nio.file.attribute.PosixFilePermission
 
 class NiFiClientFactorySpec extends Specification {
 
@@ -40,6 +46,13 @@ class NiFiClientFactorySpec extends Specification {
     def "get client for secured nifi"(){
 
         given:
+        def File tmpDir = setupTmpDir()
+        def File testDir = new File("target/tmp/keys")
+        def toolkitCommandLine = ["-O", "-o",testDir.absolutePath,"-n","localhost","-C", "CN=user1","-S", "badKeyPass", "-K", "badKeyPass", "-P", "badTrustPass"]
+
+        TlsToolkitStandaloneCommandLine tlsToolkitStandaloneCommandLine = new TlsToolkitStandaloneCommandLine()
+        tlsToolkitStandaloneCommandLine.parse(toolkitCommandLine as String[])
+        new TlsToolkitStandalone().createNifiKeystoresAndTrustStores(tlsToolkitStandaloneCommandLine.createConfig())
 
         def bootstrapConfFile = "src/test/resources/notify/conf/bootstrap.conf"
         def nifiPropertiesFile = "src/test/resources/notify/conf/nifi-secured.properties"
@@ -53,7 +66,27 @@ class NiFiClientFactorySpec extends Specification {
         then:
         client
 
+        cleanup:
+        tmpDir.deleteDir()
 
+    }
+
+    def setFilePermissions(File file, List<PosixFilePermission> permissions = []) {
+        if (SystemUtils.IS_OS_WINDOWS) {
+            file?.setReadable(permissions.contains(PosixFilePermission.OWNER_READ))
+            file?.setWritable(permissions.contains(PosixFilePermission.OWNER_WRITE))
+            file?.setExecutable(permissions.contains(PosixFilePermission.OWNER_EXECUTE))
+        } else {
+            Files.setPosixFilePermissions(file?.toPath(), permissions as Set)
+        }
+    }
+    def setupTmpDir(String tmpDirPath = "target/tmp/") {
+        File tmpDir = new File(tmpDirPath)
+        tmpDir.mkdirs()
+        setFilePermissions(tmpDir, [PosixFilePermission.OWNER_READ, PosixFilePermission.OWNER_WRITE, PosixFilePermission.OWNER_EXECUTE,
+                                    PosixFilePermission.GROUP_READ, PosixFilePermission.GROUP_WRITE, PosixFilePermission.GROUP_EXECUTE,
+                                    PosixFilePermission.OTHERS_READ, PosixFilePermission.OTHERS_WRITE, PosixFilePermission.OTHERS_EXECUTE])
+        tmpDir
     }
 
 }
