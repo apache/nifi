d6ff66c31233a7417c1d005da9f345a78ad965fd
Andy LoPresto <alopresto@apache.org>
2017-04-24 22:40:54 -0700

    NIFI-3594 Added Java JUnit test in nifi-data-provenance-utils module to trigger Maven running Groovy unit tests.

--- nifi-commons/nifi-data-provenance-utils/src/test/groovy/org/apache/nifi/provenance/AESProvenanceEventEncryptorTest.groovy
+++ nifi-commons/nifi-data-provenance-utils/src/test/groovy/org/apache/nifi/provenance/AESProvenanceEventEncryptorTest.groovy
@@ -48,7 +48,6 @@ class AESProvenanceEventEncryptorTest {
     private static final String KEY_HEX_128 = "0123456789ABCDEFFEDCBA9876543210"
     private static final String KEY_HEX_256 = KEY_HEX_128 * 2
     private static final String KEY_HEX = isUnlimitedStrengthCryptoAvailable() ? KEY_HEX_256 : KEY_HEX_128
-    private static final int IV_LENGTH = 16
 
     private static KeyProvider mockKeyProvider
     private static AESKeyedCipherProvider mockCipherProvider
