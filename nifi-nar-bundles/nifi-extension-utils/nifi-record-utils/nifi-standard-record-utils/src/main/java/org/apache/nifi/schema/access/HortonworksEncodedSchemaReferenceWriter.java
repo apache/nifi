7035694e37b79bd58d9168cb5010b14a15db0396
Mark Payne <markap14@hotmail.com>
2017-06-01 09:45:44 -0400

    NIFI-3995: Updated Hwx Encoded Schema Ref Writer to write 13 bytes for header instead of 14; added unit test to verify
    This closes #1876.
    
    Signed-off-by: Bryan Bende <bbende@apache.org>

--- nifi-nar-bundles/nifi-extension-utils/nifi-record-utils/nifi-standard-record-utils/src/main/java/org/apache/nifi/schema/access/HortonworksEncodedSchemaReferenceWriter.java
+++ nifi-nar-bundles/nifi-extension-utils/nifi-record-utils/nifi-standard-record-utils/src/main/java/org/apache/nifi/schema/access/HortonworksEncodedSchemaReferenceWriter.java
@@ -43,7 +43,7 @@ public class HortonworksEncodedSchemaReferenceWriter implements SchemaAccessWrit
         // This decoding follows the pattern that is provided for serializing data by the Hortonworks Schema Registry serializer
         // as it is provided at:
         // https://github.com/hortonworks/registry/blob/master/schema-registry/serdes/src/main/java/com/hortonworks/registries/schemaregistry/serdes/avro/AvroSnapshotSerializer.java
-        final ByteBuffer bb = ByteBuffer.allocate(14);
+        final ByteBuffer bb = ByteBuffer.allocate(13);
         bb.put((byte) LATEST_PROTOCOL_VERSION);
         bb.putLong(id);
         bb.putInt(version);
