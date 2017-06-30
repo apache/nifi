af3f7391fb5cf36bfe00ac157be738d554a41d01
yoonwon.ko <legend0328@gmail.com>
2017-06-30 16:34:50 +0900

    NIFI-4105 I 'm adding unit tests for the incremental fetch feature

--- nifi-nar-bundles/nifi-cassandra-bundle/nifi-cassandra-processors/src/test/java/org/apache/nifi/processors/cassandra/QueryCassandraTest.java
+++ nifi-nar-bundles/nifi-cassandra-bundle/nifi-cassandra-processors/src/test/java/org/apache/nifi/processors/cassandra/QueryCassandraTest.java
@@ -16,16 +16,11 @@
  */
 package org.apache.nifi.processors.cassandra;
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Configuration;
-import com.datastax.driver.core.ConsistencyLevel;
-import com.datastax.driver.core.Metadata;
-import com.datastax.driver.core.ResultSet;
-import com.datastax.driver.core.ResultSetFuture;
-import com.datastax.driver.core.Session;
+import com.datastax.driver.core.*;
 import com.datastax.driver.core.exceptions.InvalidQueryException;
 import com.datastax.driver.core.exceptions.NoHostAvailableException;
 import com.datastax.driver.core.exceptions.ReadTimeoutException;
+import com.google.common.collect.Sets;
 import org.apache.avro.Schema;
 import org.apache.nifi.processor.exception.ProcessException;
 import org.apache.nifi.stream.io.ByteArrayOutputStream;
@@ -38,8 +33,7 @@ import org.junit.Test;
 import javax.net.ssl.SSLContext;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -240,6 +234,30 @@ public class QueryCassandraTest {
                         "user2,Mary,Jones,[mjones@notareal.com],[Orlando, FL],{Wed Feb 03 14:00:00 KST 2016=Get milk and bread},true,3.0,4.0",
                 new String(files.get(0).toByteArray()));
     }
+
+//    @Test
+//    public void testProcessorIncrementalFetch() {
+//        setUpStandardProcessorConfig();
+//        testRunner.setIncomingConnection(false);
+//
+//        // Test JSON output
+//        testRunner.setProperty(QueryCassandra.OUTPUT_FORMAT, QueryCassandra.JSON_FORMAT);
+//        testRunner.setProperty(QueryCassandra.DATE_FIELD, );
+//        testRunner.run(1, true, true);
+//        testRunner.assertAllFlowFilesTransferred(QueryCassandra.REL_SUCCESS, 1);
+//        List<MockFlowFile> files = testRunner.getFlowFilesForRelationship(QueryCassandra.REL_SUCCESS);
+//
+//        assertNotNull(files);
+//        assertEquals("One file should be transferred to success", 1, files.size());
+//
+//        Row row = CassandraQueryTestUtil.createRow("user3", "James", "Ko",
+//                Sets.newHashSet("jk@notareal.com", "jk@fakedomain.com"), Arrays.asList("New York, NY", "Santa Clara, CA"),
+//                new HashMap<Date, String>() {{
+//                    put(Calendar.getInstance().getTime(), "Set my alarm for a month from now");
+//                }}, true, 1.0f, 2.0);
+//
+//
+//    }
 
     @Test
     public void testProcessorEmptyFlowFileAndExceptions() {
