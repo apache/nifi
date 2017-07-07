1574395b3ec2dbae06e78cf373cf245c44e18355
m-hogue <hogue23@gmail.com>
2017-07-07 06:46:02 -0400

    NIFI-3218: fixed incorrect assertion for self-routing flowfiles in MockProcessSession

--- nifi-mock/src/main/java/org/apache/nifi/util/MockProcessSession.java
+++ nifi-mock/src/main/java/org/apache/nifi/util/MockProcessSession.java
@@ -756,10 +756,10 @@ public class MockProcessSession implements ProcessSession {
             throw new IllegalArgumentException("I only accept MockFlowFile");
         }
 
-        // if the flowfile provided was created in this session (i.e. it's in currentVersions),
+        // if the flowfile provided was created in this session (i.e. it's in currentVersions and not in original versions),
         // then throw an exception indicating that you can't transfer flowfiles back to self.
-        // this mimics the behavior of StandardProcessSession
-        if(currentVersions.get(flowFile.getId()) != null) {
+        // this mimics the same behavior in StandardProcessSession
+        if(currentVersions.get(flowFile.getId()) != null && originalVersions.get(flowFile.getId()) == null) {
             throw new IllegalArgumentException("Cannot transfer FlowFiles that are created in this Session back to self");
         }
 
