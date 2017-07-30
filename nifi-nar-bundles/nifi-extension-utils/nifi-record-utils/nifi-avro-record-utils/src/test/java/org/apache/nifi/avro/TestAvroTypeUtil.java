9bf51999462eaf87b36472d52fba4ce292240947
Wesley-Lawrence <wesleyll505@gmail.com>
2017-07-30 14:49:00 -0400

    NIFI-4215 NiFi can now parse an Avro schema of a record that references an already defined record, including itself.

--- nifi-nar-bundles/nifi-extension-utils/nifi-record-utils/nifi-avro-record-utils/src/test/java/org/apache/nifi/avro/TestAvroTypeUtil.java
+++ nifi-nar-bundles/nifi-extension-utils/nifi-record-utils/nifi-avro-record-utils/src/test/java/org/apache/nifi/avro/TestAvroTypeUtil.java
@@ -23,6 +23,7 @@ import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.avro.Schema;
 import org.apache.avro.Schema.Field;
@@ -33,6 +34,8 @@ import org.apache.nifi.serialization.record.DataType;
 import org.apache.nifi.serialization.record.RecordField;
 import org.apache.nifi.serialization.record.RecordFieldType;
 import org.apache.nifi.serialization.record.RecordSchema;
+import org.apache.nifi.serialization.record.type.RecordDataType;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestAvroTypeUtil {
@@ -98,6 +101,142 @@ public class TestAvroTypeUtil {
         final RecordField stringField = afterConversion.getField("string").get();
         assertEquals("hola", stringField.getDefaultValue());
         assertEquals(Collections.singleton("greeting"), stringField.getAliases());
+    }
+
+    @Test
+    // Simple recursion is a record A composing itself (similar to a LinkedList Node referencing 'next')
+    public void testSimpleRecursiveSchema() {
+        Schema recursiveSchema = new Schema.Parser().parse(
+                "{\n" +
+                "  \"namespace\": \"org.apache.nifi.testing\",\n" +
+                "  \"name\": \"NodeRecord\",\n" +
+                "  \"type\": \"record\",\n" +
+                "  \"fields\": [\n" +
+                "    {\n" +
+                "      \"name\": \"id\",\n" +
+                "      \"type\": \"int\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"value\",\n" +
+                "      \"type\": \"string\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"parent\",\n" +
+                "      \"type\": [\n" +
+                "        \"null\",\n" +
+                "        \"NodeRecord\"\n" +
+                "      ]\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}\n"
+        );
+
+        // Make sure the following doesn't throw an exception
+        RecordSchema result = AvroTypeUtil.createSchema(recursiveSchema);
+
+        // Make sure it parsed correctly
+        Assert.assertEquals(3, result.getFieldCount());
+
+        Optional<RecordField> idField = result.getField("id");
+        Assert.assertTrue(idField.isPresent());
+        Assert.assertEquals(RecordFieldType.INT, idField.get().getDataType().getFieldType());
+
+        Optional<RecordField> valueField = result.getField("value");
+        Assert.assertTrue(valueField.isPresent());
+        Assert.assertEquals(RecordFieldType.STRING, valueField.get().getDataType().getFieldType());
+
+        Optional<RecordField> parentField = result.getField("parent");
+        Assert.assertTrue(parentField.isPresent());
+        Assert.assertEquals(RecordFieldType.RECORD, parentField.get().getDataType().getFieldType());
+
+        // The 'parent' field should have a circular schema reference to the top level record schema, similar to how Avro handles this
+        Assert.assertEquals(result, ((RecordDataType)parentField.get().getDataType()).getChildSchema());
+    }
+
+    @Test
+    // Complicated recursion is a record A composing record B, who composes a record A
+    public void testComplicatedRecursiveSchema() {
+        Schema recursiveSchema = new Schema.Parser().parse(
+                "{\n" +
+                "  \"namespace\": \"org.apache.nifi.testing\",\n" +
+                "  \"name\": \"Record_A\",\n" +
+                "  \"type\": \"record\",\n" +
+                "  \"fields\": [\n" +
+                "    {\n" +
+                "      \"name\": \"id\",\n" +
+                "      \"type\": \"int\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"value\",\n" +
+                "      \"type\": \"string\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"child\",\n" +
+                "      \"type\": {\n" +
+                "        \"namespace\": \"org.apache.nifi.testing\",\n" +
+                "        \"name\": \"Record_B\",\n" +
+                "        \"type\": \"record\",\n" +
+                "        \"fields\": [\n" +
+                "          {\n" +
+                "            \"name\": \"id\",\n" +
+                "            \"type\": \"int\"\n" +
+                "          },\n" +
+                "          {\n" +
+                "            \"name\": \"value\",\n" +
+                "            \"type\": \"string\"\n" +
+                "          },\n" +
+                "          {\n" +
+                "            \"name\": \"parent\",\n" +
+                "            \"type\": [\n" +
+                "              \"null\",\n" +
+                "              \"Record_A\"\n" +
+                "            ]\n" +
+                "          }\n" +
+                "        ]\n" +
+                "      }\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}\n"
+        );
+
+        // Make sure the following doesn't throw an exception
+        RecordSchema recordASchema = AvroTypeUtil.createSchema(recursiveSchema);
+
+        // Make sure it parsed correctly
+        Assert.assertEquals(3, recordASchema.getFieldCount());
+
+        Optional<RecordField> recordAIdField = recordASchema.getField("id");
+        Assert.assertTrue(recordAIdField.isPresent());
+        Assert.assertEquals(RecordFieldType.INT, recordAIdField.get().getDataType().getFieldType());
+
+        Optional<RecordField> recordAValueField = recordASchema.getField("value");
+        Assert.assertTrue(recordAValueField.isPresent());
+        Assert.assertEquals(RecordFieldType.STRING, recordAValueField.get().getDataType().getFieldType());
+
+        Optional<RecordField> recordAChildField = recordASchema.getField("child");
+        Assert.assertTrue(recordAChildField.isPresent());
+        Assert.assertEquals(RecordFieldType.RECORD, recordAChildField.get().getDataType().getFieldType());
+
+        // Get the child schema
+        RecordSchema recordBSchema = ((RecordDataType)recordAChildField.get().getDataType()).getChildSchema();
+
+        // Make sure it parsed correctly
+        Assert.assertEquals(3, recordBSchema.getFieldCount());
+
+        Optional<RecordField> recordBIdField = recordBSchema.getField("id");
+        Assert.assertTrue(recordBIdField.isPresent());
+        Assert.assertEquals(RecordFieldType.INT, recordBIdField.get().getDataType().getFieldType());
+
+        Optional<RecordField> recordBValueField = recordBSchema.getField("value");
+        Assert.assertTrue(recordBValueField.isPresent());
+        Assert.assertEquals(RecordFieldType.STRING, recordBValueField.get().getDataType().getFieldType());
+
+        Optional<RecordField> recordBParentField = recordBSchema.getField("parent");
+        Assert.assertTrue(recordBParentField.isPresent());
+        Assert.assertEquals(RecordFieldType.RECORD, recordBParentField.get().getDataType().getFieldType());
+
+        // Make sure the 'parent' field has a schema reference back to the original top level record schema
+        Assert.assertEquals(recordASchema, ((RecordDataType)recordBParentField.get().getDataType()).getChildSchema());
     }
 
 }
