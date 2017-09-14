13e42678b64953de593456363fcf3ffb97e00731
Takanobu Asanuma <tasanuma@yahoo-corp.jp>
2017-09-14 14:25:15 +0900

    NIFI-4338. This closes #2143. add docs for ssl configurations in HDFS processors
    remove redundant additionalDetails.html and add docs to CapabilityDescription in HDFS processors
    
    revert the modified CapabilityDescriptions in HDFS processors and add it to AbstractHadoopProcessor

--- nifi-nar-bundles/nifi-extension-utils/nifi-hadoop-utils/src/main/java/org/apache/nifi/processors/hadoop/AbstractHadoopProcessor.java
+++ nifi-nar-bundles/nifi-extension-utils/nifi-hadoop-utils/src/main/java/org/apache/nifi/processors/hadoop/AbstractHadoopProcessor.java
@@ -67,7 +67,8 @@ public abstract class AbstractHadoopProcessor extends AbstractProcessor {
     public static final PropertyDescriptor HADOOP_CONFIGURATION_RESOURCES = new PropertyDescriptor.Builder()
             .name("Hadoop Configuration Resources")
             .description("A file or comma separated list of files which contains the Hadoop file system configuration. Without this, Hadoop "
-                    + "will search the classpath for a 'core-site.xml' and 'hdfs-site.xml' file or will revert to a default configuration.")
+                    + "will search the classpath for a 'core-site.xml' and 'hdfs-site.xml' file or will revert to a default configuration. "
+                    + "To use swebhdfs, see 'Additional Details' section of PutHDFS's documentation.")
             .required(false)
             .addValidator(HadoopValidators.ONE_OR_MORE_FILE_EXISTS_VALIDATOR)
             .expressionLanguageSupported(true)
