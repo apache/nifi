959f90ec4bd2f5a89add89728f33b1074ce6b759
Koji Kawamura <ijokarumawak@apache.org>
2017-11-02 22:54:31 +0900

    Incorporated review comment, added method signature with details string.

--- nifi-mock/src/main/java/org/apache/nifi/util/MockProvenanceReporter.java
+++ nifi-mock/src/main/java/org/apache/nifi/util/MockProvenanceReporter.java
@@ -233,8 +233,14 @@ public class MockProvenanceReporter implements ProvenanceReporter {
 
     @Override
     public void invokeRemoteProcess(final FlowFile flowFile, final String transitUri) {
+        invokeRemoteProcess(flowFile, transitUri, null);
+    }
+
+    @Override
+    public void invokeRemoteProcess(FlowFile flowFile, String transitUri, String details) {
         try {
-            final ProvenanceEventRecord record = build(flowFile, ProvenanceEventType.REMOTE_INVOCATION).setTransitUri(transitUri).build();
+            final ProvenanceEventRecord record = build(flowFile, ProvenanceEventType.REMOTE_INVOCATION)
+                    .setTransitUri(transitUri).setDetails(details).build();
             events.add(record);
         } catch (final Exception e) {
             logger.error("Failed to generate Provenance Event due to " + e);
