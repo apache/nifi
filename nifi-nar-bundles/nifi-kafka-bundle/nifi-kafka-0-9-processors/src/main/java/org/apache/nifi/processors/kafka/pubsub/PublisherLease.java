e406f004e5c33e9387180bee8135ef7c7d224214
Koji Kawamura <ijokarumawak@apache.org>
2017-12-27 17:38:57 +0900

    NIFI-4724: Support 0 byte message with PublishKafka
    Before this fix, PublishKafka (0.9) and PublishKafka_0_10 fail with empty incoming FlowFiles due to 'transfer relationship not specified' error.
    Because the internal 'publish' method is not called as StreamDemarcator does not emit any token regardless whether demarcator is set or not.
    
    As for PublishKafka_0_11 and PublishKafka_1_0, empty FlowFiles are transferred to 'success' relationship, however no Kafka message is sent to Kafka.
    
    Since Kafka allows 0 byte body empty messages, NiFi should be able to send it, too.
    
    This commit changes above current situation to the followings, with all PublishKafka_* processors:
    
    - If demarcator is not set, then publish incoming FlowFile content as it is. This enables sending an empty Kafka message.
    - If demarcator is set, send each token as a separate message.
      Even if no token is found (empty incoming FlowFile), transfer the FlowFile to 'success'.

--- nifi-nar-bundles/nifi-kafka-bundle/nifi-kafka-0-9-processors/src/main/java/org/apache/nifi/processors/kafka/pubsub/PublisherLease.java
+++ nifi-nar-bundles/nifi-kafka-bundle/nifi-kafka-0-9-processors/src/main/java/org/apache/nifi/processors/kafka/pubsub/PublisherLease.java
@@ -17,6 +17,7 @@
 
 package org.apache.nifi.processors.kafka.pubsub;
 
+import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
@@ -29,6 +30,7 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.nifi.flowfile.FlowFile;
 import org.apache.nifi.logging.ComponentLog;
+import org.apache.nifi.stream.io.StreamUtils;
 import org.apache.nifi.stream.io.exception.TokenTooLargeException;
 import org.apache.nifi.stream.io.util.StreamDemarcator;
 
@@ -61,9 +63,18 @@ public class PublisherLease implements Closeable {
             tracker = new InFlightMessageTracker();
         }
 
-        try (final StreamDemarcator demarcator = new StreamDemarcator(flowFileContent, demarcatorBytes, maxMessageSize)) {
+        try {
             byte[] messageContent;
-            try {
+            if (demarcatorBytes == null || demarcatorBytes.length == 0) {
+                // Send FlowFile content as it is, to support sending 0 byte message.
+                final ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                StreamUtils.copy(flowFileContent, bos);
+                messageContent = bos.toByteArray();
+                publish(flowFile, messageKey, messageContent, topic, tracker);
+                return;
+            }
+
+            try (final StreamDemarcator demarcator = new StreamDemarcator(flowFileContent, demarcatorBytes, maxMessageSize)) {
                 while ((messageContent = demarcator.nextToken()) != null) {
                     publish(flowFile, messageKey, messageContent, topic, tracker);
 
@@ -72,6 +83,7 @@ public class PublisherLease implements Closeable {
                         return;
                     }
                 }
+                tracker.trackEmpty(flowFile);
             } catch (final TokenTooLargeException ttle) {
                 tracker.fail(flowFile, ttle);
             }
