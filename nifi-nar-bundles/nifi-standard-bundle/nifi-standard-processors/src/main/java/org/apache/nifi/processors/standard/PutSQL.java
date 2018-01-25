7052824d88b3b9ceb04d7dda92cb63264ccf2a65
Matthew Burgess <mattyb149@apache.org>
2018-01-25 10:24:17 -0500

    NIFI-978: Support parameterized statements in ExecuteSQL

--- nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/PutSQL.java
+++ nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/PutSQL.java
@@ -48,37 +48,19 @@ import org.apache.nifi.processor.util.pattern.PartialFunctions.FlowFileGroup;
 import org.apache.nifi.processor.util.pattern.PutGroup;
 import org.apache.nifi.processor.util.pattern.RollbackOnFailure;
 import org.apache.nifi.processor.util.pattern.RoutingResult;
+import org.apache.nifi.processors.standard.util.JdbcCommon;
 import org.apache.nifi.stream.io.StreamUtils;
 
-import javax.xml.bind.DatatypeConverter;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringReader;
-import java.io.UnsupportedEncodingException;
-import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 import java.sql.BatchUpdateException;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLDataException;
 import java.sql.SQLException;
 import java.sql.SQLNonTransientException;
 import java.sql.Statement;
-import java.sql.Time;
-import java.sql.Timestamp;
-import java.sql.Types;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAccessor;
 import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.Comparator;
@@ -89,8 +71,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.function.BiFunction;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static org.apache.nifi.processor.util.pattern.ExceptionHandler.createOnError;
 
@@ -197,14 +177,9 @@ public class PutSQL extends AbstractSessionFactoryProcessor {
                     + "such as an invalid query or an integrity constraint violation")
             .build();
 
-    private static final Pattern SQL_TYPE_ATTRIBUTE_PATTERN = Pattern.compile("sql\\.args\\.(\\d+)\\.type");
-    private static final Pattern NUMBER_PATTERN = Pattern.compile("-?\\d+");
-
     private static final String FRAGMENT_ID_ATTR = FragmentAttributes.FRAGMENT_ID.key();
     private static final String FRAGMENT_INDEX_ATTR = FragmentAttributes.FRAGMENT_INDEX.key();
     private static final String FRAGMENT_COUNT_ATTR = FragmentAttributes.FRAGMENT_COUNT.key();
-
-    private static final Pattern LONG_PATTERN = Pattern.compile("^-?\\d{1,19}$");
 
     @Override
     protected List<PropertyDescriptor> getSupportedPropertyDescriptors() {
@@ -310,7 +285,7 @@ public class PutSQL extends AbstractSessionFactoryProcessor {
 
             if(!exceptionHandler.execute(fc, flowFile, input -> {
                 final PreparedStatement stmt = enclosure.getCachedStatement(conn);
-                setParameters(stmt, flowFile.getAttributes());
+                JdbcCommon.setParameters(stmt, flowFile.getAttributes());
                 stmt.addBatch();
             }, onFlowFileError(context, session, result))) {
                 continue;
@@ -383,7 +358,7 @@ public class PutSQL extends AbstractSessionFactoryProcessor {
                     try (final PreparedStatement stmt = targetEnclosure.getNewStatement(conn, fc.obtainKeys)) {
 
                         // set the appropriate parameters on the statement.
-                        setParameters(stmt, flowFile.getAttributes());
+                        JdbcCommon.setParameters(stmt, flowFile.getAttributes());
 
                         stmt.executeUpdate();
 
@@ -677,46 +652,6 @@ public class PutSQL extends AbstractSessionFactoryProcessor {
         return sql;
     }
 
-
-    /**
-     * Sets all of the appropriate parameters on the given PreparedStatement, based on the given FlowFile attributes.
-     *
-     * @param stmt the statement to set the parameters on
-     * @param attributes the attributes from which to derive parameter indices, values, and types
-     * @throws SQLException if the PreparedStatement throws a SQLException when the appropriate setter is called
-     */
-    private void setParameters(final PreparedStatement stmt, final Map<String, String> attributes) throws SQLException {
-        for (final Map.Entry<String, String> entry : attributes.entrySet()) {
-            final String key = entry.getKey();
-            final Matcher matcher = SQL_TYPE_ATTRIBUTE_PATTERN.matcher(key);
-            if (matcher.matches()) {
-                final int parameterIndex = Integer.parseInt(matcher.group(1));
-
-                final boolean isNumeric = NUMBER_PATTERN.matcher(entry.getValue()).matches();
-                if (!isNumeric) {
-                    throw new SQLDataException("Value of the " + key + " attribute is '" + entry.getValue() + "', which is not a valid JDBC numeral type");
-                }
-
-                final int jdbcType = Integer.parseInt(entry.getValue());
-                final String valueAttrName = "sql.args." + parameterIndex + ".value";
-                final String parameterValue = attributes.get(valueAttrName);
-                final String formatAttrName = "sql.args." + parameterIndex + ".format";
-                final String parameterFormat = attributes.containsKey(formatAttrName)? attributes.get(formatAttrName):"";
-
-                try {
-                    setParameter(stmt, valueAttrName, parameterIndex, parameterValue, jdbcType, parameterFormat);
-                } catch (final NumberFormatException nfe) {
-                    throw new SQLDataException("The value of the " + valueAttrName + " is '" + parameterValue + "', which cannot be converted into the necessary data type", nfe);
-                } catch (ParseException pe) {
-                    throw new SQLDataException("The value of the " + valueAttrName + " is '" + parameterValue + "', which cannot be converted to a timestamp", pe);
-                } catch (UnsupportedEncodingException uee) {
-                    throw new SQLDataException("The value of the " + valueAttrName + " is '" + parameterValue + "', which cannot be converted to UTF-8", uee);
-                }
-            }
-        }
-    }
-
-
     /**
      * Determines which relationship the given FlowFiles should go to, based on a transaction timing out or
      * transaction information not being present. If the FlowFiles should be processed and not transferred
@@ -810,183 +745,7 @@ public class PutSQL extends AbstractSessionFactoryProcessor {
         return false;  // not enough FlowFiles for this transaction. Return them all to queue.
     }
 
-    /**
-     * Determines how to map the given value to the appropriate JDBC data type and sets the parameter on the
-     * provided PreparedStatement
-     *
-     * @param stmt the PreparedStatement to set the parameter on
-     * @param attrName the name of the attribute that the parameter is coming from - for logging purposes
-     * @param parameterIndex the index of the SQL parameter to set
-     * @param parameterValue the value of the SQL parameter to set
-     * @param jdbcType the JDBC Type of the SQL parameter to set
-     * @throws SQLException if the PreparedStatement throws a SQLException when calling the appropriate setter
-     */
-    private void setParameter(final PreparedStatement stmt, final String attrName, final int parameterIndex, final String parameterValue, final int jdbcType,
-                              final String valueFormat)
-            throws SQLException, ParseException, UnsupportedEncodingException {
-        if (parameterValue == null) {
-            stmt.setNull(parameterIndex, jdbcType);
-        } else {
-            switch (jdbcType) {
-                case Types.BIT:
-                    stmt.setBoolean(parameterIndex, "1".equals(parameterValue) || "t".equalsIgnoreCase(parameterValue) || Boolean.parseBoolean(parameterValue));
-                     break;
-                case Types.BOOLEAN:
-                    stmt.setBoolean(parameterIndex, Boolean.parseBoolean(parameterValue));
-                    break;
-                case Types.TINYINT:
-                    stmt.setByte(parameterIndex, Byte.parseByte(parameterValue));
-                    break;
-                case Types.SMALLINT:
-                    stmt.setShort(parameterIndex, Short.parseShort(parameterValue));
-                    break;
-                case Types.INTEGER:
-                    stmt.setInt(parameterIndex, Integer.parseInt(parameterValue));
-                    break;
-                case Types.BIGINT:
-                    stmt.setLong(parameterIndex, Long.parseLong(parameterValue));
-                    break;
-                case Types.REAL:
-                    stmt.setFloat(parameterIndex, Float.parseFloat(parameterValue));
-                    break;
-                case Types.FLOAT:
-                case Types.DOUBLE:
-                    stmt.setDouble(parameterIndex, Double.parseDouble(parameterValue));
-                    break;
-                case Types.DECIMAL:
-                case Types.NUMERIC:
-                    stmt.setBigDecimal(parameterIndex, new BigDecimal(parameterValue));
-                    break;
-                case Types.DATE:
-                    Date date;
 
-                    if (valueFormat.equals("")) {
-                        if(LONG_PATTERN.matcher(parameterValue).matches()){
-                            date = new Date(Long.parseLong(parameterValue));
-                        }else {
-                            String dateFormatString = "yyyy-MM-dd";
-                            SimpleDateFormat dateFormat = new SimpleDateFormat(dateFormatString);
-                            java.util.Date parsedDate = dateFormat.parse(parameterValue);
-                            date = new Date(parsedDate.getTime());
-                        }
-                    } else {
-                        final DateTimeFormatter dtFormatter = getDateTimeFormatter(valueFormat);
-                        LocalDate parsedDate = LocalDate.parse(parameterValue, dtFormatter);
-                        date = new Date(Date.from(parsedDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant()).getTime());
-                    }
-
-                    stmt.setDate(parameterIndex, date);
-                    break;
-                case Types.TIME:
-                    Time time;
-
-                    if (valueFormat.equals("")) {
-                        if (LONG_PATTERN.matcher(parameterValue).matches()) {
-                            time = new Time(Long.parseLong(parameterValue));
-                        } else {
-                            String timeFormatString = "HH:mm:ss.SSS";
-                            SimpleDateFormat dateFormat = new SimpleDateFormat(timeFormatString);
-                            java.util.Date parsedDate = dateFormat.parse(parameterValue);
-                            time = new Time(parsedDate.getTime());
-                        }
-                    } else {
-                        final DateTimeFormatter dtFormatter = getDateTimeFormatter(valueFormat);
-                        LocalTime parsedTime = LocalTime.parse(parameterValue, dtFormatter);
-                        LocalDateTime localDateTime = parsedTime.atDate(LocalDate.ofEpochDay(0));
-                        Instant instant = localDateTime.atZone(ZoneId.systemDefault()).toInstant();
-                        time = new Time(instant.toEpochMilli());
-                    }
-
-                    stmt.setTime(parameterIndex, time);
-                    break;
-                case Types.TIMESTAMP:
-                    long lTimestamp=0L;
-
-                    // Backwards compatibility note: Format was unsupported for a timestamp field.
-                    if (valueFormat.equals("")) {
-                        if(LONG_PATTERN.matcher(parameterValue).matches()){
-                            lTimestamp = Long.parseLong(parameterValue);
-                        } else {
-                            final SimpleDateFormat dateFormat  = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-                            java.util.Date parsedDate = dateFormat.parse(parameterValue);
-                            lTimestamp = parsedDate.getTime();
-                        }
-                    } else {
-                        final DateTimeFormatter dtFormatter = getDateTimeFormatter(valueFormat);
-                        TemporalAccessor accessor = dtFormatter.parse(parameterValue);
-                        java.util.Date parsedDate = java.util.Date.from(Instant.from(accessor));
-                        lTimestamp = parsedDate.getTime();
-                    }
-
-                    stmt.setTimestamp(parameterIndex, new Timestamp(lTimestamp));
-
-                    break;
-                case Types.BINARY:
-                case Types.VARBINARY:
-                case Types.LONGVARBINARY:
-                    byte[] bValue;
-
-                    switch(valueFormat){
-                        case "":
-                        case "ascii":
-                            bValue = parameterValue.getBytes("ASCII");
-                            break;
-                        case "hex":
-                            bValue = DatatypeConverter.parseHexBinary(parameterValue);
-                            break;
-                        case "base64":
-                            bValue = DatatypeConverter.parseBase64Binary(parameterValue);
-                            break;
-                        default:
-                            throw new ParseException("Unable to parse binary data using the formatter `" + valueFormat + "`.",0);
-                    }
-
-                    stmt.setBinaryStream(parameterIndex, new ByteArrayInputStream(bValue), bValue.length);
-
-                    break;
-                case Types.CHAR:
-                case Types.VARCHAR:
-                case Types.LONGNVARCHAR:
-                case Types.LONGVARCHAR:
-                    stmt.setString(parameterIndex, parameterValue);
-                    break;
-                case Types.CLOB:
-                    try (final StringReader reader = new StringReader(parameterValue)) {
-                        stmt.setCharacterStream(parameterIndex, reader);
-                    }
-                    break;
-                case Types.NCLOB:
-                    try (final StringReader reader = new StringReader(parameterValue)) {
-                        stmt.setNCharacterStream(parameterIndex, reader);
-                    }
-                    break;
-                default:
-                    stmt.setObject(parameterIndex, parameterValue, jdbcType);
-                    break;
-            }
-        }
-    }
-
-    private DateTimeFormatter getDateTimeFormatter(String pattern) {
-        switch(pattern) {
-            case "BASIC_ISO_DATE": return DateTimeFormatter.BASIC_ISO_DATE;
-            case "ISO_LOCAL_DATE": return DateTimeFormatter.ISO_LOCAL_DATE;
-            case "ISO_OFFSET_DATE": return DateTimeFormatter.ISO_OFFSET_DATE;
-            case "ISO_DATE": return DateTimeFormatter.ISO_DATE;
-            case "ISO_LOCAL_TIME": return DateTimeFormatter.ISO_LOCAL_TIME;
-            case "ISO_OFFSET_TIME": return DateTimeFormatter.ISO_OFFSET_TIME;
-            case "ISO_TIME": return DateTimeFormatter.ISO_TIME;
-            case "ISO_LOCAL_DATE_TIME": return DateTimeFormatter.ISO_LOCAL_DATE_TIME;
-            case "ISO_OFFSET_DATE_TIME": return DateTimeFormatter.ISO_OFFSET_DATE_TIME;
-            case "ISO_ZONED_DATE_TIME": return DateTimeFormatter.ISO_ZONED_DATE_TIME;
-            case "ISO_DATE_TIME": return DateTimeFormatter.ISO_DATE_TIME;
-            case "ISO_ORDINAL_DATE": return DateTimeFormatter.ISO_ORDINAL_DATE;
-            case "ISO_WEEK_DATE": return DateTimeFormatter.ISO_WEEK_DATE;
-            case "ISO_INSTANT": return DateTimeFormatter.ISO_INSTANT;
-            case "RFC_1123_DATE_TIME": return DateTimeFormatter.RFC_1123_DATE_TIME;
-            default: return DateTimeFormatter.ofPattern(pattern);
-        }
-    }
 
     /**
      * A FlowFileFilter that is responsible for ensuring that the FlowFiles returned either belong
@@ -1038,7 +797,7 @@ public class PutSQL extends AbstractSessionFactoryProcessor {
             if (selectedId.equals(fragmentId)) {
                 // fragment id's match. Find out if we have all of the necessary fragments or not.
                 final int numFragments;
-                if (fragCount != null && NUMBER_PATTERN.matcher(fragCount).matches()) {
+                if (fragCount != null && JdbcCommon.NUMBER_PATTERN.matcher(fragCount).matches()) {
                     numFragments = Integer.parseInt(fragCount);
                 } else {
                     numFragments = Integer.MAX_VALUE;
