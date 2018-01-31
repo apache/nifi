7a9dc565f43284a8535de052a812a252c6950613
Ed <edward.berezitsky@gmail.com>
2018-01-31 16:20:35 -0500

    NIFI-4833 Add ScanHBase processor

--- nifi-nar-bundles/nifi-hbase-bundle/nifi-hbase-processors/src/test/java/org/apache/nifi/hbase/MockHBaseClientService.java
+++ nifi-nar-bundles/nifi-hbase-bundle/nifi-hbase-processors/src/test/java/org/apache/nifi/hbase/MockHBaseClientService.java
@@ -16,6 +16,11 @@
  */
 package org.apache.nifi.hbase;
 
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.nifi.controller.AbstractControllerService;
 import org.apache.nifi.hbase.put.PutColumn;
@@ -33,6 +38,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class MockHBaseClientService extends AbstractControllerService implements HBaseClientService {
 
@@ -118,6 +124,24 @@ public class MockHBaseClientService extends AbstractControllerService implements
 
         numScans++;
     }
+
+	@Override
+	public void scan(String tableName, String startRow, String endRow, String filterExpression, Long timerangeMin,
+			Long timerangeMax, Integer limitRows, Boolean isReversed, Collection<Column> columns, ResultHandler handler)
+			throws IOException {
+        if (throwException) {
+            throw new IOException("exception");
+        }
+
+        // pass all the staged data to the handler
+        for (final Map.Entry<String,ResultCell[]> entry : results.entrySet()) {
+            handler.handle(entry.getKey().getBytes(StandardCharsets.UTF_8), entry.getValue());
+        }
+
+		// delegate to the handler
+		
+		numScans++;
+	}
 
     public void addResult(final String rowKey, final Map<String, String> cells, final long timestamp) {
         final byte[] rowArray = rowKey.getBytes(StandardCharsets.UTF_8);
