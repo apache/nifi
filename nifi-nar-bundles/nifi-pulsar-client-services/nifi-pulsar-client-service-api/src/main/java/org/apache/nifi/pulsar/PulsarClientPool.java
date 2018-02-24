e4f8550159132d85d0e19d61f24e087e51742dee
David Kjerrumgaard <david@streaml.io>
2018-02-23 17:40:22 -0800

    Added Pulsar processors and Controller Service

---
+++ nifi-nar-bundles/nifi-pulsar-client-services/nifi-pulsar-client-service-api/src/main/java/org/apache/nifi/pulsar/PulsarClientPool.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.pulsar;
+
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.controller.ControllerService;
+import org.apache.nifi.pulsar.pool.ResourcePool;
+
+
+@Tags({"Pulsar"})
+@CapabilityDescription("Provides the ability to create Pulsar Producer / Consumer instances on demand, based on the configuration."
+		+ "properties defined")
+public interface PulsarClientPool extends ControllerService {
+
+	/*
+	public PulsarProducer getProducer(Properties properties) throws PulsarClientException;
+	
+	public void release(PulsarProducer producer);
+	
+	public void invalidate(PulsarProducer producer);
+	
+	public PulsarConsumer getConsumer(Properties properties) throws PulsarClientException;
+	
+	public void release(PulsarConsumer consumer);
+	
+	public void invalidate(PulsarConsumer consumer);
+*/
+	
+	public ResourcePool<PulsarProducer> getProducerPool();
+	
+	public ResourcePool<PulsarConsumer> getConsumerPool();
+}
