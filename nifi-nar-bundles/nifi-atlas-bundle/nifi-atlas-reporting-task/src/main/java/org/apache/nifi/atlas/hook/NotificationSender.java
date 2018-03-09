46f0021fc2a9cb170eb84009f42ca4f2714e5a9b
Koji Kawamura <ijokarumawak@apache.org>
2018-03-09 09:27:20 +0900

    NIFI-4971: ReportLineageToAtlas complete path can miss one-time lineages
    - Separated Hook message de-duplication logic from NiFiAtlasHook to
    NotificationSender
    - NiFiAtlasHook used to send individual CREATE_ENTITY messages for each entities,
    this commit changed it to bundle all new entities into a single
    CREATE_ENTITY to preserve entity creation order, so that new DataSet
    entities can be referred from new nifi_flow_path entities
    - Added more unit tests

--- nifi-nar-bundles/nifi-atlas-bundle/nifi-atlas-reporting-task/src/main/java/org/apache/nifi/atlas/hook/NotificationSender.java
+++ nifi-nar-bundles/nifi-atlas-bundle/nifi-atlas-reporting-task/src/main/java/org/apache/nifi/atlas/hook/NotificationSender.java
@@ -14,33 +14,42 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.nifi.atlas;
+package org.apache.nifi.atlas.hook;
 
 import com.sun.jersey.api.client.ClientResponse;
 import org.apache.atlas.AtlasServiceException;
-import org.apache.atlas.hook.AtlasHook;
 import org.apache.atlas.model.instance.AtlasEntity;
 import org.apache.atlas.model.instance.AtlasObjectId;
-import org.apache.atlas.notification.hook.HookNotification.EntityPartialUpdateRequest;
-import org.apache.atlas.notification.hook.HookNotification.HookNotificationMessage;
+import org.apache.atlas.notification.hook.HookNotification;
 import org.apache.atlas.typesystem.Referenceable;
 import org.apache.atlas.typesystem.persistence.Id;
-import org.apache.nifi.atlas.provenance.lineage.LineageContext;
+import org.apache.nifi.atlas.AtlasUtils;
+import org.apache.nifi.atlas.NiFiAtlasClient;
 import org.apache.nifi.util.Tuple;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toMap;
+import static org.apache.atlas.notification.hook.HookNotification.HookNotificationType.ENTITY_CREATE;
 import static org.apache.atlas.notification.hook.HookNotification.HookNotificationType.ENTITY_PARTIAL_UPDATE;
 import static org.apache.nifi.atlas.AtlasUtils.toTypedQualifiedName;
+import static org.apache.nifi.atlas.hook.NiFiAtlasHook.NIFI_USER;
 import static org.apache.nifi.atlas.NiFiTypes.ATTR_GUID;
 import static org.apache.nifi.atlas.NiFiTypes.ATTR_INPUTS;
 import static org.apache.nifi.atlas.NiFiTypes.ATTR_OUTPUTS;
@@ -49,16 +58,12 @@ import static org.apache.nifi.atlas.NiFiTypes.ATTR_TYPENAME;
 import static org.apache.nifi.atlas.NiFiTypes.TYPE_NIFI_FLOW_PATH;
 
 /**
- * This class is not thread-safe as it holds uncommitted notification messages within instance.
- * {@link #addMessage(HookNotificationMessage)} and {@link #commitMessages()} should be used serially from a single thread.
+ * This class implements Atlas hook notification message deduplication mechanism.
+ * Separated from {@link NiFiAtlasHook} for better testability.
  */
-public class NiFiAtlasHook extends AtlasHook implements LineageContext {
+class NotificationSender {
 
-    public static final String NIFI_USER = "nifi";
-
-    private static final Logger logger = LoggerFactory.getLogger(NiFiAtlasHook.class);
-    private static final String CONF_PREFIX = "atlas.hook.nifi.";
-    private static final String HOOK_NUM_RETRIES = CONF_PREFIX + "numRetries";
+    private static final Logger logger = LoggerFactory.getLogger(NotificationSender.class);
 
     private NiFiAtlasClient atlasClient;
 
@@ -71,7 +76,6 @@ public class NiFiAtlasHook extends AtlasHook implements LineageContext {
      */
     private final Map<String, Referenceable> typedQualifiedNameToRef;
 
-
     private static <K, V> Map<K, V> createCache(final int maxSize) {
         return new LinkedHashMap<K, V>(maxSize, 0.75f, true) {
             @Override
@@ -81,7 +85,7 @@ public class NiFiAtlasHook extends AtlasHook implements LineageContext {
         };
     }
 
-    public NiFiAtlasHook() {
+    NotificationSender() {
         final int qualifiedNameCacheSize = 10_000;
         this.guidToQualifiedName = createCache(qualifiedNameCacheSize);
 
@@ -89,70 +93,160 @@ public class NiFiAtlasHook extends AtlasHook implements LineageContext {
         this.typedQualifiedNameToRef = createCache(dataSetRefCacheSize);
     }
 
-    public void setAtlasClient(NiFiAtlasClient atlasClient) {
-        this.atlasClient = atlasClient;
-    }
-
-    @Override
-    protected String getNumberOfRetriesPropertyKey() {
-        return HOOK_NUM_RETRIES;
-    }
-
-
-    private final List<HookNotificationMessage> messages = new ArrayList<>();
-
-    @Override
-    public void addMessage(HookNotificationMessage message) {
-        messages.add(message);
-    }
-
     private class Metrics {
         final long startedAt = System.currentTimeMillis();
+
+        /**
+         * The total number of messages passed to commitMessages.
+         */
         int totalMessages;
+        /**
+         * The number of CreateEntityRequest messages before de-duplication.
+         */
+        int createMessages;
+        /**
+         * The number of unique CreateEntityRequest messages for 'nifi_flow_path'.
+         */
+        int uniqueFlowPathCreates;
+        /**
+         * The number of unique CreateEntityRequest messages except 'nifi_flow_path'.
+         */
+        int uniqueOtherCreates;
         int partialNiFiFlowPathUpdates;
-        int dedupedPartialNiFiFlowPathUpdates;
+        int uniquePartialNiFiFlowPathUpdates;
         int otherMessages;
         int flowPathSearched;
         int dataSetSearched;
         int dataSetCacheHit;
-        private void log(String message) {
-            logger.debug(String.format("%s, %d ms passed, totalMessages=%d," +
-                    " partialNiFiFlowPathUpdates=%d, dedupedPartialNiFiFlowPathUpdates=%d, otherMessage=%d," +
-                    " flowPathSearched=%d, dataSetSearched=%d, dataSetCacheHit=%s," +
-                    " guidToQualifiedName.size=%d, typedQualifiedNameToRef.size=%d",
+
+        private String toLogString(String message) {
+            return String.format("%s, %d ms passed, totalMessages=%d," +
+                            " createMessages=%d, uniqueFlowPathCreates=%d, uniqueOtherCreates=%d," +
+                            " partialNiFiFlowPathUpdates=%d, uniquePartialNiFiFlowPathUpdates=%d, otherMessage=%d," +
+                            " flowPathSearched=%d, dataSetSearched=%d, dataSetCacheHit=%s," +
+                            " guidToQualifiedName.size=%d, typedQualifiedNameToRef.size=%d",
                     message, System.currentTimeMillis() - startedAt, totalMessages,
-                    partialNiFiFlowPathUpdates, dedupedPartialNiFiFlowPathUpdates, otherMessages,
+                    createMessages, uniqueFlowPathCreates, uniqueOtherCreates,
+                    partialNiFiFlowPathUpdates, uniquePartialNiFiFlowPathUpdates, otherMessages,
                     flowPathSearched, dataSetSearched, dataSetCacheHit,
-                    guidToQualifiedName.size(), typedQualifiedNameToRef.size()));
+                    guidToQualifiedName.size(), typedQualifiedNameToRef.size());
         }
     }
 
-    public void commitMessages() {
-        final Map<Boolean, List<HookNotificationMessage>> partialNiFiFlowPathUpdateAndOthers
-                = messages.stream().collect(Collectors.groupingBy(msg
-                    -> ENTITY_PARTIAL_UPDATE.equals(msg.getType())
-                        && TYPE_NIFI_FLOW_PATH.equals(((EntityPartialUpdateRequest)msg).getTypeName())
-                        && ATTR_QUALIFIED_NAME.equals(((EntityPartialUpdateRequest)msg).getAttribute())
-        ));
+    void setAtlasClient(NiFiAtlasClient atlasClient) {
+        this.atlasClient = atlasClient;
+    }
 
+    private Predicate<Referenceable> distinctReferenceable() {
+        final Set<String> keys = new HashSet<>();
+        return r -> {
+            final String key = AtlasUtils.toTypedQualifiedName(r.getTypeName(), (String) r.get(ATTR_QUALIFIED_NAME));
+            return keys.add(key);
+        };
+    }
 
-        final List<HookNotificationMessage> otherMessages = partialNiFiFlowPathUpdateAndOthers.computeIfAbsent(false, k -> Collections.emptyList());
-        final List<HookNotificationMessage> partialNiFiFlowPathUpdates = partialNiFiFlowPathUpdateAndOthers.computeIfAbsent(true, k -> Collections.emptyList());
-        logger.info("Commit messages: {} partialNiFiFlowPathUpdate and {} other messages.", partialNiFiFlowPathUpdates.size(), otherMessages.size());
+    private <K, V> List<V> safeGet(Map<K, List<V>> map, K key) {
+        return map.computeIfAbsent(key, k -> Collections.emptyList());
+    }
 
+    @SuppressWarnings("unchecked")
+    private void mergeRefs(Referenceable r1, Referenceable r2) {
+        r1.set(ATTR_INPUTS, mergeRefs((Collection<Referenceable>) r1.get(ATTR_INPUTS), (Collection<Referenceable>) r2.get(ATTR_INPUTS)));
+        r1.set(ATTR_OUTPUTS, mergeRefs((Collection<Referenceable>) r1.get(ATTR_OUTPUTS), (Collection<Referenceable>) r2.get(ATTR_OUTPUTS)));
+    }
+
+    private Collection<Referenceable> mergeRefs(Collection<Referenceable> r1, Collection<Referenceable> r2) {
+        final boolean isR1Empty = r1 == null || r1.isEmpty();
+        final boolean isR2Empty = r2 == null || r2.isEmpty();
+
+        if (isR1Empty) {
+            // r2 may or may not have entities, don't have to merge r1.
+            return r2;
+        } else if (isR2Empty) {
+            // r1 has some entities, don't have to merge r2.
+            return r1;
+        }
+
+        // If both have entities, then need to be merged.
+        return Stream.concat(r1.stream(), r2.stream()).filter(distinctReferenceable()).collect(Collectors.toList());
+    }
+
+    /**
+     * <p>Send hook notification messages.
+     * In order to notify relationships between 'nifi_flow_path' and its inputs/outputs, this method sends messages in following order:</p>
+     * <ol>
+     *     <li>As a a single {@link org.apache.atlas.notification.hook.HookNotification.EntityCreateRequest} message:
+     *         <ul>
+     *             <li>New entities except 'nifi_flow_path', including DataSets such as 'nifi_queue', 'kafka_topic' or 'hive_table' ... etc,
+     *             so that 'nifi_flow_path' can refer</li>
+     *             <li>New 'nifi_flow_path' entities, entities order is guaranteed in a single message</li>
+     *         </ul>
+     *     </li>
+     *     <li>Update 'nifi_flow_path' messages, before notifying update messages, this method fetches existing 'nifi_flow_path' entity
+     *     to merge new inputs/outputs element with existing ones, so that existing ones will not be removed.</li>
+     *     <li>Other messages except</li>
+     * </ol>
+     * <p>Messages having the same type and qualified name will be de-duplicated before being sent.</p>
+     * @param messages list of messages to be sent
+     * @param notifier responsible for sending notification messages, its accept method can be called multiple times
+     */
+    void send(final List<HookNotification.HookNotificationMessage> messages, final Consumer<List<HookNotification.HookNotificationMessage>> notifier) {
         final Metrics metrics = new Metrics();
-        metrics.totalMessages = messages.size();
-        metrics.partialNiFiFlowPathUpdates = partialNiFiFlowPathUpdates.size();
-        metrics.otherMessages = otherMessages.size();
-
         try {
-            // Notify other messages first.
-            notifyEntities(otherMessages);
+            metrics.totalMessages = messages.size();
 
-            // De-duplicate messages.
-            final List<HookNotificationMessage> deduplicatedMessages = partialNiFiFlowPathUpdates.stream().map(msg -> (EntityPartialUpdateRequest) msg)
+            final Map<Boolean, List<HookNotification.HookNotificationMessage>> createAndOthers = messages.stream().collect(groupingBy(msg -> ENTITY_CREATE.equals(msg.getType())));
+
+            final List<HookNotification.HookNotificationMessage> creates = safeGet(createAndOthers, true);
+            metrics.createMessages = creates.size();
+
+            final Map<Boolean, List<Referenceable>> newFlowPathsAndOtherEntities = creates.stream()
+                    .flatMap(msg -> ((HookNotification.EntityCreateRequest) msg).getEntities().stream())
+                    .collect(groupingBy(ref -> TYPE_NIFI_FLOW_PATH.equals(ref.typeName)));
+
+            // Deduplicate same entity creation messages.
+            final List<Referenceable> newEntitiesExceptFlowPaths = safeGet(newFlowPathsAndOtherEntities, false)
+                    .stream().filter(distinctReferenceable()).collect(Collectors.toList());
+
+            // Deduplicate same flow paths and also merge inputs and outputs
+            final Collection<Referenceable> newFlowPaths = safeGet(newFlowPathsAndOtherEntities, true).stream()
+                    .collect(toMap(ref -> ref.get(ATTR_QUALIFIED_NAME), ref -> ref, (r1, r2) -> {
+                        // Merge inputs and outputs.
+                        mergeRefs(r1, r2);
+                        return r1;
+                    })).values();
+            metrics.uniqueFlowPathCreates = newFlowPaths.size();
+            metrics.uniqueOtherCreates = newEntitiesExceptFlowPaths.size();
+
+
+            // 1-1. Notify new entities except 'nifi_flow_path'
+            // 1-2. Notify new 'nifi_flow_path'
+            List<Referenceable> newEntities = new ArrayList<>();
+            newEntities.addAll(newEntitiesExceptFlowPaths);
+            newEntities.addAll(newFlowPaths);
+            if (!newEntities.isEmpty()) {
+                notifier.accept(Collections.singletonList(new HookNotification.EntityCreateRequest(NIFI_USER, newEntities)));
+            }
+
+            final Map<Boolean, List<HookNotification.HookNotificationMessage>> partialNiFiFlowPathUpdateAndOthers
+                    = safeGet(createAndOthers, false).stream().collect(groupingBy(msg
+                    -> ENTITY_PARTIAL_UPDATE.equals(msg.getType())
+                    && TYPE_NIFI_FLOW_PATH.equals(((HookNotification.EntityPartialUpdateRequest)msg).getTypeName())
+                    && ATTR_QUALIFIED_NAME.equals(((HookNotification.EntityPartialUpdateRequest)msg).getAttribute())
+            ));
+
+
+            // These updates are made against existing flow path entities.
+            final List<HookNotification.HookNotificationMessage> partialNiFiFlowPathUpdates = safeGet(partialNiFiFlowPathUpdateAndOthers, true);
+            final List<HookNotification.HookNotificationMessage> otherMessages = safeGet(partialNiFiFlowPathUpdateAndOthers, false);
+            metrics.partialNiFiFlowPathUpdates = partialNiFiFlowPathUpdates.size();
+            metrics.otherMessages = otherMessages.size();
+
+
+            // 2. Notify de-duplicated 'nifi_flow_path' updates
+            final List<HookNotification.HookNotificationMessage> deduplicatedMessages = partialNiFiFlowPathUpdates.stream().map(msg -> (HookNotification.EntityPartialUpdateRequest) msg)
                     // Group by nifi_flow_path qualifiedName value.
-                    .collect(Collectors.groupingBy(EntityPartialUpdateRequest::getAttributeValue)).entrySet().stream()
+                    .collect(groupingBy(HookNotification.EntityPartialUpdateRequest::getAttributeValue)).entrySet().stream()
                     .map(entry -> {
                         final String flowPathQualifiedName = entry.getKey();
                         final Map<String, Referenceable> distinctInputs;
@@ -178,7 +272,7 @@ public class NiFiAtlasHook extends AtlasHook implements LineageContext {
                         }
 
                         // Merge all inputs and outputs for this nifi_flow_path.
-                        for (EntityPartialUpdateRequest msg : entry.getValue()) {
+                        for (HookNotification.EntityPartialUpdateRequest msg : entry.getValue()) {
                             fromReferenceable(msg.getEntity().get(ATTR_INPUTS), metrics)
                                     .entrySet().stream().filter(ref -> !distinctInputs.containsKey(ref.getKey()))
                                     .forEach(ref -> distinctInputs.put(ref.getKey(), ref.getValue()));
@@ -194,18 +288,22 @@ public class NiFiAtlasHook extends AtlasHook implements LineageContext {
                         // org.json4s.package$MappingException: Can't find ScalaSig for class org.apache.atlas.typesystem.Referenceable
                         flowPathRef.set(ATTR_INPUTS, new ArrayList<>(distinctInputs.values()));
                         flowPathRef.set(ATTR_OUTPUTS, new ArrayList<>(distinctOutputs.values()));
-                        return new EntityPartialUpdateRequest(NIFI_USER, TYPE_NIFI_FLOW_PATH,
+                        return new HookNotification.EntityPartialUpdateRequest(NIFI_USER, TYPE_NIFI_FLOW_PATH,
                                 ATTR_QUALIFIED_NAME, flowPathQualifiedName, flowPathRef);
                     })
                     .filter(Objects::nonNull)
                     .collect(Collectors.toList());
 
-            metrics.dedupedPartialNiFiFlowPathUpdates = deduplicatedMessages.size();
-            notifyEntities(deduplicatedMessages);
+            metrics.uniquePartialNiFiFlowPathUpdates = deduplicatedMessages.size();
+            notifier.accept(deduplicatedMessages);
+
+            // 3. Notify other messages
+            notifier.accept(otherMessages);
+
         } finally {
-            metrics.log("Committed");
-            messages.clear();
+            logger.info(metrics.toLogString("Finished"));
         }
+
     }
 
     /**
@@ -223,7 +321,7 @@ public class NiFiAtlasHook extends AtlasHook implements LineageContext {
             return new HashMap<>();
         }
 
-        final List<Map<String, Object>> refs = (List<Map<String, Object>>) _refs;
+        final Collection<Map<String, Object>> refs = (Collection<Map<String, Object>>) _refs;
         return refs.stream().map(ref -> {
             // Existing reference should has a GUID.
             final String typeName = (String) ref.get(ATTR_TYPENAME);
@@ -256,8 +354,8 @@ public class NiFiAtlasHook extends AtlasHook implements LineageContext {
             return new Tuple<>(refQualifiedName, typedQualifiedNameToRef.get(toTypedQualifiedName(typeName, refQualifiedName)));
         }).filter(Objects::nonNull).filter(tuple -> tuple.getValue() != null)
                 // If duplication happens, use new value.
-                .collect(Collectors.toMap(Tuple::getKey, Tuple::getValue, (oldValue, newValue) -> {
-                    logger.warn("Duplicated qualified name was found, use the new one. oldValue={}, newValue={}", new Object[]{oldValue, newValue});
+                .collect(toMap(Tuple::getKey, Tuple::getValue, (oldValue, newValue) -> {
+                    logger.debug("Duplicated qualified name was found, use the new one. oldValue={}, newValue={}", new Object[]{oldValue, newValue});
                     return newValue;
                 }));
     }
@@ -268,7 +366,7 @@ public class NiFiAtlasHook extends AtlasHook implements LineageContext {
             return Collections.emptyMap();
         }
 
-        final List<Referenceable> refs = (List<Referenceable>) _refs;
+        final Collection<Referenceable> refs = (Collection<Referenceable>) _refs;
         return refs.stream().map(ref -> {
             // This ref is created within this reporting cycle, and it may not have GUID assigned yet, if it is a brand new reference.
             // If cache has the Reference, then use it because instances in the cache are guaranteed to have GUID assigned.
@@ -288,13 +386,7 @@ public class NiFiAtlasHook extends AtlasHook implements LineageContext {
             });
 
             return new Tuple<>(refQualifiedName, refFromCacheIfAvailable);
-        }).filter(Objects::nonNull).filter(tuple -> tuple.getValue() != null)
-                .collect(Collectors.toMap(Tuple::getKey, Tuple::getValue));
-    }
-
-    public void close() {
-        if (notificationInterface != null) {
-            notificationInterface.close();
-        }
+        }).filter(tuple -> tuple.getValue() != null)
+                .collect(toMap(Tuple::getKey, Tuple::getValue));
     }
 }
