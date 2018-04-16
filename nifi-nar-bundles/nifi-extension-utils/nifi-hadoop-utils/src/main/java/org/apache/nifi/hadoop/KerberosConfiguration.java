e021782164c8168e6561783f0b5367a6696fbed2
Peter Toth <ptoth@hortonworks.com>
2018-04-16 08:53:22 +0200

    NIFI-5041: fixes Kerberos configuration
    Change-Id: I868fdf3ea7cfd28cf415164e420f23bf3f6eefeb

--- nifi-nar-bundles/nifi-extension-utils/nifi-hadoop-utils/src/main/java/org/apache/nifi/hadoop/KerberosConfiguration.java
+++ nifi-nar-bundles/nifi-extension-utils/nifi-hadoop-utils/src/main/java/org/apache/nifi/hadoop/KerberosConfiguration.java
@@ -23,17 +23,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Kerberos configuration class from {@link org.apache.hadoop.security.authentication.client.KerberosAuthenticator.KerberosConfiguration}.
+ * Modified Kerberos configuration class from {@link org.apache.hadoop.security.authentication.client.KerberosAuthenticator.KerberosConfiguration}
+ * that requires authentication from a keytab.
  */
 public class KerberosConfiguration extends javax.security.auth.login.Configuration {
 
-    private static final boolean IBM_JAVA = System.getProperty("java.vendor").contains("IBM");
-    private static final boolean WINDOWS = System.getProperty("os.name").startsWith("Windows");
-    private static final boolean IS_64_BIT = System.getProperty("os.arch").contains("64");
-    private static final boolean AIX = System.getProperty("os.name").equals("AIX");
     private static final Map<String, String> USER_KERBEROS_OPTIONS = new HashMap<>();
-    private static final String OS_LOGIN_MODULE_NAME;
-    private static final AppConfigurationEntry OS_SPECIFIC_LOGIN;
     private static final AppConfigurationEntry USER_KERBEROS_LOGIN;
     private static final AppConfigurationEntry[] USER_KERBEROS_CONF;
 
@@ -42,35 +37,16 @@ public class KerberosConfiguration extends javax.security.auth.login.Configurati
         USER_KERBEROS_OPTIONS.put("keyTab", keytab);
     }
 
-    private static String getOSLoginModuleName() {
-        if (IBM_JAVA) {
-            if (WINDOWS) {
-                return IS_64_BIT ? "com.ibm.security.auth.module.Win64LoginModule" : "com.ibm.security.auth.module.NTLoginModule";
-            } else if (AIX) {
-                return IS_64_BIT ? "com.ibm.security.auth.module.AIX64LoginModule" : "com.ibm.security.auth.module.AIXLoginModule";
-            } else {
-                return "com.ibm.security.auth.module.LinuxLoginModule";
-            }
-        } else {
-            return WINDOWS ? "com.sun.security.auth.module.NTLoginModule" : "com.sun.security.auth.module.UnixLoginModule";
-        }
-    }
-
     public AppConfigurationEntry[] getAppConfigurationEntry(String appName) {
         return USER_KERBEROS_CONF;
     }
 
     static {
         USER_KERBEROS_OPTIONS.put("doNotPrompt", "true");
-        USER_KERBEROS_OPTIONS.put("useTicketCache", "true");
-        USER_KERBEROS_OPTIONS.put("renewTGT", "true");
         USER_KERBEROS_OPTIONS.put("useKeyTab", "true");
-        USER_KERBEROS_OPTIONS.put("storeKey", "true");
         USER_KERBEROS_OPTIONS.put("refreshKrb5Config", "true");
-        OS_LOGIN_MODULE_NAME = getOSLoginModuleName();
-        OS_SPECIFIC_LOGIN = new AppConfigurationEntry(OS_LOGIN_MODULE_NAME, AppConfigurationEntry.LoginModuleControlFlag.REQUIRED, new HashMap<>());
-        USER_KERBEROS_LOGIN = new AppConfigurationEntry(KerberosUtil.getKrb5LoginModuleName(), AppConfigurationEntry.LoginModuleControlFlag.OPTIONAL, USER_KERBEROS_OPTIONS);
-        USER_KERBEROS_CONF = new AppConfigurationEntry[]{OS_SPECIFIC_LOGIN, USER_KERBEROS_LOGIN};
+        USER_KERBEROS_LOGIN = new AppConfigurationEntry(KerberosUtil.getKrb5LoginModuleName(), AppConfigurationEntry.LoginModuleControlFlag.REQUIRED, USER_KERBEROS_OPTIONS);
+        USER_KERBEROS_CONF = new AppConfigurationEntry[]{USER_KERBEROS_LOGIN};
     }
 
 }
