db259628c7ea359c0783a3cb800f5d58da9e09ed
zenfenan <sivaprasanna246@gmail.com>
2018-04-24 21:05:05 +0530

    NIFI-5073: JMSConnectionFactoryProvider now resolves EL Expression from VariableRegistry - CLIENT_LIB_PATH is updated to include 'dynamicallyModifiesClasspath(true)'

--- nifi-nar-bundles/nifi-jms-bundle/nifi-jms-processors/src/main/java/org/apache/nifi/jms/cf/Utils.java
+++ nifi-nar-bundles/nifi-jms-bundle/nifi-jms-processors/src/main/java/org/apache/nifi/jms/cf/Utils.java
@@ -16,10 +16,7 @@
  */
 package org.apache.nifi.jms.cf;
 
-import java.io.File;
 import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -101,40 +98,4 @@ public final class Utils {
         }
     }
 
-    /**
-     * Adds content of the directory specified with 'path' to the classpath. It
-     * does so by creating a new instance of the {@link URLClassLoader} using
-     * {@link URL}s created from listing the contents of the directory denoted
-     * by 'path' and setting it as thread context class loader.
-     */
-    static void addResourcesToClasspath(String path) {
-        if (logger.isDebugEnabled()) {
-            logger.debug("Adding additional resources from '" + path + "' to the classpath.");
-        }
-        if (path == null) {
-            throw new IllegalArgumentException("'path' must not be null");
-        }
-        File libraryDir = new File(path);
-        if (libraryDir.exists() && libraryDir.isDirectory()) {
-            String[] cpResourceNames = libraryDir.list();
-            URL[] urls = new URL[cpResourceNames.length];
-            try {
-                for (int i = 0; i < urls.length; i++) {
-                    urls[i] = new File(libraryDir, cpResourceNames[i]).toURI().toURL();
-                    if (logger.isDebugEnabled()) {
-                        logger.debug("Identifying additional resource to the classpath: " + urls[i]);
-                    }
-                }
-            } catch (Exception e) {
-                throw new IllegalStateException(
-                        "Failed to parse user libraries from '" + libraryDir.getAbsolutePath() + "'", e);
-            }
-
-            URLClassLoader cl = new URLClassLoader(urls, Utils.class.getClassLoader());
-            Thread.currentThread().setContextClassLoader(cl);
-        } else {
-            throw new IllegalArgumentException("Path '" + libraryDir.getAbsolutePath()
-                    + "' is not valid because it doesn't exist or does not point to a directory.");
-        }
-    }
 }
