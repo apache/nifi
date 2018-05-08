61fe493786425826e46cab737be9def11300dd19
Otto Fowler <ottobackwards@gmail.com>
2018-05-08 15:53:20 -0400

    NIFI-5170 Upgrad Grok to version 0.1.9
    This closes #2691
    
    Signed-off-by: Mike Thomsen <mikerthomsen@gmail.com>

--- nifi-nar-bundles/nifi-standard-services/nifi-record-serialization-services-bundle/nifi-record-serialization-services/src/main/java/org/apache/nifi/grok/GrokExpressionValidator.java
+++ nifi-nar-bundles/nifi-standard-services/nifi-record-serialization-services-bundle/nifi-record-serialization-services/src/main/java/org/apache/nifi/grok/GrokExpressionValidator.java
@@ -17,18 +17,45 @@
 
 package org.apache.nifi.grok;
 
+import io.krakens.grok.api.GrokCompiler;
 import org.apache.nifi.components.ValidationContext;
 import org.apache.nifi.components.ValidationResult;
 import org.apache.nifi.components.Validator;
 
-import io.thekraken.grok.api.Grok;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class GrokExpressionValidator implements Validator {
+    private GrokCompiler grokCompiler;
+    private String patternFileName;
+
+    public GrokExpressionValidator(String patternFileName, GrokCompiler compiler) {
+        this.patternFileName = patternFileName;
+        this.grokCompiler = compiler;
+    }
+
+    public GrokExpressionValidator() {
+        this.grokCompiler = GrokCompiler.newInstance();
+    }
 
     @Override
     public ValidationResult validate(final String subject, final String input, final ValidationContext context) {
         try {
-            new Grok().compile(input);
+            try (final InputStream in = getClass().getResourceAsStream(GrokReader.DEFAULT_PATTERN_NAME);
+                 final Reader reader = new InputStreamReader(in)) {
+                grokCompiler.register(in);
+            }
+
+            if (patternFileName != null) {
+                try (final InputStream in = new FileInputStream(new File(patternFileName));
+                     final Reader reader = new InputStreamReader(in)) {
+                    grokCompiler.register(reader);
+                }
+            }
+            grokCompiler.compile(input);
         } catch (final Exception e) {
             return new ValidationResult.Builder()
                 .input(input)
