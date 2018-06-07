7b28b914cd6ca4c5f0d566a8726ca02ebd38f3c2
Peter Toth <ptoth@hortonworks.com>
2018-06-07 12:13:21 +0200

    NIFI-5278: fixes JSON escaping of code
    Change-Id: I2cb0e6c658d4a0f2aad9c4aab9201a3334ee54df
    
    NIFI-5278: adds Apache Commons Text to NOTICE
    
    Change-Id: I8185239b0a888c16159b18f13d6682ba350cc766
    
    NIFI-5278: adds tests
    
    Change-Id: I9286ac71bc7399e5bdc1e6602609b5e8829db27e
    
    NIFI-5278: fixes review findings
    
    Change-Id: I292c93dae877cf1cd146f3897b7e132b6afac801
    Signed-off-by: Matthew Burgess <mattyb149@apache.org>
    
    This closes #2768

--- nifi-nar-bundles/nifi-spark-bundle/nifi-livy-processors/src/test/java/org/apache/nifi/processors/livy/ExecuteSparkInteractiveTestBase.java
+++ nifi-nar-bundles/nifi-spark-bundle/nifi-livy-processors/src/test/java/org/apache/nifi/processors/livy/ExecuteSparkInteractiveTestBase.java
@@ -16,37 +16,43 @@
  */
 package org.apache.nifi.processors.livy;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
+import org.apache.nifi.web.util.TestServer;
+import org.apache.nifi.util.MockFlowFile;
+import org.apache.nifi.util.TestRunner;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
-public class ExecuteSparkInteractiveTestBase {
+class ExecuteSparkInteractiveTestBase {
 
     public static class LivyAPIHandler extends AbstractHandler {
 
         int session1Requests = 0;
 
         @Override
-        public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException {
             baseRequest.setHandled(true);
 
-            response.setStatus(200);
+            int responseStatus = 404;
+            String responseContentType = "text/plain";
+            String responseBody = "Not found";
 
             if ("GET".equalsIgnoreCase(request.getMethod())) {
-
-                String responseBody = "{}";
-                response.setContentType("application/json");
-
+                responseStatus = 200;
+                responseBody = "{}";
+                responseContentType = "application/json";
                 if ("/sessions".equalsIgnoreCase(target)) {
                     responseBody = "{\"sessions\": [{\"id\": 1, \"kind\": \"spark\", \"state\": \"idle\"}]}";
                 } else if (target.startsWith("/sessions/") && !target.contains("statement")) {
                     responseBody = "{\"id\": 1, \"kind\": \"spark\", \"state\": \"idle\"}";
-
                 } else if ("/sessions/1/statements/7".equalsIgnoreCase(target)) {
                     switch (session1Requests) {
                         case 0:
@@ -64,33 +70,55 @@ public class ExecuteSparkInteractiveTestBase {
                     }
                     session1Requests++;
                 }
-
-                response.setContentLength(responseBody.length());
-
-                try (PrintWriter writer = response.getWriter()) {
-                    writer.print(responseBody);
-                    writer.flush();
-                }
-
             } else if ("POST".equalsIgnoreCase(request.getMethod())) {
+                String requestBody = IOUtils.toString(request.getReader());
+                try {
+                    // validate JSON payload
+                    new ObjectMapper().readTree(requestBody);
 
-                String responseBody = "{}";
-                response.setContentType("application/json");
-
-                if ("/sessions".equalsIgnoreCase(target)) {
-                    responseBody = "{\"id\": 1, \"kind\": \"spark\", \"state\": \"idle\"}";
-                } else if ("/sessions/1/statements".equalsIgnoreCase(target)) {
-                    responseBody = "{\"id\": 7}";
+                    responseStatus = 200;
+                    responseBody = "{}";
+                    responseContentType = "application/json";
+                    if ("/sessions".equalsIgnoreCase(target)) {
+                        responseBody = "{\"id\": 1, \"kind\": \"spark\", \"state\": \"idle\"}";
+                    } else if ("/sessions/1/statements".equalsIgnoreCase(target)) {
+                        responseBody = "{\"id\": 7}";
+                    }
+                } catch (JsonProcessingException e) {
+                    responseStatus = 400;
+                    responseContentType = "text/plain";
+                    responseBody = "Bad request";
                 }
-
-                response.setContentLength(responseBody.length());
-
-                try (PrintWriter writer = response.getWriter()) {
-                    writer.print(responseBody);
-                    writer.flush();
-                }
-
             }
+
+            response.setStatus(responseStatus);
+            response.setContentType(responseContentType);
+            response.setContentLength(responseBody.length());
+
+            try (PrintWriter writer = response.getWriter()) {
+                writer.print(responseBody);
+                writer.flush();
+            }
+
         }
     }
+
+    TestRunner runner;
+
+    void testCode(TestServer server, String code) throws Exception {
+        server.addHandler(new LivyAPIHandler());
+
+        runner.enqueue(code);
+        runner.run();
+        List<MockFlowFile> waitingFlowfiles = runner.getFlowFilesForRelationship(ExecuteSparkInteractive.REL_WAIT);
+        while (!waitingFlowfiles.isEmpty()) {
+          Thread.sleep(1000);
+          runner.clearTransferState();
+          runner.enqueue(code);
+          runner.run();
+          waitingFlowfiles = runner.getFlowFilesForRelationship(ExecuteSparkInteractive.REL_WAIT);
+        }
+        runner.assertTransferCount(ExecuteSparkInteractive.REL_SUCCESS, 1);
+    }
+
 }
