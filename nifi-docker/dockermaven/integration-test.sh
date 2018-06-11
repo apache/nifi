0c4f9a0ba4b64e258386ef3526c7b44b1f4ba9f7
pepov <peterwilcsinszky@gmail.com>
2018-06-11 20:10:43 +0200

    NIFI-5249 cover dockerhub image with the same tests as dockermaven is covered

--- nifi-docker/dockermaven/integration-test.sh
+++ nifi-docker/dockermaven/integration-test.sh
@@ -2,31 +2,34 @@
 
 set -exuo pipefail
 
-VERSION=${1:-}
+TAG=$1
+VERSION=$2
 
-trap "{ docker rm -f nifi-${VERSION}-integration-test; }" EXIT
+trap "{ docker rm -f nifi-${TAG}-integration-test; }" EXIT
 
 echo "Checking that all files are owned by NiFi"
-test -z $(docker run --rm --entrypoint /bin/bash apache/nifi:${VERSION} -c "find /opt/nifi ! -user nifi")
+test -z $(docker run --rm --entrypoint /bin/bash apache/nifi:${TAG} -c "find /opt/nifi ! -user nifi")
 
 echo "Checking environment variables"
-test "/opt/nifi/nifi-${VERSION}" = "$(docker run --rm --entrypoint /bin/bash apache/nifi:${VERSION} -c 'echo -n $NIFI_HOME')"
-test "/opt/nifi/nifi-${VERSION}/logs" = "$(docker run --rm --entrypoint /bin/bash apache/nifi:${VERSION} -c 'echo -n $NIFI_LOG_DIR')"
-test "/opt/nifi/nifi-${VERSION}/run" = "$(docker run --rm --entrypoint /bin/bash apache/nifi:${VERSION} -c 'echo -n $NIFI_PID_DIR')"
-test "/opt/nifi" = "$(docker run --rm --entrypoint /bin/bash apache/nifi:${VERSION} -c 'echo -n $NIFI_BASE_DIR')"
+test "/opt/nifi/nifi-${VERSION}" = "$(docker run --rm --entrypoint /bin/bash apache/nifi:${TAG} -c 'echo -n $NIFI_HOME')"
+test "/opt/nifi/nifi-${VERSION}/logs" = "$(docker run --rm --entrypoint /bin/bash apache/nifi:${TAG} -c 'echo -n $NIFI_LOG_DIR')"
+test "/opt/nifi/nifi-${VERSION}/run" = "$(docker run --rm --entrypoint /bin/bash apache/nifi:${TAG} -c 'echo -n $NIFI_PID_DIR')"
+test "/opt/nifi" = "$(docker run --rm --entrypoint /bin/bash apache/nifi:${TAG} -c 'echo -n $NIFI_BASE_DIR')"
 
 echo "Starting NiFi container..."
-docker run -d --name nifi-${VERSION}-integration-test apache/nifi:${VERSION}
+docker run -d --name nifi-${TAG}-integration-test apache/nifi:${TAG}
+
+IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nifi-${TAG}-integration-test)
 
 for i in $(seq 1 10) :; do
-    if docker exec nifi-${VERSION}-integration-test bash -c "ss -ntl | grep 8080"; then
+    if docker exec nifi-${TAG}-integration-test bash -c "ss -ntl | grep 8080"; then
         break
     fi
     sleep 10
 done
 
 echo "Checking system diagnostics"
-test ${VERSION} = $(docker exec nifi-${VERSION}-integration-test bash -c "curl -s localhost:8080/nifi-api/system-diagnostics | jq .systemDiagnostics.aggregateSnapshot.versionInfo.niFiVersion -r")
+test ${VERSION} = $(docker exec nifi-${TAG}-integration-test bash -c "curl -s $IP:8080/nifi-api/system-diagnostics | jq .systemDiagnostics.aggregateSnapshot.versionInfo.niFiVersion -r")
 
 echo "Stopping NiFi container"
-time docker stop nifi-${VERSION}-integration-test
+time docker stop nifi-${TAG}-integration-test
