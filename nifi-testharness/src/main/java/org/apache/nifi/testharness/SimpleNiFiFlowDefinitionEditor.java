994d40478c3c1b90a9df960bf37208e0df009be1
Peter G. Horvath <peter.gergely.horvath@gmail.com>
2018-07-11 12:45:57 +0200

    NIFI-5318 Implement NiFi test harness: initial commit of nifi-testharness

---
+++ nifi-testharness/src/main/java/org/apache/nifi/testharness/SimpleNiFiFlowDefinitionEditor.java
@@ -0,0 +1,143 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+package org.apache.nifi.testharness;
+
+import org.apache.nifi.testharness.api.FlowFileEditorCallback;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+import java.util.LinkedList;
+
+
+/**
+ * <p>
+ * A facility to describe simple, common changes to a NiFi flow before it is installed to the test
+ * NiFi instance. Intended to be used by
+ * {@link TestNiFiInstance.Builder#modifyFlowXmlBeforeInstalling(FlowFileEditorCallback)}
+ * </p>
+ *
+ * <p>
+ * The desired edits can be configured via the {@link Builder} object returned by the {@link #builder()}
+ * method. Once fully configured, the {@link Builder#build()} emits a {@code FlowFileEditorCallback}
+ * object that can be passed to
+ * {@link TestNiFiInstance.Builder#modifyFlowXmlBeforeInstalling(FlowFileEditorCallback)}.
+ * </p>
+ *
+ * <p>
+ * <strong>CAUTION: THIS IS AN EXPERIMENTAL API: EXPECT CHANGES!</strong>
+ * Efforts will be made to retain backwards API compatibility, but
+ * no guarantee is given.
+ * </p>
+ *
+ * @see TestNiFiInstance.Builder#modifyFlowXmlBeforeInstalling(FlowFileEditorCallback)
+ *
+ */
+
+public final class SimpleNiFiFlowDefinitionEditor implements FlowFileEditorCallback {
+
+
+    private final LinkedList<FlowFileEditorCallback> delegateActions;
+
+    private SimpleNiFiFlowDefinitionEditor(LinkedList<FlowFileEditorCallback> delegateActions) {
+        this.delegateActions = delegateActions;
+    }
+
+    @Override
+    public Document edit(Document document) throws Exception {
+
+        for (FlowFileEditorCallback change : delegateActions) {
+            document = change.edit(document);
+        }
+
+        return document;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+
+        private Builder() {
+            // no external instance
+        }
+
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        private final LinkedList<FlowFileEditorCallback> actions = new LinkedList<>();
+
+        public Builder rawXmlChange(FlowFileEditorCallback flowFileEditorCallback) {
+            actions.addLast(flowFileEditorCallback);
+            return this;
+        }
+
+        public Builder setSingleProcessorProperty(String processorName, String propertyName, String newValue) {
+
+            return rawXmlChange(document -> {
+                String xpathString = "//processor[name/text() = '" + processorName
+                        + "']/property[name/text() = '" + propertyName + "']/value";
+
+                Node propertyValueNode = (Node) xpath.evaluate(xpathString, document, XPathConstants.NODE);
+
+                if (propertyValueNode == null) {
+                    throw new IllegalArgumentException("Reference to processor '"+ processorName +"' with property '"
+                            + propertyName + "' not found: " + xpathString);
+                }
+
+                propertyValueNode.setTextContent(newValue);
+
+                return document;
+            });
+
+
+        }
+
+        public Builder setClassOfSingleProcessor(String processorName, Class<?> mockProcessor) {
+
+            return setClassOfSingleProcessor(processorName, mockProcessor.getName());
+        }
+
+        public Builder setClassOfSingleProcessor(String processorName, String newFullyQualifiedClassName) {
+
+            return rawXmlChange(document -> {
+                String xpathString = "//processor[name/text() = '" + processorName + "']/class";
+
+                Node classNameNode = (Node) xpath.evaluate(xpathString, document, XPathConstants.NODE);
+
+                if (classNameNode == null) {
+                    throw new IllegalArgumentException("Reference to processor '"+ processorName +" not found: " +
+                            xpathString);
+                }
+
+                classNameNode.setTextContent(newFullyQualifiedClassName);
+
+                return document;
+            });
+        }
+
+
+        public SimpleNiFiFlowDefinitionEditor build() {
+            return new SimpleNiFiFlowDefinitionEditor(actions);
+        }
+
+    }
+}
---
+++ nifi-testharness/src/test/java/org/apache/nifi/testharness/samples/NiFiFlowTest.java
@@ -0,0 +1,157 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.apache.nifi.testharness.samples;
+
+
+
+import org.apache.nifi.testharness.TestNiFiInstance;
+import org.apache.nifi.testharness.util.FileUtils;
+import org.apache.nifi.testharness.SimpleNiFiFlowDefinitionEditor;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
+
+
+/**
+ * This test demonstrates how to mock the source data by starting a mock HTTP server (using Jetty)
+ * and rewrite the URL in flow definition.
+ */
+public class NiFiFlowTest {
+
+    private static final SimpleNiFiFlowDefinitionEditor CONFIGURE_MOCKS_IN_NIFI_FLOW = SimpleNiFiFlowDefinitionEditor.builder()
+            .setSingleProcessorProperty("GetHTTP", "URL", "http://localhost:12345")
+            .build();
+
+    // used by mocked GetHTTP; serves test data
+    private static Server testJettyServer;
+
+    private final TestNiFiInstance testNiFiInstance;
+
+
+    public NiFiFlowTest() {
+
+        File nifiZipFile = TestUtils.getBinaryDistributionZipFile(Constants.NIFI_ZIP_DIR);
+
+        this.testNiFiInstance = TestNiFiInstance.builder()
+                .setNiFiBinaryDistributionZip(nifiZipFile)
+                .setFlowXmlToInstallForTesting(Constants.FLOW_XML_FILE)
+                .modifyFlowXmlBeforeInstalling(CONFIGURE_MOCKS_IN_NIFI_FLOW)
+                .build();
+    }
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        NiFiFlowTest.testJettyServer = new Server(12345);
+
+
+        Handler handler = new TestHandler();
+        NiFiFlowTest.testJettyServer.setHandler(handler);
+        NiFiFlowTest.testJettyServer.start();
+    }
+
+
+    @BeforeMethod
+    public void bootstrapNiFi() throws Exception {
+
+        if (Constants.OUTPUT_DIR.exists()) {
+            FileUtils.deleteDirectoryRecursive(Constants.OUTPUT_DIR.toPath());
+        }
+
+        testNiFiInstance.install();
+        testNiFiInstance.start();
+    }
+
+    @Test
+    public void testFlowCreatesFilesInCorrectLocation() throws IOException {
+
+        // We deleted the output directory: our NiFi flow should create it
+
+        assertTrue(Constants.OUTPUT_DIR.exists(), "Output directory not found: " + Constants.OUTPUT_DIR);
+
+        File outputFile = new File(Constants.OUTPUT_DIR, "bbc-world.rss.xml");
+
+        assertTrue(outputFile.exists(), "Output file not found: " + outputFile);
+
+        List<String> strings = Files.readAllLines(outputFile.toPath());
+
+        boolean atLeastOneLineContainsBBC = strings.stream().anyMatch(line -> line.toLowerCase().contains("bbc"));
+
+        assertTrue(atLeastOneLineContainsBBC, "There was no line containing BBC");
+
+        boolean atLeastOneLineContainsIPhone = strings.stream().anyMatch(line -> line.toLowerCase().contains("iphone"));
+
+        assertTrue(atLeastOneLineContainsIPhone, "There was no line containing IPhone");
+
+    }
+
+    @AfterMethod
+    public void shutdownNiFi() throws IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+
+        testNiFiInstance.stopAndCleanup();
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        NiFiFlowTest.testJettyServer.stop();
+    }
+
+
+    private static class TestHandler extends org.eclipse.jetty.server.handler.AbstractHandler {
+        @Override
+        public void handle(
+                String target,
+                Request baseRequest,
+                HttpServletRequest httpServletRequest,
+                HttpServletResponse response) throws IOException, ServletException {
+
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_OK);
+            baseRequest.setHandled(true);
+
+            InputStream resource = TestHandler.class.getResourceAsStream("/sample_technology_rss.xml");
+            ServletOutputStream outputStream = response.getOutputStream();
+
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = resource.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, len);
+            }
+        }
+    }
+}
