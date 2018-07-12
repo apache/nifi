e959630c22c9a52ec717141f6cf9f018830a38bf
Andy LoPresto <alopresto@apache.org>
2018-07-12 01:19:19 -0700

    NIFI-5414 Fixed checkstyle error due to unused import.

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/main/java/org/apache/nifi/controller/service/ServiceStateTransition.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/main/java/org/apache/nifi/controller/service/ServiceStateTransition.java
@@ -21,7 +21,6 @@ import org.apache.nifi.controller.ComponentNode;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 public class ServiceStateTransition {
