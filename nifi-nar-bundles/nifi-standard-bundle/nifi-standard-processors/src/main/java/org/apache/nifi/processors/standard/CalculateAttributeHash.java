c54ca6323b2f0ebca384681c52b810f5404b4e7d
Andy LoPresto <alopresto@apache.org>
2018-08-30 20:07:18 -0700

    NIFI-5147 [WIP] Used HashAlgorithm enum in CalculateHashAttribute. Cleaned up typos and descriptions. Added unit test demonstrating missing Blake2 algorithm.

--- nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/CalculateAttributeHash.java
+++ nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/CalculateAttributeHash.java
@@ -17,6 +17,17 @@
 
 package org.apache.nifi.processors.standard;
 
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicReference;
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -37,30 +48,19 @@ import org.apache.nifi.processor.ProcessContext;
 import org.apache.nifi.processor.ProcessSession;
 import org.apache.nifi.processor.Relationship;
 import org.apache.nifi.processor.util.StandardValidators;
-
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.SortedMap;
-import java.util.TreeMap;
-import java.util.concurrent.atomic.AtomicReference;
+import org.apache.nifi.security.util.crypto.HashAlgorithm;
 
 @EventDriven
 @SideEffectFree
 @SupportsBatching
 @Tags({"attributes", "hash"})
 @InputRequirement(InputRequirement.Requirement.INPUT_REQUIRED)
-@CapabilityDescription("Calculates a hash value for the value specified attributes and write it to an output attribute.  Please refer to https://csrc.nist.gov/Projects/Hash-Functions/NIST-Policy-on-Hash-Functions help decide on which algorithm to use.")
+@CapabilityDescription("Calculates a hash value for each of the specified attributes using the given algorithm and writes it to an output attribute. Please refer to https://csrc.nist.gov/Projects/Hash-Functions/NIST-Policy-on-Hash-Functions for help to decide which algorithm to use.")
 @WritesAttribute(attribute = "<Specified Attribute Name per Dynamic Property>", description = "This Processor adds an attribute whose value is the result of "
-        + "Hashing of the found attribute. The name of this attribute is specified by the value of the dynamic property.")
+        + "hashing the specified attribute. The name of this attribute is specified by the value of the dynamic property.")
 @DynamicProperty(name = "A flowfile attribute key for attribute inspection", value = "Attribute Name",
-        description = "The property name defines the attribute to look for and hash in the incoming flowfile."
-                + "The property value defines the name to give the generated attribute."
+        description = "The property name defines the attribute to look for and hash in the incoming flowfile. "
+                + "The property value defines the name to give the generated attribute. "
                 + "Attribute names must be unique.")
 public class CalculateAttributeHash extends AbstractProcessor {
     public enum PartialAttributePolicy {
@@ -68,33 +68,26 @@ public class CalculateAttributeHash extends AbstractProcessor {
         PROHIBIT
     }
 
-    static final AllowableValue MD2_VALUE = new AllowableValue("MD2", "MD2 Hashing Algorithm", "MD2 Hashing Algorithm");
-    static final AllowableValue MD5_VALUE = new AllowableValue("MD5", "MD5 Hashing Algorithm", "MD5 Hashing Algorithm");
-    static final AllowableValue SHA1_VALUE = new AllowableValue("SHA-1", "SHA-1 Hashing Algorithm", "SHA-1 Hashing Algorithm");
-    static final AllowableValue SHA256_VALUE = new AllowableValue("SHA-256", "SHA-256 Hashing Algorithm", "SHA-256 Hashing Algorithm");
-    static final AllowableValue SHA384_VALUE = new AllowableValue("SHA-384", "SHA-384 Hashing Algorithm", "SHA-384 Hashing Algorithm");
-    static final AllowableValue SHA512_VALUE = new AllowableValue("SHA-512", "SHA-512 Hashing Algorithm", "SHA-512 Hashing Algorithm");
-
-    static final AllowableValue ALLOW_PARITAL_ATTRIBUTES_VALUE = new AllowableValue(PartialAttributePolicy.ALLOW.name(),
+    private static final AllowableValue ALLOW_PARTIAL_ATTRIBUTES_VALUE = new AllowableValue(PartialAttributePolicy.ALLOW.name(),
             "Allow missing attributes",
-            "Do not route to failure if there are attributes configured for hashing that are not present in the flow file");
+            "Do not route to failure if there are attributes configured for hashing that are not present in the flowfile");
 
-    static final AllowableValue FAIL_PARTIAL_ATTRIBUTES_VALUE = new AllowableValue(PartialAttributePolicy.PROHIBIT.name(),
+    private static final AllowableValue FAIL_PARTIAL_ATTRIBUTES_VALUE = new AllowableValue(PartialAttributePolicy.PROHIBIT.name(),
             "Fail if missing attributes",
-            "Route to failure if there are attributes configured for hashing that are not present in the flow file");
+            "Route to failure if there are attributes configured for hashing that are not present in the flowfile");
 
-    public static final PropertyDescriptor CHARACTER_SET = new PropertyDescriptor.Builder()
-            .name("Character Set")
-            .description("The Character Set in which the file is encoded")
+    private static final PropertyDescriptor CHARACTER_SET = new PropertyDescriptor.Builder()
+            .name("character_set")
+            .description("The Character Set used to encode the attribute being hashed")
             .required(true)
             .addValidator(StandardValidators.CHARACTER_SET_VALIDATOR)
             .defaultValue("UTF-8")
             .build();
 
-    public static final PropertyDescriptor FAIL_WHEN_EMPTY = new PropertyDescriptor.Builder()
+    static final PropertyDescriptor FAIL_WHEN_EMPTY = new PropertyDescriptor.Builder()
             .name("fail_when_empty")
-            .displayName("Fail when no attributes")
-            .description("Route to failure when none of the attributes that are configured for hashing are found.  " +
+            .displayName("Fail when no attributes present")
+            .description("Route to failure when none of the attributes that are configured for hashing are found. " +
             "If set to false, then flow files that do not contain any of the attributes that are configured for hashing will just pass through to success.")
             .allowableValues("true","false")
             .required(true)
@@ -102,38 +95,49 @@ public class CalculateAttributeHash extends AbstractProcessor {
             .defaultValue("true")
             .build();
 
-    public static final PropertyDescriptor HASH_ALGORITHM = new PropertyDescriptor.Builder()
+    static final PropertyDescriptor HASH_ALGORITHM = new PropertyDescriptor.Builder()
             .name("hash_algorithm")
             .displayName("Hash Algorithm")
-            .description("The Hash Algorithm to use.  Note that not all of the algorithms available are recommended for use.  " +
-            "There are many things to consider when picking an algorithm, it is recommended that use most secure algorithm when possible.")
+            .description("The Hash Algorithm to use. Note that not all of the algorithms available are recommended for use (some are provided for legacy use). " +
+            "There are many things to consider when picking an algorithm; it is recommended to use the most secure algorithm possible.")
             .required(true)
-            .allowableValues(MD2_VALUE, MD5_VALUE, SHA1_VALUE, SHA256_VALUE, SHA384_VALUE, SHA512_VALUE)
-            .defaultValue(SHA256_VALUE.getValue())
+            .allowableValues(buildHashAlgorithmAllowableValues())
+            .defaultValue(HashAlgorithm.SHA256.name())
             .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
             .build();
 
-    public static final PropertyDescriptor PARTIAL_ATTR_ROUTE_POLICY = new PropertyDescriptor.Builder()
+    static final PropertyDescriptor PARTIAL_ATTR_ROUTE_POLICY = new PropertyDescriptor.Builder()
             .name("missing_attr_policy")
             .displayName("Missing attribute policy")
             .description("Policy for how the processor handles attributes that are configured for hashing but are not found in the flowfile.")
             .required(true)
-            .allowableValues(ALLOW_PARITAL_ATTRIBUTES_VALUE, FAIL_PARTIAL_ATTRIBUTES_VALUE)
+            .allowableValues(ALLOW_PARTIAL_ATTRIBUTES_VALUE, FAIL_PARTIAL_ATTRIBUTES_VALUE)
             .addValidator(StandardValidators.NON_BLANK_VALIDATOR)
-            .defaultValue(ALLOW_PARITAL_ATTRIBUTES_VALUE.getValue())
+            .defaultValue(ALLOW_PARTIAL_ATTRIBUTES_VALUE.getValue())
             .build();
 
     public static final Relationship REL_SUCCESS = new Relationship.Builder()
             .name("success")
             .description("Used for FlowFiles that have a hash value added")
             .build();
+
     public static final Relationship REL_FAILURE = new Relationship.Builder()
             .name("failure")
             .description("Used for FlowFiles that are missing required attributes")
             .build();
-
     private final static Set<Relationship> relationships;
+
     private final static List<PropertyDescriptor> properties;
+
+    private static AllowableValue[] buildHashAlgorithmAllowableValues() {
+        final HashAlgorithm[] hashAlgorithms = HashAlgorithm.values();
+        List<AllowableValue> allowableValues = new ArrayList<>(hashAlgorithms.length);
+        for (HashAlgorithm algorithm : hashAlgorithms) {
+            allowableValues.add(new AllowableValue(algorithm.name(), algorithm.getName(), algorithm.getDescription()));
+        }
+
+        return allowableValues.toArray(new AllowableValue[0]);
+    }
 
     private final AtomicReference<Map<String, String>> attributeToGenerateNameMapRef = new AtomicReference<>(Collections.emptyMap());
 
@@ -164,7 +168,9 @@ public class CalculateAttributeHash extends AbstractProcessor {
     @Override
     protected PropertyDescriptor getSupportedDynamicPropertyDescriptor(final String propertyDescriptorName) {
         return new PropertyDescriptor.Builder()
-                .name(propertyDescriptorName).addValidator(StandardValidators.NON_BLANK_VALIDATOR).build();
+                .name(propertyDescriptorName)
+                .addValidator(StandardValidators.NON_BLANK_VALIDATOR)
+                .build();
     }
 
     @Override
@@ -196,18 +202,19 @@ public class CalculateAttributeHash extends AbstractProcessor {
         final SortedMap<String, String> attributes = getRelevantAttributes(flowFile, attributeToGeneratedNameMap);
         if(attributes.isEmpty()) {
             if( context.getProperty(FAIL_WHEN_EMPTY).asBoolean()) {
-                logger.info("routing {} to 'failure' because of missing all attributes: {}", new Object[]{flowFile, getMissingKeysString(null,attributeToGeneratedNameMap.keySet())});
+                logger.info("Routing {} to 'failure' because of missing all attributes: {}", new Object[]{flowFile, getMissingKeysString(null,attributeToGeneratedNameMap.keySet())});
                 session.transfer(flowFile, REL_FAILURE);
                 return;
             }
         }
         if (attributes.size() != attributeToGeneratedNameMap.size()) {
             if (PartialAttributePolicy.valueOf(context.getProperty(PARTIAL_ATTR_ROUTE_POLICY).getValue()) == PartialAttributePolicy.PROHIBIT) {
-                logger.info("routing {} to 'failure' because of missing attributes: {}", new Object[]{flowFile, getMissingKeysString(attributes.keySet(),attributeToGeneratedNameMap.keySet())});
+                logger.info("Routing {} to 'failure' because of missing attributes: {}", new Object[]{flowFile, getMissingKeysString(attributes.keySet(),attributeToGeneratedNameMap.keySet())});
                 session.transfer(flowFile, REL_FAILURE);
                 return;
             }
         }
+
         // Generate a hash with the configured algorithm for each attribute value
         // and create a new attribute with the configured name
         for (final Map.Entry<String, String> entry : attributes.entrySet()) {
