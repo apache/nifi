6ad763918116aca7426053b39dcb4d2a339b1810
Peter G. Horvath <peter.gergely.horvath@gmail.com>
2018-11-10 14:58:25 +0100

    NIFI-5318 Implement NiFi test harness: better reporting in case NiFi ZIP is not there

--- nifi-testharness/src/test/java/org/apache/nifi/testharness/samples/TestUtils.java
+++ nifi-testharness/src/test/java/org/apache/nifi/testharness/samples/TestUtils.java
@@ -20,6 +20,16 @@
 package org.apache.nifi.testharness.samples;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 final class TestUtils {
 
@@ -29,24 +39,41 @@ final class TestUtils {
 
     static File getBinaryDistributionZipFile(File binaryDistributionZipDir) {
 
-        File[] files = binaryDistributionZipDir.listFiles((dir, name) ->
-                name.startsWith("nifi-") && name.endsWith("-bin.zip"));
+        AtomicReference<File> binaryDistributionZipFileRef = new AtomicReference<>();
 
-        if (files == null) {
-            throw new IllegalStateException(
-                    "Not a directory or I/O error reading: " + binaryDistributionZipDir);
+        List<Path> visitedFiles = new LinkedList<>();
+
+        try {
+            Files.walkFileTree(binaryDistributionZipDir.toPath(), new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    visitedFiles.add(file);
+
+                    String name = file.toFile().getName();
+
+                    if (name.startsWith("nifi-") && name.endsWith("-bin.zip")) {
+                        binaryDistributionZipFileRef.set(file.toFile());
+                    }
+
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException ioEx) {
+            throw new RuntimeException(ioEx);
         }
 
-        if (files.length == 0) {
+        File binaryDistributionZipFile = binaryDistributionZipFileRef.get();
+        if (binaryDistributionZipFile == null) {
+
+            String visitedFilesString =
+                    visitedFiles.stream().map(Path::toString).collect(Collectors.joining(", "));
+
             throw new IllegalStateException(
-                    "No NiFi distribution ZIP file is found in: " + binaryDistributionZipDir);
+                    "No NiFi distribution ZIP file is found in: "
+                            + binaryDistributionZipDir + ", while visited: "
+                            + visitedFilesString);
         }
 
-        if (files.length != 1) {
-            throw new IllegalStateException(
-                    "Multiple NiFi distribution ZIP files are found in: " + binaryDistributionZipDir);
-        }
-
-        return files[0];
+        return binaryDistributionZipFile;
     }
 }
