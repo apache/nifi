986a2a484285a342e20494107abe52ff98ad2880
Pierre Villard <pierre.villard.fr@gmail.com>
2018-11-22 18:50:11 +0100

    NIFI-5838 - Improve the schema validation method in Kite processors
    review
    
    Add empty check
    
    This closes #3182.
    
    Signed-off-by: Koji Kawamura <ijokarumawak@apache.org>

--- nifi-nar-bundles/nifi-kite-bundle/nifi-kite-processors/src/test/java/org/apache/nifi/processors/kite/TestCSVToAvroProcessor.java
+++ nifi-nar-bundles/nifi-kite-bundle/nifi-kite-processors/src/test/java/org/apache/nifi/processors/kite/TestCSVToAvroProcessor.java
@@ -19,6 +19,7 @@
 package org.apache.nifi.processors.kite;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
@@ -66,6 +67,23 @@ public class TestCSVToAvroProcessor {
 
     public static final String FAILURE_SUMMARY = "" +
             "Field id: cannot make \"long\" value: '': Field id type:LONG pos:0 not set and has no default value";
+
+
+    /**
+     * Test for a schema that is not a JSON but does not throw exception when trying to parse as an URI
+     */
+    @Test
+    public void testSchemeValidation() throws IOException {
+        TestRunner runner = TestRunners.newTestRunner(ConvertCSVToAvro.class);
+        runner.setProperty(ConvertCSVToAvro.SCHEMA, "column1;column2");
+        runner.assertNotValid();
+        runner.setProperty(ConvertCSVToAvro.SCHEMA, "src/test/resources/Shapes_header.csv.avro");
+        runner.assertNotValid();
+        runner.setProperty(ConvertCSVToAvro.SCHEMA, "file:" + new File("src/test/resources/Shapes_header.csv.avro").getAbsolutePath());
+        runner.assertValid();
+        runner.setProperty(ConvertCSVToAvro.SCHEMA, "");
+        runner.assertNotValid();
+    }
 
     /**
      * Basic test for tab separated files, similar to #test
