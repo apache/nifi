c25afd74a2b6cbe8fea789d6cdc0e7ab934dff5b
Karthik Narayanan <rkarthik29@gmail.com>
2019-02-19 16:59:06 -0500

    adding a phoenix dbcp connecton pool CS

---
+++ nifi-nar-bundles/nifi-phoenix-bundle/nifi-phoenix-service/src/test/java/org/apache/nifi/dbcp/phoenix/TTTestStandardPhoenixDBCPService.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.dbcp.phoenix;
+
+import org.apache.nifi.reporting.InitializationException;
+import org.apache.nifi.util.TestRunner;
+import org.apache.nifi.util.TestRunners;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TTTestStandardPhoenixDBCPService {
+
+    @Before
+    public void init() {
+
+    }
+
+    @Test
+    public void testService() throws InitializationException {
+        final TestRunner runner = TestRunners.newTestRunner(TestProcessor.class);
+        
+        final StandardPhoenixDBCPService service = new StandardPhoenixDBCPService();
+        runner.addControllerService("test-good", service);
+        runner.setProperty(service,"phoenix.schema.isNameSpaceMappingEnabled","true");
+        runner.setProperty(service, StandardPhoenixDBCPService.DATABASE_URL, "jdbc:phoenix:ssahihdp0.field.hortonworks.com:2181:/hbase-unsecure");
+        //runner.setProperty(service, StandardPhoenixDBCPService.DB_USER, "hdfs");
+        runner.setProperty(service, StandardPhoenixDBCPService.VALIDATION_QUERY,"SELECT 1 FROM SYSTEM.CATALOG LIMIT 1");
+        runner.enableControllerService(service);
+        
+        service.getConnection();
+        runner.assertValid(service);
+    }
+
+}
