76dc783aa19a7fadb1edb4dee775ccc32f5628e4
Mike Thomsen <mikerthomsen@gmail.com>
2019-02-25 20:29:30 -0500

    NIFI-5537 Removed Mac-specific documentation in int test.

--- nifi-nar-bundles/nifi-cypher-bundle/nifi-neo4j-cypher-service/src/test/java/org/apache/nifi/cypher/ITNeo4JCypherExecutor.java
+++ nifi-nar-bundles/nifi-cypher-bundle/nifi-neo4j-cypher-service/src/test/java/org/apache/nifi/cypher/ITNeo4JCypherExecutor.java
@@ -36,21 +36,6 @@ import static org.junit.Assert.assertEquals;
 
 /**
  * Neo4J Cypher integration tests.  Please set the neo4j url, user and password according to your setup.
- * The steps to setup neo4j are
- * <ol>
- *   <li> Install Neo4J</li>
- *      <code>brew install neo4j</code>
- *   <li>Setup neo4j</li>
- * <code>neo4j start</code>
- *    <li>Log into cypher shell using default username/password - neo4j/neo4j</li>
- * <code>cypher-shell</code>
- *    <li>Change password to testing1234</li>
- * <code>CALL dbms.changePassword('testing1234')</code>
- *    <li> Restart neo4j</li>
- * <code>neo4j restart</code>
- *    <li>Log into cypher shell using new password (testing1234)</li>
- * <code>cypher-shell</code>
- * </ol>
  */
 public class ITNeo4JCypherExecutor {
     protected TestRunner runner;
