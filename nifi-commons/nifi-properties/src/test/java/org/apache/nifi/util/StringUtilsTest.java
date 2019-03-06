1bda9597d5a78f48613b7f3b13153a46c5d66ecd
Freddy Tuxworth <freddytuxworth@gmail.com>
2019-03-06 16:36:56 +0000

    Fix import asterisk warning

--- nifi-commons/nifi-properties/src/test/java/org/apache/nifi/util/StringUtilsTest.java
+++ nifi-commons/nifi-properties/src/test/java/org/apache/nifi/util/StringUtilsTest.java
@@ -1,6 +1,9 @@
 package org.apache.nifi.util;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import java.util.ArrayList;
 import org.junit.Test;
