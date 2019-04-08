26a289ad6fa54494742e036672200f62a07d62e8
samhjelmfelt <samhjelmfelt@yahoo.com>
2019-04-08 15:16:52 -0700

    Renaming from NiFi-Fn to NiFi-Stateless

---
+++ nifi-stateless/nifi-stateless-core/src/main/java/org/apache/nifi/stateless/core/ComponentFactory.java
@@ -0,0 +1,212 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.stateless.core;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.nifi.attribute.expression.language.StandardPropertyValue;
+import org.apache.nifi.bundle.Bundle;
+import org.apache.nifi.bundle.BundleCoordinate;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.controller.ControllerService;
+import org.apache.nifi.controller.exception.ControllerServiceInstantiationException;
+import org.apache.nifi.controller.exception.ProcessorInstantiationException;
+import org.apache.nifi.logging.ComponentLog;
+import org.apache.nifi.nar.ExtensionManager;
+import org.apache.nifi.processor.Processor;
+import org.apache.nifi.processor.ProcessorInitializationContext;
+import org.apache.nifi.processor.Relationship;
+import org.apache.nifi.registry.VariableRegistry;
+import org.apache.nifi.registry.flow.VersionedControllerService;
+import org.apache.nifi.registry.flow.VersionedProcessor;
+import org.apache.nifi.util.file.classloader.ClassLoaderUtils;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class ComponentFactory {
+    private final ExtensionManager extensionManager;
+
+    public ComponentFactory(final ExtensionManager extensionManager) {
+        this.extensionManager = extensionManager;
+    }
+
+
+    public StatelessProcessorWrapper createProcessor(final VersionedProcessor versionedProcessor, final boolean materializeContent, final StatelessControllerServiceLookup controllerServiceLookup,
+                                                     final VariableRegistry variableRegistry, final Set<URL> classpathUrls) throws ProcessorInstantiationException {
+        final String type = versionedProcessor.getType();
+        final String identifier = versionedProcessor.getIdentifier();
+
+        final Bundle bundle = getAvailableBundle(versionedProcessor.getBundle(), type);
+        if (bundle == null) {
+            throw new IllegalStateException("Unable to find bundle for coordinate "
+                + versionedProcessor.getBundle().getGroup() + ":"
+                + versionedProcessor.getBundle().getArtifact() + ":"
+                + versionedProcessor.getBundle().getVersion());
+        }
+
+        final ClassLoader ctxClassLoader = Thread.currentThread().getContextClassLoader();
+        try {
+            final ClassLoader detectedClassLoader = extensionManager.createInstanceClassLoader(type, identifier, bundle,
+                classpathUrls == null ? Collections.emptySet() : classpathUrls);
+
+            System.out.println("Setting context class loader to " + detectedClassLoader + " (parent = " + detectedClassLoader.getParent() + ") to create " + type);
+            final Class<?> rawClass = Class.forName(type, true, detectedClassLoader);
+            Thread.currentThread().setContextClassLoader(detectedClassLoader);
+
+            final Object extensionInstance = rawClass.newInstance();
+            final ComponentLog componentLog = new SLF4JComponentLog(extensionInstance);
+
+            final Processor processor = (Processor) extensionInstance;
+            final ProcessorInitializationContext initializationContext = new StatelessProcessorInitializationContext(versionedProcessor.getIdentifier(), processor, controllerServiceLookup);
+            processor.initialize(initializationContext);
+
+            // If no classpath urls were provided, check if we need to add additional classpath URL's based on configured properties.
+            if (classpathUrls == null) {
+                final Set<URL> additionalClasspathUrls = getAdditionalClasspathResources(processor.getPropertyDescriptors(), processor.getIdentifier(), versionedProcessor.getProperties(),
+                    variableRegistry,componentLog);
+
+                if (!additionalClasspathUrls.isEmpty()) {
+                    return createProcessor(versionedProcessor, materializeContent, controllerServiceLookup, variableRegistry, additionalClasspathUrls);
+                }
+            }
+
+            final StatelessProcessorWrapper processorWrapper = new StatelessProcessorWrapper(versionedProcessor.getIdentifier(), processor, null,
+                controllerServiceLookup, variableRegistry, materializeContent, detectedClassLoader);
+
+            // Configure the Processor
+            processorWrapper.setAnnotationData(versionedProcessor.getAnnotationData());
+            versionedProcessor.getProperties().forEach(processorWrapper::setProperty);
+            for (String relationship : versionedProcessor.getAutoTerminatedRelationships()) {
+                processorWrapper.addAutoTermination(new Relationship.Builder().name(relationship).build());
+            }
+
+            return processorWrapper;
+        } catch (final Exception e) {
+            throw new ProcessorInstantiationException(type, e);
+        } finally {
+            if (ctxClassLoader != null) {
+                Thread.currentThread().setContextClassLoader(ctxClassLoader);
+            }
+        }
+    }
+
+
+    private Set<URL> getAdditionalClasspathResources(final List<PropertyDescriptor> propertyDescriptors, final String componentId, final Map<String, String> properties,
+                                                     final VariableRegistry variableRegistry, final ComponentLog logger) {
+        final Set<String> modulePaths = new LinkedHashSet<>();
+        for (final PropertyDescriptor descriptor : propertyDescriptors) {
+            if (descriptor.isDynamicClasspathModifier()) {
+                final String value = properties.get(descriptor.getName());
+                if (!StringUtils.isEmpty(value)) {
+                    final StandardPropertyValue propertyValue = new StandardPropertyValue(value, null, variableRegistry);
+                    modulePaths.add(propertyValue.evaluateAttributeExpressions().getValue());
+                }
+            }
+        }
+
+        final Set<URL> additionalUrls = new LinkedHashSet<>();
+        try {
+            final URL[] urls = ClassLoaderUtils.getURLsForClasspath(modulePaths, null, true);
+            if (urls != null) {
+                additionalUrls.addAll(Arrays.asList(urls));
+            }
+        } catch (MalformedURLException mfe) {
+            logger.error("Error processing classpath resources for " + componentId + ": " + mfe.getMessage(), mfe);
+        }
+
+        return additionalUrls;
+    }
+
+
+    public ControllerService createControllerService(final VersionedControllerService versionedControllerService, final VariableRegistry variableRegistry) {
+        return createControllerService(versionedControllerService, variableRegistry, null);
+    }
+
+
+    private ControllerService createControllerService(final VersionedControllerService versionedControllerService, final VariableRegistry variableRegistry, final Set<URL> classpathUrls) {
+
+        final String type = versionedControllerService.getType();
+        final String identifier = versionedControllerService.getIdentifier();
+
+        final Bundle bundle = getAvailableBundle(versionedControllerService.getBundle(), type);
+        if (bundle == null) {
+            throw new IllegalStateException("Unable to find bundle for coordinate "
+                + versionedControllerService.getBundle().getGroup() + ":"
+                + versionedControllerService.getBundle().getArtifact() + ":"
+                + versionedControllerService.getBundle().getVersion());
+        }
+
+        final ClassLoader ctxClassLoader = Thread.currentThread().getContextClassLoader();
+        try {
+            final ClassLoader detectedClassLoader = extensionManager.createInstanceClassLoader(type, identifier, bundle,
+                classpathUrls == null ? Collections.emptySet() : classpathUrls);
+
+            System.out.println("Setting context class loader to " + detectedClassLoader + " (parent = " + detectedClassLoader.getParent() + ") to create " + type);
+            final Class<?> rawClass = Class.forName(type, true, detectedClassLoader);
+            Thread.currentThread().setContextClassLoader(detectedClassLoader);
+
+            final Object extensionInstance = rawClass.newInstance();
+            final ComponentLog componentLog = new SLF4JComponentLog(extensionInstance);
+
+            final ControllerService service = (ControllerService) extensionInstance;
+
+            // If no classpath urls were provided, check if we need to add additional classpath URL's based on configured properties.
+            if (classpathUrls == null) {
+                final Set<URL> additionalClasspathUrls = getAdditionalClasspathResources(service.getPropertyDescriptors(), service.getIdentifier(), versionedControllerService.getProperties(),
+                    variableRegistry, componentLog);
+
+                if (!additionalClasspathUrls.isEmpty()) {
+                    return createControllerService(versionedControllerService, variableRegistry, additionalClasspathUrls);
+                }
+            }
+
+            return service;
+        } catch (final Exception e) {
+            throw new ControllerServiceInstantiationException(type, e);
+        } finally {
+            if (ctxClassLoader != null) {
+                Thread.currentThread().setContextClassLoader(ctxClassLoader);
+            }
+        }
+    }
+
+    private Bundle getAvailableBundle(final org.apache.nifi.registry.flow.Bundle bundle, final String componentType) {
+        final BundleCoordinate bundleCoordinate = new BundleCoordinate(bundle.getGroup(), bundle.getArtifact(), bundle.getVersion());
+        final Bundle availableBundle = extensionManager.getBundle(bundleCoordinate);
+        if (availableBundle != null) {
+            return availableBundle;
+        }
+
+        final List<Bundle> possibleBundles = extensionManager.getBundles(componentType);
+        if (possibleBundles.isEmpty()) {
+            throw new IllegalStateException("Could not find any NiFi Bundles that contain the Extension [" + componentType + "]");
+        }
+
+        if (possibleBundles.size() > 1) {
+            throw new IllegalStateException("Found " + possibleBundles.size() + " different NiFi Bundles that contain the Extension [" + componentType + "] but none of them had a version of " +
+                bundle.getVersion());
+        }
+
+        return possibleBundles.get(0);
+    }
+}
---
+++ nifi-stateless/nifi-stateless-core/src/test/java/org/apache/nifi/stateless/core/StreamingIT.java
@@ -0,0 +1,136 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.stateless.core;
+
+import org.apache.nifi.stateless.bootstrap.InMemoryFlowFile;
+import org.apache.nifi.processor.Processor;
+import org.apache.nifi.processor.Relationship;
+import org.apache.nifi.processors.standard.PutFile;
+import org.apache.nifi.processors.standard.ReplaceText;
+import org.apache.nifi.processors.standard.SplitText;
+import org.apache.nifi.processors.standard.TailFile;
+import org.apache.nifi.registry.VariableRegistry;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class StreamingIT {
+
+    @org.junit.Test
+    public void Scenario1_Test() throws InvocationTargetException, IllegalAccessException, IOException, InterruptedException {
+        ///////////////////////////////////////////
+        // Setup
+        ///////////////////////////////////////////
+        VariableRegistry registry = VariableRegistry.EMPTY_REGISTRY;
+        boolean materializeData = true;
+        StatelessControllerServiceLookup serviceLookup = new StatelessControllerServiceLookup();
+        File file = new File("/tmp/nifistateless/input/test.txt");
+        file.getParentFile().mkdirs();
+        file.createNewFile();
+        try (PrintStream out = new PrintStream(new FileOutputStream(file))) {
+            out.print("hello world");
+        }
+
+        ///////////////////////////////////////////
+        // Build Flow
+        ///////////////////////////////////////////
+        StatelessProcessorWrapper tailFile = wrapProcessor(new TailFile(), serviceLookup, registry);
+        tailFile.setProperty("File to Tail","/tmp/nifistateless/input/test.txt");
+
+        Set<Relationship> relationships = tailFile.getRelationships();
+        Relationship tailFile_Success = relationships.stream().filter(r->r.getName().equals("success")).findFirst().get();
+
+        StatelessProcessorWrapper splitText = wrapProcessor(new SplitText(), serviceLookup, registry);
+        tailFile.addChild(splitText, tailFile_Success);
+        splitText.setProperty(SplitText.LINE_SPLIT_COUNT,"1");
+        splitText.addAutoTermination(SplitText.REL_FAILURE);
+        splitText.addAutoTermination(SplitText.REL_ORIGINAL);
+
+        StatelessProcessorWrapper replaceText = wrapProcessor(new ReplaceText(), serviceLookup, registry);
+        splitText.addChild(replaceText, SplitText.REL_SPLITS);
+        replaceText.setProperty(ReplaceText.REPLACEMENT_VALUE,"$1!!!");
+        replaceText.addAutoTermination(ReplaceText.REL_FAILURE);
+
+
+        StatelessProcessorWrapper putFile = wrapProcessor(new PutFile(), serviceLookup, registry);
+        replaceText.addChild(putFile, ReplaceText.REL_SUCCESS);
+        putFile.addAutoTermination(PutFile.REL_FAILURE);
+        putFile.addAutoTermination(PutFile.REL_SUCCESS);
+        putFile.setProperty(PutFile.DIRECTORY,"/tmp/nifistateless/output/");
+        putFile.setProperty(PutFile.CONFLICT_RESOLUTION, PutFile.REPLACE_RESOLUTION);
+
+
+        ///////////////////////////////////////////
+        // Run Flow
+        ///////////////////////////////////////////
+
+        StatelessFlow flow = new StatelessFlow(tailFile);
+        Queue<InMemoryFlowFile> output = new LinkedList<>();
+        AtomicBoolean successful = new AtomicBoolean(true);
+        Thread t = new Thread(()->
+                successful.set(flow.run(output))
+        );
+
+        Thread.sleep(5000);
+
+        ///////////////////////////////////////////
+        // Validate
+        ///////////////////////////////////////////
+
+        String outputFile = "/tmp/nifistateless/output/test.txt";
+        assertTrue(new File(outputFile).isFile());
+
+        List<String> lines = Files.readAllLines(Paths.get(outputFile), StandardCharsets.UTF_8);
+        assertEquals(1,lines.size());
+        System.out.println("data: "+lines.get(0));
+        assertEquals("hello world!!!", lines.get(0));
+
+
+        System.out.println("Stopping...");
+        flow.shutdown();
+        t.join();
+
+        assertTrue(new File(outputFile).isFile());
+
+        lines = Files.readAllLines(Paths.get(outputFile), StandardCharsets.UTF_8);
+        assertTrue(successful.get());
+        assertTrue(output.isEmpty());
+        assertEquals(1,lines.size());
+        assertEquals("hello world!!!", lines.get(0));
+    }
+
+    private StatelessProcessorWrapper wrapProcessor(final Processor processor, StatelessControllerServiceLookup serviceLookup, final VariableRegistry registry)
+        throws InvocationTargetException, IllegalAccessException {
+        return new StatelessProcessorWrapper(UUID.randomUUID().toString(), processor, null, serviceLookup, registry, true, ClassLoader.getSystemClassLoader());
+    }
+
+}
