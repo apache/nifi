1186ed655583b9a4805b74492ea9c8f08aec9f45
rwithers <withers.ryan@gmail.com>
2019-05-01 11:36:03 -0500

    NIFI-1693 Test Grok Reader commit with modifications to support FileExpansionUtil.

--- nifi-nar-bundles/nifi-standard-services/nifi-record-serialization-services-bundle/nifi-record-serialization-services/src/test/java/org/apache/nifi/grok/TestGrokReader.java
+++ nifi-nar-bundles/nifi-standard-services/nifi-record-serialization-services-bundle/nifi-record-serialization-services/src/test/java/org/apache/nifi/grok/TestGrokReader.java
@@ -1,4 +1,71 @@
 package org.apache.nifi.grok;
 
+import org.apache.nifi.reporting.InitializationException;
+import org.apache.nifi.serialization.SchemaRegistryService;
+import org.apache.nifi.util.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.*;
+
 public class TestGrokReader {
+
+    private String originalHome = "";
+
+    @Before
+    public void beforeEach() {
+        originalHome = System.getProperty("user.home");
+    }
+
+    @After
+    public void afterEach() {
+        System.setProperty("user.home", originalHome);
+    }
+
+    @Test
+    public void testSetupOfGrokReaderService() throws InitializationException {
+        final TestRunner runner = TestRunners.newTestRunner(TestRegistryProcessor.class);
+        final GrokReader reader = new GrokReader();
+
+        runner.addControllerService("grok-schema-registry-service", reader);
+
+        runner.setProperty(reader, GrokReader.PATTERN_FILE, "src/test/resources/grok/grok-pattern-file.txt");
+        runner.setProperty(reader, GrokReader.GROK_EXPRESSION, "%{GREEDYDATA:message}");
+        runner.setProperty(reader, GrokReader.NO_MATCH_BEHAVIOR, GrokReader.SKIP_LINE);
+        runner.enableControllerService(reader);
+        runner.assertValid(reader);
+
+        final GrokReader readerService =
+                (GrokReader) runner.getProcessContext()
+                        .getControllerServiceLookup()
+                          .getControllerService("grok-schema-registry-service");
+
+        assertThat(readerService, instanceOf(SchemaRegistryService.class));
+    }
+
+    @Test
+    public void testSetupOfGrokReaderServiceHonorsPathExpansion() throws InitializationException {
+        System.setProperty("user.home", "src/test/resources/grok");
+
+        final TestRunner runner = TestRunners.newTestRunner(TestRegistryProcessor.class);
+        final GrokReader reader = new GrokReader();
+
+        runner.addControllerService("grok-schema-registry-service", reader);
+
+        runner.setProperty(reader, GrokReader.PATTERN_FILE, "~/grok-pattern-file.txt");
+        runner.setProperty(reader, GrokReader.GROK_EXPRESSION, "%{GREEDYDATA:message}");
+        runner.setProperty(reader, GrokReader.NO_MATCH_BEHAVIOR, GrokReader.SKIP_LINE);
+        runner.enableControllerService(reader);
+        runner.assertValid(reader);
+
+        final GrokReader readerService =
+                (GrokReader) runner.getProcessContext()
+                        .getControllerServiceLookup()
+                        .getControllerService("grok-schema-registry-service");
+
+        assertThat(readerService, instanceOf(SchemaRegistryService.class));
+    }
+
 }
--- nifi-nar-bundles/nifi-standard-services/nifi-record-serialization-services-bundle/nifi-record-serialization-services/src/test/java/org/apache/nifi/grok/TestRegistryProcessor.java
+++ nifi-nar-bundles/nifi-standard-services/nifi-record-serialization-services-bundle/nifi-record-serialization-services/src/test/java/org/apache/nifi/grok/TestRegistryProcessor.java
@@ -1,4 +1,31 @@
 package org.apache.nifi.grok;
 
-public class TestRegistryProcessor {
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.processor.AbstractProcessor;
+import org.apache.nifi.processor.ProcessContext;
+import org.apache.nifi.processor.ProcessSession;
+import org.apache.nifi.processor.exception.ProcessException;
+import org.apache.nifi.serialization.SchemaRegistryService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestRegistryProcessor extends AbstractProcessor {
+
+    @Override
+    public void onTrigger(ProcessContext context, ProcessSession session) throws ProcessException {
+    }
+
+    @Override
+    protected List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        List<PropertyDescriptor> properties = new ArrayList<>();
+        properties.add(new PropertyDescriptor.Builder()
+                .name("Schema Registry Service test processor")
+                .description("Schema Registry Service test processor")
+                .identifiesControllerService(SchemaRegistryService.class)
+                .required(true)
+                .build());
+        return properties;
+    }
+
 }
