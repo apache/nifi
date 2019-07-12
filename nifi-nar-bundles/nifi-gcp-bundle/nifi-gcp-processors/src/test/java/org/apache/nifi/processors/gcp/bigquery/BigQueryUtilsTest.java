ee4eebb14cc1485bd728883d856467ec1da71343
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2019-07-12 14:22:48 +0200

    Fixed indentation in test

--- nifi-nar-bundles/nifi-gcp-bundle/nifi-gcp-processors/src/test/java/org/apache/nifi/processors/gcp/bigquery/BigQueryUtilsTest.java
+++ nifi-nar-bundles/nifi-gcp-bundle/nifi-gcp-processors/src/test/java/org/apache/nifi/processors/gcp/bigquery/BigQueryUtilsTest.java
@@ -14,37 +14,37 @@ import com.google.cloud.bigquery.Field.Mode;
 
 public class BigQueryUtilsTest {
 
-	@Test
-	public void can_create_a_simple_boolean_schema() throws IOException {
+    @Test
+    public void can_create_a_simple_boolean_schema() throws IOException {
         final String jsonRead = new String(
                 Files.readAllBytes(Paths.get("src/test/resources/schemas/simple_boolean_schema.json"))
         );
         Field booleanField = Field.newBuilder("boolean", LegacySQLTypeName.BOOLEAN).setMode(Mode.NULLABLE).build();
-		Schema expected = Schema.of(booleanField);
-		Assert.assertEquals(BigQueryUtils.schemaFromString(jsonRead), expected);
-	}
+        Schema expected = Schema.of(booleanField);
+        Assert.assertEquals(BigQueryUtils.schemaFromString(jsonRead), expected);
+    }
 
-	public void can_create_a_simple_record_schema_with_bad_type_case() throws IOException {
+    public void can_create_a_simple_record_schema_with_bad_type_case() throws IOException {
         final String jsonRead = new String(
                 Files.readAllBytes(Paths.get("src/test/resources/schemas/simple_record_schema_with_bad_type_case.json"))
         );
         Field booleanField = Field.newBuilder("boolean", LegacySQLTypeName.BOOLEAN).setMode(Mode.NULLABLE).build();
-		Schema expected = Schema.of(Field.of("Consent", LegacySQLTypeName.RECORD,
-        		booleanField
-        		).toBuilder().setMode(Mode.NULLABLE).build());
-		Assert.assertEquals(BigQueryUtils.schemaFromString(jsonRead), expected);
-	}
+        Schema expected = Schema.of(Field.of("Consent", LegacySQLTypeName.RECORD,
+                booleanField
+                ).toBuilder().setMode(Mode.NULLABLE).build());
+        Assert.assertEquals(BigQueryUtils.schemaFromString(jsonRead), expected);
+    }
 
-	@Test
-	public void can_create_a_simple_record_schema() throws IOException {
+    @Test
+    public void can_create_a_simple_record_schema() throws IOException {
         final String jsonRead = new String(
                 Files.readAllBytes(Paths.get("src/test/resources/schemas/simple_record_schema.json"))
         );
         Field booleanField = Field.newBuilder("boolean", LegacySQLTypeName.BOOLEAN).setMode(Mode.NULLABLE).build();
-		Schema expected = Schema.of(Field.of("Consent", LegacySQLTypeName.RECORD,
-        		booleanField
-        		).toBuilder().setMode(Mode.NULLABLE).build());
-		Assert.assertEquals(BigQueryUtils.schemaFromString(jsonRead), expected);
-	}
+        Schema expected = Schema.of(Field.of("Consent", LegacySQLTypeName.RECORD,
+                booleanField
+                ).toBuilder().setMode(Mode.NULLABLE).build());
+        Assert.assertEquals(BigQueryUtils.schemaFromString(jsonRead), expected);
+    }
 
 }
