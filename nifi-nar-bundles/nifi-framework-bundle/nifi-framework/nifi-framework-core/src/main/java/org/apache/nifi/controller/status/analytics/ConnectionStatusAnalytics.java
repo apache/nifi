8e1452a3f342ee45dfd589d343c9ecf6a7cf6825
Yolanda M. Davis <yolanda.m.davis@gmail.com>
2019-09-10 15:54:42 -0400

    NIFI-6649 - added separate query interval configuration for observation queries NIFI-6649 - documentation update
    NIFI-6649 - add debug logging for score and prediction information
    
    NIFI-6649 - fix to ensure counts return minimum value of 0 if not infinite or NaN
    
    Signed-off-by: Matthew Burgess <mattyb149@apache.org>
    
    This closes #3719

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/main/java/org/apache/nifi/controller/status/analytics/ConnectionStatusAnalytics.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/main/java/org/apache/nifi/controller/status/analytics/ConnectionStatusAnalytics.java
@@ -24,6 +24,7 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.nifi.connectable.Connection;
 import org.apache.nifi.controller.flow.FlowManager;
@@ -56,6 +57,7 @@ public class ConnectionStatusAnalytics implements StatusAnalytics {
     private final Boolean supportOnlineLearning;
     private Boolean extendWindow = false;
     private long intervalMillis = 3L * 60 * 1000; // Default is 3 minutes
+    private long queryIntervalMillis = 3L * 60 * 1000;  //Default is 3 minutes
     private String scoreName = "rSquared";
     private double scoreThreshold = .90;
 
@@ -78,7 +80,7 @@ public class ConnectionStatusAnalytics implements StatusAnalytics {
             //Obtain latest observations when available, extend window if needed to obtain minimum observations
             this.queryWindow = new QueryWindow(extendWindow ? queryWindow.getStartTimeMillis() : queryWindow.getEndTimeMillis(), System.currentTimeMillis());
         } else {
-            this.queryWindow = new QueryWindow(System.currentTimeMillis() - getIntervalTimeMillis(), System.currentTimeMillis());
+            this.queryWindow = new QueryWindow(System.currentTimeMillis() - getQueryIntervalMillis(), System.currentTimeMillis());
         }
 
         modelMap.forEach((metric, modelFunction) -> {
@@ -94,6 +96,13 @@ public class ConnectionStatusAnalytics implements StatusAnalytics {
                 try {
                     LOG.debug("Refreshing model with new data for connection id: {} ", connectionIdentifier);
                     model.learn(Stream.of(features), Stream.of(values));
+
+                    if(MapUtils.isNotEmpty(model.getScores())){
+                        model.getScores().forEach((key, value) -> {
+                            LOG.debug("Model Scores for prediction metric {} for connection id {}: {}={} ", metric, connectionIdentifier, key, value);
+                        });
+                    }
+
                     extendWindow = false;
                 } catch (Exception ex) {
                     LOG.debug("Exception encountered while training model for connection id {}: {}", connectionIdentifier, ex.getMessage());
@@ -137,6 +146,7 @@ public class ConnectionStatusAnalytics implements StatusAnalytics {
             predictFeatures.put(1, inOutRatio);
             return convertTimePrediction(bytesModel.predictVariable(0, predictFeatures, backPressureBytes), System.currentTimeMillis());
         } else {
+            LOG.debug("Model is not valid for calculating time back pressure by content size in bytes. Returning -1");
             return -1L;
         }
     }
@@ -164,6 +174,7 @@ public class ConnectionStatusAnalytics implements StatusAnalytics {
             predictFeatures.put(1, inOutRatio);
             return convertTimePrediction(countModel.predictVariable(0, predictFeatures, backPressureCountThreshold), System.currentTimeMillis());
         } else {
+            LOG.debug("Model is not valid for calculating time to back pressure by object count. Returning -1");
             return -1L;
         }
     }
@@ -186,6 +197,7 @@ public class ConnectionStatusAnalytics implements StatusAnalytics {
             predictFeatures.add(inOutRatio);
             return convertCountPrediction(bytesModel.predict(predictFeatures.toArray(new Double[2])));
         } else {
+            LOG.debug("Model is not valid for predicting content size in bytes for next interval. Returning -1");
             return -1L;
         }
     }
@@ -208,6 +220,7 @@ public class ConnectionStatusAnalytics implements StatusAnalytics {
             predictFeatures.add(inOutRatio);
             return convertCountPrediction(countModel.predict(predictFeatures.toArray(new Double[2])));
         } else {
+            LOG.debug("Model is not valid for predicting object count for next interval. Returning -1");
             return -1L;
         }
 
@@ -264,6 +277,14 @@ public class ConnectionStatusAnalytics implements StatusAnalytics {
 
     public void setIntervalTimeMillis(long intervalTimeMillis) {
         this.intervalMillis = intervalTimeMillis;
+    }
+
+    public long getQueryIntervalMillis() {
+        return queryIntervalMillis;
+    }
+
+    public void setQueryIntervalMillis(long queryIntervalMillis) {
+        this.queryIntervalMillis = queryIntervalMillis;
     }
 
     public String getScoreName() {
@@ -334,6 +355,7 @@ public class ConnectionStatusAnalytics implements StatusAnalytics {
      */
     private Long convertTimePrediction(Double prediction, Long timeMillis) {
         if (Double.isNaN(prediction) || Double.isInfinite(prediction) || prediction < timeMillis) {
+            LOG.debug("Time prediction value is invalid: {}. Returning -1.",prediction);
             return -1L;
         } else {
             return Math.max(0, Math.round(prediction) - timeMillis);
@@ -346,7 +368,8 @@ public class ConnectionStatusAnalytics implements StatusAnalytics {
      * @return prediction prediction value converted into valid value for consumption
      */
     private Long convertCountPrediction(Double prediction) {
-        if (Double.isNaN(prediction) || Double.isInfinite(prediction) || prediction < 0) {
+        if (Double.isNaN(prediction) || Double.isInfinite(prediction)) {
+            LOG.debug("Count prediction value is invalid: {}. Returning -1.",prediction);
             return -1L;
         } else {
             return Math.max(0, Math.round(prediction));
