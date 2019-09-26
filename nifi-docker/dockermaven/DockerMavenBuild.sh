fad0e73f998b25c99edb7db4a7ca83dd5260613c
sjyang18 <ilsong02@hotmail.com>
2019-09-26 23:17:27 +0000

    NIFI-6722: override nifi-version in docker build script and add dockermaven explict build script

--- nifi-docker/dockermaven/DockerMavenBuild.sh
+++ nifi-docker/dockermaven/DockerMavenBuild.sh
@@ -15,29 +15,4 @@
 
 #!/bin/bash
 
-DOCKER_UID=1000
-if [ -n "$1" ]; then
-  DOCKER_UID="$1"
-fi
-
-DOCKER_GID=1000
-if [ -n "$2" ]; then
-  DOCKER_GID="$2"
-fi
-
-MIRROR=https://archive.apache.org/dist
-if [ -n "$3" ]; then
-  MIRROR="$3"
-fi
-
-
-if [ -n "$4" ]; then
-  DOCKER_IMAGE="apache/nifi:$4"
-  NIFI_IMAGE_VERSION="$4"
-else
-  DOCKER_IMAGE="$(egrep -v '(^#|^\s*$|^\s*\t*#)' DockerImage.txt)"
-  NIFI_IMAGE_VERSION="$(echo $DOCKER_IMAGE | cut -d : -f 2)"
-fi
-
-echo "Building NiFi Image: '$DOCKER_IMAGE' Version: $NIFI_IMAGE_VERSION Mirror: $MIRROR"
-docker build --build-arg UID="$DOCKER_UID" --build-arg GID="$DOCKER_GID" --build-arg NIFI_VERSION="$NIFI_IMAGE_VERSION" --build-arg MIRROR="$MIRROR" -t $DOCKER_IMAGE .
+ mvn package -P docker
