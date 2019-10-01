b1e8d4b0441e2d91cc3b7505d3242c3d6f187a9c
Tamas Palfy <tpalfy@cloudera.com>
2019-10-01 11:25:30 +0200

    NIFI-6729 - Created AbstractSingleAttributeBasedControllerServiceLookup and updated DBCPConnectionPoolLookup and AzureStorageCredentialsControllerServiceLookup to inherit from it.

--- nifi-nar-bundles/nifi-standard-services/nifi-dbcp-service-bundle/nifi-dbcp-service/src/main/java/org/apache/nifi/dbcp/DBCPConnectionPoolLookup.java
+++ nifi-nar-bundles/nifi-standard-services/nifi-dbcp-service-bundle/nifi-dbcp-service/src/main/java/org/apache/nifi/dbcp/DBCPConnectionPoolLookup.java
@@ -16,27 +16,15 @@
  */
 package org.apache.nifi.dbcp;
 
-import org.apache.commons.lang3.StringUtils;
+
 import org.apache.nifi.annotation.behavior.DynamicProperty;
 import org.apache.nifi.annotation.documentation.CapabilityDescription;
 import org.apache.nifi.annotation.documentation.Tags;
-import org.apache.nifi.annotation.lifecycle.OnDisabled;
-import org.apache.nifi.annotation.lifecycle.OnEnabled;
-import org.apache.nifi.components.PropertyDescriptor;
-import org.apache.nifi.components.ValidationContext;
-import org.apache.nifi.components.ValidationResult;
-import org.apache.nifi.controller.AbstractControllerService;
-import org.apache.nifi.controller.ConfigurationContext;
 import org.apache.nifi.expression.ExpressionLanguageScope;
 import org.apache.nifi.processor.exception.ProcessException;
-import org.apache.nifi.processor.util.StandardValidators;
+import org.apache.nifi.lookup.AbstractSingleAttributeBasedControllerServiceLookup;
 
 import java.sql.Connection;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Tags({ "dbcp", "jdbc", "database", "connection", "pooling", "store" })
@@ -47,70 +35,19 @@ import java.util.Map;
         "dynamically at runtime by tagging flow files with the appropriate 'database.name' attribute.")
 @DynamicProperty(name = "The ", value = "JDBC property value", expressionLanguageScope = ExpressionLanguageScope.NONE,
         description = "")
-public class DBCPConnectionPoolLookup extends AbstractControllerService implements DBCPService {
+public class DBCPConnectionPoolLookup
+        extends AbstractSingleAttributeBasedControllerServiceLookup<DBCPService> implements DBCPService {
 
     public static final String DATABASE_NAME_ATTRIBUTE = "database.name";
 
-    private volatile Map<String,DBCPService> dbcpServiceMap;
-
     @Override
-    protected PropertyDescriptor getSupportedDynamicPropertyDescriptor(final String propertyDescriptorName) {
-        return new PropertyDescriptor.Builder()
-                .name(propertyDescriptorName)
-                .description("The DBCPService to return when database.name = '" + propertyDescriptorName + "'")
-                .identifiesControllerService(DBCPService.class)
-                .addValidator(StandardValidators.NON_BLANK_VALIDATOR)
-                .build();
+    protected String lookupAttribute() {
+        return DATABASE_NAME_ATTRIBUTE;
     }
 
     @Override
-    protected Collection<ValidationResult> customValidate(ValidationContext context) {
-        final List<ValidationResult> results = new ArrayList<>();
-
-        int numDefinedServices = 0;
-        for (final PropertyDescriptor descriptor : context.getProperties().keySet()) {
-            if (descriptor.isDynamic()) {
-                numDefinedServices++;
-            }
-
-            final String referencedId = context.getProperty(descriptor).getValue();
-            if (this.getIdentifier().equals(referencedId)) {
-                results.add(new ValidationResult.Builder()
-                        .subject(descriptor.getDisplayName())
-                        .explanation("the current service cannot be registered as a DBCPService to lookup")
-                        .valid(false)
-                        .build());
-            }
-        }
-
-        if (numDefinedServices == 0) {
-            results.add(new ValidationResult.Builder()
-                    .subject(this.getClass().getSimpleName())
-                    .explanation("at least one DBCPService must be defined via dynamic properties")
-                    .valid(false)
-                    .build());
-        }
-
-        return results;
-    }
-
-    @OnEnabled
-    public void onEnabled(final ConfigurationContext context) {
-        final Map<String,DBCPService> serviceMap = new HashMap<>();
-
-        for (final PropertyDescriptor descriptor : context.getProperties().keySet()) {
-            if (descriptor.isDynamic()) {
-                final DBCPService dbcpService = context.getProperty(descriptor).asControllerService(DBCPService.class);
-                serviceMap.put(descriptor.getName(), dbcpService);
-            }
-        }
-
-        dbcpServiceMap = Collections.unmodifiableMap(serviceMap);
-    }
-
-    @OnDisabled
-    public void onDisabled() {
-        dbcpServiceMap = null;
+    public Class<DBCPService> getServiceType() {
+        return DBCPService.class;
     }
 
     @Override
@@ -119,22 +56,7 @@ public class DBCPConnectionPoolLookup extends AbstractControllerService implemen
     }
 
     @Override
-    public Connection getConnection(Map<String, String> attributes) throws ProcessException {
-        if (!attributes.containsKey(DATABASE_NAME_ATTRIBUTE)) {
-            throw new ProcessException("Attributes must contain an attribute name '" + DATABASE_NAME_ATTRIBUTE + "'");
-        }
-
-        final String databaseName = attributes.get(DATABASE_NAME_ATTRIBUTE);
-        if (StringUtils.isBlank(databaseName)) {
-            throw new ProcessException(DATABASE_NAME_ATTRIBUTE + " cannot be null or blank");
-        }
-
-        final DBCPService dbcpService = dbcpServiceMap.get(databaseName);
-        if (dbcpService == null) {
-            throw new ProcessException("No DBCPService was found for database.name '" + databaseName + "'");
-        }
-
-        return dbcpService.getConnection(attributes);
+    public Connection getConnection(Map<String, String> attributes) {
+        return lookupService(attributes).getConnection(attributes);
     }
-
 }
