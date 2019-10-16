7c9d34f820ea063349f7405c31d5587a040eb779
Yolanda M. Davis <yolanda.m.davis@gmail.com>
2019-10-16 10:49:37 -0400

    NIFI-6778 - Added rules engine service api and Easy Rules Implementation
    NIFI-6778 - added comments and component description annotations
    
    NIFI-6778 - added license and notice details
    
    NIFI-6778 - added additional details section and updated description of service
    
    NIFI-6778 - fixed checkstyle error
    
    NIFI-6778 - addressed review comments for documentation and description corrections
    
    NIFI-6778 - added include-rules profile for assembly build
    
    NIFI-6778 - add MVEL and ASM to license and notice
    
    NIFI-6778 - switch to use Jackson for NiFi Rules Json deserialization
    
    Signed-off-by: Matthew Burgess <mattyb149@apache.org>
    
    This closes #3824

---
+++ nifi-nar-bundles/nifi-easyrules-bundle/nifi-easyrules-service/src/test/java/org/apache/nifi/rules/TestRulesFactory.java
@@ -0,0 +1,136 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.rules;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.fail;
+
+public class TestRulesFactory {
+    @Test
+    public void testCreateRulesFromNiFiYaml(){
+        try {
+            String testYamlFile = "src/test/resources/test_nifi_rules.yml";
+            List<Rule> rules = RulesFactory.createRules(testYamlFile,"YAML", "NIFI");
+            assertEquals(2, rules.size());
+            assert confirmEntries(rules);
+        }catch (Exception ex){
+            fail("Unexpected exception occurred: "+ex.getMessage());
+        }
+    }
+
+    @Test
+    public void testCreateRulesFromMvelYaml(){
+        try {
+            String testYamlFile = "src/test/resources/test_mvel_rules.yml";
+            List<Rule> rules = RulesFactory.createRules(testYamlFile,"YAML", "MVEL");
+            assertEquals(2, rules.size());
+            assert confirmEntries(rules);
+            assertSame("EXPRESSION", rules.get(0).getActions().get(0).getType());
+        }catch (Exception ex){
+            fail("Unexpected exception occurred: "+ex.getMessage());
+        }
+    }
+
+    @Test
+    public void testCreateRulesFromSpelYaml(){
+        try {
+            String testYamlFile = "src/test/resources/test_spel_rules.yml";
+            List<Rule> rules = RulesFactory.createRules(testYamlFile,"YAML", "SPEL");
+            assertEquals(2, rules.size());
+            assertSame("EXPRESSION", rules.get(0).getActions().get(0).getType());
+        }catch (Exception ex){
+            fail("Unexpected exception occurred: "+ex.getMessage());
+        }
+    }
+
+    @Test
+    public void testCreateRulesFromNiFiJson(){
+        try {
+            String testJsonFile = "src/test/resources/test_nifi_rules.json";
+            List<Rule> rules = RulesFactory.createRules(testJsonFile,"JSON", "NIFI");
+            assertEquals(2, rules.size());
+            assert confirmEntries(rules);
+        }catch (Exception ex){
+            fail("Unexpected exception occurred: "+ex.getMessage());
+        }
+    }
+
+    @Test
+    public void testCreateRulesFromMvelJson(){
+        try {
+            String testJsonFile = "src/test/resources/test_mvel_rules.json";
+            List<Rule> rules = RulesFactory.createRules(testJsonFile,"JSON", "MVEL");
+            assertEquals(2, rules.size());
+            assertSame("EXPRESSION", rules.get(0).getActions().get(0).getType());
+            assert confirmEntries(rules);
+        }catch (Exception ex){
+            fail("Unexpected exception occurred: "+ex.getMessage());
+        }
+    }
+
+    @Test
+    public void testCreateRulesFromSpelJson(){
+        try {
+            String testJsonFile = "src/test/resources/test_spel_rules.json";
+            List<Rule> rules = RulesFactory.createRules(testJsonFile,"JSON", "SPEL");
+            assertEquals(2, rules.size());
+            assertSame("EXPRESSION", rules.get(0).getActions().get(0).getType());
+        }catch (Exception ex){
+            fail("Unexpected exception occurred: "+ex.getMessage());
+        }
+    }
+
+    @Test
+    public void testFakeTypeNotSupported(){
+        try {
+            RulesFactory.createRules("FAKEFILE", "FAKE", "NIFI");
+        }catch (Exception ex){
+            return;
+        }
+        fail("Exception should have been thrown for unexpected type");
+    }
+
+    @Test
+    public void testFakeFormatNotSupported(){
+        try {
+            RulesFactory.createRules("FAKEFILE", "JSON", "FAKE");
+        }catch (Exception ex){
+            return;
+        }
+        fail("Exception should have been thrown for unexpected type");
+    }
+
+
+    private boolean confirmEntries(List<Rule> rules){
+        Rule rule1= rules.get(0);
+        Rule rule2 = rules.get(1);
+
+        boolean checkDiagnostic = rule1.getName().equals("Queue Size") && rule1.getDescription().equals("Queue size check greater than 50")
+                && rule1.getPriority() == 1 && rule1.getCondition().equals("predictedQueuedCount > 50");
+
+        checkDiagnostic = rule2.getName().equals("Time To Back Pressure") && rule2.getDescription().equals("Back pressure time less than 5 minutes")
+                && rule2.getPriority() == 2 && rule2.getCondition().equals("predictedTimeToBytesBackpressureMillis >= 300000") && checkDiagnostic;
+
+        return checkDiagnostic;
+
+    }
+}
