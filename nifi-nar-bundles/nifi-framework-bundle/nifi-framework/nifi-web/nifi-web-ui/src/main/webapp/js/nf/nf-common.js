e05d11c0b4fef2935c2ae0c3265853bbe63ab2b0
Mark Payne <markap14@hotmail.com>
2019-12-13 15:09:15 -0500

    NIFI-6787: Added a comment to the UI tooltip, user guide, and javadocs to indicate that round robin may skip a node if that node is not receiving the data as fast as other nodes in the cluster, in order to maximum throughput.

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-ui/src/main/webapp/js/nf/nf-common.js
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-ui/src/main/webapp/js/nf/nf-common.js
@@ -1233,7 +1233,8 @@
             }, {
                 text: 'Round robin',
                 value: 'ROUND_ROBIN',
-                description: 'FlowFiles will be distributed to nodes in the cluster in a Round-Robin fashion.'
+                description: 'FlowFiles will be distributed to nodes in the cluster in a Round-Robin fashion. However, if a node in the cluster is not able to receive data as fast as other nodes,'
+                                + ' that node may be skipped in one or more iterations in order to maximize throughput of data distribution across the cluster.'
             }, {
                 text: 'Single node',
                 value: 'SINGLE_NODE',
