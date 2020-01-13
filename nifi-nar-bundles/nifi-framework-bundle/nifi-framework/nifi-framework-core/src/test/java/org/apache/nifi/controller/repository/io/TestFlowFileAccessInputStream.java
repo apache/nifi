76ecb3ee3098ec6264195e17da0df2600ac5aa1a
mdayakar <daya.apache@gmail.com>
2020-01-13 13:57:21 +0530

    NIFI-6923:If FlowFile content is truncated in the Content Repository, NiFi does not throw Exception when reading the content.

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/test/java/org/apache/nifi/controller/repository/io/TestFlowFileAccessInputStream.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/test/java/org/apache/nifi/controller/repository/io/TestFlowFileAccessInputStream.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.nifi.controller.repository.io;
 
 import static org.junit.Assert.assertTrue;
@@ -28,7 +44,11 @@ public class TestFlowFileAccessInputStream {
         in = mock(InputStream.class);
         flowFile = mock(FlowFile.class);
         claim = mock(ContentClaim.class);
+    }
 
+    @Test
+    public void testThrowExceptionWhenLessContentReadFromFlowFile() throws Exception {
+        // Read only 16 bytes
         Mockito.when(in.read(data, 0, 16)).thenAnswer(new Answer<Integer>() {
             private int count = 0;
 
@@ -41,18 +61,15 @@ public class TestFlowFileAccessInputStream {
                 return -1;
             }
         });
+        // Flow file size is 32
         Mockito.when(flowFile.getSize()).thenReturn(32l);
-    }
-
-    @Test
-    public void testThrowExceptionWhenLessContentReadFromFlowFile() throws Exception {
         FlowFileAccessInputStream flowFileAccessInputStream = new FlowFileAccessInputStream(in, flowFile, claim);
         try {
             while (flowFileAccessInputStream.read(data) != -1) {
             }
             fail("Should throw ContentNotFoundException when lesser bytes read from flow file.");
         } catch (ContentNotFoundException e) {
-            assertTrue(e.getMessage().contains("Stream contained only 16 bytes but should have contained 32"));
+            assertTrue("ContentNotFoundException message not matched.", e.getMessage().contains("Stream contained only 16 bytes but should have contained 32"));
         } finally {
             flowFileAccessInputStream.close();
         }
