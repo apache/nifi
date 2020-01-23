fc89f9861d7e7988737ddad9d1646f67391402d3
mdayakar <daya.apache@gmail.com>
2020-01-23 23:45:21 +0530

    NIFI-7049 : SFTP processors shouldn't silently try to access known hosts file of the user
    Signed-off-by: Arpad Boda <aboda@apache.org>
    
    This closes #4014

--- nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/util/SFTPTransfer.java
+++ nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/util/SFTPTransfer.java
@@ -94,7 +94,10 @@ public class SFTPTransfer implements FileTransfer {
         .build();
     public static final PropertyDescriptor HOST_KEY_FILE = new PropertyDescriptor.Builder()
         .name("Host Key File")
-        .description("If supplied, the given file will be used as the Host Key; otherwise, no use host key file will be used")
+        .description("If supplied, the given file will be used as the Host Key;" +
+                " otherwise, if 'Strict Host Key Checking' property is applied (set to true)" +
+                " then uses the 'known_hosts' and 'known_hosts2' files from ~/.ssh directory" +
+                " else no host key file will be used")
         .addValidator(StandardValidators.FILE_EXISTS_VALIDATOR)
         .required(false)
         .build();
@@ -548,18 +551,19 @@ public class SFTPTransfer implements FileTransfer {
             });
         }
 
-        // Load known hosts file if specified, otherwise load default
-        final String hostKeyVal = ctx.getProperty(HOST_KEY_FILE).getValue();
-        if (hostKeyVal != null) {
-            sshClient.loadKnownHosts(new File(hostKeyVal));
-        } else {
-            sshClient.loadKnownHosts();
-        }
-
         // If strict host key checking is false, add a HostKeyVerifier that always returns true
         final boolean strictHostKeyChecking = ctx.getProperty(STRICT_HOST_KEY_CHECKING).asBoolean();
         if (!strictHostKeyChecking) {
             sshClient.addHostKeyVerifier(new PromiscuousVerifier());
+        }
+
+        // Load known hosts file if specified, otherwise load default
+        final String hostKeyVal = ctx.getProperty(HOST_KEY_FILE).getValue();
+        if (hostKeyVal != null) {
+            sshClient.loadKnownHosts(new File(hostKeyVal));
+            // Load default known_hosts file only when 'Strict Host Key Checking' property is enabled
+        } else if (strictHostKeyChecking) {
+            sshClient.loadKnownHosts();
         }
 
         // Enable compression on the client if specified in properties
