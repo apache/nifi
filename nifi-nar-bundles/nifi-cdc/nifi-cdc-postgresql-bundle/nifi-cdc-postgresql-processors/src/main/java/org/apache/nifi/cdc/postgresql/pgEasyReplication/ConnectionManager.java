1e36c370fd235f0e5ecdf00db68fd30c6b2849da
gerdan.santos <gerdan@gmail.com>
2020-02-20 02:13:46 -0300

    NIFI-4239 - Adding CaptureChangePostgreSQL processor to capture data changes (INSERT/UPDATE/DELETE) in PostgreSQL tables via Logical Replication.
    Applying correction of run mvn clean install -Pcontrib-check
    
    Signed-off-by: Davy Machado <machado.davy@gmail.com>
    
    Signed-off-by: Gerdan Santos <gerdan@gmail.com>

--- nifi-nar-bundles/nifi-cdc/nifi-cdc-postgresql-bundle/nifi-cdc-postgresql-processors/src/main/java/org/apache/nifi/cdc/postgresql/pgEasyReplication/ConnectionManager.java
+++ nifi-nar-bundles/nifi-cdc/nifi-cdc-postgresql-bundle/nifi-cdc-postgresql-processors/src/main/java/org/apache/nifi/cdc/postgresql/pgEasyReplication/ConnectionManager.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.nifi.cdc.postgresql.pgEasyReplication;
 
 import java.sql.Connection;
@@ -20,7 +37,7 @@ public class ConnectionManager {
         ConnectionManager.user = user;
         ConnectionManager.driverName = driverName;
 
-        if(password == null) {
+        if (password == null) {
             password = "";
         }
 
@@ -49,11 +66,11 @@ public class ConnectionManager {
         return ConnectionManager.repConnection;
     }
 
-    public static void closeReplicationConnection() throws Exception{
+    public static void closeReplicationConnection() throws Exception {
         ConnectionManager.repConnection.close();
     }
 
-    public static void createSQLConnection() throws Exception{
+    public static void createSQLConnection() throws Exception {
 
         String url = "jdbc:postgresql://" + ConnectionManager.server + "/" + ConnectionManager.database;
 
@@ -62,7 +79,7 @@ public class ConnectionManager {
         props.put("password", ConnectionManager.password);
 
         Connection conn = null;
-        Class.forName(ConnectionManager.driverName);			
+        Class.forName(ConnectionManager.driverName);
         conn = DriverManager.getConnection(url, props);
         conn.setAutoCommit(true);
         ConnectionManager.sqlConnection = conn;
@@ -72,10 +89,8 @@ public class ConnectionManager {
         return ConnectionManager.sqlConnection;
     }
 
-    public static void closeSQLConnection() throws Exception{
+    public static void closeSQLConnection() throws Exception {
         ConnectionManager.sqlConnection.close();
     }
-
-
 
 }
--- nifi-nar-bundles/nifi-cdc/nifi-cdc-postgresql-bundle/nifi-cdc-postgresql-processors/src/main/java/org/apache/nifi/cdc/postgresql/pgEasyReplication/PGEasyReplication.java
+++ nifi-nar-bundles/nifi-cdc/nifi-cdc-postgresql-bundle/nifi-cdc-postgresql-processors/src/main/java/org/apache/nifi/cdc/postgresql/pgEasyReplication/PGEasyReplication.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.nifi.cdc.postgresql.pgEasyReplication;
 
 import java.io.IOException;
@@ -26,13 +43,12 @@ public class PGEasyReplication {
 
     public void initializeLogicalReplication() {
         try {
-            PreparedStatement stmt = ConnectionManager.getSQLConnection()
-                    .prepareStatement("select 1 from pg_catalog.pg_replication_slots WHERE slot_name = ?");
+            PreparedStatement stmt = ConnectionManager.getSQLConnection().prepareStatement("select 1 from pg_catalog.pg_replication_slots WHERE slot_name = ?");
 
             stmt.setString(1, this.slot);
             ResultSet rs = stmt.executeQuery();
 
-            if(rs.next()) {	// If slot exists
+            if (rs.next()) { // If slot exists
                 if (this.slotDropIfExists) {
                     this.dropReplicationSlot();
                     this.createReplicationSlot();
@@ -46,23 +62,20 @@ public class PGEasyReplication {
         }
     }
 
-    public void createReplicationSlot() {						
+    public void createReplicationSlot() {
         try {
             PGConnection pgcon = ConnectionManager.getReplicationConnection().unwrap(PGConnection.class);
 
-            pgcon.getReplicationAPI()
-            .createReplicationSlot()
-            .logical()
-            .withSlotName(this.slot)
-            .withOutputPlugin("pgoutput") // More details about pgoutput options: https://github.com/postgres/postgres/blob/master/src/backend/replication/pgoutput/pgoutput.c
-            .make();
+            pgcon.getReplicationAPI().createReplicationSlot().logical().withSlotName(this.slot).withOutputPlugin("pgoutput") // More details about pgoutput options:
+                                                                                                                             // https://github.com/postgres/postgres/blob/master/src/backend/replication/pgoutput/pgoutput.c
+                    .make();
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void dropReplicationSlot() {	
+    public void dropReplicationSlot() {
         try {
             PGConnection pgcon = ConnectionManager.getReplicationConnection().unwrap(PGConnection.class);
             pgcon.getReplicationAPI().dropReplicationSlot(this.slot);
@@ -102,8 +115,8 @@ public class PGEasyReplication {
     public Event readEvent(boolean isSimpleEvent, boolean withBeginCommit, Long startLSN) {
         Event event = null;
 
-        try {			
-            if(this.stream == null)	{	// First read
+        try {
+            if (this.stream == null) { // First read
                 this.stream = new Stream(this.publication, this.slot, startLSN);
             }
 
