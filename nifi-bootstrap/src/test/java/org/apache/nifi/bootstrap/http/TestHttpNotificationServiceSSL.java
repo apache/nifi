d07f70478d02c7a24969aca6a29183bb76647b9c
Nathan Gough <thenatog@gmail.com>
2020-03-10 21:22:29 +1100

    NIFI-7223 - Fixed a minor issue where the OkHttpReplicationClient class loaded blank properties as empty string instead of an expected null value. Added a isNotBlank check. Added unit tests for replication client and HTTPNotificationService.

--- nifi-bootstrap/src/test/java/org/apache/nifi/bootstrap/http/TestHttpNotificationServiceSSL.java
+++ nifi-bootstrap/src/test/java/org/apache/nifi/bootstrap/http/TestHttpNotificationServiceSSL.java
@@ -16,13 +16,23 @@
  */
 package org.apache.nifi.bootstrap.http;
 
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import okhttp3.mockwebserver.MockWebServer;
+import org.apache.nifi.bootstrap.NotificationServiceManager;
 import org.apache.nifi.security.util.SslContextFactory;
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.mockito.internal.util.io.IOUtil;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
+import ch.qos.logback.classic.Logger;
 
 import javax.net.ssl.SSLContext;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -32,11 +42,50 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
+import java.util.List;
 
-public class TestHttpNotificationServiceSSL extends  TestHttpNotificationServiceCommon{
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TestHttpNotificationServiceSSL extends TestHttpNotificationServiceCommon {
+
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(TestHttpNotificationServiceSSL.class);
 
 
     static final String CONFIGURATION_FILE_TEXT = "\n"+
+            "<services>\n"+
+            "         <service>\n"+
+            "            <id>http-notification</id>\n"+
+            "            <class>org.apache.nifi.bootstrap.notification.http.HttpNotificationService</class>\n"+
+            "            <property name=\"URL\">${test.server}</property>\n"+
+            "            <property name=\"Truststore Filename\">./src/test/resources/truststore.jks</property>\n"+
+            "            <property name=\"Truststore Type\">JKS</property>\n"+
+            "            <property name=\"Truststore Password\">passwordpassword</property>\n"+
+            "            <property name=\"Keystore Filename\">./src/test/resources/keystore.jks</property>\n"+
+            "            <property name=\"Keystore Type\">JKS</property>\n"+
+            "            <property name=\"Key Password\">passwordpassword</property>\n"+
+            "            <property name=\"Keystore Password\">passwordpassword</property>\n"+
+            "            <property name=\"testProp\">${literal('testing')}</property>\n"+
+            "         </service>\n"+
+            "</services>";
+
+    static final String CONFIGURATION_FILE_TEXT_NO_KEYSTORE_PASSWORD = "\n"+
+            "<services>\n"+
+            "         <service>\n"+
+            "            <id>http-notification</id>\n"+
+            "            <class>org.apache.nifi.bootstrap.notification.http.HttpNotificationService</class>\n"+
+            "            <property name=\"URL\">${test.server}</property>\n"+
+            "            <property name=\"Truststore Filename\">./src/test/resources/truststore.jks</property>\n"+
+            "            <property name=\"Truststore Type\">JKS</property>\n"+
+            "            <property name=\"Truststore Password\">passwordpassword</property>\n"+
+            "            <property name=\"Keystore Filename\">./src/test/resources/keystore.jks</property>\n"+
+            "            <property name=\"Keystore Type\">JKS</property>\n"+
+            "            <property name=\"Key Password\">passwordpassword</property>\n"+
+            "            <property name=\"testProp\">${literal('testing')}</property>\n"+
+            "         </service>\n"+
+            "</services>";
+
+    static final String CONFIGURATION_FILE_TEXT_NO_KEY_PASSWORD = "\n"+
             "<services>\n"+
             "         <service>\n"+
             "            <id>http-notification</id>\n"+
@@ -52,9 +101,42 @@ public class TestHttpNotificationServiceSSL extends  TestHttpNotificationService
             "         </service>\n"+
             "</services>";
 
+    static final String CONFIGURATION_FILE_TEXT_BLANK_KEY_PASSWORD = "\n"+
+            "<services>\n"+
+            "         <service>\n"+
+            "            <id>http-notification</id>\n"+
+            "            <class>org.apache.nifi.bootstrap.notification.http.HttpNotificationService</class>\n"+
+            "            <property name=\"URL\">${test.server}</property>\n"+
+            "            <property name=\"Truststore Filename\">./src/test/resources/truststore.jks</property>\n"+
+            "            <property name=\"Truststore Type\">JKS</property>\n"+
+            "            <property name=\"Truststore Password\">passwordpassword</property>\n"+
+            "            <property name=\"Keystore Filename\">./src/test/resources/keystore.jks</property>\n"+
+            "            <property name=\"Keystore Type\">JKS</property>\n"+
+            "            <property name=\"Keystore Password\">passwordpassword</property>\n"+
+            "            <property name=\"Key Password\"></property>\n"+
+            "            <property name=\"testProp\">${literal('testing')}</property>\n"+
+            "         </service>\n"+
+            "</services>";
 
-    @BeforeClass
-    public static void startServer() throws IOException, UnrecoverableKeyException, CertificateException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+    static final String CONFIGURATION_FILE_TEXT_BLANK_KEYSTORE_PASSWORD = "\n"+
+            "<services>\n"+
+            "         <service>\n"+
+            "            <id>http-notification</id>\n"+
+            "            <class>org.apache.nifi.bootstrap.notification.http.HttpNotificationService</class>\n"+
+            "            <property name=\"URL\">${test.server}</property>\n"+
+            "            <property name=\"Truststore Filename\">./src/test/resources/truststore.jks</property>\n"+
+            "            <property name=\"Truststore Type\">JKS</property>\n"+
+            "            <property name=\"Truststore Password\">passwordpassword</property>\n"+
+            "            <property name=\"Keystore Filename\">./src/test/resources/keystore.jks</property>\n"+
+            "            <property name=\"Keystore Type\">JKS</property>\n"+
+            "            <property name=\"Keystore Password\"></property>\n"+
+            "            <property name=\"Key Password\">passwordpassword</property>\n"+
+            "            <property name=\"testProp\">${literal('testing')}</property>\n"+
+            "         </service>\n"+
+            "</services>";
+
+    @Before
+    public void startServer() throws IOException, UnrecoverableKeyException, CertificateException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
         tempConfigFilePath = "./target/TestHttpNotificationService-config.xml";
 
         Files.deleteIfExists(Paths.get(tempConfigFilePath));
@@ -78,10 +160,109 @@ public class TestHttpNotificationServiceSSL extends  TestHttpNotificationService
         IOUtil.writeText(configFileOutput, new File(tempConfigFilePath));
     }
 
-    @AfterClass
-    public static void shutdownServer() throws IOException {
+    @After
+    public void shutdownServer() throws IOException {
         Files.deleteIfExists(Paths.get(tempConfigFilePath));
         mockWebServer.shutdown();
     }
 
+    @Test
+    public void testStartNotificationSucceedsNoKeystorePasswd() throws ParserConfigurationException, SAXException, IOException {
+        Logger fooLogger = (Logger) LoggerFactory.getLogger(NotificationServiceManager.class);
+        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+        listAppender.start();
+        fooLogger.addAppender(listAppender);
+
+        String configFileOutput = CONFIGURATION_FILE_TEXT_NO_KEYSTORE_PASSWORD.replace("${test.server}", String.valueOf(mockWebServer.url("/")));
+        IOUtil.writeText(configFileOutput, new File(tempConfigFilePath));
+
+        NotificationServiceManager notificationServiceManager = new NotificationServiceManager();
+        notificationServiceManager.setMaxNotificationAttempts(1);
+        notificationServiceManager.loadNotificationServices(new File(tempConfigFilePath));
+
+        List<ILoggingEvent> logsList = listAppender.list;
+        boolean notificationServiceFailed = false;
+        for(ILoggingEvent logMessage : logsList) {
+            if(logMessage.getFormattedMessage().contains("is not valid for the following reasons")) {
+                    notificationServiceFailed = true;
+            }
+        }
+
+        assertFalse(notificationServiceFailed);
+    }
+
+    @Test
+    public void testStartNotificationSucceedsNoKeyPasswd() throws ParserConfigurationException, SAXException, IOException {
+        Logger fooLogger = (Logger) LoggerFactory.getLogger(NotificationServiceManager.class);
+        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+        listAppender.start();
+        fooLogger.addAppender(listAppender);
+
+        String configFileOutput = CONFIGURATION_FILE_TEXT_NO_KEY_PASSWORD.replace("${test.server}", String.valueOf(mockWebServer.url("/")));
+        IOUtil.writeText(configFileOutput, new File(tempConfigFilePath));
+
+        NotificationServiceManager notificationServiceManager = new NotificationServiceManager();
+        notificationServiceManager.setMaxNotificationAttempts(1);
+        notificationServiceManager.loadNotificationServices(new File(tempConfigFilePath));
+
+        List<ILoggingEvent> logsList = listAppender.list;
+        boolean notificationServiceFailed = false;
+        for(ILoggingEvent logMessage : logsList) {
+            if(logMessage.getFormattedMessage().contains("is not valid for the following reasons")) {
+                notificationServiceFailed = true;
+            }
+        }
+
+        assertFalse(notificationServiceFailed);
+    }
+
+    @Test
+    public void testStartNotificationFailsBlankKeystorePasswdCorrectKeypasswd() throws ParserConfigurationException, SAXException, IOException {
+        Logger fooLogger = (Logger) LoggerFactory.getLogger(NotificationServiceManager.class);
+        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+        listAppender.start();
+        fooLogger.addAppender(listAppender);
+
+        String configFileOutput = CONFIGURATION_FILE_TEXT_BLANK_KEYSTORE_PASSWORD.replace("${test.server}", String.valueOf(mockWebServer.url("/")));
+        IOUtil.writeText(configFileOutput, new File(tempConfigFilePath));
+
+        NotificationServiceManager notificationServiceManager = new NotificationServiceManager();
+        notificationServiceManager.setMaxNotificationAttempts(1);
+        notificationServiceManager.loadNotificationServices(new File(tempConfigFilePath));
+
+        List<ILoggingEvent> logsList = listAppender.list;
+        boolean notificationServiceFailed = false;
+        for(ILoggingEvent logMessage : logsList) {
+            if(logMessage.getFormattedMessage().contains("'Keystore Password' validated against '' is invalid because Keystore Password cannot be empty")) {
+                notificationServiceFailed = true;
+            }
+        }
+
+        assertTrue(notificationServiceFailed);
+    }
+
+    @Test
+    public void testStartNotificationFailsCorrectKeystorePasswdBlankKeypasswd() throws ParserConfigurationException, SAXException, IOException {
+        Logger fooLogger = (Logger) LoggerFactory.getLogger(NotificationServiceManager.class);
+        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+        listAppender.start();
+        fooLogger.addAppender(listAppender);
+
+        String configFileOutput = CONFIGURATION_FILE_TEXT_BLANK_KEY_PASSWORD.replace("${test.server}", String.valueOf(mockWebServer.url("/")));
+        IOUtil.writeText(configFileOutput, new File(tempConfigFilePath));
+
+        NotificationServiceManager notificationServiceManager = new NotificationServiceManager();
+        notificationServiceManager.setMaxNotificationAttempts(1);
+        notificationServiceManager.loadNotificationServices(new File(tempConfigFilePath));
+
+        List<ILoggingEvent> logsList = listAppender.list;
+        boolean notificationServiceFailed = false;
+        for(ILoggingEvent logMessage : logsList) {
+            if(logMessage.getFormattedMessage().contains("'Key Password' validated against '' is invalid because Key Password cannot be empty")) {
+                notificationServiceFailed = true;
+            }
+        }
+
+        assertTrue(notificationServiceFailed);
+    }
 }
