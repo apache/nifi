e64815d68e537a88db6ac3db6ef9abfdc1838583
Nathan Gough <thenatog@gmail.com>
2020-03-14 04:19:39 +1100

    NIFI-7223 - Added another test for when keyPasswd is not present.

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-cluster/src/test/groovy/org/apache/nifi/cluster/coordination/http/replication/okhttp/OkHttpReplicationClientTest.groovy
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-cluster/src/test/groovy/org/apache/nifi/cluster/coordination/http/replication/okhttp/OkHttpReplicationClientTest.groovy
@@ -164,6 +164,29 @@ class OkHttpReplicationClientTest extends GroovyTestCase {
     }
 
     @Test
+    void testShouldUseKeystorePasswdIfKeyPasswdIsNull() {
+        // Arrange
+        Map flowfileEncryptionProps = [
+                (NiFiProperties.SECURITY_TRUSTSTORE): "./src/test/resources/conf/truststore.jks",
+                (NiFiProperties.SECURITY_TRUSTSTORE_TYPE): "JKS",
+                (NiFiProperties.SECURITY_TRUSTSTORE_PASSWD): "passwordpassword",
+                (NiFiProperties.SECURITY_KEYSTORE): "./src/test/resources/conf/keystore.jks",
+                (NiFiProperties.SECURITY_KEYSTORE_TYPE): "JKS",
+                (NiFiProperties.SECURITY_KEYSTORE_PASSWD): "passwordpassword",
+                (NiFiProperties.WEB_HTTPS_HOST): "localhost",
+                (NiFiProperties.WEB_HTTPS_PORT): "51552",
+        ]
+        NiFiProperties mockNiFiProperties = new StandardNiFiProperties(new Properties(flowfileEncryptionProps))
+
+        // Act
+        OkHttpReplicationClient client = new OkHttpReplicationClient(mockNiFiProperties)
+
+        // Assert
+        assertNotNull(client.okHttpClient.sslSocketFactory)
+        assertEquals(SunX509KeyManagerImpl.class, client.okHttpClient.sslSocketFactory.context.getX509KeyManager().getClass())
+        assertNotNull(client.okHttpClient.sslSocketFactory.context.getX509KeyManager().credentialsMap["nifi-key"])    }
+
+    @Test
     void testShouldFailIfKeyPasswdIsSetButKeystorePasswdIsBlank() {
         // Arrange
         Map flowfileEncryptionProps = [
@@ -208,6 +231,8 @@ class OkHttpReplicationClientTest extends GroovyTestCase {
         OkHttpReplicationClient client = new OkHttpReplicationClient(mockNiFiProperties)
 
         // Assert
+        // The replication client will fail to initialize if the keystore password is missing, and will use
+        // a default empty DummyX509KeyManager instead. This is considered a failure to start the service.
         assertEquals(DummyX509KeyManager.class, client.okHttpClient.sslSocketFactory.context.getX509KeyManager().getClass())
     }
 }
