564f580245db2f10d5152fb824a8e479e5b1b35d
MuazmaZ <38145027+MuazmaZ@users.noreply.github.com>
2020-03-19 16:20:09 -0700

    Fixed indentation and logging

--- nifi-nar-bundles/nifi-azure-bundle/nifi-azure-processors/src/main/java/org/apache/nifi/processors/azure/storage/PutAzureDataLakeStorage.java
+++ nifi-nar-bundles/nifi-azure-bundle/nifi-azure-processors/src/main/java/org/apache/nifi/processors/azure/storage/PutAzureDataLakeStorage.java
@@ -46,10 +46,10 @@ import com.azure.storage.file.datalake.DataLakeServiceClientBuilder;
 import org.apache.nifi.processors.azure.AbstractAzureDataLakeStorageProcessor;
 import com.azure.storage.file.datalake.implementation.models.StorageErrorException;
 
-@Tags({ "azure", "microsoft", "cloud", "storage", "adlsgen2", "datalake" })
+@Tags({"azure", "microsoft", "cloud", "storage", "adlsgen2", "datalake"})
 @CapabilityDescription("Puts content into an Azure Data Lake Storage Gen 2")
 @SeeAlso({})
-@WritesAttributes({ @WritesAttribute(attribute = "azure.filesystem", description = "The name of the Azure File System"),
+@WritesAttributes({@WritesAttribute(attribute = "azure.filesystem", description = "The name of the Azure File System"),
         @WritesAttribute(attribute = "azure.filename", description = "The name of the Azure File Name"),
         @WritesAttribute(attribute = "azure.primaryUri", description = "Primary location for file content"),
         @WritesAttribute(attribute = "azure.length", description = "Length of the file")})
@@ -57,65 +57,65 @@ import com.azure.storage.file.datalake.implementation.models.StorageErrorExcepti
 
 public class PutAzureDataLakeStorage extends AbstractAzureDataLakeStorageProcessor {
 
- @Override
+    @Override
     public void onTrigger(final ProcessContext context, final ProcessSession session) throws ProcessException {
         FlowFile flowFile = session.get();
         if (flowFile == null) {
             return;
         }
         final long startNanos = System.nanoTime();
-    try {
-        final String fileSystem = context.getProperty(FILESYSTEM).evaluateAttributeExpressions(flowFile).getValue();
-        final String directory = context.getProperty(DIRECTORY).evaluateAttributeExpressions(flowFile).getValue();
-        final String fileName = context.getProperty(FILE).evaluateAttributeExpressions(flowFile).getValue();
-        final String accountName = context.getProperty(ACCOUNT_NAME).evaluateAttributeExpressions(flowFile).getValue();
-        final String accountKey = context.getProperty(ACCOUNT_KEY).evaluateAttributeExpressions(flowFile).getValue();
-        final String sasToken = context.getProperty(PROP_SAS_TOKEN).evaluateAttributeExpressions(flowFile).getValue();
-        final String endpoint = String.format(Locale.ROOT, "https://%s.dfs.core.windows.net", accountName);
-        DataLakeServiceClient storageClient;
-                if (StringUtils.isNotBlank(accountKey)) {
-                    final StorageSharedKeyCredential credential = new StorageSharedKeyCredential(accountName,
+        try {
+            final String fileSystem = context.getProperty(FILESYSTEM).evaluateAttributeExpressions(flowFile).getValue();
+            final String directory = context.getProperty(DIRECTORY).evaluateAttributeExpressions(flowFile).getValue();
+            final String fileName = context.getProperty(FILE).evaluateAttributeExpressions(flowFile).getValue();
+            final String accountName = context.getProperty(ACCOUNT_NAME).evaluateAttributeExpressions(flowFile).getValue();
+            final String accountKey = context.getProperty(ACCOUNT_KEY).evaluateAttributeExpressions(flowFile).getValue();
+            final String sasToken = context.getProperty(PROP_SAS_TOKEN).evaluateAttributeExpressions(flowFile).getValue();
+            final String endpoint = String.format(Locale.ROOT, "https://%s.dfs.core.windows.net", accountName);
+            DataLakeServiceClient storageClient;
+            if (StringUtils.isNotBlank(accountKey)) {
+                final StorageSharedKeyCredential credential = new StorageSharedKeyCredential(accountName,
                         accountKey);
-                    storageClient = new DataLakeServiceClientBuilder().endpoint(endpoint).credential(credential)
+                storageClient = new DataLakeServiceClientBuilder().endpoint(endpoint).credential(credential)
                         .buildClient();
-                } else if (StringUtils.isNotBlank(sasToken)) {
-                    storageClient = new DataLakeServiceClientBuilder().endpoint(endpoint).sasToken(sasToken)
+            } else if (StringUtils.isNotBlank(sasToken)) {
+                storageClient = new DataLakeServiceClientBuilder().endpoint(endpoint).sasToken(sasToken)
                         .buildClient();
-                } else {
-                    throw new IllegalArgumentException(String.format("Either '%s' or '%s' must be defined.",
+            } else {
+                throw new IllegalArgumentException(String.format("Either '%s' or '%s' must be defined.",
                         ACCOUNT_KEY.getDisplayName(), PROP_SAS_TOKEN.getDisplayName()));
-                }
-        final DataLakeFileSystemClient dataLakeFileSystemClient = storageClient.getFileSystemClient(fileSystem);
-        final DataLakeDirectoryClient directoryClient = dataLakeFileSystemClient.getDirectoryClient(directory);
-
-        final long length = flowFile.getSize();
-        final Map<String, String> attributes = new HashMap<>();
-        final DataLakeFileClient fileClient = directoryClient.createFile(fileName);
-        if (length > 0) {
-            try (final InputStream rawIn = session.read(flowFile); final BufferedInputStream in = new BufferedInputStream(rawIn)) {
-                fileClient.append( in , 0, length);
-
-            } catch (final IOException|StorageErrorException e) {
-                getLogger().error("Failed to create file. Reasons: " + e.getMessage());
-                session.transfer(flowFile, REL_FAILURE);
             }
-        }
-        attributes.put("azure.filesystem", fileSystem);
-        attributes.put("azure.filename", fileName);
-        attributes.put("azure.primaryUri", fileClient.getFileUrl().toString());
-        attributes.put("azure.length", String.valueOf(length));
-        fileClient.flush(length);
+            final DataLakeFileSystemClient dataLakeFileSystemClient = storageClient.getFileSystemClient(fileSystem);
+            final DataLakeDirectoryClient directoryClient = dataLakeFileSystemClient.getDirectoryClient(directory);
 
-        if (!attributes.isEmpty()) {
-            flowFile = session.putAllAttributes(flowFile, attributes);
-          }
-        session.transfer(flowFile, REL_SUCCESS);
-        final long transferMillis = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNanos);
-        session.getProvenanceReporter().send(flowFile, fileClient.getFileUrl().toString(), transferMillis);
+            final long length = flowFile.getSize();
+            final Map<String, String> attributes = new HashMap<>();
+            final DataLakeFileClient fileClient = directoryClient.createFile(fileName);
+            if (length > 0) {
+                try (final InputStream rawIn = session.read(flowFile); final BufferedInputStream in = new BufferedInputStream(rawIn)) {
+                    fileClient.append(in, 0, length);
+
+                } catch (final IOException | StorageErrorException e) {
+                    getLogger().error("Failed to create file. Reasons: " + e.getMessage());
+                    session.transfer(flowFile, REL_FAILURE);
+                }
+            }
+            attributes.put("azure.filesystem", fileSystem);
+            attributes.put("azure.filename", fileName);
+            attributes.put("azure.primaryUri", fileClient.getFileUrl().toString());
+            attributes.put("azure.length", String.valueOf(length));
+            fileClient.flush(length);
+
+            if (!attributes.isEmpty()) {
+                flowFile = session.putAllAttributes(flowFile, attributes);
+            }
+            session.transfer(flowFile, REL_SUCCESS);
+            final long transferMillis = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNanos);
+            session.getProvenanceReporter().send(flowFile, fileClient.getFileUrl().toString(), transferMillis);
         } catch (IllegalArgumentException e) {
-        getLogger().error("Failed to create file. Reasons: " + e.getMessage());
-        flowFile = session.penalize(flowFile);
-        session.transfer(flowFile, REL_FAILURE);
+            getLogger().error("Failed to create {}, due to {}", new Object[]{flowFile, e}, e);
+            flowFile = session.penalize(flowFile);
+            session.transfer(flowFile, REL_FAILURE);
+        }
     }
-}
 }
