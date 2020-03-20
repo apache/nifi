7f32aa56db9156d197666821fc90a1edd2ba755d
Bryan Bende <bbende@apache.org>
2020-03-20 11:55:37 -0400

    NIFI-7271 Make command timeout configurable for ShellUserGroupProvider - Changing ShellRunner to use a separate thread for reading the output of the process - Removing unused member variable - Addressing review feedback
    This closes #4154.

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-shell-authorizer/src/main/java/org/apache/nifi/authorization/util/ShellRunner.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-shell-authorizer/src/main/java/org/apache/nifi/authorization/util/ShellRunner.java
@@ -23,9 +23,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 
 public class ShellRunner {
@@ -33,50 +35,93 @@ public class ShellRunner {
 
     static String SHELL = "sh";
     static String OPTS = "-c";
-    static Integer TIMEOUT = 60;
 
-    public static List<String> runShell(String command) throws IOException {
+    private final int timeoutSeconds;
+    private final ExecutorService executor;
+
+    public ShellRunner(final int timeoutSeconds) {
+        this.timeoutSeconds = timeoutSeconds;
+        this.executor = Executors.newFixedThreadPool(1, new ThreadFactory() {
+            @Override
+            public Thread newThread(final Runnable r) {
+                final Thread t = Executors.defaultThreadFactory().newThread(r);
+                t.setName("ShellRunner");
+                t.setDaemon(true);
+                return t;
+            }
+        });
+    }
+
+    public List<String> runShell(String command) throws IOException {
         return runShell(command, "<unknown>");
     }
 
-    public static List<String> runShell(String command, String description) throws IOException {
+    public List<String> runShell(String command, String description) throws IOException {
         final ProcessBuilder builder = new ProcessBuilder(SHELL, OPTS, command);
-        final List<String> builderCommand = builder.command();
+        builder.redirectErrorStream(true);
 
-        logger.debug("Run Command '" + description + "': " + builderCommand);
+        final List<String> builderCommand = builder.command();
+        logger.debug("Run Command '{}': {}", new Object[]{description, builderCommand});
+
         final Process proc = builder.start();
+
+        final List<String> lines = new ArrayList<>();
+        executor.submit(() -> {
+            try {
+                try (final Reader stdin = new InputStreamReader(proc.getInputStream());
+                     final BufferedReader reader = new BufferedReader(stdin)) {
+                    logger.trace("Reading process input stream...");
+
+                    String line;
+                    int lineCount = 0;
+                    while ((line = reader.readLine()) != null) {
+                        if (logger.isTraceEnabled()) {
+                            logger.trace((++lineCount) + " - " + line);
+                        }
+                        lines.add(line.trim());
+                    }
+
+                    logger.trace("Finished reading process input stream");
+                }
+            } catch (IOException e) {
+                logger.error(e.getMessage(), e);
+            }
+        });
 
         boolean completed;
         try {
-            completed = proc.waitFor(TIMEOUT, TimeUnit.SECONDS);
+            completed = proc.waitFor(timeoutSeconds, TimeUnit.SECONDS);
         } catch (InterruptedException irexc) {
             throw new IOException(irexc.getMessage(), irexc.getCause());
         }
 
         if (!completed) {
+            logger.debug("Process did not complete in allotted time, attempting to forcibly destroy process...");
+            try {
+                proc.destroyForcibly();
+            } catch (Exception e) {
+                logger.debug("Process failed to destroy: " + e.getMessage(), e);
+            }
             throw new IllegalStateException("Shell command '" + command + "' did not complete during the allotted time period");
         }
 
         if (proc.exitValue() != 0) {
-            try (final Reader stderr = new InputStreamReader(proc.getErrorStream());
-                 final BufferedReader reader = new BufferedReader(stderr)) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    logger.warn(line.trim());
-                }
-            }
-            throw new IOException("Command exit non-zero: " + proc.exitValue());
-        }
-
-        final List<String> lines = new ArrayList<>();
-        try (final Reader stdin = new InputStreamReader(proc.getInputStream());
-             final BufferedReader reader = new BufferedReader(stdin)) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                lines.add(line.trim());
-            }
+            throw new IOException("Process exited with non-zero value: " + proc.exitValue());
         }
 
         return lines;
+    }
+
+    public void shutdown() {
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(5000L, TimeUnit.MILLISECONDS)) {
+                logger.info("Failed to stop ShellRunner executor in 5 seconds. Terminating");
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException ie) {
+            executor.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
     }
 }
