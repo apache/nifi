66ae15a34b20094b3cfe6094b6018fdca539271e
Bryan Bende <bbende@apache.org>
2020-03-25 12:33:19 -0400

    NIFI-7281 Use BufferedInputStream in StandardSocketChannelRecordReader in order to support mark/reset

--- nifi-nar-bundles/nifi-extension-utils/nifi-record-utils/nifi-standard-record-utils/src/main/java/org/apache/nifi/record/listen/StandardSocketChannelRecordReader.java
+++ nifi-nar-bundles/nifi-extension-utils/nifi-record-utils/nifi-standard-record-utils/src/main/java/org/apache/nifi/record/listen/StandardSocketChannelRecordReader.java
@@ -22,6 +22,7 @@ import org.apache.nifi.serialization.MalformedRecordException;
 import org.apache.nifi.serialization.RecordReader;
 import org.apache.nifi.serialization.RecordReaderFactory;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
@@ -53,7 +54,8 @@ public class StandardSocketChannelRecordReader implements SocketChannelRecordRea
             throw new IllegalStateException("Cannot create RecordReader because already created");
         }
 
-        final InputStream in = socketChannel.socket().getInputStream();
+        final InputStream socketIn = socketChannel.socket().getInputStream();
+        final InputStream in = new BufferedInputStream(socketIn);
         recordReader = readerFactory.createRecordReader(Collections.emptyMap(), in, -1, logger);
         return recordReader;
     }
