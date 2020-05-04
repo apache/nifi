0d5d93dec38e7be53ac106d8e473ab2480934530
David Voit <david.voit@osram-os.com>
2020-05-04 07:44:45 +0200

    NIFI-7307 - Indent to 4 spaces

--- nifi-nar-bundles/nifi-azure-bundle/nifi-azure-processors/src/main/java/org/apache/nifi/services/azure/storage/AzureBlobStorageUserMetadataStringLookupService.java
+++ nifi-nar-bundles/nifi-azure-bundle/nifi-azure-processors/src/main/java/org/apache/nifi/services/azure/storage/AzureBlobStorageUserMetadataStringLookupService.java
@@ -19,7 +19,13 @@ import org.apache.nifi.processors.azure.AbstractAzureBlobProcessor;
 import org.apache.nifi.processors.azure.storage.utils.AzureStorageUtils;
 
 import java.net.URISyntaxException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Collection;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -28,88 +34,88 @@ import java.util.stream.Stream;
     "Each property that is added will be looked up in the User Metadata on the Blob")
 public class AzureBlobStorageUserMetadataStringLookupService extends AbstractControllerService implements StringLookupService {
 
-  protected static final String KEY = "key";
-  protected static final Set<String> REQUIRED_KEYS = Collections.unmodifiableSet(Stream.of(KEY).collect(Collectors.toSet()));
+    protected static final String KEY = "key";
+    protected static final Set<String> REQUIRED_KEYS = Collections.unmodifiableSet(Stream.of(KEY).collect(Collectors.toSet()));
 
-  private static final List<PropertyDescriptor> PROPERTIES = Collections
-      .unmodifiableList(Arrays.asList(
-          AzureStorageUtils.CONTAINER,
-          AzureStorageUtils.STORAGE_CREDENTIALS_SERVICE,
-          AzureStorageUtils.ACCOUNT_NAME,
-          AzureStorageUtils.ACCOUNT_KEY,
-          AzureStorageUtils.PROP_SAS_TOKEN,
-          AbstractAzureBlobProcessor.BLOB
-      ));
+    private static final List<PropertyDescriptor> PROPERTIES = Collections
+        .unmodifiableList(Arrays.asList(
+            AzureStorageUtils.CONTAINER,
+            AzureStorageUtils.STORAGE_CREDENTIALS_SERVICE,
+            AzureStorageUtils.ACCOUNT_NAME,
+            AzureStorageUtils.ACCOUNT_KEY,
+            AzureStorageUtils.PROP_SAS_TOKEN,
+            AbstractAzureBlobProcessor.BLOB
+        ));
 
-  private volatile String containerName;
-  private volatile AzureStorageCredentialsService storageCredentialsService;
-  private volatile ConfigurationContext context;
+    private volatile String containerName;
+    private volatile AzureStorageCredentialsService storageCredentialsService;
+    private volatile ConfigurationContext context;
 
-  private AzureStorageCredentialsDetails getStorageCredentialsDetails(Map<String, String> attributes) {
-    if (storageCredentialsService != null) {
-      return storageCredentialsService.getStorageCredentialsDetails(attributes);
-    } else {
-      return AzureStorageUtils.createStorageCredentialsDetails(context, attributes);
-    }
-  }
-
-  @OnEnabled
-  public void onEnabled(final ConfigurationContext context) {
-    containerName = context.getProperty(AzureStorageUtils.CONTAINER).evaluateAttributeExpressions().getValue();
-    storageCredentialsService = context.getProperty(AzureStorageUtils.STORAGE_CREDENTIALS_SERVICE)
-        .asControllerService(AzureStorageCredentialsService.class);
-    this.context = context;
-  }
-
-  @Override
-  protected Collection<ValidationResult> customValidate(ValidationContext validationContext) {
-    final Collection<ValidationResult> results = AzureStorageUtils.validateCredentialProperties(validationContext);
-    AzureStorageUtils.validateProxySpec(validationContext, results);
-    return results;
-  }
-
-  @Override
-  protected List<PropertyDescriptor> getSupportedPropertyDescriptors() {
-    return PROPERTIES;
-  }
-
-  @Override
-  public Optional<String> lookup(Map<String, Object> coordinates, Map<String, String> context) throws LookupFailureException {
-    String key = (String) coordinates.get(KEY);
-    String blobPath = getProperty(AbstractAzureBlobProcessor.BLOB).evaluateAttributeExpressions(context).getValue();
-
-    if (blobPath.isEmpty()) {
-      return Optional.empty();
+    private AzureStorageCredentialsDetails getStorageCredentialsDetails(Map<String, String> attributes) {
+        if (storageCredentialsService != null) {
+            return storageCredentialsService.getStorageCredentialsDetails(attributes);
+        } else {
+            return AzureStorageUtils.createStorageCredentialsDetails(context, attributes);
+        }
     }
 
-    try {
-      AzureStorageCredentialsDetails storageCredentialsDetails = getStorageCredentialsDetails(context);
-      final CloudStorageAccount cloudStorageAccount = new CloudStorageAccount(storageCredentialsDetails.getStorageCredentials(),
-          true, null, storageCredentialsDetails.getStorageAccountName());
-      final CloudBlobClient blobClient = cloudStorageAccount.createCloudBlobClient();
-      final CloudBlobContainer container = blobClient.getContainerReference(containerName);
-
-      final CloudBlob blob = container.getBlockBlobReference(blobPath);
-      if (!blob.exists()) {
-        return Optional.empty();
-      }
-
-      blob.downloadAttributes();
-      String metadataValue = blob.getMetadata().get(key);
-
-      return Optional.ofNullable(metadataValue);
-    } catch (URISyntaxException | StorageException e) {
-      throw new LookupFailureException(e);
+    @OnEnabled
+    public void onEnabled(final ConfigurationContext context) {
+        containerName = context.getProperty(AzureStorageUtils.CONTAINER).evaluateAttributeExpressions().getValue();
+        storageCredentialsService = context.getProperty(AzureStorageUtils.STORAGE_CREDENTIALS_SERVICE)
+            .asControllerService(AzureStorageCredentialsService.class);
+        this.context = context;
     }
-  }
 
-  @Override
-  public Optional<String> lookup(Map<String, Object> coordinates) throws LookupFailureException {
-    return lookup(coordinates, Collections.emptyMap());
-  }
+    @Override
+    protected Collection<ValidationResult> customValidate(ValidationContext validationContext) {
+        final Collection<ValidationResult> results = AzureStorageUtils.validateCredentialProperties(validationContext);
+        AzureStorageUtils.validateProxySpec(validationContext, results);
+        return results;
+    }
 
-  @Override
-  public Set<String> getRequiredKeys() {
-    return REQUIRED_KEYS;
-  }
+    @Override
+    protected List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        return PROPERTIES;
+    }
+
+    @Override
+    public Optional<String> lookup(Map<String, Object> coordinates, Map<String, String> context) throws LookupFailureException {
+        String key = (String) coordinates.get(KEY);
+        String blobPath = getProperty(AbstractAzureBlobProcessor.BLOB).evaluateAttributeExpressions(context).getValue();
+
+        if (blobPath.isEmpty()) {
+            return Optional.empty();
+        }
+
+        try {
+            AzureStorageCredentialsDetails storageCredentialsDetails = getStorageCredentialsDetails(context);
+            final CloudStorageAccount cloudStorageAccount = new CloudStorageAccount(storageCredentialsDetails.getStorageCredentials(),
+                true, null, storageCredentialsDetails.getStorageAccountName());
+            final CloudBlobClient blobClient = cloudStorageAccount.createCloudBlobClient();
+            final CloudBlobContainer container = blobClient.getContainerReference(containerName);
+
+            final CloudBlob blob = container.getBlockBlobReference(blobPath);
+            if (!blob.exists()) {
+                return Optional.empty();
+            }
+
+            blob.downloadAttributes();
+            String metadataValue = blob.getMetadata().get(key);
+
+            return Optional.ofNullable(metadataValue);
+        } catch (URISyntaxException | StorageException e) {
+            throw new LookupFailureException(e);
+        }
+    }
+
+    @Override
+    public Optional<String> lookup(Map<String, Object> coordinates) throws LookupFailureException {
+        return lookup(coordinates, Collections.emptyMap());
+    }
+
+    @Override
+    public Set<String> getRequiredKeys() {
+        return REQUIRED_KEYS;
+    }
 }
--- nifi-nar-bundles/nifi-azure-bundle/nifi-azure-processors/src/test/java/org/apache/nifi/processors/azure/storage/ITLookupAttributeAzureBlobStorage.java
+++ nifi-nar-bundles/nifi-azure-bundle/nifi-azure-processors/src/test/java/org/apache/nifi/processors/azure/storage/ITLookupAttributeAzureBlobStorage.java
@@ -15,59 +15,59 @@ import java.util.Map;
 
 public class ITLookupAttributeAzureBlobStorage extends AbstractAzureBlobStorageIT {
 
-  @Override
-  protected Class<? extends Processor> getProcessorClass() {
-    return LookupAttribute.class;
-  }
-
-  @Before
-  public void setup() throws Exception {
-    // We override the runner created by the Abstract super class, we don't need the connections strings on the Processor
-    runner = TestRunners.newTestRunner(getProcessorClass());
-
-    AzureBlobStorageUserMetadataStringLookupService lookupService = new AzureBlobStorageUserMetadataStringLookupService();
-
-    runner.addControllerService("azure-blob-string-lookup", lookupService);
-    runner.setProperty(LookupAttribute.LOOKUP_SERVICE, "azure-blob-string-lookup");
-    runner.setProperty(lookupService, AzureStorageUtils.CONTAINER, containerName);
-    runner.setProperty(lookupService, AzureStorageUtils.ACCOUNT_NAME, getAccountName());
-    runner.setProperty(lookupService, AzureStorageUtils.ACCOUNT_KEY, getAccountKey());
-    runner.setProperty(LookupAttribute.INCLUDE_EMPTY_VALUES, "false");
-    runner.setProperty("lookup_key", "${key}");
-
-    runner.enableControllerService(lookupService);
-
-    uploadTestBlob();
-  }
-
-  @Test
-  public void testLookupBlob() throws Exception {
-    runner.assertValid();
-    Map<String, String> attributes = new HashMap<>();
-    attributes.put("azure.blobname", TEST_BLOB_NAME);
-    attributes.put("key", TEST_USER_METADATA_KEY);
-    runner.enqueue("0123456789", attributes);
-    attributes = new HashMap<>();
-    attributes.put("azure.blobname", TEST_BLOB_NAME);
-    attributes.put("key", "dontexists");
-    runner.enqueue("0123456789", attributes);
-    attributes = new HashMap<>();
-    attributes.put("azure.blobname", "dontexists");
-    attributes.put("key", "dontexists");
-    runner.enqueue("0123456789", attributes);
-    runner.run();
-
-    assertResult();
-  }
-
-  private void assertResult() throws Exception {
-    runner.assertTransferCount(LookupAttribute.REL_MATCHED, 1);
-    runner.assertTransferCount(LookupAttribute.REL_UNMATCHED, 2);
-    List<MockFlowFile> flowFilesForRelationship = runner.getFlowFilesForRelationship(LookupAttribute.REL_MATCHED);
-    for (MockFlowFile flowFile : flowFilesForRelationship) {
-      flowFile.assertContentEquals("0123456789".getBytes());
-      flowFile.assertAttributeEquals("lookup_key", TEST_USER_METADATA_VALUE);
+    @Override
+    protected Class<? extends Processor> getProcessorClass() {
+        return LookupAttribute.class;
     }
-  }
+
+    @Before
+    public void setup() throws Exception {
+        // We override the runner created by the Abstract super class, we don't need the connections strings on the Processor
+        runner = TestRunners.newTestRunner(getProcessorClass());
+
+        AzureBlobStorageUserMetadataStringLookupService lookupService = new AzureBlobStorageUserMetadataStringLookupService();
+
+        runner.addControllerService("azure-blob-string-lookup", lookupService);
+        runner.setProperty(LookupAttribute.LOOKUP_SERVICE, "azure-blob-string-lookup");
+        runner.setProperty(lookupService, AzureStorageUtils.CONTAINER, containerName);
+        runner.setProperty(lookupService, AzureStorageUtils.ACCOUNT_NAME, getAccountName());
+        runner.setProperty(lookupService, AzureStorageUtils.ACCOUNT_KEY, getAccountKey());
+        runner.setProperty(LookupAttribute.INCLUDE_EMPTY_VALUES, "false");
+        runner.setProperty("lookup_key", "${key}");
+
+        runner.enableControllerService(lookupService);
+
+        uploadTestBlob();
+    }
+
+    @Test
+    public void testLookupBlob() throws Exception {
+        runner.assertValid();
+        Map<String, String> attributes = new HashMap<>();
+        attributes.put("azure.blobname", TEST_BLOB_NAME);
+        attributes.put("key", TEST_USER_METADATA_KEY);
+        runner.enqueue("0123456789", attributes);
+        attributes = new HashMap<>();
+        attributes.put("azure.blobname", TEST_BLOB_NAME);
+        attributes.put("key", "dontexists");
+        runner.enqueue("0123456789", attributes);
+        attributes = new HashMap<>();
+        attributes.put("azure.blobname", "dontexists");
+        attributes.put("key", "dontexists");
+        runner.enqueue("0123456789", attributes);
+        runner.run();
+
+        assertResult();
+    }
+
+    private void assertResult() throws Exception {
+        runner.assertTransferCount(LookupAttribute.REL_MATCHED, 1);
+        runner.assertTransferCount(LookupAttribute.REL_UNMATCHED, 2);
+        List<MockFlowFile> flowFilesForRelationship = runner.getFlowFilesForRelationship(LookupAttribute.REL_MATCHED);
+        for (MockFlowFile flowFile : flowFilesForRelationship) {
+            flowFile.assertContentEquals("0123456789".getBytes());
+            flowFile.assertAttributeEquals("lookup_key", TEST_USER_METADATA_VALUE);
+        }
+    }
 
 }
