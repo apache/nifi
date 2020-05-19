659156433b899df5d2861a35f56a7ff166e4a86a
Miguel Rodrigues <miguel.b.rodrigues@tecnico.ulisboa.pt>
2020-05-19 13:55:29 +0100

    [NIFI-7470]
    Change "_source_include" to "_source_includes" in response to [NIFI-7470], so that queries work.

--- nifi-nar-bundles/nifi-elasticsearch-bundle/nifi-elasticsearch-processors/src/main/java/org/apache/nifi/processors/elasticsearch/AbstractElasticsearchHttpProcessor.java
+++ nifi-nar-bundles/nifi-elasticsearch-bundle/nifi-elasticsearch-processors/src/main/java/org/apache/nifi/processors/elasticsearch/AbstractElasticsearchHttpProcessor.java
@@ -67,7 +67,7 @@ import java.util.stream.Collectors;
  */
 public abstract class AbstractElasticsearchHttpProcessor extends AbstractElasticsearchProcessor {
 
-    static final String FIELD_INCLUDE_QUERY_PARAM = "_source_include";
+    static final String FIELD_INCLUDE_QUERY_PARAM = "_source_includes";
     static final String QUERY_QUERY_PARAM = "q";
     static final String SORT_QUERY_PARAM = "sort";
     static final String SIZE_QUERY_PARAM = "size";
