e0dd6d466e07b3c41fc64bcc40f5e4abe7680c71
Mark Payne <markap14@hotmail.com>
2020-06-02 15:12:54 -0400

    NIFI-7369: Consider DECIMAL type as a numeric type when using a CHOICE type in QueryRecord
    This closes #4223.

--- nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/queryrecord/FlowFileTable.java
+++ nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/queryrecord/FlowFileTable.java
@@ -279,6 +279,7 @@ public class FlowFileTable extends AbstractTable implements QueryableTable, Tran
         switch (dataType.getFieldType()) {
             case BIGINT:
             case BYTE:
+            case DECIMAL:
             case DOUBLE:
             case FLOAT:
             case INT:
