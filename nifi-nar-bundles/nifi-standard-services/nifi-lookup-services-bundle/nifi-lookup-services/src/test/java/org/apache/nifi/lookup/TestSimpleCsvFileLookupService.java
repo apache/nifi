daef184854db2a0cd7a36f5280c1dcff6daae281
Tamás Bunth <btomi96@gmail.com>
2020-08-25 11:10:02 +0200

    NIFI-7770 Add features to csv lookup services
    Add the following functionalities:
    - Custom value separator (default is comma)
    - Custom quote char to use (default is " i.e. quote sign)
    - Quote mode
    - Escape character to use (default is no escape character)
    - Comment marker
    - Trim fields
    - Character set to use
    
    The above features use a common implementation with "CSVReader".
    
    Also append a sentence to the capability description that first line of
    csv file is considered header. Setting custom header instead of using
    the first line is not supported (yet).
    
    Also, a minor refactor: CSVRecordLoopkupService and
    SimpleCsvFileLookupService now share common logic in implementation.
    
    CSV Format is extended to the same list as CSVReader. In addition,
    lookup services still have the "default" csv format for compatibility
    reasons.
    
    Signed-off-by: Pierre Villard <pierre.villard.fr@gmail.com>
    
    This closes #4494.

--- nifi-nar-bundles/nifi-standard-services/nifi-lookup-services-bundle/nifi-lookup-services/src/test/java/org/apache/nifi/lookup/TestSimpleCsvFileLookupService.java
+++ nifi-nar-bundles/nifi-standard-services/nifi-lookup-services-bundle/nifi-lookup-services/src/test/java/org/apache/nifi/lookup/TestSimpleCsvFileLookupService.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.Optional;
 
+import org.apache.nifi.csv.CSVUtils;
 import org.apache.nifi.reporting.InitializationException;
 import org.apache.nifi.util.TestRunner;
 import org.apache.nifi.util.TestRunners;
@@ -82,5 +83,27 @@ public class TestSimpleCsvFileLookupService {
         final Optional<String> property1 = service.lookup(Collections.singletonMap("key", "property.1"));
         assertThat(property1.isPresent(), is(true));
         assertThat(property1.get(), is("this is property \uff11"));
+    }
+
+    @Test
+    public void testSimpleCsvFileLookupServiceWithCustomSeparatorQuotedEscaped() throws InitializationException, LookupFailureException {
+        final TestRunner runner = TestRunners.newTestRunner(TestProcessor.class);
+        final SimpleCsvFileLookupService service = new SimpleCsvFileLookupService();
+
+        runner.addControllerService("csv-file-lookup-service", service);
+        runner.setProperty(service, SimpleCsvFileLookupService.CSV_FORMAT, "custom");
+        runner.setProperty(service, SimpleCsvFileLookupService.CSV_FILE, "src/test/resources/test_sep_escape_comment.csv");
+        runner.setProperty(service, SimpleCsvFileLookupService.LOOKUP_KEY_COLUMN, "key");
+        runner.setProperty(service, SimpleCsvFileLookupService.LOOKUP_VALUE_COLUMN, "value");
+        runner.setProperty(service, CSVUtils.VALUE_SEPARATOR, "|");
+        runner.setProperty(service, CSVUtils.QUOTE_CHAR, "\"");
+        runner.setProperty(service, CSVUtils.ESCAPE_CHAR, "%");
+        runner.setProperty(service, CSVUtils.COMMENT_MARKER, "#");
+        runner.setProperty(service, CSVUtils.QUOTE_MODE, CSVUtils.QUOTE_ALL);
+        runner.enableControllerService(service);
+        runner.assertValid(service);
+
+        final Optional<String> value = service.lookup(Collections.singletonMap("key", "my_key"));
+        assertEquals(Optional.of("my_value with an escaped |."), value);
     }
 }
