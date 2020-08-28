1b1b261ae560b66c4ebf5bbc43af2cb5de4702e0
mark-weghorst <mark.weghorst@outlook.com>
2020-08-28 16:38:33 -0600

    NIFI-7459 fix missed unused import

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-runtime/src/main/java/org/apache/nifi/StatelessNiFi.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-runtime/src/main/java/org/apache/nifi/StatelessNiFi.java
@@ -27,7 +27,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class StatelessNiFi {
