f2ef5eb3d2b4f912bb6d55ba402ec8ebdf49e312
Matthew Burgess <mattyb149@apache.org>
2020-09-01 09:01:53 -0400

    NIFI-7740: Restore RecordsEOFException superclass to SerializationError

--- nifi-nar-bundles/nifi-hive-bundle/nifi-hive3-processors/src/main/java/org/apache/hive/streaming/RecordsEOFException.java
+++ nifi-nar-bundles/nifi-hive-bundle/nifi-hive3-processors/src/main/java/org/apache/hive/streaming/RecordsEOFException.java
@@ -20,7 +20,7 @@ package org.apache.hive.streaming;
  * This is a "marker class" used by the HiveRecordWriter to indicate there are no more records in the input stream.
  * It is used by PutHive3Streaming to determine that all records have been written to transaction(s).
  */
-public class RecordsEOFException extends RuntimeException {
+public class RecordsEOFException extends SerializationError {
 
     RecordsEOFException(String msg, Exception e) {
         super(msg, e);
