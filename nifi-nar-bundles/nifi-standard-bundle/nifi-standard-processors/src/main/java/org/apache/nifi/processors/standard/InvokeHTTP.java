497f5feabdbc31292c9428db4e185ebde59b7263
Andrew Greenburg <agreenburg@gmail.com>
2020-09-09 17:12:25 -0400

    Fix whitespace formatting

--- nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/InvokeHTTP.java
+++ nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/InvokeHTTP.java
@@ -1276,7 +1276,7 @@ public class InvokeHTTP extends AbstractProcessor {
         // create a new hashmap to store the values from the connection
         Map<String, String> map = new HashMap<>();
         responseHttp.headers().names().forEach((key) -> {
-        	if (StringUtils.isBlank(key)) {
+            if (StringUtils.isBlank(key)) {
                 return;
             }
 
