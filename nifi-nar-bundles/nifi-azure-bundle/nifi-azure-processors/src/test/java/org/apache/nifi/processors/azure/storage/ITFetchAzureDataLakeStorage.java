8ffed9eff30deae7d98314274d01231ec2c52eab
Peter Turcsanyi <turcsanyi@apache.org>
2020-09-29 11:06:23 +0200

    NIFI-7830: Ignore ITFetchAzureDataLakeStorage.testFetchLargeFile() as it was ignored originally.

--- nifi-nar-bundles/nifi-azure-bundle/nifi-azure-processors/src/test/java/org/apache/nifi/processors/azure/storage/ITFetchAzureDataLakeStorage.java
+++ nifi-nar-bundles/nifi-azure-bundle/nifi-azure-processors/src/test/java/org/apache/nifi/processors/azure/storage/ITFetchAzureDataLakeStorage.java
@@ -23,6 +23,7 @@ import org.apache.nifi.processor.exception.ProcessException;
 import org.apache.nifi.provenance.ProvenanceEventRecord;
 import org.apache.nifi.provenance.ProvenanceEventType;
 import org.apache.nifi.util.MockFlowFile;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Collections;
@@ -216,7 +217,7 @@ public class ITFetchAzureDataLakeStorage extends AbstractAzureDataLakeStorageIT 
         testFailedFetch(fileSystemName, directory, filename, inputFlowFileContent, inputFlowFileContent, 404);
     }
 
-    //@Ignore("Takes some time, only recommended for manual testing.")
+    @Ignore("Takes some time, only recommended for manual testing.")
     @Test
     public void testFetchLargeFile() {
         // GIVEN
