fae9b7369c2d6587cea809fa6aa070e3c195590c
Joey Frazee <jfrazee@apache.org>
2020-10-28 20:04:44 -0500

    NIFI-7964 Remove unused import

--- nifi-nar-bundles/nifi-azure-bundle/nifi-azure-processors/src/main/java/org/apache/nifi/processors/azure/storage/PutAzureBlobStorage.java
+++ nifi-nar-bundles/nifi-azure-bundle/nifi-azure-processors/src/main/java/org/apache/nifi/processors/azure/storage/PutAzureBlobStorage.java
@@ -16,7 +16,6 @@
  */
 package org.apache.nifi.processors.azure.storage;
 
-import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.FilterInputStream;
