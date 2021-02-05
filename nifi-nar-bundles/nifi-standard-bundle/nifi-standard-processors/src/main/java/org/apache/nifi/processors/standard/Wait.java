2d70644395cbb8da8a686419a6796b41c4fd9305
Peter Gyori <peter.gyori.dev@gmail.com>
2021-02-05 18:49:39 +0100

    NIFI-8205: Documentation improvements for the Wait processor
    Signed-off-by: Pierre Villard <pierre.villard.fr@gmail.com>
    
    This closes #4808.

--- nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/Wait.java
+++ nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/Wait.java
@@ -72,9 +72,15 @@ import static org.apache.nifi.processor.FlowFileFilter.FlowFileFilterResult.REJE
 @InputRequirement(Requirement.INPUT_REQUIRED)
 @CapabilityDescription("Routes incoming FlowFiles to the 'wait' relationship until a matching release signal "
         + "is stored in the distributed cache from a corresponding Notify processor. "
-        + "When a matching release signal is identified, a waiting FlowFile is routed to the 'success' relationship, "
-        + "with attributes copied from the FlowFile that produced the release signal from the Notify processor.  "
-        + "The release signal entry is then removed from the cache. Waiting FlowFiles will be routed to 'expired' if they exceed the Expiration Duration. "
+        + "When a matching release signal is identified, a waiting FlowFile is routed to the 'success' relationship. "
+        + "The release signal entry is then removed from the cache. "
+        + "The attributes of the FlowFile that produced the release signal are copied to the waiting FlowFile if the Attribute Cache Regex "
+        + "property of the corresponding Notify processor is set properly. "
+        + "If there are multiple release signals in the cache identified by the Release Signal Identifier, "
+        + "and the Notify processor is configured to copy the FlowFile attributes to the cache, "
+        + "then the FlowFile passing the Wait processor receives the union of the attributes of the FlowFiles "
+        + "that produced the release signals in the cache (identified by Release Signal Identifier). "
+        + "Waiting FlowFiles will be routed to 'expired' if they exceed the Expiration Duration. "
 
         + "If you need to wait for more than one signal, specify the desired number of signals via the 'Target Signal Count' property. "
         + "This is particularly useful with processors that split a source FlowFile into multiple fragments, such as SplitText. "
@@ -88,8 +94,9 @@ import static org.apache.nifi.processor.FlowFileFilter.FlowFileFilterResult.REJE
         @WritesAttribute(attribute = "wait.start.timestamp", description = "All FlowFiles will have an attribute 'wait.start.timestamp', which sets the "
         + "initial epoch timestamp when the file first entered this processor.  This is used to determine the expiration time of the FlowFile.  "
         + "This attribute is not written when the FlowFile is transferred to failure, expired or success"),
-        @WritesAttribute(attribute = "wait.counter.<counterName>", description = "If a signal exists when the processor runs, "
-        + "each count value in the signal is copied.")
+        @WritesAttribute(attribute = "wait.counter.<counterName>", description = "The name of each counter for which at least one signal "
+        + "has been present in the cache since the last time the cache was empty "
+        + "gets copied to the current FlowFile as an attribute.")
 })
 @SeeAlso(classNames = {"org.apache.nifi.distributed.cache.client.DistributedMapCacheClientService", "org.apache.nifi.distributed.cache.server.map.DistributedMapCacheServer",
         "org.apache.nifi.processors.standard.Notify"})
@@ -110,8 +117,9 @@ public class Wait extends AbstractProcessor {
     public static final PropertyDescriptor RELEASE_SIGNAL_IDENTIFIER = new PropertyDescriptor.Builder()
             .name("release-signal-id")
             .displayName("Release Signal Identifier")
-            .description("A value, or the results of an Attribute Expression Language statement, which will " +
-                "be evaluated against a FlowFile in order to determine the release signal cache key")
+            .description("A value that specifies the key to a specific release signal cache. "
+                + "To decide whether the FlowFile that is being processed by the Wait processor should be sent to the 'success' "
+                + "or the 'wait' relationship, the processor checks the signals in the cache specified by this key.")
             .required(true)
             .addValidator(StandardValidators.createAttributeExpressionLanguageValidator(ResultType.STRING, true))
             .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
@@ -120,11 +128,12 @@ public class Wait extends AbstractProcessor {
     public static final PropertyDescriptor TARGET_SIGNAL_COUNT = new PropertyDescriptor.Builder()
             .name("target-signal-count")
             .displayName("Target Signal Count")
-            .description("A value, or the results of an Attribute Expression Language statement, which will " +
-                    "be evaluated against a FlowFile in order to determine the target signal count. " +
-                    "This processor checks whether the signal count has reached this number. " +
-                    "If Signal Counter Name is specified, this processor checks a particular counter, " +
-                    "otherwise checks against total count in a signal.")
+            .description("The number of signals that need to be in the cache (specified by the Release Signal Identifier) "
+                + "in order for the FlowFile processed by the Wait processor to be sent to the ‘success’ relationship. "
+                + "If the number of signals in the cache has reached this number, the FlowFile is routed to the "
+                + "'success' relationship and the number of signals in the cache is decreased by this value. "
+                + "If Signal Counter Name is specified, this processor checks a particular counter, "
+                + "otherwise checks against the total number of signals in the cache.")
             .required(true)
             .addValidator(StandardValidators.NON_NEGATIVE_INTEGER_VALIDATOR)
             .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
@@ -134,9 +143,9 @@ public class Wait extends AbstractProcessor {
     public static final PropertyDescriptor SIGNAL_COUNTER_NAME = new PropertyDescriptor.Builder()
             .name("signal-counter-name")
             .displayName("Signal Counter Name")
-            .description("A value, or the results of an Attribute Expression Language statement, which will " +
-                    "be evaluated against a FlowFile in order to determine the signal counter name. " +
-                    "If not specified, this processor checks the total count in a signal.")
+            .description("Within the cache (specified by the Release Signal Identifier) the signals may belong to different counters. "
+                + "If this property is specified, the processor checks the number of signals in the cache that belong to this particular counter. "
+                + "If not specified, the processor checks the total number of signals in the cache.")
             .required(false)
             .addValidator(StandardValidators.createAttributeExpressionLanguageValidator(ResultType.STRING, true))
             .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
