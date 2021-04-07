8ca6eeb6ec135e9d627f14094ba4f1c3ba886691
Joe Gresock <jgresock@cloudera.com>
2021-04-07 14:15:56 -0400

    NIFI-7198: Adding optional SSL Context Service to RedisConnectionPoolService
    This closes #4981
    
    Signed-off-by: David Handermann <exceptionfactory@apache.org>

--- nifi-nar-bundles/nifi-redis-bundle/nifi-redis-extensions/src/test/java/org/apache/nifi/redis/service/TestRedisConnectionPoolService.java
+++ nifi-nar-bundles/nifi-redis-bundle/nifi-redis-extensions/src/test/java/org/apache/nifi/redis/service/TestRedisConnectionPoolService.java
@@ -19,16 +19,36 @@ package org.apache.nifi.redis.service;
 import org.apache.nifi.redis.RedisConnectionPool;
 import org.apache.nifi.redis.util.RedisUtils;
 import org.apache.nifi.reporting.InitializationException;
+import org.apache.nifi.ssl.RestrictedSSLContextService;
+import org.apache.nifi.util.MockConfigurationContext;
+import org.apache.nifi.util.MockProcessContext;
+import org.apache.nifi.util.StandardProcessorTestRunner;
 import org.apache.nifi.util.TestRunner;
 import org.apache.nifi.util.TestRunners;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 public class TestRedisConnectionPoolService {
 
+    public static final String SSL_CONTEXT_IDENTIFIER = "ssl-context-service";
     private TestRunner testRunner;
     private FakeRedisProcessor proc;
     private RedisConnectionPool redisService;
+
+    private static SSLContext sslContext;
+
+    @BeforeClass
+    public static void classSetup() throws IOException, GeneralSecurityException {
+        sslContext = SSLContext.getDefault();
+    }
 
     @Before
     public void setup() throws InitializationException {
@@ -37,6 +57,69 @@ public class TestRedisConnectionPoolService {
 
         redisService = new RedisConnectionPoolService();
         testRunner.addControllerService("redis-service", redisService);
+    }
+
+    private void enableSslContextService() throws InitializationException {
+        final RestrictedSSLContextService sslContextService = Mockito.mock(RestrictedSSLContextService.class);
+        Mockito.when(sslContextService.getIdentifier()).thenReturn(SSL_CONTEXT_IDENTIFIER);
+        Mockito.when(sslContextService.createContext()).thenReturn(sslContext);
+        testRunner.addControllerService(SSL_CONTEXT_IDENTIFIER, sslContextService);
+        testRunner.enableControllerService(sslContextService);
+        testRunner.setProperty(redisService, RedisUtils.SSL_CONTEXT_SERVICE, SSL_CONTEXT_IDENTIFIER);
+    }
+
+    @Test
+    public void testSSLContextService() throws InitializationException {
+        this.setDefaultRedisProperties();
+
+        this.enableSslContextService();
+
+        testRunner.assertValid(redisService);
+        testRunner.enableControllerService(redisService);
+
+        JedisConnectionFactory connectionFactory = getJedisConnectionFactory(); // Uses config from test runner
+
+        // Verify that the client configuration will be using an SSL socket factory and SSL parameters
+        Assert.assertTrue(connectionFactory.getClientConfiguration().getSslSocketFactory().isPresent());
+        Assert.assertTrue(connectionFactory.getClientConfiguration().getSslParameters().isPresent());
+
+        // Now remove the SSL context service
+        testRunner.disableControllerService(redisService);
+        testRunner.removeProperty(redisService, RedisUtils.SSL_CONTEXT_SERVICE);
+        testRunner.enableControllerService(redisService);
+        connectionFactory = getJedisConnectionFactory();
+
+        // Now the client configuration will not use SSL
+        Assert.assertFalse(connectionFactory.getClientConfiguration().getSslSocketFactory().isPresent());
+        Assert.assertFalse(connectionFactory.getClientConfiguration().getSslParameters().isPresent());
+    }
+
+    private void setDefaultRedisProperties() {
+        testRunner.setProperty(redisService, RedisUtils.REDIS_MODE, "Standalone");
+        testRunner.setProperty(redisService, RedisUtils.CONNECTION_STRING, "localhost:6379");
+        testRunner.setProperty(redisService, RedisUtils.DATABASE, "0");
+        testRunner.setProperty(redisService, RedisUtils.COMMUNICATION_TIMEOUT, "60s");
+        testRunner.setProperty(redisService, RedisUtils.CLUSTER_MAX_REDIRECTS, "5");
+        testRunner.setProperty(redisService, RedisUtils.POOL_MAX_TOTAL, "1");
+        testRunner.setProperty(redisService, RedisUtils.POOL_MAX_IDLE, "1");
+        testRunner.setProperty(redisService, RedisUtils.POOL_MIN_IDLE, "1");
+        testRunner.setProperty(redisService, RedisUtils.POOL_BLOCK_WHEN_EXHAUSTED, "true");
+        testRunner.setProperty(redisService, RedisUtils.POOL_MAX_WAIT_TIME, "1s");
+        testRunner.setProperty(redisService, RedisUtils.POOL_MIN_EVICTABLE_IDLE_TIME, "1s");
+        testRunner.setProperty(redisService, RedisUtils.POOL_TIME_BETWEEN_EVICTION_RUNS, "1s");
+        testRunner.setProperty(redisService, RedisUtils.POOL_NUM_TESTS_PER_EVICTION_RUN, "1");
+        testRunner.setProperty(redisService, RedisUtils.POOL_TEST_ON_CREATE, "false");
+        testRunner.setProperty(redisService, RedisUtils.POOL_TEST_ON_BORROW, "false");
+        testRunner.setProperty(redisService, RedisUtils.POOL_TEST_ON_RETURN, "false");
+        testRunner.setProperty(redisService, RedisUtils.POOL_TEST_WHILE_IDLE, "false");
+    }
+
+    private JedisConnectionFactory getJedisConnectionFactory() {
+        MockProcessContext processContext = ((StandardProcessorTestRunner) testRunner).getProcessContext();
+        MockConfigurationContext configContext = new MockConfigurationContext(processContext.getControllerServices()
+                .get(redisService.getIdentifier()).getProperties(), processContext);
+        JedisConnectionFactory connectionFactory = RedisUtils.createConnectionFactory(configContext, testRunner.getLogger());
+        return connectionFactory;
     }
 
     @Test
