6659c9a3c6e283d4bb6f9a8daf03a01409a05d7a
Paul Grey <greyp9@yahoo.com>
2021-05-03 12:57:14 -0400

    orient unit test to CI-specified locales

--- nifi-commons/nifi-utils/src/test/java/org/apache/nifi/util/locale/TestLocaleOfTestSuite.java
+++ nifi-commons/nifi-utils/src/test/java/org/apache/nifi/util/locale/TestLocaleOfTestSuite.java
@@ -22,67 +22,52 @@ import org.junit.Test;
 
 import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
-import java.util.Arrays;
 import java.util.Locale;
-import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
 /**
- * Testing of the test suite environment {@link java.util.Locale}.
+ * Testing of the test suite environment {@link java.util.Locale}.  The locales specified
+ * in ".github/workflows/ci-workflow.yml" are exercised.  This test is inert when run in alternate locales.
  */
 public class TestLocaleOfTestSuite {
 
     /**
-     * Utility test that logs the {@link java.util.Locale} in which the project test suite is running.  The
-     * assumptions validate the expected environment when run in Github CI.
-     *
-     * See also: "nifi/.github/workflows/ci-workflow.yml"
+     * Test behaviors associated with non-standard ".github/workflows/ci-workflow.yml" {@link java.util.Locale} "en-AU".
      */
     @Test
-    public void testLocaleOfTestSuiteExecution() {
-        final Logger logger = Logger.getLogger(getClass().getName());
-        final String userLanguage = System.getProperty("user.language");
-        final String userCountry = System.getProperty("user.country");
-        final String userRegion = System.getProperty("user.region");
-        final String userTimezone = System.getProperty("user.timezone");
-        final String languageTag = Locale.getDefault().toLanguageTag();
-        logger.info(String.format(
-                "Test environment: locale=[%s] user.language=[%s], user.country=[%s], user.region=[%s], user.timezone=[%s]",
-                languageTag, userLanguage, userCountry, userRegion, userTimezone));
-        Assume.assumeTrue(Arrays.asList("en", "fr", "ja").contains(userLanguage));
-        Assume.assumeTrue(Arrays.asList("AU", "FR", "JP").contains(userCountry));
-        Assume.assumeTrue(Arrays.asList("en-AU", "fr-FR", "ja-JP").contains(languageTag));
-    }
+    public void testLocaleCI_EN_AU() {
+        final Locale locale = Locale.getDefault();
+        Assume.assumeTrue(locale.toLanguageTag().equals("en-AU"));
 
-    /**
-     * Some validation of expected outputs, given different {@link java.util.Locale} settings.  Limited to a few
-     * locales for brevity.  The locales here are all absolute (no dependence on test environment).
-     */
-    @Test
-    public void testLocaleExpectedOutputs() {
-        Assert.assertEquals("1\u00a0000", NumberFormat.getInstance(Locale.FRANCE).format(1000));
-        Assert.assertEquals("1\u00a0000", NumberFormat.getInstance(Locale.CANADA_FRENCH).format(1000));
-        Assert.assertEquals("1\u00a0000", NumberFormat.getInstance(Locale.FRENCH).format(1000));
-        Assert.assertEquals("1,000", NumberFormat.getInstance(Locale.US).format(1000));
-        Assert.assertEquals("1,000", NumberFormat.getInstance(Locale.JAPAN).format(1000));
-
-        Assert.assertEquals("E", DecimalFormatSymbols.getInstance(Locale.US).getExponentSeparator());
-        Assert.assertEquals("E", DecimalFormatSymbols.getInstance(Locale.FRANCE).getExponentSeparator());
-        Assert.assertEquals("E", DecimalFormatSymbols.getInstance(Locale.JAPAN).getExponentSeparator());
-        Assert.assertEquals("E", DecimalFormatSymbols.getInstance(Locale.forLanguageTag("en-NZ")).getExponentSeparator());
-        Assert.assertEquals("E", DecimalFormatSymbols.getInstance(Locale.forLanguageTag("fr-CH")).getExponentSeparator());
-        Assert.assertEquals("E", DecimalFormatSymbols.getInstance(Locale.forLanguageTag("xx-YY")).getExponentSeparator());
-    }
-
-    /**
-     * Document behavior of {@link DecimalFormatSymbols} in JRE on use of non-standard {@link Locale}.
-     */
-    @Test
-    public void testExponentSeparatorByLocaleAndJavaVersion() {
         final String runtimeJavaVersion = System.getProperty("java.version");
         final boolean isJava8 = Pattern.compile("1\\.8.+").matcher(runtimeJavaVersion).matches();
-        final String expected = (isJava8 ? "E" : "e");
-        // used by project CI
-        Assert.assertEquals(expected, DecimalFormatSymbols.getInstance(Locale.forLanguageTag("en-AU")).getExponentSeparator());
+        final String expected = (isJava8 ? "E" : "e");  // tested in Java 8 and Java 11
+
+        Assert.assertEquals(expected, DecimalFormatSymbols.getInstance(locale).getExponentSeparator());
+        Assert.assertEquals("1,000", NumberFormat.getInstance(locale).format(1000));
+    }
+
+    /**
+     * Test behaviors associated with ".github/workflows/ci-workflow.yml" {@link java.util.Locale#JAPAN}.
+     */
+    @Test
+    public void testLocaleCI_JA_JP() {
+        final Locale locale = Locale.getDefault();
+        Assume.assumeTrue(locale.toLanguageTag().equals("ja-JP"));
+
+        Assert.assertEquals("E", DecimalFormatSymbols.getInstance(locale).getExponentSeparator());
+        Assert.assertEquals("1,000", NumberFormat.getInstance(locale).format(1000));
+    }
+
+    /**
+     * Test behaviors associated with ".github/workflows/ci-workflow.yml" {@link java.util.Locale#FRANCE}.
+     */
+    @Test
+    public void testLocaleCI_FR_FR() {
+        final Locale locale = Locale.getDefault();
+        Assume.assumeTrue(locale.toLanguageTag().equals("fr-FR"));
+
+        Assert.assertEquals("E", DecimalFormatSymbols.getInstance(locale).getExponentSeparator());
+        Assert.assertEquals("1\u00a0000", NumberFormat.getInstance(locale).format(1000));
     }
 }
