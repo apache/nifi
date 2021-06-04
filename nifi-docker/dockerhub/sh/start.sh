8641b54a49d2215b870acc0f96203cbc7f61abe2
Emilio Setiadarma <emiliosetiadarma@gmail.com>
2021-06-04 09:07:42 -0700

    NIFI-6667 Added variable to accept nifi.sensitive.props.key in Dockerfiles
    This closes #5128
    
    Signed-off-by: David Handermann <exceptionfactory@apache.org>

--- nifi-docker/dockerhub/sh/start.sh
+++ nifi-docker/dockerhub/sh/start.sh
@@ -63,6 +63,8 @@ prop_replace 'nifi.analytics.connection.model.implementation'   "${NIFI_ANALYTIC
 prop_replace 'nifi.analytics.connection.model.score.name'       "${NIFI_ANALYTICS_MODEL_SCORE_NAME:-rSquared}"
 prop_replace 'nifi.analytics.connection.model.score.threshold'  "${NIFI_ANALYTICS_MODEL_SCORE_THRESHOLD:-.90}"
 
+prop_replace 'nifi.sensitive.props.key'   "${NIFI_SENSITIVE_PROPS_KEY:-}"
+
 . "${scripts_dir}/update_cluster_state_management.sh"
 
 # Check if we are secured or unsecured
