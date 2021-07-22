78e9dd2c0cfc2d4031410dcc7a8cc6a8cbd18dc9
Joe Gresock <jgresock@gmail.com>
2021-07-22 10:25:48 -0400

    Removing abstract from ApplicationProperties

--- nifi-commons/nifi-property-utils/src/main/java/org/apache/nifi/properties/ApplicationProperties.java
+++ nifi-commons/nifi-property-utils/src/main/java/org/apache/nifi/properties/ApplicationProperties.java
@@ -22,7 +22,7 @@ import java.util.Properties;
 /**
  * A tagging class that represents the main configuration properties for an application (e.g. NiFi or NiFi Registry).
  */
-public abstract class ApplicationProperties extends StandardReadableProperties {
+public class ApplicationProperties extends StandardReadableProperties {
     public ApplicationProperties(Properties properties) {
         super(properties);
     }
