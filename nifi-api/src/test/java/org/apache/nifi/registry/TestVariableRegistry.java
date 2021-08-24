6a55c6ca447ea902e454daf29eb9d17720310219
Mike Thomsen <mthomsen@apache.org>
2021-08-24 10:41:26 -0400

    NIFI-9081 Updated nifi-framework-api tests to JUnit 5
    - Updated nifi-external tests to JUnit 5
    - Updated nifi-stateless to use JUnit 5
    - Updated nifi-api to use JUnit 5
    
    Signed-off-by: David Handermann <exceptionfactory@apache.org>

--- nifi-api/src/test/java/org/apache/nifi/registry/TestVariableRegistry.java
+++ nifi-api/src/test/java/org/apache/nifi/registry/TestVariableRegistry.java
@@ -16,10 +16,10 @@
  */
 package org.apache.nifi.registry;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class TestVariableRegistry {
 
--- nifi-stateless/nifi-stateless-bundle/nifi-stateless-engine/src/test/java/org/apache/nifi/stateless/session/TestAsynchronousCommitTracker.java
+++ nifi-stateless/nifi-stateless-bundle/nifi-stateless-engine/src/test/java/org/apache/nifi/stateless/session/TestAsynchronousCommitTracker.java
@@ -22,15 +22,15 @@ import org.apache.nifi.connectable.ConnectableType;
 import org.apache.nifi.connectable.Connection;
 import org.apache.nifi.controller.queue.FlowFileQueue;
 import org.apache.nifi.groups.ProcessGroup;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.Collections;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestAsynchronousCommitTracker {
 
