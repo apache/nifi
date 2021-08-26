0cf09840eabd6131be8a9a928c1b2e7df03eb0ee
exceptionfactory <exceptionfactory@apache.org>
2021-08-26 14:12:34 -0500

    NIFI-9082 Add nifi.zookeeper.jute.maxbuffer property (#5335)
    * NIFI-9082 Added nifi.zookeeper.jute.maxbuffer property
    
    - Updated ZooKeeperStateProvider to check jute.maxbuffer instead of hard-coded number
    - Updated internal ZooKeeperClientConfig class to include new juteMaxBuffer property
    
    * NIFI-9082 Updated jute.maxbuffer documentation and added hexadecimal representation
    
    - Renamed DefaultWatcher to NoOpWatcher
    - Removed unnecessary validateDataSize() from createNode()

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/test/java/org/apache/nifi/controller/state/providers/zookeeper/TestZooKeeperStateProvider.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/test/java/org/apache/nifi/controller/state/providers/zookeeper/TestZooKeeperStateProvider.java
@@ -38,8 +38,10 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class TestZooKeeperStateProvider extends AbstractTestStateProvider {
 
@@ -188,7 +190,7 @@ public class TestZooKeeperStateProvider extends AbstractTestStateProvider {
     }
 
     @Test(timeout = 30000)
-    public void testStateTooLargeExceptionThrownOnReplace() throws IOException, InterruptedException {
+    public void testStateTooLargeExceptionThrownOnReplace() throws InterruptedException {
         final Map<String, String> state = new HashMap<>();
         final StringBuilder sb = new StringBuilder();
 
@@ -218,15 +220,48 @@ public class TestZooKeeperStateProvider extends AbstractTestStateProvider {
             }
         }
 
-        try {
-            getProvider().replace(getProvider().getState(componentId), state, componentId);
-            Assert.fail("Expected StateTooLargeException");
-        } catch (final StateTooLargeException stle) {
-            // expected behavior.
-        } catch (final Exception e) {
-            Assert.fail("Expected StateTooLargeException", e);
-        }
+        assertThrows(StateTooLargeException.class, () -> getProvider().replace(getProvider().getState(componentId), state, componentId));
+    }
 
+    @Test(timeout = 5000)
+    public void testStateTooLargeExceptionThrownOnReplaceSmallJuteMaxbuffer() throws Exception {
+        final Map<String, String> initialState = new HashMap<>();
+        final String stateValue = UUID.randomUUID().toString();
+        final int maxBufferSize = 100;
+        initialState.put("1", stateValue);
+
+        final Map<PropertyDescriptor, String> properties = new HashMap<>(defaultProperties);
+        properties.put(ZooKeeperStateProvider.CONNECTION_STRING, zkServer.getConnectString());
+
+        final ZooKeeperStateProvider stateProvider = new ZooKeeperStateProvider();
+        final Properties applicationProperties = new Properties();
+        applicationProperties.setProperty(NiFiProperties.ZOOKEEPER_JUTE_MAXBUFFER, Integer.toString(maxBufferSize));
+        final NiFiProperties providerProperties = NiFiProperties.createBasicNiFiProperties(null, applicationProperties);
+        stateProvider.setNiFiProperties(providerProperties);
+        initializeProvider(stateProvider, properties);
+
+        try {
+            stateProvider.enable();
+
+            while (true) {
+                try {
+                    stateProvider.setState(initialState, componentId);
+                    break;
+                } catch (final IOException ioe) {
+                    // Retry initial connection
+                    Thread.sleep(1000L);
+                }
+            }
+
+            final Map<String, String> state = new HashMap<>();
+            state.put("1", stateValue);
+            state.put("2", stateValue);
+            state.put("3", stateValue);
+            state.put("4", stateValue);
+            assertThrows(StateTooLargeException.class, () -> stateProvider.replace(getProvider().getState(componentId), state, componentId));
+        } finally {
+            stateProvider.shutdown();
+        }
     }
 
     @Test
