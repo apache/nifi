fbf6a712b542d4416a39b0e0ba757b6e3bc60e49
Gabriel Barbu <gabriel.barbu@nagarro.com>
2021-10-29 16:03:42 +0300

    NIFI-9348 NIFI-7863 This closes #5495. Added temporary suffix and fixed [NIFI-7863] creation of the directories
    Signed-off-by: Joe Witt <joewitt@apache.org>

--- nifi-nar-bundles/nifi-smb-bundle/nifi-smb-processors/src/test/java/org/apache/nifi/processors/smb/PutSmbFileTest.java
+++ nifi-nar-bundles/nifi-smb-bundle/nifi-smb-processors/src/test/java/org/apache/nifi/processors/smb/PutSmbFileTest.java
@@ -22,6 +22,7 @@ import com.hierynomus.smbj.SMBClient;
 import com.hierynomus.smbj.auth.AuthenticationContext;
 import com.hierynomus.smbj.connection.Connection;
 import com.hierynomus.smbj.session.Session;
+import com.hierynomus.smbj.share.DiskEntry;
 import com.hierynomus.smbj.share.DiskShare;
 import com.hierynomus.smbj.share.File;
 import org.apache.nifi.util.TestRunner;
@@ -42,6 +43,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -55,6 +57,7 @@ public class PutSmbFileTest {
     private Connection connection;
     private Session session;
     private DiskShare diskShare;
+    private DiskEntry diskEntry;
     private File smbfile;
     private ByteArrayOutputStream baOutputStream;
 
@@ -75,6 +78,7 @@ public class PutSmbFileTest {
         connection = mock(Connection.class);
         session = mock(Session.class);
         diskShare = mock(DiskShare.class);
+        diskEntry = mock(DiskEntry.class);
         smbfile = mock(File.class);
         baOutputStream = new ByteArrayOutputStream();
 
@@ -89,6 +93,14 @@ public class PutSmbFileTest {
                 any(SMB2CreateDisposition.class),
                 anySet()
         )).thenReturn(smbfile);
+        when(diskShare.open(
+                any(String.class),
+                anySet(),
+                anySet(),
+                anySet(),
+                any(SMB2CreateDisposition.class),
+                anySet()
+        )).thenReturn(diskEntry);
         when(smbfile.getOutputStream()).thenReturn(baOutputStream);
 
         testRunner.setProperty(PutSmbFile.HOSTNAME, HOSTNAME);
@@ -172,8 +184,25 @@ public class PutSmbFileTest {
     }
 
     @Test
+    public void testDirectoriesCreatedWhenDontExists() throws IOException {
+        final String directory = new java.io.File("a\\b\\c\\b\\e").getPath();
+        final int count = directory.split(java.util.regex.Pattern.quote(java.io.File.separator)).length;
+        when(diskShare.folderExists(DIRECTORY)).thenReturn(false);
+
+        testRunner.setProperty(PutSmbFile.CREATE_DIRS, "true");
+        testRunner.setProperty(PutSmbFile.DIRECTORY, directory);
+        testRunner.enqueue("data");
+        testRunner.run();
+
+        verify(diskShare, times(count)).mkdir(
+            any(String.class)
+        );
+    }
+
+    @Test
     public void testFileShareNone() throws IOException {
         testRunner.setProperty(PutSmbFile.SHARE_ACCESS, PutSmbFile.SHARE_ACCESS_NONE);
+        testRunner.setProperty(PutSmbFile.CREATE_DIRS, "true");
         Set<SMB2ShareAccess> shareAccessSet = testOpenFileShareAccess();
         assertTrue(shareAccessSet.isEmpty());
     }
@@ -181,6 +210,7 @@ public class PutSmbFileTest {
     @Test
     public void testFileShareRead() throws IOException {
         testRunner.setProperty(PutSmbFile.SHARE_ACCESS, PutSmbFile.SHARE_ACCESS_READ);
+        testRunner.setProperty(PutSmbFile.CREATE_DIRS, "true");
         Set<SMB2ShareAccess> shareAccessSet = testOpenFileShareAccess();
         assertTrue(shareAccessSet.contains(SMB2ShareAccess.FILE_SHARE_READ));
     }
@@ -188,6 +218,7 @@ public class PutSmbFileTest {
     @Test
     public void testFileShareReadWriteDelete() throws IOException {
         testRunner.setProperty(PutSmbFile.SHARE_ACCESS, PutSmbFile.SHARE_ACCESS_READWRITEDELETE);
+        testRunner.setProperty(PutSmbFile.CREATE_DIRS, "true");
         Set<SMB2ShareAccess> shareAccessSet = testOpenFileShareAccess();
         assertTrue(shareAccessSet.contains(SMB2ShareAccess.FILE_SHARE_READ));
         assertTrue(shareAccessSet.contains(SMB2ShareAccess.FILE_SHARE_WRITE));
@@ -206,6 +237,76 @@ public class PutSmbFileTest {
         testRunner.setProperty(PutSmbFile.CONFLICT_RESOLUTION, PutSmbFile.IGNORE_RESOLUTION);
         when(diskShare.fileExists(any(String.class))).thenReturn(true);
         testRunner.assertAllFlowFilesTransferred(PutSmbFile.REL_SUCCESS);
+    }
+
+    @Test
+    public void testTemporarySuffixIsUnset() throws IOException {
+        testRunner.enqueue("data");
+        testRunner.run();
+
+        verify(diskShare, never()).open(
+            any(String.class),
+            anySet(),
+            anySet(),
+            anySet(),
+            any(SMB2CreateDisposition.class),
+            anySet()
+        );
+    }
+
+    @Test
+    public void testTemporarySuffixIsSet() throws IOException {
+        final String suffix = ".test";
+
+        testRunner.setProperty(PutSmbFile.RENAME_SUFFIX, suffix);
+        testRunner.setProperty(PutSmbFile.CREATE_DIRS, "true");
+        testRunner.enqueue("data");
+        testRunner.run();
+
+        ArgumentCaptor<String> filename = ArgumentCaptor.forClass(String.class);
+
+        verify(diskShare, times(1)).open(
+            filename.capture(),
+            anySet(),
+            anySet(),
+            anySet(),
+            any(SMB2CreateDisposition.class),
+            anySet()
+        );
+
+        assertTrue(filename.getValue().endsWith(suffix), "Suffix is not present");
+    }
+
+    @Test
+    public void testTemporarySuffixIsSetRenameIsCalled() throws IOException {
+        final String suffix = ".test";
+
+        testRunner.setProperty(PutSmbFile.RENAME_SUFFIX, suffix);
+        testRunner.setProperty(PutSmbFile.CREATE_DIRS, "true");
+        testRunner.enqueue("data");
+        testRunner.run();
+
+        ArgumentCaptor<String> initialFilename = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> finalFilename = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<Boolean> replace = ArgumentCaptor.forClass(Boolean.class);
+
+        verify(diskShare, times(1)).open(
+            initialFilename.capture(),
+            anySet(),
+            anySet(),
+            anySet(),
+            any(SMB2CreateDisposition.class),
+            anySet()
+        );
+
+        verify(diskEntry, times(1)).rename(
+            finalFilename.capture(),
+            replace.capture()
+        );
+
+        assertTrue(initialFilename.getValue().endsWith(suffix), "Suffix is not present and it should be");
+        assertTrue(!finalFilename.getValue().endsWith(suffix), "Suffix is present and it shouldn't be");
+        assertTrue(replace.getValue(), "Replace flag shold be true");
     }
 
     @Test
