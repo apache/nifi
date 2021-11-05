c2d8eed1318f4fbff507ad0ecde62e657b4818b5
Chris Sampson <chris.sampson82@gmail.com>
2021-11-05 10:14:41 +0000

    Allow override of DISTRO_PATH for NiFi dockerhub build to enable image creation from the Apache Dev server during Release Candidate voting

--- nifi-docker/dockerhub/DockerBuild.sh
+++ nifi-docker/dockerhub/DockerBuild.sh
@@ -22,9 +22,13 @@ DOCKER_UID="${1:-1000}"
 DOCKER_GID="${2:-1000}"
 MIRROR="${3:-https://archive.apache.org/dist}"
 BASE="${4:-${MIRROR}}"
+DISTRO_PATH="${5:-}"
 
 DOCKER_IMAGE="$(grep -Ev '(^#|^\s*$|^\s*\t*#)' DockerImage.txt)"
 NIFI_IMAGE_VERSION="$(echo "${DOCKER_IMAGE}" | cut -d : -f 2)"
+if [ -z "${DISTRO_PATH}" ]; then
+  DISTRO_PATH="${NIFI_VERSION}"
+fi
 
-echo "Building NiFi Image: '${DOCKER_IMAGE}' Version: '${NIFI_IMAGE_VERSION}' Mirror: '${MIRROR}' Base: '${BASE} User/Group: '${DOCKER_UID}/${DOCKER_GID}'"
-docker build --build-arg UID="${DOCKER_UID}" --build-arg GID="${DOCKER_GID}" --build-arg NIFI_VERSION="${NIFI_IMAGE_VERSION}" --build-arg MIRROR_BASE_URL="${MIRROR}" --build-arg BASE_URL="${BASE}" -t "${DOCKER_IMAGE}" .
+echo "Building NiFi Image: '${DOCKER_IMAGE}' Version: '${NIFI_IMAGE_VERSION}' Mirror: '${MIRROR}' Base: '${BASE} Path: '${DISTRO_PATH}' User/Group: '${DOCKER_UID}/${DOCKER_GID}'"
+docker build --build-arg UID="${DOCKER_UID}" --build-arg GID="${DOCKER_GID}" --build-arg NIFI_VERSION="${NIFI_IMAGE_VERSION}" --build-arg MIRROR_BASE_URL="${MIRROR}" --build-arg BASE_URL="${BASE}" --build-arg DISTRO_PATH="${DISTRO_PATH}" -t "${DOCKER_IMAGE}" .
