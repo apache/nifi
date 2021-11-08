ce91209d6d45ec11a1498bdac31f9d2ee6db3519
Peter Turcsanyi <turcsanyi@apache.org>
2021-11-08 17:56:40 +0100

    NIFI-9368: Fixed the ordering of the log entries on the processor level bulletin pop-up
    Signed-off-by: Pierre Villard <pierre.villard.fr@gmail.com>
    
    This closes #5512.

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-components/src/main/java/org/apache/nifi/events/VolatileBulletinRepository.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-components/src/main/java/org/apache/nifi/events/VolatileBulletinRepository.java
@@ -179,6 +179,7 @@ public class VolatileBulletinRepository implements BulletinRepository {
             }
 
             final List<Bulletin> bulletinsForComponent = ringBuffer.getSelectedElements(filter, max);
+            Collections.sort(bulletinsForComponent);
             return bulletinsForComponent;
         }
 
