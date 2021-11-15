f864722a9f08161c38035e6c32d0f6634d76cc72
Chris Sampson <chris.sampson82@gmail.com>
2021-11-15 14:56:59 +0000

    Rationalise nifi-docker builds and integration-tests (remove duplicate script directories)

--- nifi-docker/dockermaven/integration-test.sh
+++ nifi-docker/dockermaven/integration-test.sh
@@ -20,7 +20,11 @@ set -exuo pipefail
 TAG=$1
 VERSION=$2
 
-trap '{ docker ps -qaf Name=nifi-${TAG}-integration-test | xargs docker rm -f; }' EXIT
+
+trap '{ docker ps -qaf Name="nifi-${TAG}-integration-test" | xargs --no-run-if-empty docker rm -f; }' EXIT
+
+echo "Deleting any existing nifi-${TAG}-integration-test containers"
+docker ps -qaf Name="nifi-${TAG}-integration-test" | xargs --no-run-if-empty docker rm -f;
 
 echo "Checking that all files are owned by NiFi"
 test -z "$(docker run --rm --entrypoint /bin/bash "apache/nifi:${TAG}" -c "find /opt/nifi ! -user nifi")"
@@ -41,7 +45,7 @@ IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}
 
 for i in $(seq 1 10) :; do
     echo "Iteration: ${i}"
-    if docker exec "nifi-${TAG}-integration-test" bash -c "ss -ntl | grep 8443"; then
+    if docker exec "nifi-${TAG}-integration-test" bash -c " echo Running < /dev/tcp/${IP}/8443"; then
         break
     fi
     sleep 10
