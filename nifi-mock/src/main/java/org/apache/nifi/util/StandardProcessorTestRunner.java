52494fbd81fe63a153b4e130d76ec0a3758361d9
Peter Turcsanyi <turcsanyi@apache.org>
2021-11-23 23:31:07 +0100

    NIFI-9391: Modified MergeRecord to process FlowFiles within a loop in a single onTrigger
    MergeRecord processed the FlowFiles in multiple onTrigger-s and it needed an extra onTrigger call
    (with no incoming FFs) to realize that no more FFs are available and it is time to send the merged FF downstream.
    It was not compatible with Stateless Runtime which does not trigger the flow any more if no FFs available.
    
    Also changed "unschedule" logic in StandardProcessorTestRunner: @OnUnscheduled methods were called immediately after
    the 1st FlowFile was processed. Unschedule the processor only at the end of the execution (onTrigger finished)
    and only if stopOnFinish has been requested by the test case.

--- nifi-mock/src/main/java/org/apache/nifi/util/StandardProcessorTestRunner.java
+++ nifi-mock/src/main/java/org/apache/nifi/util/StandardProcessorTestRunner.java
@@ -221,28 +221,18 @@ public class StandardProcessorTestRunner implements TestRunner {
             } catch (final InterruptedException e1) {
             }
 
-            int finishedCount = 0;
-            boolean unscheduledRun = false;
             for (final Future<Throwable> future : futures) {
                 try {
                     final Throwable thrown = future.get(); // wait for the result
                     if (thrown != null) {
                         throw new AssertionError(thrown);
                     }
-
-                    if (++finishedCount == 1) {
-                        unscheduledRun = true;
-                        unSchedule();
-                    }
                 } catch (final Exception e) {
                 }
             }
 
-            if (!unscheduledRun) {
-                unSchedule();
-            }
-
             if (stopOnFinish) {
+                unSchedule();
                 stop();
             }
         } finally {
