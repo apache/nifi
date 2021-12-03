98d23de7e9312600fe5daa2bff5a2ffb45e3daf8
exceptionfactory <exceptionfactory@apache.org>
2021-12-03 15:30:11 -0600

    NIFI-9435 Added registries and names include parameters to Flow Metrics
    - Added optional includedRegistries query parameter to Flow Metrics Resource method supporting one or more registries
    - Added optional includedNames query parameter to Flow Metrics Resource method supporting one or more metric family names
    - Added sampleName and sampleLabelValue optional pattern parameters
    - Added FilteringMetricFamilySamplesEnumeration to support streamed filtering
    - Added PrometheusMetricsWriter and TextFormat implementation
    
    Signed-off-by: Joe Gresock <jgresock@gmail.com>
    
    This closes #5571.

---
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-api/src/main/java/org/apache/nifi/web/api/request/FlowMetricsRegistry.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.web.api.request;
+
+import org.apache.nifi.prometheus.util.AbstractMetricsRegistry;
+import org.apache.nifi.prometheus.util.BulletinMetricsRegistry;
+import org.apache.nifi.prometheus.util.ConnectionAnalyticsMetricsRegistry;
+import org.apache.nifi.prometheus.util.JvmMetricsRegistry;
+import org.apache.nifi.prometheus.util.NiFiMetricsRegistry;
+
+/**
+ * Flow Metrics Registries
+ */
+public enum FlowMetricsRegistry {
+    NIFI("NIFI", NiFiMetricsRegistry.class),
+
+    JVM("JVM", JvmMetricsRegistry.class),
+
+    BULLETIN("BULLETIN", BulletinMetricsRegistry.class),
+
+    CONNECTION("CONNECTION", ConnectionAnalyticsMetricsRegistry.class);
+
+    private final String registry;
+
+    private final Class<? extends AbstractMetricsRegistry> registryClass;
+
+    FlowMetricsRegistry(final String registry, final Class<? extends AbstractMetricsRegistry> registryClass) {
+        this.registry = registry;
+        this.registryClass = registryClass;
+    }
+
+    public String getRegistry() {
+        return registry;
+    }
+
+    public Class<? extends AbstractMetricsRegistry> getRegistryClass() {
+        return registryClass;
+    }
+}
