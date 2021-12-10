264a6f76007cf03862f0e1aae4a66615dfa394c0
Paul Grey <greyp9@yahoo.com>
2021-12-10 18:41:43 -0500

    NIFI-9330 - Kafka 3.0 support

---
+++ nifi-nar-bundles/nifi-kafka-bundle/nifi-kafka-3-0-processors/src/test/java/org/apache/nifi/processors/kafka/pubsub/ITKafkaEmbedded.java
@@ -0,0 +1,319 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.processors.kafka.pubsub;
+
+import kafka.server.KafkaConfig;
+import kafka.server.KafkaServer;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryOneTime;
+import org.apache.curator.test.TestingServer;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.CreateTopicsResult;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.KafkaFuture;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.utils.Time;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scala.Option;
+
+import java.io.File;
+import java.net.URL;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Queue;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+/**
+ * Test simple interaction with dynamic instance of Kafka 3.0 service.  Kafka service is started and stopped in
+ * the context of the unit test.  To avoid port conflicts with any running Kafka instance on localhost, non-default
+ * ports are used.
+ *
+ * This test might be useful in distinguishing NiFi client library usage issues from issues with the kafka client
+ * libraries themselves.
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ITKafkaEmbedded {
+    private static final Logger logger = LoggerFactory.getLogger(ITKafkaEmbedded.class);
+
+    private static TestingServer zookeeperServer;
+    private static CuratorFramework curatorFramework;
+    private static KafkaServer kafkaServer;
+
+    /**
+     * The Kafka endpoint for the test class.
+     */
+    private static final int ZOOKEEPER_PORT = 2182;  // use non-default (2181) to avoid conflict with running instance
+    private static final int BOOTSTRAP_PORT = 9192;  // use non-default (9092) to avoid conflict with running instance
+    private static final String BOOTSTRAP_SERVER = String.format("localhost:%d", BOOTSTRAP_PORT);
+
+    /**
+     * Ensure fresh data for each test run.
+     */
+    private static final long TIMESTAMP = System.currentTimeMillis();
+
+    /**
+     * The name of the test kafka topic to be created.
+     */
+    private static final String TOPIC_NAME = "nifi-events-" + TIMESTAMP;
+
+    /**
+     * Number of test events to send.
+     */
+    private static final int EVENT_COUNT = 3;
+
+    /**
+     * Target folder for embedded Kafka server state.
+     */
+    private static File kafkaFolderIT;
+
+    /**
+     * Test framework should be configured to use Kafka 3 release.
+     */
+    @Test
+    public void testKafkaVersion() {
+        final Class<KafkaServer> c = KafkaServer.class;
+        final URL url = c.getProtectionDomain().getCodeSource().getLocation();
+        Assert.assertEquals("file", url.getProtocol());
+        final String filename = new File(url.getFile()).getName();
+        Assert.assertTrue(filename.contains("3.0.0"));
+    }
+
+    /**
+     * Validate expectations of default Kafka configuration values.
+     */
+    @Test
+    public void testKafkaConfig() {
+        final Map<String, String> properties = new TreeMap<>();
+        properties.put(KafkaConfig.ZkConnectProp(), "");
+        final KafkaConfig config = new KafkaConfig(properties);
+        final String kafkaPathDefault = config.getString(KafkaConfig.LogDirProp());
+        Assert.assertNotNull(kafkaPathDefault);
+        final File folderKafkaDefault = new File(kafkaPathDefault);
+        Assert.assertTrue(folderKafkaDefault.getParentFile().exists());
+    }
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        logger.trace("ENTERING beforeClass()");
+        zookeeperServer = new TestingServer(ZOOKEEPER_PORT);
+        curatorFramework = CuratorFrameworkFactory.newClient(
+                zookeeperServer.getConnectString(), new RetryOneTime(2000));
+        curatorFramework.start();
+
+        final Map<String, String> properties = new TreeMap<>();
+        properties.put(KafkaConfig.ZkConnectProp(), zookeeperServer.getConnectString());
+        properties.put(KafkaConfig.ZkConnectionTimeoutMsProp(), "5000");
+
+        // redirect Kafka target folder to a custom location for this test
+        final KafkaConfig configGetDefault = new KafkaConfig(properties);
+        final String kafkaPathDefault = configGetDefault.getString(KafkaConfig.LogDirProp());
+        Assert.assertNotNull(kafkaPathDefault);
+        final File folderKafkaDefault = new File(kafkaPathDefault);
+        Assert.assertTrue(folderKafkaDefault.getParentFile().exists());
+        kafkaFolderIT = new File(folderKafkaDefault.getParentFile(),
+                String.format("%s-%d", folderKafkaDefault.getName(), TIMESTAMP));
+        logger.debug(kafkaFolderIT.getPath());
+
+        properties.put(KafkaConfig.ListenersProp(), String.format("PLAINTEXT://:%d", BOOTSTRAP_PORT));
+        properties.put(KafkaConfig.LogDirProp(), kafkaFolderIT.getPath());
+        properties.put(KafkaConfig.LogDirsProp(), kafkaFolderIT.getPath());
+        properties.put(KafkaConfig.OffsetsTopicReplicationFactorProp(), "1");
+        properties.put(KafkaConfig.GroupInitialRebalanceDelayMsProp(), "0");
+
+        final KafkaConfig config = new KafkaConfig(properties);
+        kafkaServer = new KafkaServer(config, Time.SYSTEM, Option.empty(), false);
+        kafkaServer.startup();
+        logger.trace("EXITING beforeClass()");
+    }
+
+    @Test
+    public void testKafka3_StepA_CreateTopic() throws ExecutionException, InterruptedException {
+        logger.trace("ENTERING testKafka3_StepA_CreateTopic(): {}", TOPIC_NAME);
+        final Properties properties = new Properties();
+        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+        final AdminClient adminClient = AdminClient.create(properties);
+        final NewTopic newTopic = new NewTopic(TOPIC_NAME, 1, (short) 1);
+        final CreateTopicsResult topics = adminClient.createTopics(Collections.singleton(newTopic));
+        final KafkaFuture<Void> topicFuture = topics.values().get(TOPIC_NAME);
+        topicFuture.get();  // throw on commit failure
+        logger.trace("EXITING testKafka3_StepA_CreateTopic(): {}", TOPIC_NAME);
+    }
+
+    @Test
+    public void testKafka3_StepB_ProducerWriteMessage() throws ExecutionException, InterruptedException {
+        logger.trace("ENTERING testKafka3_Step1_ProducerWriteMessage()");
+        final long timestamp = System.currentTimeMillis();
+        final Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        final KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        for (int i = 0; (i < EVENT_COUNT); ++i) {
+            final ProducerRecord<String, String> record = new ProducerRecord<>(
+                    TOPIC_NAME, "mykey", toDateString(timestamp));
+            logger.debug("RECORD TO SEND: {}", record.value());
+            final Future<RecordMetadata> future = producer.send(record);
+            final RecordMetadata metadata = future.get();
+            logger.debug("RECORD SENT: OFFSET={}  TIMESTAMP={}",
+                    metadata.hasOffset() ? metadata.offset() : "",
+                    metadata.hasTimestamp() ? toDateString(metadata.timestamp()) : "");
+            Assert.assertTrue(metadata.hasOffset());
+            Assert.assertTrue(metadata.hasTimestamp());
+            Assert.assertEquals(i, metadata.offset());
+            Assert.assertTrue(timestamp < metadata.timestamp());
+            Assert.assertTrue(System.currentTimeMillis() > metadata.timestamp());
+        }
+        producer.close();
+        logger.trace("EXITING testKafka3_StepB_ProducerWriteMessage()");
+    }
+
+    @Test
+    public void testKafka3_StepC1_ConsumerReadMessageSubscribe() {
+        logger.trace("ENTERING testKafka3_StepC1_ConsumerReadMessageSubscribe()");
+        final Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "my-consumer-group");
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, Boolean.FALSE.toString());
+        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        final KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Collections.singletonList(TOPIC_NAME));
+        final Set<String> subscription = consumer.subscription();
+        Assert.assertEquals(1, subscription.size());
+        final List<PartitionInfo> partitionInfos = consumer.partitionsFor(TOPIC_NAME);
+        Assert.assertEquals(1, partitionInfos.size());
+        final Collection<ConsumerRecord<String, String>> records = queryRecords(consumer, 10);
+        Assert.assertEquals(EVENT_COUNT, records.size());
+        consumer.unsubscribe();
+        Assert.assertEquals(0, consumer.assignment().size());
+        logger.trace("EXITING testKafka3_StepC1_ConsumerReadMessageSubscribe()");
+    }
+
+    @Test
+    public void testKafka3_StepC2_ConsumerReadMessageAssign() {
+        logger.trace("ENTERING testKafka3_StepC2_ConsumerReadMessageAssign()");
+        final Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "my-consumer-group");
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, Boolean.FALSE.toString());
+        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        final KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+        final TopicPartition partition0 = new TopicPartition(TOPIC_NAME, 0);
+        consumer.assign(Collections.singletonList(partition0));
+        final Set<TopicPartition> assignments = consumer.assignment();
+        Assert.assertEquals(1, assignments.size());
+        final List<PartitionInfo> partitionInfos = consumer.partitionsFor(TOPIC_NAME);
+        Assert.assertEquals(1, partitionInfos.size());
+        final Collection<ConsumerRecord<String, String>> records = queryRecords(consumer, 10);
+        Assert.assertEquals(EVENT_COUNT, records.size());
+        consumer.unsubscribe();
+        Assert.assertEquals(0, consumer.assignment().size());
+        logger.trace("EXITING testKafka3_StepC2_ConsumerReadMessageAssign()");
+    }
+
+    private Collection<ConsumerRecord<String, String>> queryRecords(
+            final KafkaConsumer<String, String> consumer, final int iterations) {
+        final Collection<ConsumerRecord<String, String>> records = new ArrayList<>();
+        for (int i = 0; (i < iterations); ++i) {
+            final ConsumerRecords<String, String> recordsIt = consumer.poll(Duration.ofMillis(100));
+            for (final ConsumerRecord<String, String> record : recordsIt) {
+                records.add(record);
+                logger.debug("RECORD @ {}/{} {}={}", record.partition(), record.offset(), record.key(), record.value());
+            }
+        }
+        return records;
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        logger.trace("ENTERING afterClass()");
+        kafkaServer.shutdown();
+        curatorFramework.close();
+        zookeeperServer.stop();
+        discardTestState(kafkaFolderIT);
+        logger.trace("EXITING afterClass()");
+    }
+
+    private String toDateString(final long timestampMillis) {
+        return ZonedDateTime.ofInstant(Instant.ofEpochMilli(timestampMillis),
+                ZoneId.of(ZoneOffset.UTC.getId())).format(DateTimeFormatter.ISO_INSTANT);
+    }
+
+    private static void discardTestState(final File folder) {
+        logger.debug(folder.getPath());
+        final Queue<File> filesToDiscard = new LinkedList<>(Collections.singleton(folder));
+        while (!filesToDiscard.isEmpty()) {
+            final File file = filesToDiscard.poll();
+            logger.trace("POLL {}", file.getPath());
+            if (file.isDirectory()) {
+                final File[] filesChild = file.listFiles();
+                if ((filesChild == null) || (filesChild.length == 0)) {
+                    final boolean delete = file.delete();
+                    logger.trace("DELETE {} {}", file.getPath(), delete);
+                } else {
+                    filesToDiscard.addAll(Arrays.asList(filesChild));
+                    filesToDiscard.add(file);
+                }
+            } else if (file.isFile()) {
+                final boolean delete = file.delete();
+                logger.trace("DELETE {} {}", file.getPath(), delete);
+            }
+        }
+    }
+}
---
+++ nifi-nar-bundles/nifi-kafka-bundle/nifi-kafka-3-0-processors/src/test/java/org/apache/nifi/processors/kafka/pubsub/TestConsumeKafka_3_0.java
@@ -0,0 +1,168 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.processors.kafka.pubsub;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.apache.nifi.kerberos.KerberosCredentialsService;
+import org.apache.nifi.kerberos.KerberosUserService;
+import org.apache.nifi.kerberos.SelfContainedKerberosUserService;
+import org.apache.nifi.reporting.InitializationException;
+import org.apache.nifi.util.TestRunner;
+import org.apache.nifi.util.TestRunners;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TestConsumeKafka_3_0 {
+
+    ConsumerLease mockLease = null;
+    ConsumerPool mockConsumerPool = null;
+
+    @Before
+    public void setup() {
+        mockLease = mock(ConsumerLease.class);
+        mockConsumerPool = mock(ConsumerPool.class);
+    }
+
+    @Test
+    public void validateCustomValidatorSettings() throws Exception {
+        ConsumeKafka_3_0 consumeKafka = new ConsumeKafka_3_0();
+        TestRunner runner = TestRunners.newTestRunner(consumeKafka);
+        runner.setProperty(KafkaProcessorUtils.BOOTSTRAP_SERVERS, "okeydokey:1234");
+        runner.setProperty(ConsumeKafka_3_0.TOPICS, "foo");
+        runner.setProperty(ConsumeKafka_3_0.GROUP_ID, "foo");
+        runner.setProperty(ConsumeKafka_3_0.AUTO_OFFSET_RESET, ConsumeKafka_3_0.OFFSET_EARLIEST);
+        runner.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
+        runner.assertValid();
+        runner.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "Foo");
+        runner.assertNotValid();
+        runner.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
+        runner.assertValid();
+        runner.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        runner.assertValid();
+        runner.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        runner.assertNotValid();
+    }
+
+    @Test
+    public void validatePropertiesValidation() throws Exception {
+        ConsumeKafka_3_0 consumeKafka = new ConsumeKafka_3_0();
+        TestRunner runner = TestRunners.newTestRunner(consumeKafka);
+        runner.setProperty(KafkaProcessorUtils.BOOTSTRAP_SERVERS, "okeydokey:1234");
+        runner.setProperty(ConsumeKafka_3_0.TOPICS, "foo");
+        runner.setProperty(ConsumeKafka_3_0.GROUP_ID, "foo");
+        runner.setProperty(ConsumeKafka_3_0.AUTO_OFFSET_RESET, ConsumeKafka_3_0.OFFSET_EARLIEST);
+
+        runner.removeProperty(ConsumeKafka_3_0.GROUP_ID);
+        try {
+            runner.assertValid();
+            fail();
+        } catch (AssertionError e) {
+            assertTrue(e.getMessage().contains("invalid because Group ID is required"));
+        }
+
+        runner.setProperty(ConsumeKafka_3_0.GROUP_ID, "");
+        try {
+            runner.assertValid();
+            fail();
+        } catch (AssertionError e) {
+            assertTrue(e.getMessage().contains("must contain at least one character that is not white space"));
+        }
+
+        runner.setProperty(ConsumeKafka_3_0.GROUP_ID, "  ");
+        try {
+            runner.assertValid();
+            fail();
+        } catch (AssertionError e) {
+            assertTrue(e.getMessage().contains("must contain at least one character that is not white space"));
+        }
+    }
+
+    @Test
+    public void testJaasGssApiConfiguration() throws Exception {
+        ConsumeKafka_3_0 consumeKafka = new ConsumeKafka_3_0();
+        TestRunner runner = TestRunners.newTestRunner(consumeKafka);
+        runner.setProperty(KafkaProcessorUtils.BOOTSTRAP_SERVERS, "okeydokey:1234");
+        runner.setProperty(ConsumeKafka_3_0.TOPICS, "foo");
+        runner.setProperty(ConsumeKafka_3_0.GROUP_ID, "foo");
+        runner.setProperty(ConsumeKafka_3_0.AUTO_OFFSET_RESET, ConsumeKafka_3_0.OFFSET_EARLIEST);
+
+        runner.setProperty(KafkaProcessorUtils.SECURITY_PROTOCOL, KafkaProcessorUtils.SEC_SASL_PLAINTEXT);
+        runner.setProperty(KafkaProcessorUtils.SASL_MECHANISM, KafkaProcessorUtils.GSSAPI_VALUE);
+        runner.assertNotValid();
+
+        runner.setProperty(KafkaProcessorUtils.JAAS_SERVICE_NAME, "kafka");
+        runner.assertNotValid();
+
+        runner.setProperty(KafkaProcessorUtils.USER_PRINCIPAL, "nifi@APACHE.COM");
+        runner.assertNotValid();
+
+        runner.setProperty(KafkaProcessorUtils.USER_KEYTAB, "not.A.File");
+        runner.assertNotValid();
+
+        runner.setProperty(KafkaProcessorUtils.USER_KEYTAB, "src/test/resources/server.properties");
+        runner.assertValid();
+
+        runner.setVariable("keytab", "src/test/resources/server.properties");
+        runner.setVariable("principal", "nifi@APACHE.COM");
+        runner.setVariable("service", "kafka");
+        runner.setProperty(KafkaProcessorUtils.USER_PRINCIPAL, "${principal}");
+        runner.setProperty(KafkaProcessorUtils.USER_KEYTAB, "${keytab}");
+        runner.setProperty(KafkaProcessorUtils.JAAS_SERVICE_NAME, "${service}");
+        runner.assertValid();
+
+        final KerberosUserService kerberosUserService = enableKerberosUserService(runner);
+        runner.setProperty(KafkaProcessorUtils.SELF_CONTAINED_KERBEROS_USER_SERVICE, kerberosUserService.getIdentifier());
+        runner.assertNotValid();
+
+        runner.removeProperty(KafkaProcessorUtils.USER_PRINCIPAL);
+        runner.removeProperty(KafkaProcessorUtils.USER_KEYTAB);
+        runner.assertValid();
+
+        final KerberosCredentialsService kerberosCredentialsService = enabledKerberosCredentialsService(runner);
+        runner.setProperty(KafkaProcessorUtils.KERBEROS_CREDENTIALS_SERVICE, kerberosCredentialsService.getIdentifier());
+        runner.assertNotValid();
+
+        runner.removeProperty(KafkaProcessorUtils.SELF_CONTAINED_KERBEROS_USER_SERVICE);
+        runner.assertValid();
+    }
+
+    private SelfContainedKerberosUserService enableKerberosUserService(final TestRunner runner) throws InitializationException {
+        final SelfContainedKerberosUserService kerberosUserService = mock(SelfContainedKerberosUserService.class);
+        when(kerberosUserService.getIdentifier()).thenReturn("userService1");
+        runner.addControllerService(kerberosUserService.getIdentifier(), kerberosUserService);
+        runner.enableControllerService(kerberosUserService);
+        return kerberosUserService;
+    }
+
+    private KerberosCredentialsService enabledKerberosCredentialsService(final TestRunner runner) throws InitializationException {
+        final KerberosCredentialsService credentialsService = mock(KerberosCredentialsService.class);
+        when(credentialsService.getIdentifier()).thenReturn("credsService1");
+        when(credentialsService.getPrincipal()).thenReturn("principal1");
+        when(credentialsService.getKeytab()).thenReturn("keytab1");
+
+        runner.addControllerService(credentialsService.getIdentifier(), credentialsService);
+        runner.enableControllerService(credentialsService);
+        return credentialsService;
+    }
+
+}
