20392d5ef417b71968ef1e8de553a45367e19f33
Mark Payne <markap14@hotmail.com>
2021-12-18 13:34:16 -0500

    NIFI-9503: Fixed issue in system-tests
    - Fixed system-tests workflow so that troubleshooting info is always uploaded for failures
    
    This closes #5614
    
    Signed-off-by: David Handermann <exceptionfactory@apache.org>

--- nifi-system-tests/nifi-system-test-suite/src/test/java/org/apache/nifi/tests/system/controllerservice/ControllerServiceApiValidationIT.java
+++ nifi-system-tests/nifi-system-test-suite/src/test/java/org/apache/nifi/tests/system/controllerservice/ControllerServiceApiValidationIT.java
@@ -87,26 +87,27 @@ public class ControllerServiceApiValidationIT extends NiFiSystemIT {
     }
 
     @Test
-    public void testNonMatchingDynamicPropertyControllerService() throws NiFiClientException, IOException {
-        final ControllerServiceEntity fakeServiceEntity = getClientUtil().createControllerService(
+    public void testNonMatchingDynamicPropertyControllerService() throws NiFiClientException, IOException, InterruptedException {
+        final ControllerServiceEntity controllerService = getClientUtil().createControllerService(
                 NiFiSystemIT.TEST_CS_PACKAGE + ".FakeControllerService2",
                 "root",
                 NiFiSystemIT.NIFI_GROUP_ID,
                 "nifi-system-test-extensions2-nar",
                 getNiFiVersion());
-        final ProcessorEntity fakeProcessorEntity = getClientUtil().createProcessor("FakeDynamicPropertiesProcessor");
-        fakeProcessorEntity.getComponent().getConfig().setProperties(Collections.singletonMap("FCS.fakeControllerService", fakeServiceEntity.getId()));
-        getNifiClient().getProcessorClient().updateProcessor(fakeProcessorEntity);
+
+        final ProcessorEntity processor = getClientUtil().createProcessor("FakeDynamicPropertiesProcessor");
+        processor.getComponent().getConfig().setProperties(Collections.singletonMap("FCS.fakeControllerService", controllerService.getId()));
+        getNifiClient().getProcessorClient().updateProcessor(processor);
         final ControllerServiceRunStatusEntity runStatusEntity = new ControllerServiceRunStatusEntity();
         runStatusEntity.setState("ENABLED");
-        runStatusEntity.setRevision(fakeServiceEntity.getRevision());
-        getNifiClient().getControllerServicesClient().activateControllerService(fakeServiceEntity.getId(), runStatusEntity);
+        runStatusEntity.setRevision(controllerService.getRevision());
+        getNifiClient().getControllerServicesClient().activateControllerService(controllerService.getId(), runStatusEntity);
         getClientUtil().waitForControllerSerivcesEnabled("root");
-        String controllerStatus = getNifiClient().getControllerServicesClient().getControllerService(fakeServiceEntity.getId()).getStatus().getRunStatus();
-        String processorStatus = getNifiClient().getProcessorClient().getProcessor(fakeProcessorEntity.getId()).getStatus().getRunStatus();
 
+        final String controllerStatus = getNifiClient().getControllerServicesClient().getControllerService(controllerService.getId()).getStatus().getRunStatus();
         assertEquals("ENABLED", controllerStatus);
-        assertEquals("Invalid", processorStatus);
+
+        getClientUtil().waitForInvalidProcessor(processor.getId());
     }
 
     @Test
