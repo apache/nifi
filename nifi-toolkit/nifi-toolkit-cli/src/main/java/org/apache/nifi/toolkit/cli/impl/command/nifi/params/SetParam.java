c21b99a9e0ee3f3b6c2084532824c8157ea6d43f
Joe Gresock <jgresock@gmail.com>
2022-02-10 05:33:55 -0500

    NIFI-9668: Adding informative error when setting same value in 'set-param' CLI command
    Signed-off-by: Pierre Villard <pierre.villard.fr@gmail.com>
    
    This closes #5757.

--- nifi-toolkit/nifi-toolkit-cli/src/main/java/org/apache/nifi/toolkit/cli/impl/command/nifi/params/SetParam.java
+++ nifi-toolkit/nifi-toolkit-cli/src/main/java/org/apache/nifi/toolkit/cli/impl/command/nifi/params/SetParam.java
@@ -88,6 +88,10 @@ public class SetParam extends AbstractUpdateParamContextCommand<VoidResult> {
             throw new IllegalArgumentException("A parameter value is required when creating a new parameter");
         }
 
+        if (existingParam.isPresent() && existingParam.get().getValue().equals(paramValue)) {
+            throw new IllegalArgumentException(String.format("Parameter value supplied for parameter [%s] is the same as its current value", paramName));
+        }
+
         // Construct the objects for the update...
         final ParameterDTO parameterDTO = existingParam.isPresent() ? existingParam.get() : new ParameterDTO();
         parameterDTO.setName(paramName);
