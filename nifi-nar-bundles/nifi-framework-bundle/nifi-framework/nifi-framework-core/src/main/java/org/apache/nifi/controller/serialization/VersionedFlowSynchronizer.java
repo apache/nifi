1bbf55e8d7d801402c8c9acd282e472cd5f0359d
Mark Payne <markap14@hotmail.com>
2022-02-24 12:29:53 -0500

    NIFI-9723: Fixed typo in log/exception message

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/main/java/org/apache/nifi/controller/serialization/VersionedFlowSynchronizer.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/main/java/org/apache/nifi/controller/serialization/VersionedFlowSynchronizer.java
@@ -727,7 +727,7 @@ public class VersionedFlowSynchronizer implements FlowSynchronizer {
                 return encryptor.decrypt(value.substring(FlowSerializer.ENC_PREFIX.length(), value.length() - FlowSerializer.ENC_SUFFIX.length()));
             } catch (EncryptionException e) {
                 final String moreDescriptiveMessage = "There was a problem decrypting a sensitive flow configuration value. " +
-                    "Check that the nifi.sensitive.props.key value in nifi.properties matches the value used to encrypt the flow.xml.gz file";
+                    "Check that the nifi.sensitive.props.key value in nifi.properties matches the value used to encrypt the flow.json.gz file";
                 logger.error(moreDescriptiveMessage, e);
                 throw new EncryptionException(moreDescriptiveMessage, e);
             }
