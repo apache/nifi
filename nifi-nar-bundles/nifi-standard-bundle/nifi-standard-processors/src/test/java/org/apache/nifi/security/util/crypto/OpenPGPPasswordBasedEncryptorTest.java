83f137ceeaf1fb9bcf9fc7ba362f8a906a49e9ba
exceptionfactory <exceptionfactory@apache.org>
2022-03-28 23:05:09 -0500

    NIFI-9844 Refactored Encryptor tests using JUnit 5
    - Refactored Keyed and Password Based Encryptor tests from Groovy to Java
    
    Signed-off-by: Nathan Gough <thenatog@gmail.com>
    
    This closes #5913.

--- nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/test/java/org/apache/nifi/security/util/crypto/OpenPGPPasswordBasedEncryptorTest.java
+++ nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/test/java/org/apache/nifi/security/util/crypto/OpenPGPPasswordBasedEncryptorTest.java
@@ -18,125 +18,42 @@ package org.apache.nifi.security.util.crypto;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.security.Security;
-import org.apache.commons.codec.binary.Hex;
-import org.apache.commons.lang3.SystemUtils;
+
 import org.apache.nifi.processor.io.StreamCallback;
 import org.apache.nifi.security.util.EncryptionMethod;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openpgp.PGPEncryptedData;
-import org.bouncycastle.openpgp.PGPUtil;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Assume;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class OpenPGPPasswordBasedEncryptorTest {
-    private static final Logger logger = LoggerFactory.getLogger(OpenPGPPasswordBasedEncryptorTest.class);
+    private static final String FILENAME = OpenPGPPasswordBasedEncryptorTest.class.getSimpleName();
 
-    private final File plainFile = new File("src/test/resources/TestEncryptContent/text.txt");
-    private final File encryptedFile = new File("src/test/resources/TestEncryptContent/text.txt.asc");
+    private static final int CIPHER = PGPEncryptedData.AES_128;
 
-    private static final String PASSWORD = "thisIsABadPassword";
-    private static final String LEGACY_PASSWORD = "Hello, World!";
+    private static final byte[] PLAINTEXT = new byte[]{0, 1, 2, 3, 4, 5, 6, 7, 8};
 
-    @BeforeClass
-    public static void setUpOnce() throws Exception {
-        Assume.assumeTrue("Test only runs on *nix", !SystemUtils.IS_OS_WINDOWS);
-        Security.addProvider(new BouncyCastleProvider());
-    }
-
-    @Before
-    public void setUp() throws Exception {
-
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
-    }
+    private static final String PASSWORD = OpenPGPPasswordBasedEncryptorTest.class.getName();
 
     @Test
-    public void testShouldEncryptAndDecrypt() throws Exception {
+    public void testEncryptDecrypt() throws Exception {
+        final ByteArrayInputStream plainStream = new ByteArrayInputStream(PLAINTEXT);
 
-        for (int i = 1; i<14; i++) {
-            if (PGPEncryptedData.SAFER != i) { // SAFER cipher is not supported and therefore its test is skipped
-                Integer cipher = i;
-                logger.info("Testing PGP encryption with " + PGPUtil.getSymmetricCipherName(cipher) + " cipher.");
+        final OpenPGPPasswordBasedEncryptor encryptor = new OpenPGPPasswordBasedEncryptor(EncryptionMethod.PGP.getAlgorithm(),
+                CIPHER, EncryptionMethod.PGP.getProvider(), PASSWORD.toCharArray(), FILENAME);
 
-                // Arrange
-                final String PLAINTEXT = "This is a plaintext message.";
-                logger.info("Plaintext: {}", PLAINTEXT);
-                InputStream plainStream = new java.io.ByteArrayInputStream(PLAINTEXT.getBytes("UTF-8"));
-                OutputStream cipherStream = new ByteArrayOutputStream();
-                OutputStream recoveredStream = new ByteArrayOutputStream();
+        final StreamCallback encryptionCallback = encryptor.getEncryptionCallback();
+        final StreamCallback decryptionCallback = encryptor.getDecryptionCallback();
 
-                // No file, just streams
-                String filename = "tempFile.txt";
+        final ByteArrayOutputStream encryptedStream = new ByteArrayOutputStream();
+        encryptionCallback.process(plainStream, encryptedStream);
 
-                OpenPGPPasswordBasedEncryptor encryptor = new OpenPGPPasswordBasedEncryptor(EncryptionMethod.PGP.getAlgorithm(),
-                        cipher, EncryptionMethod.PGP.getProvider(), PASSWORD.toCharArray(), filename);
+        final InputStream encryptedInputStream = new ByteArrayInputStream(encryptedStream.toByteArray());
+        final ByteArrayOutputStream decryptedStream = new ByteArrayOutputStream();
+        decryptionCallback.process(encryptedInputStream, decryptedStream);
 
-                StreamCallback encryptionCallback = encryptor.getEncryptionCallback();
-                StreamCallback decryptionCallback = encryptor.getDecryptionCallback();
-
-                // Act
-                encryptionCallback.process(plainStream, cipherStream);
-
-                final byte[] cipherBytes = ((ByteArrayOutputStream) cipherStream).toByteArray();
-                logger.info("Encrypted: {}", Hex.encodeHexString(cipherBytes));
-                InputStream cipherInputStream = new ByteArrayInputStream(cipherBytes);
-
-                decryptionCallback.process(cipherInputStream, recoveredStream);
-
-                // Assert
-                byte[] recoveredBytes = ((ByteArrayOutputStream) recoveredStream).toByteArray();
-                String recovered = new String(recoveredBytes, "UTF-8");
-                logger.info("Recovered: {}", recovered);
-                assert PLAINTEXT.equals(recovered);
-            }
-        }
-    }
-
-    @Test
-    public void testShouldDecryptExternalFile() throws Exception {
-        for (int i = 1; i<14; i++) {
-            if (PGPEncryptedData.SAFER != i) {  // SAFER cipher is not supported and therefore its test is skipped
-                Integer cipher = i;
-                // Arrange
-                byte[] plainBytes = Files.readAllBytes(Paths.get(plainFile.getPath()));
-                final String PLAINTEXT = new String(plainBytes, "UTF-8");
-
-                InputStream cipherStream = new FileInputStream(encryptedFile);
-                OutputStream recoveredStream = new ByteArrayOutputStream();
-
-                // No file, just streams
-                String filename = encryptedFile.getName();
-
-                OpenPGPPasswordBasedEncryptor encryptor = new OpenPGPPasswordBasedEncryptor(EncryptionMethod.PGP.getAlgorithm(), cipher,
-                        EncryptionMethod.PGP.getProvider(), LEGACY_PASSWORD.toCharArray(), filename);
-
-                StreamCallback decryptionCallback = encryptor.getDecryptionCallback();
-
-                // Act
-                decryptionCallback.process(cipherStream, recoveredStream);
-
-                // Assert
-                byte[] recoveredBytes = ((ByteArrayOutputStream) recoveredStream).toByteArray();
-                String recovered = new String(recoveredBytes, "UTF-8");
-                logger.info("Recovered: {}", recovered);
-                Assert.assertEquals("Recovered text", PLAINTEXT, recovered);
-            }
-        }
+        byte[] decryptedBytes = decryptedStream.toByteArray();
+        assertArrayEquals(PLAINTEXT, decryptedBytes);
     }
 }
