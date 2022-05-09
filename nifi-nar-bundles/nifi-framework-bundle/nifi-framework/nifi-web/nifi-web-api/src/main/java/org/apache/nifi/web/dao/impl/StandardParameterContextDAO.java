462af16c81bf5fe8385c5b6af450165ba5edef5f
Chris Sampson <chris.sampson82@gmail.com>
2022-05-09 20:45:34 +0100

    NIFI-9470 This closes #6027. Allow creation of Parameter Context without any Inherited Parameter Contexts
    Signed-off-by: Joe Witt <joewitt@apache.org>

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-api/src/main/java/org/apache/nifi/web/dao/impl/StandardParameterContextDAO.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-api/src/main/java/org/apache/nifi/web/dao/impl/StandardParameterContextDAO.java
@@ -82,9 +82,9 @@ public class StandardParameterContextDAO implements ParameterContextDAO {
 
         final AtomicReference<ParameterContext> parameterContextReference = new AtomicReference<>();
         flowManager.withParameterContextResolution(() -> {
-            final List<String> referencedIds = parameterContextDto.getInheritedParameterContexts().stream()
-                .map(ParameterContextReferenceEntity::getId)
-                .collect(Collectors.toList());
+            final List<String> referencedIds = parameterContextDto.getInheritedParameterContexts() == null
+                    ? new ArrayList<>(0)
+                    : parameterContextDto.getInheritedParameterContexts().stream().map(ParameterContextReferenceEntity::getId).collect(Collectors.toList());
 
             final ParameterContext parameterContext = flowManager.createParameterContext(parameterContextDto.getId(), parameterContextDto.getName(), parameters, referencedIds);
             if (parameterContextDto.getDescription() != null) {
