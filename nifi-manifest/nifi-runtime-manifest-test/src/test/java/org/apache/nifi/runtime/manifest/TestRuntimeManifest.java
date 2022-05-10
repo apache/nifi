e2b57730272d9fb49caa306d104817b8fb504f5b
Bryan Bende <bbende@gmail.com>
2022-05-10 11:11:37 -0400

    NIFI-9969 Add extension restrictions to C2 component definition model
    Update test to verify ListHDFS does not have restrictions
    
    Fix checkstyle issue
    
    Signed-off-by: Matthew Burgess <mattyb149@apache.org>
    
    This closes #6031

--- nifi-manifest/nifi-runtime-manifest-test/src/test/java/org/apache/nifi/runtime/manifest/TestRuntimeManifest.java
+++ nifi-manifest/nifi-runtime-manifest-test/src/test/java/org/apache/nifi/runtime/manifest/TestRuntimeManifest.java
@@ -27,8 +27,10 @@ import org.apache.nifi.c2.protocol.component.api.PropertyDescriptor;
 import org.apache.nifi.c2.protocol.component.api.PropertyResourceDefinition;
 import org.apache.nifi.c2.protocol.component.api.Relationship;
 import org.apache.nifi.c2.protocol.component.api.ReportingTaskDefinition;
+import org.apache.nifi.c2.protocol.component.api.Restriction;
 import org.apache.nifi.c2.protocol.component.api.RuntimeManifest;
 import org.apache.nifi.c2.protocol.component.api.SchedulingDefaults;
+import org.apache.nifi.components.RequiredPermission;
 import org.apache.nifi.components.resource.ResourceCardinality;
 import org.apache.nifi.components.resource.ResourceType;
 import org.apache.nifi.scheduling.SchedulingStrategy;
@@ -39,10 +41,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestRuntimeManifest {
@@ -138,6 +142,24 @@ public class TestRuntimeManifest {
         assertNotNull(resourceDefinition.getResourceTypes());
         assertEquals(1, resourceDefinition.getResourceTypes().size());
         assertEquals(ResourceType.FILE, resourceDefinition.getResourceTypes().stream().findFirst().get());
+
+        assertNull(listHdfsDefinition.isRestricted());
+        assertNull(listHdfsDefinition.getRestrictedExplanation());
+        assertNull(listHdfsDefinition.getExplicitRestrictions());
+
+        // Verify FetchHDFS definition has restrictions
+        final ProcessorDefinition fetchHdfsDefinition = getProcessorDefinition(bundles, "nifi-hadoop-nar",
+                "org.apache.nifi.processors.hadoop.FetchHDFS");
+        assertNotNull(fetchHdfsDefinition.isRestricted());
+        assertTrue(fetchHdfsDefinition.isRestricted());
+
+        final Set<Restriction> restrictions = fetchHdfsDefinition.getExplicitRestrictions();
+        assertNotNull(restrictions);
+        assertEquals(1, restrictions.size());
+
+        final Restriction restriction = restrictions.stream().findFirst().orElse(null);
+        assertEquals(RequiredPermission.READ_DISTRIBUTED_FILESYSTEM.getPermissionLabel(), restriction.getRequiredPermission());
+        assertNotNull(restriction.getExplanation());
 
         // Verify ConsumeKafka_2_6 definition which has properties with dependencies
         final ProcessorDefinition consumeKafkaDefinition = getProcessorDefinition(bundles, "nifi-kafka-2-6-nar",
