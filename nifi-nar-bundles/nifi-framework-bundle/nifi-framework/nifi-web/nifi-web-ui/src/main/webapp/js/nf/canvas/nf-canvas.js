23fe4410378159d50a2b4263336032453371a83a
exceptionfactory <exceptionfactory@apache.org>
2022-06-25 09:15:55 -0500

    NIFI-9849 Added Access Token Expiration resource method
    - Removed Saml2AccessResource and replaced with Access Token Expiration to avoid unnecessary conflicts with SAML login consumer

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-ui/src/main/webapp/js/nf/canvas/nf-canvas.js
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-ui/src/main/webapp/js/nf/canvas/nf-canvas.js
@@ -109,11 +109,11 @@
         urls: {
             api: '../nifi-api',
             accessConfig: '../nifi-api/access/config',
+            accessTokenExpiration: '../nifi-api/access/token/expiration',
             currentUser: '../nifi-api/flow/current-user',
             controllerBulletins: '../nifi-api/flow/controller/bulletins',
             kerberos: '../nifi-api/access/kerberos',
             oidc: '../nifi-api/access/oidc/exchange',
-            saml: '../nifi-api/access/saml/login/exchange',
             revision: '../nifi-api/flow/revision',
             banners: '../nifi-api/flow/banners'
         }
@@ -911,11 +911,17 @@
                             successfulAuthentication(jwt)
                         }).fail(function () {
                             $.ajax({
-                                type: 'POST',
-                                url: config.urls.saml,
-                                dataType: 'text'
-                            }).done(function (jwt) {
-                                successfulAuthentication(jwt)
+                                type: 'GET',
+                                url: config.urls.accessTokenExpiration,
+                                dataType: 'json'
+                            }).done(function (accessTokenExpirationEntity) {
+                                var accessTokenExpiration = accessTokenExpirationEntity.accessTokenExpiration;
+                                // Convert ISO 8601 string to session expiration in seconds
+                                var expiration = Date.parse(accessTokenExpiration.expiration);
+                                var expirationSeconds = expiration / 1000;
+                                var sessionExpiration = Math.round(expirationSeconds);
+                                nfAuthorizationStorage.setToken(sessionExpiration);
+                                deferred.resolve();
                             }).fail(function () {
                                 deferred.reject();
                             });
