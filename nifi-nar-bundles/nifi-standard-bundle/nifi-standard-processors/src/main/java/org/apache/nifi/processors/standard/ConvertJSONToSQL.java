02f452a4c25431f2972c4afdb4a8c50d535aa730
Kirill Morozov <kmor76@googlemail.com>
2022-07-14 10:11:00 +0300

    NIFI-10202 ConvertJSONToSQL issue with primary keys for UPDATE statement from "statement.type" attribute fixed

--- nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/ConvertJSONToSQL.java
+++ nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/ConvertJSONToSQL.java
@@ -282,6 +282,18 @@ public class ConvertJSONToSQL extends AbstractProcessor {
                 .build();
     }
 
+    private boolean isUpdateStatement(String statementType, String statementTypeAttribute) {
+        if (UPDATE_TYPE.equals(statementType)) {
+            return true;
+        }
+        if (USE_ATTR_TYPE.equals(statementType)) {
+            if (UPDATE_TYPE.equals(statementTypeAttribute)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public void onTrigger(final ProcessContext context, final ProcessSession session) throws ProcessException {
         final FlowFile flowFile = session.get();
@@ -298,7 +310,7 @@ public class ConvertJSONToSQL extends AbstractProcessor {
         final String schemaName = context.getProperty(SCHEMA_NAME).evaluateAttributeExpressions(flowFile).getValue();
         final String tableName = context.getProperty(TABLE_NAME).evaluateAttributeExpressions(flowFile).getValue();
         final SchemaKey schemaKey = new SchemaKey(catalog, tableName);
-        final boolean includePrimaryKeys = UPDATE_TYPE.equals(statementType) && updateKeys == null;
+        final boolean includePrimaryKeys = isUpdateStatement(statementType, flowFile.getAttribute(STATEMENT_TYPE_ATTRIBUTE)) && updateKeys == null;
 
         // Is the unmatched column behaviour fail or warning?
         final boolean failUnmappedColumns = FAIL_UNMATCHED_COLUMN.getValue().equalsIgnoreCase(context.getProperty(UNMATCHED_COLUMN_BEHAVIOR).getValue());
