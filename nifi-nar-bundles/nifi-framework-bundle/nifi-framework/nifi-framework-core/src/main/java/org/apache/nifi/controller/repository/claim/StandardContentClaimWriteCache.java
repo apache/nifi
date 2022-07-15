a104e8ec4754b3df069d69bd301a6adb0764a96d
mathemare <mathemare@hotmail.fr>
2022-07-15 11:37:33 +0200

    fixed comment

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/main/java/org/apache/nifi/controller/repository/claim/StandardContentClaimWriteCache.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/main/java/org/apache/nifi/controller/repository/claim/StandardContentClaimWriteCache.java
@@ -85,7 +85,7 @@ public class StandardContentClaimWriteCache implements ContentClaimWriteCache {
         }
 
         if (!(claim instanceof CachableContentClaim)) {
-            // we know that we will only create Content Claims that are of type StandardContentClaim, so if we get anything
+            // we know that we will only create Content Claims that are of type CachableContentClaim, so if we get anything
             // else, just throw an Exception because it is not valid for this Repository
             throw new IllegalArgumentException("Cannot write to " + claim + " because that Content Claim does belong to this Claim Cache");
         }
