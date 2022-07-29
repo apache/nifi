3eed6541adc42d85cc397e1ff5034c97b7f3c492
Emilio Setiadarma <emiliosetiadarma@gmail.com>
2022-07-28 17:02:32 -0700

    NIFI-10293: refactored tests in nifi-jolt-transform-json-ui to use JUnit 5

--- nifi-nar-bundles/nifi-standard-bundle/nifi-jolt-transform-json-ui/src/test/java/org/apache/nifi/web/standard/api/processor/TestProcessorWebUtils.java
+++ nifi-nar-bundles/nifi-standard-bundle/nifi-jolt-transform-json-ui/src/test/java/org/apache/nifi/web/standard/api/processor/TestProcessorWebUtils.java
@@ -22,8 +22,8 @@ import org.apache.nifi.web.HttpServletConfigurationRequestContext;
 import org.apache.nifi.web.HttpServletRequestContext;
 import org.apache.nifi.web.NiFiWebConfigurationContext;
 import org.apache.nifi.web.NiFiWebRequestContext;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.CacheControl;
@@ -32,8 +32,9 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -53,7 +54,7 @@ public class TestProcessorWebUtils {
     }
 
     @Test
-    @Ignore("Capability to get specific client revision has been removed as of commit 05a1d63090d97d06bf823821d1b0e9b5482c7cd2")
+    @Disabled("Capability to get specific client revision has been removed as of commit 05a1d63090d97d06bf823821d1b0e9b5482c7cd2")
     // FIXME java11 Capability to get specific client revision has been removed as of commit 05a1d63090d97d06bf823821d1b0e9b5482c7cd2
     //  Add test case for ProcessorWebUtils.getRequestContext() to verify proper revision is returned
     public void testGetComponentDetailsForProcessorWithSpecificClientRevision(){
@@ -77,7 +78,7 @@ public class TestProcessorWebUtils {
         method.setAccessible(true);
         NiFiWebRequestContext requestContext = (NiFiWebRequestContext) method.invoke(null,"1",mock(HttpServletRequest.class));
         assertTrue(requestContext instanceof HttpServletRequestContext);
-        assertTrue(requestContext.getId().equals("1"));
+        assertEquals("1", requestContext.getId());
 
     }
 
