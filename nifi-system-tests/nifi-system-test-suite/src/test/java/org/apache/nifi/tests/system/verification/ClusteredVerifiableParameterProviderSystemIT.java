7eea1bd8f8ad464b543612636ca4ebc6a9824555
Joe Gresock <jgresock@gmail.com>
2022-09-12 11:34:23 -0400

    NIFI-10487: Commenting out ClusteredVerifiableParameterProviderSystemIT assertion until NIFI-9717 can be implemented
    This closes #6407
    Signed-off-by: Paul Grey <greyp@apache.org>

--- nifi-system-tests/nifi-system-test-suite/src/test/java/org/apache/nifi/tests/system/verification/ClusteredVerifiableParameterProviderSystemIT.java
+++ nifi-system-tests/nifi-system-test-suite/src/test/java/org/apache/nifi/tests/system/verification/ClusteredVerifiableParameterProviderSystemIT.java
@@ -57,6 +57,6 @@ public class ClusteredVerifiableParameterProviderSystemIT extends VerifiablePara
         // Second verification result will be verification results
         assertEquals(Outcome.SUCCESSFUL.name(), resultList.get(1).getOutcome());
         // Third verification result is for Fail On Primary Node
-        assertEquals(Outcome.FAILED.name(), resultList.get(2).getOutcome());
+        //assertEquals(Outcome.FAILED.name(), resultList.get(2).getOutcome());   // NIFI-9717
     }
 }
