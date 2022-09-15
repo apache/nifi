3efcb7d424d4aea8dd6e6951f001639e0edd2e0c
Tanmaya Panda <tanmayapanda@microsoft.com>
2022-09-15 18:04:04 +0530

    removed unused imports

--- nifi-nar-bundles/nifi-adx-bundle/nifi-adx/src/test/java/org/apache/nifi/adx/TestAzureAdxConnectionService.java
+++ nifi-nar-bundles/nifi-adx-bundle/nifi-adx/src/test/java/org/apache/nifi/adx/TestAzureAdxConnectionService.java
@@ -30,7 +30,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestAzureAdxConnectionService {
