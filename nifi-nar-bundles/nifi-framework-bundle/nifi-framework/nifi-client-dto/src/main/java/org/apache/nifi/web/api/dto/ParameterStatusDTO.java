2cb71f15092f273bcfc20e787bbca21b8b09e635
Joe Gresock <jgresock@gmail.com>
2022-09-23 13:44:52 -0400

    NIFI-10473: Removing referencing components check on param provider f… (#6388)
    * NIFI-10473: Removing referencing components check on param provider fetch
    
    * NIFI-10473: Adding parameter status DTO to ParameterProviderDTO
    
    * Allowing parameterStatus to be populated even when no parameters were updated
    
    * Adding ParameterStatus enum for parameter fetching
    
    * Adding MISSING_BUT_REFERENCED ParameterStatus
    
    This closes #6388

---
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-client-dto/src/main/java/org/apache/nifi/web/api/dto/ParameterStatusDTO.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.web.api.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import org.apache.nifi.web.api.entity.ParameterEntity;
+
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType(name = "parameterStatus")
+public class ParameterStatusDTO {
+    private ParameterEntity parameter;
+    private ParameterStatus status;
+
+    @ApiModelProperty("The name of the Parameter")
+    public ParameterEntity getParameter() {
+        return parameter;
+    }
+
+    public void setParameter(final ParameterEntity parameter) {
+        this.parameter = parameter;
+    }
+
+    @ApiModelProperty("Indicates the status of the parameter, compared to the existing parameter context")
+    public ParameterStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ParameterStatus status) {
+        this.status = status;
+    }
+}
