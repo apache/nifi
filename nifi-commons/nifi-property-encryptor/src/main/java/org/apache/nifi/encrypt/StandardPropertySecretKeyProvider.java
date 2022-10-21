f47e92d9ba409866c552a43b4e375bed84129ee3
Nathan Gough <thenatog@gmail.com>
2022-10-21 15:31:18 -0400

    NIFI-9953 - Checkstyle fix.

--- nifi-commons/nifi-property-encryptor/src/main/java/org/apache/nifi/encrypt/StandardPropertySecretKeyProvider.java
+++ nifi-commons/nifi-property-encryptor/src/main/java/org/apache/nifi/encrypt/StandardPropertySecretKeyProvider.java
@@ -16,7 +16,6 @@
  */
 package org.apache.nifi.encrypt;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.nifi.security.util.KeyDerivationFunction;
 import org.apache.nifi.security.util.crypto.Argon2SecureHasher;
 import org.apache.nifi.security.util.crypto.KeyDerivationBcryptSecureHasher;
