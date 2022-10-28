47b8574773c943a47bab1be793e4fda7007c33ef
Yiwei Li <yiweili6@illinois.edu>
2022-10-27 22:37:21 -0500

    Fix flaky test in TestGetHDFSFileInfo

--- nifi-nar-bundles/nifi-hadoop-bundle/nifi-hdfs-processors/src/test/java/org/apache/nifi/processors/hadoop/TestGetHDFSFileInfo.java
+++ nifi-nar-bundles/nifi-hadoop-bundle/nifi-hdfs-processors/src/test/java/org/apache/nifi/processors/hadoop/TestGetHDFSFileInfo.java
@@ -35,6 +35,8 @@ import org.apache.nifi.util.TestRunner;
 import org.apache.nifi.util.TestRunners;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -379,7 +381,7 @@ public class TestGetHDFSFileInfo {
         mff.assertAttributeNotExists("hdfs.status");
 
         final String expected = new String(Files.readAllBytes(Paths.get("src/test/resources/TestGetHDFSFileInfo/testRecursiveGroupAllToAttributes.json")));
-        mff.assertAttributeEquals("hdfs.full.tree", expected);
+        JSONAssert.assertEquals(mff.getAttribute("hdfs.full.tree"), expected, JSONCompareMode.NON_EXTENSIBLE);
     }
 
     @Test
