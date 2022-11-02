889254a9dce63e189388c9921c0edcf605a2dadd
sopan98 <phaltankarsopan@gmail.com>
2022-11-01 20:12:11 -0500

    NIFI-10744 Used LinkedHashMap in TestScanHBase methods
    This closes #6633
    
    Signed-off-by: David Handermann <exceptionfactory@apache.org>

--- nifi-nar-bundles/nifi-hbase-bundle/nifi-hbase-processors/src/test/java/org/apache/nifi/hbase/MockHBaseClientService.java
+++ nifi-nar-bundles/nifi-hbase-bundle/nifi-hbase-processors/src/test/java/org/apache/nifi/hbase/MockHBaseClientService.java
@@ -29,12 +29,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class MockHBaseClientService extends AbstractControllerService implements HBaseClientService {
 
-    private Map<String,ResultCell[]> results = new HashMap<>();
+    private Map<String,ResultCell[]> results = new LinkedHashMap<>();
     private Map<String, List<PutFlowFile>> flowFilePuts = new HashMap<>();
     private boolean throwException = false;
     private boolean throwExceptionDuringBatchDelete = false;
