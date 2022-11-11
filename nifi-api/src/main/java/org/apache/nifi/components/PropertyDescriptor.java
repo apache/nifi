241d6191384ea688e7a7deedaf85475cbba78ba7
Nandor Soma Abonyi <abonyisoma@gmail.com>
2022-11-11 01:26:09 +0100

    NIFI-10775 Improve support for DescribedValue in PropertyDescriptor.Builder
    This closes #6650.
    
    Signed-off-by: Peter Turcsanyi <turcsanyi@apache.org>

--- nifi-api/src/main/java/org/apache/nifi/components/PropertyDescriptor.java
+++ nifi-api/src/main/java/org/apache/nifi/components/PropertyDescriptor.java
@@ -209,7 +209,6 @@ public final class PropertyDescriptor implements Comparable<PropertyDescriptor> 
         return lastResult;
     }
 
-
     public static final class Builder {
 
         private String displayName = null;
@@ -322,7 +321,7 @@ public final class PropertyDescriptor implements Comparable<PropertyDescriptor> 
          * if the user does not specify a value. When {@link #build()} is
          * called, if Allowable Values have been set (see
          * {@link #allowableValues(AllowableValue...)}) and this value is not
-         * one of those Allowable Values and Exception will be thrown. If the
+         * one of those Allowable Values, an Exception will be thrown. If the
          * Allowable Values have been set using the
          * {@link #allowableValues(AllowableValue...)} method, the default value
          * should be set to the "Value" of the {@link AllowableValue} object
@@ -562,7 +561,6 @@ public final class PropertyDescriptor implements Comparable<PropertyDescriptor> 
             return this;
         }
 
-
         /**
          * Establishes a relationship between this Property and the given property by declaring that this Property is only relevant if the given Property has a value equal to one of the given
          * <code>String</code> arguments.
@@ -589,6 +587,38 @@ public final class PropertyDescriptor implements Comparable<PropertyDescriptor> 
             }
 
             return dependsOn(property, dependentValues);
+        }
+
+        /**
+         * Establishes a relationship between this Property and the given property by declaring that this Property is only relevant if the given Property has a value equal to one of the given
+         * {@link DescribedValue} arguments.
+         * If this method is called multiple times, each with a different dependency, then a relationship is established such that this Property is relevant only if all dependencies are satisfied.
+         *
+         * In the case that this property is NOT considered to be relevant (meaning that it depends on a property whose value is not specified, or whose value does not match one of the given
+         * Described Values), the property will not be shown in the component's configuration in the User Interface. Additionally, this property's value will not be considered for
+         * validation. That is, if this property is configured with an invalid value and this property depends on Property Foo, and Property Foo does not have a value set, then the component
+         * will still be valid, because the value of this property is irrelevant.
+         *
+         * If the given property is not relevant (because its dependencies are not satisfied), this property is also considered not to be valid.
+         *
+         * @param property the property that must be set in order for this property to become relevant
+         * @param firstDependentValue the first value for the given property for which this Property is relevant
+         * @param additionalDependentValues any other values for the given property for which this Property is relevant
+         * @return the builder
+         */
+        public Builder dependsOn(final PropertyDescriptor property, final DescribedValue firstDependentValue, final DescribedValue... additionalDependentValues) {
+            final AllowableValue[] dependentValues = new AllowableValue[additionalDependentValues.length + 1];
+            dependentValues[0] = toAllowableValue(firstDependentValue);
+            int i = 1;
+            for (final DescribedValue additionalDependentValue : additionalDependentValues) {
+                dependentValues[i++] = toAllowableValue(additionalDependentValue);
+            }
+
+            return dependsOn(property, dependentValues);
+        }
+
+        private AllowableValue toAllowableValue(DescribedValue describedValue) {
+            return new AllowableValue(describedValue.getValue(), describedValue.getDisplayName(), describedValue.getDescription());
         }
 
         /**
