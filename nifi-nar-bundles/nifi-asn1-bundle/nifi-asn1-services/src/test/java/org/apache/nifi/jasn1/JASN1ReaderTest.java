3445f166bb1b60d17edbfbf6d4d433b93069ff3f
Tamas Palfy <tpalfy@apache.org>
2022-11-22 19:24:55 +0100

    NIFI-10765 - Added better error reporting in JASN1Reader.

--- nifi-nar-bundles/nifi-asn1-bundle/nifi-asn1-services/src/test/java/org/apache/nifi/jasn1/JASN1ReaderTest.java
+++ nifi-nar-bundles/nifi-asn1-bundle/nifi-asn1-services/src/test/java/org/apache/nifi/jasn1/JASN1ReaderTest.java
@@ -19,18 +19,29 @@ package org.apache.nifi.jasn1;
 import org.apache.nifi.controller.ConfigurationContext;
 import org.apache.nifi.controller.ControllerServiceInitializationContext;
 import org.apache.nifi.logging.ComponentLog;
+import org.apache.nifi.processor.exception.ProcessException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.apache.nifi.jasn1.JASN1Reader.ASN_FILES;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Answers.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class JASN1ReaderTest {
@@ -77,5 +88,83 @@ public class JASN1ReaderTest {
         // THEN
         assertEquals("org.apache.nifi.jasn1.test.RootType", actualRootModelName);
         assertNotNull(actual);
+    }
+
+    @Test
+    public void testAsnFileDoesntExist() throws Exception {
+        // GIVEN
+        ConfigurationContext context = mock(ConfigurationContext.class, RETURNS_DEEP_STUBS);
+        when(context.getProperty(ASN_FILES).isSet()).thenReturn(true);
+        when(context.getProperty(ASN_FILES).evaluateAttributeExpressions().getValue()).thenReturn(
+                "src/test/resources/test.asn,src/test/resources/doesnt_exist.asn"
+        );
+
+        // WHEN
+        ProcessException processException = assertThrows(
+                ProcessException.class,
+                () -> testSubject.onEnabled(context)
+        );
+        Throwable cause = processException.getCause();
+
+        assertEquals(FileNotFoundException.class, cause.getClass());
+        assertThat(cause.getMessage(), containsString("src/test/resources/doesnt_exist.asn"));
+    }
+
+    @Test
+    public void testCantParseAsn() throws Exception {
+        // GIVEN
+        String asnFiles = "src/test/resources/cant_parse.asn";
+
+        List<String> expectedErrorMessages = Arrays.asList(
+                "line 11:5: unexpected token: field3",
+                "line 17:33: unexpected token: ["
+        );
+
+        // WHEN
+        // THEN
+        testError(asnFiles, expectedErrorMessages);
+    }
+
+    @Test
+    public void testCantCompileAsn() throws Exception {
+        // GIVEN
+        String asnFiles = "src/test/resources/cant_compile.asn";
+
+        List<String> expectedErrorMessages = Arrays.asList(
+                "class SAMENAMEWithDifferentCase is public, should be declared in a file named SAMENAMEWithDifferentCase.java",
+                "cannot find symbol\n" +
+                        "  symbol:   class SameNameWithDifferentCase\n" +
+                        "  location: class org.apache.nifi.jasn1.test.SAMENAMEWithDifferentCase",
+                "incompatible types: com.beanit.asn1bean.ber.types.BerInteger cannot be converted to com.beanit.asn1bean.ber.BerLength",
+                "incompatible types: boolean cannot be converted to java.io.OutputStream",
+                "Some messages have been simplified; recompile with -Xdiags:verbose to get full output"
+        );
+
+        // WHEN
+        // THEN
+        testError(asnFiles, expectedErrorMessages);
+    }
+
+    private void testError(String asnFiles, List<String> expectedErrorMessages) {
+        // GIVEN
+        ConfigurationContext context = mock(ConfigurationContext.class, RETURNS_DEEP_STUBS);
+        when(context.getProperty(ASN_FILES).isSet()).thenReturn(true);
+        when(context.getProperty(ASN_FILES).evaluateAttributeExpressions().getValue())
+                .thenReturn(asnFiles);
+
+
+        // WHEN
+        assertThrows(
+                ProcessException.class,
+                () -> testSubject.onEnabled(context)
+        );
+
+        // THEN
+        ArgumentCaptor<String> errorCaptor = ArgumentCaptor.forClass(String.class);
+        verify(testSubject.logger, times(expectedErrorMessages.size())).error(errorCaptor.capture());
+
+        List<String> actualErrorMessages = errorCaptor.getAllValues();
+
+        assertEquals(expectedErrorMessages, actualErrorMessages);
     }
 }
