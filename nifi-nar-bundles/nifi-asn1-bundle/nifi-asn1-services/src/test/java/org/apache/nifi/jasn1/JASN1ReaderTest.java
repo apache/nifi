c36c4c0bbce4d3de690b58611a979373ce3d6ff0
Tamas Palfy <tpalfy@apache.org>
2022-11-22 19:24:55 +0100

    NIFI-10765 Added better error reporting in JASN1Reader
    This closes #6703
    
    Signed-off-by: David Handermann <exceptionfactory@apache.org>

--- nifi-nar-bundles/nifi-asn1-bundle/nifi-asn1-services/src/test/java/org/apache/nifi/jasn1/JASN1ReaderTest.java
+++ nifi-nar-bundles/nifi-asn1-bundle/nifi-asn1-services/src/test/java/org/apache/nifi/jasn1/JASN1ReaderTest.java
@@ -19,18 +19,35 @@ package org.apache.nifi.jasn1;
 import org.apache.nifi.controller.ConfigurationContext;
 import org.apache.nifi.controller.ControllerServiceInitializationContext;
 import org.apache.nifi.logging.ComponentLog;
+import org.apache.nifi.processor.exception.ProcessException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
+
 import static org.apache.nifi.jasn1.JASN1Reader.ASN_FILES;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Answers.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class JASN1ReaderTest {
@@ -66,7 +83,7 @@ public class JASN1ReaderTest {
         // GIVEN
         ConfigurationContext context = mock(ConfigurationContext.class, RETURNS_DEEP_STUBS);
         when(context.getProperty(ASN_FILES).isSet()).thenReturn(true);
-        when(context.getProperty(ASN_FILES).evaluateAttributeExpressions().getValue()).thenReturn("src/test/resources/test.asn");
+        when(context.getProperty(ASN_FILES).evaluateAttributeExpressions().getValue()).thenReturn(Paths.get("src", "test", "resources", "test.asn").toString());
 
         // WHEN
         testSubject.onEnabled(context);
@@ -77,5 +94,136 @@ public class JASN1ReaderTest {
         // THEN
         assertEquals("org.apache.nifi.jasn1.test.RootType", actualRootModelName);
         assertNotNull(actual);
+    }
+
+    @Test
+    public void testAsnFileDoesntExist() throws Exception {
+        // GIVEN
+        ConfigurationContext context = mock(ConfigurationContext.class, RETURNS_DEEP_STUBS);
+        when(context.getProperty(ASN_FILES).isSet()).thenReturn(true);
+        when(context.getProperty(ASN_FILES).evaluateAttributeExpressions().getValue()).thenReturn(
+                new StringJoiner(",")
+                        .add(Paths.get("src", "test", "resources", "test.asn").toString())
+                        .add(Paths.get("src", "test", "resources", "doesnt_exist.asn").toString())
+                        .toString()
+        );
+
+        // WHEN
+        ProcessException processException = assertThrows(
+                ProcessException.class,
+                () -> testSubject.onEnabled(context)
+        );
+        Throwable cause = processException.getCause();
+
+        assertEquals(FileNotFoundException.class, cause.getClass());
+        assertThat(cause.getMessage(), containsString("doesnt_exist.asn"));
+    }
+
+    @Test
+    /*
+     * Checks reported messages of underlying libraries that are explained in additionalDetails.html.
+     * In case of changes to this test additionalDetails.html may need to be updated as well.
+     */
+    public void testCantParseAsn() throws Exception {
+        // GIVEN
+        String asnFile = Paths.get("src", "test", "resources", "cant_parse.asn").toString();
+
+        List<String> expectedErrorMessages = Arrays.asList(
+                "line 11:5: unexpected token: field3",
+                "line 17:33: unexpected token: ["
+        );
+
+        // WHEN
+        // THEN
+        testParseError(asnFile, expectedErrorMessages);
+    }
+
+    @Test
+    /*
+     * Checks reported messages of underlying libraries that are explained in additionalDetails.html.
+     * In case of changes to this test additionalDetails.html may need to be updated as well.
+     */
+    public void testCantCompileAsn() throws Exception {
+        // GIVEN
+        String asnFiles = Paths.get("src", "test", "resources", "cant_compile.asn").toString();
+
+        List<String> expectedErrorMessages = Arrays.asList(
+                ".*com\\.beanit\\.asn1bean\\.ber\\.types\\.BerInteger.*com\\.beanit\\.asn1bean\\.ber\\.BerLength.*",
+                ".*boolean.*java\\.io\\.OutputStream.*",
+                ".*-Xdiags:verbose.*"
+        );
+
+        // WHEN
+        // THEN
+        testCompileError(asnFiles, expectedErrorMessages);
+    }
+
+    @EnabledOnOs({ OS.MAC, OS.WINDOWS })
+    @Test
+    /*
+     * Checks reported messages of underlying libraries that are explained in additionalDetails.html.
+     * In case of changes to this test additionalDetails.html may need to be updated as well.
+     */
+    public void testCantCompileAsnOnMacWindows() throws Exception {
+        // GIVEN
+        String asnFiles = Paths.get("src", "test", "resources", "cant_compile_mac_windows.asn").toString();
+
+        List<String> expectedErrorMessages = Arrays.asList(
+                ".*SAMENAMEWithDifferentCase.*SAMENAMEWithDifferentCase.*"
+        );
+
+        // WHEN
+        // THEN
+        testCompileError(asnFiles, expectedErrorMessages);
+    }
+
+    private void testParseError(String asnFile, List<String> expectedErrorMessages) {
+        // GIVEN
+        ConfigurationContext context = mock(ConfigurationContext.class, RETURNS_DEEP_STUBS);
+        when(context.getProperty(ASN_FILES).isSet()).thenReturn(true);
+        when(context.getProperty(ASN_FILES).evaluateAttributeExpressions().getValue())
+                .thenReturn(asnFile);
+
+
+        // WHEN
+        assertThrows(
+                ProcessException.class,
+                () -> testSubject.onEnabled(context)
+        );
+
+        // THEN
+        ArgumentCaptor<String> errorCaptor = ArgumentCaptor.forClass(String.class);
+        verify(testSubject.logger, atLeastOnce()).error(eq("{} - {}"), anyString(), errorCaptor.capture());
+
+        List<String> actualErrorMessages = errorCaptor.getAllValues();
+
+        assertEquals(expectedErrorMessages, actualErrorMessages);
+    }
+
+    private void testCompileError(String asnFiles, List<String> expectedErrorMessages) {
+        // GIVEN
+        ConfigurationContext context = mock(ConfigurationContext.class, RETURNS_DEEP_STUBS);
+        when(context.getProperty(ASN_FILES).isSet()).thenReturn(true);
+        when(context.getProperty(ASN_FILES).evaluateAttributeExpressions().getValue())
+                .thenReturn(asnFiles);
+
+        // WHEN
+        assertThrows(
+                ProcessException.class,
+                () -> testSubject.onEnabled(context)
+        );
+
+        // THEN
+        ArgumentCaptor<String> errorCaptor = ArgumentCaptor.forClass(String.class);
+        verify(testSubject.logger, atLeastOnce()).error(errorCaptor.capture());
+
+        List<String> actualErrorMessages = errorCaptor.getAllValues();
+        assertEquals(expectedErrorMessages.size(), actualErrorMessages.size());
+
+        for (int errorMessageIndex = 0; errorMessageIndex < actualErrorMessages.size(); errorMessageIndex++) {
+            String expectedErrorMessage = expectedErrorMessages.get(errorMessageIndex);
+            String actualErrorMessage = actualErrorMessages.get(errorMessageIndex);
+            assertTrue(actualErrorMessage.matches(expectedErrorMessage), "Expected string matching '" + expectedErrorMessage + "', got '" + actualErrorMessage + "'");
+        }
     }
 }
