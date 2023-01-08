1a76743202446345c8aa56589b30dd84b0156f54
Paul Grey <greyp9@yahoo.com>
2023-01-08 12:31:15 -0500

    remove spring test boot dependency; refactor unit test setup

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-api/src/test/java/org/apache/nifi/audit/TestProcessGroupAuditor.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-api/src/test/java/org/apache/nifi/audit/TestProcessGroupAuditor.java
@@ -32,16 +32,21 @@ import org.apache.nifi.controller.StandardProcessorNode;
 import org.apache.nifi.controller.flow.FlowManager;
 import org.apache.nifi.groups.ProcessGroup;
 import org.apache.nifi.web.dao.impl.StandardProcessGroupDAO;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Arrays;
@@ -56,28 +61,44 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @SuppressWarnings("unused")
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = {
-        StandardProcessGroupDAO.class,
-        ProcessGroupAuditor.class
-})
-@EnableAspectJAutoProxy(proxyTargetClass = true)
+@ExtendWith({SpringExtension.class, MockitoExtension.class})
+@ContextConfiguration(classes = {TestProcessGroupAuditor.AuditorConfiguration.class})
 public class TestProcessGroupAuditor {
+
+    private static final String PG_1 = "processGroup1";
+    private static final String PROC_1 = "processor1";
+    private static final String PROC_2 = "processor2";
+    private static final String USER_ID = "user-id";
 
     @Autowired
     private StandardProcessGroupDAO processGroupDAO;
     @Autowired
     private ProcessGroupAuditor processGroupAuditor;
 
+    @Mock
+    private AuditService auditService;
+    @Mock
+    private FlowController flowController;
+    @Mock
+    private Authentication authentication;
+    @Mock
+    private FlowManager flowManager;
+
     @Captor
     private ArgumentCaptor<List<Action>> argumentCaptorActions;
+
+    @BeforeEach
+    void setUp() {
+        processGroupDAO.setFlowController(flowController);
+        processGroupAuditor.setAuditService(auditService);
+        processGroupAuditor.setProcessGroupDAO(processGroupDAO);
+    }
 
     @Test
     void testVerifyProcessGroupAuditing() {
         final SecurityContext securityContext = SecurityContextHolder.getContext();
-        final Authentication authentication = mock(Authentication.class);
         securityContext.setAuthentication(authentication);
-        final NiFiUser user = new StandardNiFiUser.Builder().identity("user-id").build();
+        final NiFiUser user = new StandardNiFiUser.Builder().identity(USER_ID).build();
         final NiFiUserDetails userDetail = new NiFiUserDetails(user);
         when(authentication.getPrincipal()).thenReturn(userDetail);
 
@@ -89,20 +110,14 @@ public class TestProcessGroupAuditor {
         when(processor1.getConnectableType()).thenReturn(ConnectableType.PROCESSOR);
         when(processor2.getConnectableType()).thenReturn(ConnectableType.PROCESSOR);
 
-        final FlowManager flowManager = mock(FlowManager.class);
-        when(flowManager.getGroup(eq("pg1"))).thenReturn(processGroup);
-        when(flowManager.findConnectable(eq("proc1"))).thenReturn(processor1);
-        when(flowManager.findConnectable(eq("proc2"))).thenReturn(processor2);
-        final FlowController flowController = mock(FlowController.class);
+        when(flowManager.getGroup(eq(PG_1))).thenReturn(processGroup);
+        when(flowManager.findConnectable(eq(PROC_1))).thenReturn(processor1);
+        when(flowManager.findConnectable(eq(PROC_2))).thenReturn(processor2);
         when(flowController.getFlowManager()).thenReturn(flowManager);
 
-        processGroupAuditor.setProcessGroupDAO(processGroupDAO);
-        final AuditService auditServiceMock = mock(AuditService.class);
-        processGroupAuditor.setAuditService(auditServiceMock);
-        processGroupDAO.setFlowController(flowController);
-        processGroupDAO.scheduleComponents("pg1", ScheduledState.RUNNING, new HashSet<>(Arrays.asList("proc1", "proc2")));
+        processGroupDAO.scheduleComponents(PG_1, ScheduledState.RUNNING, new HashSet<>(Arrays.asList(PROC_1, PROC_2)));
 
-        verify(auditServiceMock).addActions(argumentCaptorActions.capture());
+        verify(auditService).addActions(argumentCaptorActions.capture());
         final List<Action> actions = argumentCaptorActions.getValue();
         assertEquals(1, actions.size());
         final Action action = actions.iterator().next();
@@ -110,5 +125,19 @@ public class TestProcessGroupAuditor {
         assertEquals(user.getIdentity(), action.getUserIdentity());
         assertEquals("ProcessGroup", action.getSourceType().name());
         assertEquals(Operation.Start, action.getOperation());
+    }
+
+    @Configuration
+    @EnableAspectJAutoProxy(proxyTargetClass = true)
+    public static class AuditorConfiguration {
+        @Bean
+        public StandardProcessGroupDAO processGroupDAO() {
+            return new StandardProcessGroupDAO();
+        }
+
+        @Bean
+        public ProcessGroupAuditor processGroupAuditor() {
+            return new ProcessGroupAuditor();
+        }
     }
 }
