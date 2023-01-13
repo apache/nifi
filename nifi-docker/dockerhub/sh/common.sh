8198ab5cbd728e48fd7472b4fb6aac13d5ad40cd
Dominique Jean-Prost <djeanprost@gmail.com>
2023-01-13 17:34:44 +0100

    NIFI-11057 Added Registry NAR Provider Variable for Docker
    This closes #6847
    
    Signed-off-by: David Handermann <exceptionfactory@apache.org>

--- nifi-docker/dockerhub/sh/common.sh
+++ nifi-docker/dockerhub/sh/common.sh
@@ -19,14 +19,28 @@
 # 3 - file to perform replacement inline
 prop_replace () {
   target_file=${3:-${nifi_props_file}}
-  echo 'replacing target file ' ${target_file}
+  echo "File [${target_file}] replacing [${1}]"
   sed -i -e "s|^$1=.*$|$1=$2|"  ${target_file}
 }
 
 uncomment() {
-	target_file=${2}
-	echo "Uncommenting ${target_file}"
-	sed -i -e "s|^\#$1|$1|" ${target_file}
+  target_file=${2}
+  echo "File [${target_file}] uncommenting [${1}]"
+  sed -i -e "s|^\#$1|$1|" ${target_file}
+}
+
+# 1 - property key to add or replace
+# 2 - property value to use
+# 3 - file to perform replacement inline
+prop_add_or_replace () {
+  target_file=${3:-${nifi_props_file}}
+  property_found=$(awk -v property="${1}" 'index($0, property) == 1')
+  if [ -z "${property_found}" ]; then
+    echo "File [${target_file}] adding [${1}]"
+    echo "$1=$2" >> ${target_file}
+  else
+    prop_replace $1 $2 $3  
+  fi
 }
 
 # NIFI_HOME is defined by an ENV command in the backing Dockerfile
