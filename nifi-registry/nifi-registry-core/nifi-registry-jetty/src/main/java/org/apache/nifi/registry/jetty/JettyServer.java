2320d062ff8e6925cbecd120dcad5af1eeae33ea
Emilio Setiadarma <emiliosetiadarma@gmail.com>
2023-02-09 09:40:43 -0800

    NIFI-11061: removed unused imports

--- nifi-registry/nifi-registry-core/nifi-registry-jetty/src/main/java/org/apache/nifi/registry/jetty/JettyServer.java
+++ nifi-registry/nifi-registry-core/nifi-registry-jetty/src/main/java/org/apache/nifi/registry/jetty/JettyServer.java
@@ -63,10 +63,8 @@ import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 
 public class JettyServer {
