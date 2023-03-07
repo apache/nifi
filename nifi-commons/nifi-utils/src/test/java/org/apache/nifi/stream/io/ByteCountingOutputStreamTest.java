0c5c86985a8dc61bf7cbf554ca9583b3c80bd742
Matthew Burgess <mattyb149@apache.org>
2023-03-07 13:54:33 -0500

    NIFI-8326: Fixed CheckStyle error

--- nifi-commons/nifi-utils/src/test/java/org/apache/nifi/stream/io/ByteCountingOutputStreamTest.java
+++ nifi-commons/nifi-utils/src/test/java/org/apache/nifi/stream/io/ByteCountingOutputStreamTest.java
@@ -22,7 +22,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class ByteCountingOutputStreamTest {
 
