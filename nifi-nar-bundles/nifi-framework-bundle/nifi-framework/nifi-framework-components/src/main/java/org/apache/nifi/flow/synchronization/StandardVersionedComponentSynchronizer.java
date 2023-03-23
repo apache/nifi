9de93f19a4cce60856fa14ee1396ef003b089995
Joe Gresock <jgresock@gmail.com>
2023-03-23 13:13:22 -0400

    NIFI-11330: Restarting service referenced by parameter only when service is active (#7078)

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-components/src/main/java/org/apache/nifi/flow/synchronization/StandardVersionedComponentSynchronizer.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-components/src/main/java/org/apache/nifi/flow/synchronization/StandardVersionedComponentSynchronizer.java
@@ -1555,9 +1555,12 @@ public class StandardVersionedComponentSynchronizer implements VersionedComponen
                     final Set<ControllerServiceNode> referencingServices = referenceManager.getControllerServicesReferencing(parameterContext, paramName);
 
                     for (final ControllerServiceNode referencingService : referencingServices) {
+                        final boolean isServiceActive = referencingService.isActive();
                         stopControllerService(referencingService, null, timeout, synchronizationOptions.getComponentStopTimeoutAction(), componentsToRestart, servicesToRestart,
                                 synchronizationOptions);
-                        servicesToRestart.add(referencingService);
+                        if (isServiceActive) {
+                            servicesToRestart.add(referencingService);
+                        }
                     }
                 }
 
