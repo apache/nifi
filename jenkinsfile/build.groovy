3568eaadbeb9c8773e8779bb8b71c1e81cad8d92
Brandan <brandan.tanck@gmail.com>
2023-04-03 18:35:29 +0800

    build nifi-registry

--- jenkinsfile/build.groovy
+++ jenkinsfile/build.groovy
@@ -4,6 +4,8 @@
 buildPipeline projectName: 'alp-nifi-base',
               dockerFilesInfo: [
                 ["fileNameWithPath":"./nifi-docker/dockermaven/Dockerfile", "baseContextPath": ".", "imageName": "alp-nifi-base"],
+                ["fileNameWithPath":"./nifi-registry/nifi-registry-docker-maven/dockermaven/Dockerfile", "baseContextPath": ".", "imageName": "alp-nifi-registry-base"],
+                
               ],
               runAnchore: [defaultBranches: true, featureBranches: false],
               mapForBranchBasedTag: [:]
