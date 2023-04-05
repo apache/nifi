d382b378a8d6bc09ec252ce1b37cc494973c358a
exceptionfactory <exceptionfactory@apache.org>
2023-04-05 16:29:53 -0500

    NIFI-11387 Added Transport Type property to Azure Event Hub Components
    - Transport Type defaults to AMQP and also supports AMQP with WebSockets
    
    Signed-off-by: Pierre Villard <pierre.villard.fr@gmail.com>
    
    This closes #7126.

---
+++ nifi-nar-bundles/nifi-azure-bundle/nifi-azure-processors/src/main/java/org/apache/nifi/shared/azure/eventhubs/AzureEventHubComponent.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.shared.azure.eventhubs;
+
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.expression.ExpressionLanguageScope;
+import org.apache.nifi.processor.util.StandardValidators;
+
+/**
+ * Azure Event Hub Component interface with shared properties
+ */
+public interface AzureEventHubComponent {
+    PropertyDescriptor TRANSPORT_TYPE = new PropertyDescriptor.Builder()
+            .name("Transport Type")
+            .displayName("Transport Type")
+            .description("Advanced Message Queuing Protocol Transport Type for communication with Azure Event Hubs")
+            .allowableValues(AzureEventHubTransportType.class)
+            .defaultValue(AzureEventHubTransportType.AMQP.getValue())
+            .required(true)
+            .addValidator(StandardValidators.NON_BLANK_VALIDATOR)
+            .expressionLanguageSupported(ExpressionLanguageScope.NONE)
+            .build();
+}
