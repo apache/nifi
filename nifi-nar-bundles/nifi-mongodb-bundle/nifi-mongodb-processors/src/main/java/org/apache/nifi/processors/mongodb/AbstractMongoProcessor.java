5a6e46a29185fd2eeaee40190e309ec107ec0bd3
Mike Thomsen <mthomsen@apache.org>
2023-04-30 08:38:22 -0400

    NIFI-11222 Added deprecationlogger to MongoDB processors that extend AbstractMongoProcessor.

--- nifi-nar-bundles/nifi-mongodb-bundle/nifi-mongodb-processors/src/main/java/org/apache/nifi/processors/mongodb/AbstractMongoProcessor.java
+++ nifi-nar-bundles/nifi-mongodb-bundle/nifi-mongodb-processors/src/main/java/org/apache/nifi/processors/mongodb/AbstractMongoProcessor.java
@@ -44,6 +44,7 @@ import org.apache.nifi.components.AllowableValue;
 import org.apache.nifi.components.PropertyDescriptor;
 import org.apache.nifi.components.ValidationContext;
 import org.apache.nifi.components.ValidationResult;
+import org.apache.nifi.deprecation.log.DeprecationLoggerFactory;
 import org.apache.nifi.expression.ExpressionLanguageScope;
 import org.apache.nifi.flowfile.FlowFile;
 import org.apache.nifi.mongodb.MongoDBClientService;
@@ -372,6 +373,11 @@ public abstract class AbstractMongoProcessor extends AbstractProcessor {
 
         boolean clientIsSet = context.getProperty(CLIENT_SERVICE).isSet();
         boolean uriIsSet    = context.getProperty(URI).isSet();
+
+        if (uriIsSet) {
+            DeprecationLoggerFactory.getLogger(AbstractMongoProcessor.class).warn("The MongoDB URI and other client " +
+                    "configuration properties on this proessor are deprecated and will be removed in NiFi 2.0");
+        }
 
         if (clientIsSet && uriIsSet) {
             String msg = "The client service and URI fields cannot be set at the same time.";
