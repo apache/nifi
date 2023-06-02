4aae517cca997b12e254a7778199a6fe048d2439
Timea Barna <timeabarna@apache.org>
2023-06-02 10:58:44 +0200

    NIFI-11658 Streamline using single Parameter Context for nested PGs
    Including applicable modifications from NIFI-12101 by Mark Payne
    This closes #7353
    
    Co-authored-by: Matt Gilman <mcgilman@apache.org>
    Signed-off-by: David Handermann <exceptionfactory@apache.org>
    
    (cherry picked from commit 42910e80d1d0a71c8f3de51bdd5c6dd0183da26d)
    Signed-off-by: Pierre Villard <pierre.villard.fr@gmail.com>
    
    This closes #8868.

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-api/src/main/java/org/apache/nifi/web/dao/impl/StandardProcessGroupDAO.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-api/src/main/java/org/apache/nifi/web/dao/impl/StandardProcessGroupDAO.java
@@ -45,6 +45,7 @@ import org.apache.nifi.web.api.dto.ProcessGroupDTO;
 import org.apache.nifi.web.api.dto.VariableRegistryDTO;
 import org.apache.nifi.web.api.dto.VersionControlInformationDTO;
 import org.apache.nifi.web.api.entity.ParameterContextReferenceEntity;
+import org.apache.nifi.web.api.entity.ProcessGroupRecursivity;
 import org.apache.nifi.web.api.entity.VariableEntity;
 import org.apache.nifi.web.dao.ProcessGroupDAO;
 
@@ -136,9 +137,13 @@ public class StandardProcessGroupDAO extends ComponentDAO implements ProcessGrou
     }
 
     @Override
-    public Set<ProcessGroup> getProcessGroups(String parentGroupId) {
+    public Set<ProcessGroup> getProcessGroups(final String parentGroupId, final ProcessGroupRecursivity processGroupUpdateStrategy) {
         ProcessGroup group = locateProcessGroup(flowController, parentGroupId);
-        return group.getProcessGroups();
+        if (processGroupUpdateStrategy == ProcessGroupRecursivity.ALL_DESCENDANTS) {
+            return new HashSet<>(group.findAllProcessGroups());
+        } else {
+            return group.getProcessGroups();
+        }
     }
 
     @Override
