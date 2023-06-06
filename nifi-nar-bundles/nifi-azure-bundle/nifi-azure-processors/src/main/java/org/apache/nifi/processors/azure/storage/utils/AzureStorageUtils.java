9195b586e5a4cbd927770630a38d69edafba70f3
exceptionfactory <exceptionfactory@apache.org>
2023-06-06 14:23:37 -0500

    NIFI-11656 Removed deprecated Azure Queue Storage Processors
    - Removed AzureStorageCredentialsControllerService and related implementations
    - Removed com.micrsoft.azure:azure-storage dependencies
    
    This closes #7350
    
    Signed-off-by: Nandor Soma Abonyi <nsabonyi@apache.org>

--- nifi-nar-bundles/nifi-azure-bundle/nifi-azure-processors/src/main/java/org/apache/nifi/processors/azure/storage/utils/AzureStorageUtils.java
+++ nifi-nar-bundles/nifi-azure-bundle/nifi-azure-processors/src/main/java/org/apache/nifi/processors/azure/storage/utils/AzureStorageUtils.java
@@ -19,41 +19,20 @@ package org.apache.nifi.processors.azure.storage.utils;
 import com.azure.core.http.ProxyOptions;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 
-import com.microsoft.azure.storage.CloudStorageAccount;
-import com.microsoft.azure.storage.OperationContext;
-import com.microsoft.azure.storage.StorageCredentials;
-import com.microsoft.azure.storage.StorageCredentialsAccountAndKey;
-import com.microsoft.azure.storage.StorageCredentialsSharedAccessSignature;
-
-import org.apache.commons.lang3.StringUtils;
 import org.apache.nifi.components.PropertyDescriptor;
 import org.apache.nifi.components.ValidationContext;
 import org.apache.nifi.components.ValidationResult;
 import org.apache.nifi.context.PropertyContext;
 import org.apache.nifi.expression.ExpressionLanguageScope;
-import org.apache.nifi.flowfile.FlowFile;
-import org.apache.nifi.processor.ProcessContext;
 import org.apache.nifi.processor.util.StandardValidators;
 import org.apache.nifi.proxy.ProxyConfiguration;
 import org.apache.nifi.proxy.ProxySpec;
 import org.apache.nifi.proxy.SocksVersion;
-import org.apache.nifi.services.azure.storage.AzureStorageCredentialsDetails;
-import org.apache.nifi.services.azure.storage.AzureStorageCredentialsService;
-import org.apache.nifi.services.azure.storage.AzureStorageEmulatorCredentialsDetails;
 import reactor.netty.http.client.HttpClient;
 
 public final class AzureStorageUtils {
-    public static final String BLOCK = "Block";
-    public static final String PAGE = "Page";
-
     public static final String STORAGE_ACCOUNT_NAME_PROPERTY_DESCRIPTOR_NAME = "storage-account-name";
     public static final String STORAGE_ACCOUNT_KEY_PROPERTY_DESCRIPTOR_NAME = "storage-account-key";
     public static final String STORAGE_SAS_TOKEN_PROPERTY_DESCRIPTOR_NAME = "storage-sas-token";
@@ -148,17 +127,6 @@ public final class AzureStorageUtils {
             .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
             .build();
 
-    public static final PropertyDescriptor STORAGE_CREDENTIALS_SERVICE = new PropertyDescriptor.Builder()
-            .name("storage-credentials-service")
-            .displayName("Storage Credentials")
-            .description("The Controller Service used to obtain Azure Storage Credentials. Instead of the processor level properties, " +
-                    "the credentials can be configured here through a common/shared controller service, which is the preferred way. " +
-                    "The 'Lookup' version of the service can also be used to select the credentials dynamically at runtime " +
-                    "based on a FlowFile attribute (if the processor has FlowFile input).")
-            .identifiesControllerService(AzureStorageCredentialsService.class)
-            .required(false)
-            .build();
-
     public static final PropertyDescriptor MANAGED_IDENTITY_CLIENT_ID = new PropertyDescriptor.Builder()
             .name("managed-identity-client-id")
             .displayName("Managed Identity Client ID")
@@ -204,103 +172,12 @@ public final class AzureStorageUtils {
         // do not instantiate
     }
 
-    public static CloudStorageAccount getCloudStorageAccount(final AzureStorageCredentialsDetails storageCredentialsDetails) throws URISyntaxException {
-        final CloudStorageAccount cloudStorageAccount;
-        if (storageCredentialsDetails instanceof AzureStorageEmulatorCredentialsDetails) {
-            AzureStorageEmulatorCredentialsDetails emulatorCredentials = (AzureStorageEmulatorCredentialsDetails) storageCredentialsDetails;
-            final String proxyUri = emulatorCredentials.getDevelopmentStorageProxyUri();
-            if (proxyUri != null) {
-                cloudStorageAccount = CloudStorageAccount.getDevelopmentStorageAccount(new URI(proxyUri));
-            } else {
-                cloudStorageAccount = CloudStorageAccount.getDevelopmentStorageAccount();
-            }
-        } else {
-            cloudStorageAccount = new CloudStorageAccount(
-                storageCredentialsDetails.getStorageCredentials(),
-                true,
-                storageCredentialsDetails.getStorageSuffix(),
-                storageCredentialsDetails.getStorageAccountName());
-        }
-        return cloudStorageAccount;
-    }
-
-    public static AzureStorageCredentialsDetails getStorageCredentialsDetails(PropertyContext context, FlowFile flowFile) {
-        final Map<String, String> attributes = flowFile != null ? flowFile.getAttributes() : Collections.emptyMap();
-
-        final AzureStorageCredentialsService storageCredentialsService = context.getProperty(STORAGE_CREDENTIALS_SERVICE).asControllerService(AzureStorageCredentialsService.class);
-
-        if (storageCredentialsService != null) {
-            return storageCredentialsService.getStorageCredentialsDetails(attributes);
-        } else {
-            return createStorageCredentialsDetails(context, attributes);
-        }
-    }
-
-    public static AzureStorageCredentialsDetails createStorageCredentialsDetails(PropertyContext context, Map<String, String> attributes) {
-        final String accountName = context.getProperty(ACCOUNT_NAME).evaluateAttributeExpressions(attributes).getValue();
-        final String storageSuffix = context.getProperty(ENDPOINT_SUFFIX).evaluateAttributeExpressions(attributes).getValue();
-        final String accountKey = context.getProperty(ACCOUNT_KEY).evaluateAttributeExpressions(attributes).getValue();
-        final String sasToken = context.getProperty(PROP_SAS_TOKEN).evaluateAttributeExpressions(attributes).getValue();
-
-        if (StringUtils.isBlank(accountName)) {
-            throw new IllegalArgumentException(String.format("'%s' must not be empty.", ACCOUNT_NAME.getDisplayName()));
-        }
-
-        StorageCredentials storageCredentials;
-
-        if (StringUtils.isNotBlank(accountKey)) {
-            storageCredentials = new StorageCredentialsAccountAndKey(accountName, accountKey);
-        } else if (StringUtils.isNotBlank(sasToken)) {
-            storageCredentials = new StorageCredentialsSharedAccessSignature(sasToken);
-        } else {
-            throw new IllegalArgumentException(String.format("Either '%s' or '%s' must be defined.", ACCOUNT_KEY.getDisplayName(), PROP_SAS_TOKEN.getDisplayName()));
-        }
-
-        return new AzureStorageCredentialsDetails(accountName, storageSuffix, storageCredentials);
-    }
-
-    public static Collection<ValidationResult> validateCredentialProperties(ValidationContext validationContext) {
-        final List<ValidationResult> results = new ArrayList<>();
-
-        final String storageCredentials = validationContext.getProperty(STORAGE_CREDENTIALS_SERVICE).getValue();
-        final String accountName = validationContext.getProperty(ACCOUNT_NAME).getValue();
-        final String accountKey = validationContext.getProperty(ACCOUNT_KEY).getValue();
-        final String sasToken = validationContext.getProperty(PROP_SAS_TOKEN).getValue();
-        final String endpointSuffix = validationContext.getProperty(ENDPOINT_SUFFIX).getValue();
-
-        if (!((StringUtils.isNotBlank(storageCredentials) && StringUtils.isBlank(accountName) && StringUtils.isBlank(accountKey) && StringUtils.isBlank(sasToken))
-                || (StringUtils.isBlank(storageCredentials) && StringUtils.isNotBlank(accountName) && StringUtils.isNotBlank(accountKey) && StringUtils.isBlank(sasToken))
-                || (StringUtils.isBlank(storageCredentials) && StringUtils.isNotBlank(accountName) && StringUtils.isBlank(accountKey) && StringUtils.isNotBlank(sasToken)))) {
-            results.add(new ValidationResult.Builder().subject("AzureStorageUtils Credentials")
-                    .valid(false)
-                    .explanation("either " + STORAGE_CREDENTIALS_SERVICE.getDisplayName()
-                            + ", or " + ACCOUNT_NAME.getDisplayName() + " with " + ACCOUNT_KEY.getDisplayName()
-                            + " or " + ACCOUNT_NAME.getDisplayName() + " with " + PROP_SAS_TOKEN.getDisplayName() + " must be specified")
-                    .build());
-        }
-
-        if(StringUtils.isNotBlank(storageCredentials) && StringUtils.isNotBlank(endpointSuffix)) {
-            String errMsg = "Either " + STORAGE_CREDENTIALS_SERVICE.getDisplayName() + " or " + ENDPOINT_SUFFIX.getDisplayName()
-                + " should be specified, not both.";
-            results.add(new ValidationResult.Builder().subject("AzureStorageUtils Credentials")
-                        .explanation(errMsg)
-                        .build());
-        }
-
-        return results;
-    }
-
     private static final ProxySpec[] PROXY_SPECS = {ProxySpec.HTTP, ProxySpec.SOCKS};
     public static final PropertyDescriptor PROXY_CONFIGURATION_SERVICE
             = ProxyConfiguration.createProxyConfigPropertyDescriptor(false, PROXY_SPECS);
 
     public static void validateProxySpec(ValidationContext context, Collection<ValidationResult> results) {
         ProxyConfiguration.validateProxySpec(context, results, PROXY_SPECS);
-    }
-
-    public static void setProxy(final OperationContext operationContext, final ProcessContext processContext) {
-        final ProxyConfiguration proxyConfig = ProxyConfiguration.getConfiguration(processContext);
-        operationContext.setProxy(proxyConfig.createProxy());
     }
 
     /**
