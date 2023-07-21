806d5a75f3ae0a813ee1d6574f5e01ba6c0fd978
markap14 <markap14@hotmail.com>
2023-07-21 15:09:02 -0400

    Update nifi-commons/nifi-properties/src/main/java/org/apache/nifi/util/NiFiProperties.java
    Co-authored-by: exceptionfactory <exceptionfactory@apache.org>

--- nifi-commons/nifi-properties/src/main/java/org/apache/nifi/util/NiFiProperties.java
+++ nifi-commons/nifi-properties/src/main/java/org/apache/nifi/util/NiFiProperties.java
@@ -341,7 +341,7 @@ public class NiFiProperties extends ApplicationProperties {
     public static final String PYTHON_MAX_PROCESSES_PER_TYPE = "nifi.python.max.processes.per.extension.type";
     public static final String PYTHON_COMMS_TIMEOUT = "nifi.python.comms.timeout";
 
-    public static final String PYTHON_DEBUG_CONTROLLER_ENABLE = "nifi.python.controller.debugpy.enable";
+    public static final String PYTHON_CONTROLLER_DEBUGPY_ENABLED = "nifi.python.controller.debugpy.enabled";
     public static final String PYTHON_CONTROLLER_DEBUGPY_PORT = "nifi.python.controller.debugpy.port";
     public static final String PYTHON_DEBUG_CONTROLLER_HOST = "nifi.python.controller.debugpy.host";
     public static final String PYTHON_DEBUG_CONTROLLER_LOGS_DIR = "nifi.python.controller.debugpy.logs.directory";
