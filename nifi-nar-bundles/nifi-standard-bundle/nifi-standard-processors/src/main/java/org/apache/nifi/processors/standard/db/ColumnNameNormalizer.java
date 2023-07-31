20cc866e12aec9c210b0a4559ba4c1ba11370152
ravisingh <ravi.singh@stockviz.com>
2023-07-31 14:39:01 -0700

    added the license.txt  to the ColumnNameNormalizer and TranslationStrategy

--- nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/db/ColumnNameNormalizer.java
+++ nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/db/ColumnNameNormalizer.java
@@ -1,10 +1,28 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.nifi.processors.standard.db;
 
 import java.util.Objects;
 
 /**
- * ColumnNameNormalizer is a utility class that helps to normalize column names. It provides various strategies to modify column names based on the TranslationStrategy enum.
- * Column names can be normalized by removing underscores, spaces, all special characters, or by using a custom regular expression defined by the user.
+ * ColumnNameNormalizer is a utility class that helps to normalize column names. It provides various strategies to
+ * modify column names based on the TranslationStrategy enum. Column names can be normalized by removing underscores,
+ * spaces, all special characters, or by using a custom regular expression defined by the user.
  */
 public class ColumnNameNormalizer {
 
