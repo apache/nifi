caec9ab6a68eca695cc961aa0d39a4d8efc58af9
Matt Gilman <matt.c.gilman@gmail.com>
2023-08-24 10:34:35 -0400

    NIFI-11481: - Add Tailwind.

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-frontend/src/main/nifi/README.md
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-frontend/src/main/nifi/README.md
@@ -1,4 +1,4 @@
-# NifiWebFrontend
+# NiFi
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.0.
 
---
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-frontend/src/main/nifi/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
