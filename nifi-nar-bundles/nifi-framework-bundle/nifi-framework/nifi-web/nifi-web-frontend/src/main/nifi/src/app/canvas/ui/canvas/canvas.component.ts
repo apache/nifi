4cd3434c23fd4daa749991a1405df419dda3f6f3
Matt Gilman <matt.c.gilman@gmail.com>
2023-09-08 14:05:51 -0400

    NIFI-11481: - Selection box.

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-frontend/src/main/nifi/src/app/canvas/ui/canvas/canvas.component.ts
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-frontend/src/main/nifi/src/app/canvas/ui/canvas/canvas.component.ts
@@ -16,12 +16,13 @@
  */
 
 import { Component, OnInit } from '@angular/core';
-import { CanvasState } from '../../state';
-import { Store } from '@ngrx/store';
-import { enterProcessGroup } from '../../state/flow/flow.actions';
+import { CanvasState, Position } from '../../state';
+import { select, Store } from '@ngrx/store';
+import { enterProcessGroup, setSelectedComponents } from '../../state/flow/flow.actions';
 import * as d3 from 'd3';
 import { CanvasView } from '../../service/canvas-view.service';
 import { INITIAL_SCALE, INITIAL_TRANSLATE } from '../../state/transform/transform.reducer';
+import { selectTransform } from '../../state/transform/transform.selectors';
 
 @Component({
     selector: 'fd-canvas',
@@ -32,10 +33,17 @@ export class CanvasComponent implements OnInit {
     private svg: any;
     private canvas: any;
 
+    private scale: number = INITIAL_SCALE;
+    private canvasClicked: boolean = false;
+
     constructor(
         private store: Store<CanvasState>,
         private canvasView: CanvasView
-    ) {}
+    ) {
+        this.store.pipe(select(selectTransform)).subscribe((transform) => {
+            this.scale = transform.scale;
+        });
+    }
 
     ngOnInit(): void {
         // initialize the canvas svg
@@ -54,18 +62,33 @@ export class CanvasComponent implements OnInit {
     }
 
     private createSvg(): void {
-        this.svg = d3.select('#canvas-container').append('svg').attr('class', 'canvas-svg');
+        const self: CanvasComponent = this;
+
+        this.svg = d3
+            .select('#canvas-container')
+            .append('svg')
+            .attr('class', 'canvas-svg')
+            .on('contextmenu', function (event) {
+                // reset the canvas click flag
+                self.canvasClicked = false;
+
+                // since the context menu event propagated back to the canvas, clear the selection
+                // nfCanvasUtils.getSelection().classed('selected', false);
+
+                // update URL deep linking params
+                // nfCanvasUtils.setURLParameters();
+
+                // show the context menu on the canvas
+                // nfContextMenu.show(event);
+
+                // prevent default browser behavior
+                event.preventDefault();
+            });
 
         this.createDefs();
 
-        const t = [INITIAL_TRANSLATE.x, INITIAL_TRANSLATE.y];
-
-        // create the canvas element
-        this.canvas = this.svg
-            .append('g')
-            .attr('transform', 'translate(' + t + ') scale(' + INITIAL_SCALE + ')')
-            .attr('pointer-events', 'all')
-            .attr('id', 'canvas');
+        // initialize the canvas element
+        this.initCanvas();
     }
 
     private createDefs(): void {
@@ -183,5 +206,171 @@ export class CanvasComponent implements OnInit {
         const connectionFullFeMerge = connectionFullDropShadowFilter.append('feMerge');
         connectionFullFeMerge.append('feMergeNode').attr('in', 'offsetColorBlur');
         connectionFullFeMerge.append('feMergeNode').attr('in', 'SourceGraphic');
+    }
+
+    private initCanvas(): void {
+        const self: CanvasComponent = this;
+
+        const t = [INITIAL_TRANSLATE.x, INITIAL_TRANSLATE.y];
+        this.canvas = this.svg
+            .append('g')
+            .attr('transform', 'translate(' + t + ') scale(' + INITIAL_SCALE + ')')
+            .attr('pointer-events', 'all')
+            .attr('id', 'canvas');
+
+        // handle canvas events
+        this.svg
+            .on('mousedown.selection', function (event: MouseEvent) {
+                self.canvasClicked = true;
+
+                if (event.button !== 0) {
+                    // prevent further propagation (to parents and others handlers
+                    // on the same element to prevent zoom behavior)
+                    event.stopImmediatePropagation();
+                    return;
+                }
+
+                // show selection box if shift is held down
+                if (event.shiftKey) {
+                    const position: any = d3.pointer(event, self.canvas.node());
+                    self.canvas
+                        .append('rect')
+                        .attr('rx', 6)
+                        .attr('ry', 6)
+                        .attr('x', position[0])
+                        .attr('y', position[1])
+                        .attr('class', 'component-selection')
+                        .attr('width', 0)
+                        .attr('height', 0)
+                        .attr('stroke-width', function () {
+                            return 1 / self.scale;
+                        })
+                        .attr('stroke-dasharray', function () {
+                            return 4 / self.scale;
+                        })
+                        .datum(position);
+
+                    // prevent further propagation (to parents and others handlers
+                    // on the same element to prevent zoom behavior)
+                    event.stopImmediatePropagation();
+
+                    // prevents the browser from changing to a text selection cursor
+                    event.preventDefault();
+                }
+            })
+            .on('mousemove.selection', function (event: MouseEvent) {
+                // update selection box if shift is held down
+                if (event.shiftKey) {
+                    // get the selection box
+                    const selectionBox: any = d3.select('rect.component-selection');
+                    if (!selectionBox.empty()) {
+                        // get the original position
+                        const originalPosition: any = selectionBox.datum();
+                        const position: any = d3.pointer(event, self.canvas.node());
+
+                        const d: any = {};
+                        if (originalPosition[0] < position[0]) {
+                            d.x = originalPosition[0];
+                            d.width = position[0] - originalPosition[0];
+                        } else {
+                            d.x = position[0];
+                            d.width = originalPosition[0] - position[0];
+                        }
+
+                        if (originalPosition[1] < position[1]) {
+                            d.y = originalPosition[1];
+                            d.height = position[1] - originalPosition[1];
+                        } else {
+                            d.y = position[1];
+                            d.height = originalPosition[1] - position[1];
+                        }
+
+                        // update the selection box
+                        selectionBox.attr('width', d.width).attr('height', d.height).attr('x', d.x).attr('y', d.y);
+
+                        // prevent further propagation (to parents)
+                        event.stopPropagation();
+                    }
+                }
+            })
+            .on('mouseup.selection', function (this: any) {
+                // ensure this originated from clicking the canvas, not a component.
+                // when clicking on a component, the event propagation is stopped so
+                // it never reaches the canvas. we cannot do this however on up events
+                // since the drag events break down
+                if (!self.canvasClicked) {
+                    return;
+                }
+
+                // reset the canvas click flag
+                self.canvasClicked = false;
+
+                // get the selection box
+                const selectionBox: any = d3.select('rect.component-selection');
+                if (!selectionBox.empty()) {
+                    const selection: string[] = [];
+
+                    const selectionBoundingBox: any = {
+                        x: parseInt(selectionBox.attr('x'), 10),
+                        y: parseInt(selectionBox.attr('y'), 10),
+                        width: parseInt(selectionBox.attr('width'), 10),
+                        height: parseInt(selectionBox.attr('height'), 10)
+                    };
+
+                    // see if a component should be selected or not
+                    d3.selectAll('g.component').each(function (d: any) {
+                        // consider it selected if its already selected or enclosed in the bounding box
+                        if (
+                            d3.select(this).classed('selected') ||
+                            (d.position.x >= selectionBoundingBox.x &&
+                                d.position.x + d.dimensions.width <=
+                                    selectionBoundingBox.x + selectionBoundingBox.width &&
+                                d.position.y >= selectionBoundingBox.y &&
+                                d.position.y + d.dimensions.height <=
+                                    selectionBoundingBox.y + selectionBoundingBox.height)
+                        ) {
+                            selection.push(d.id);
+                        }
+                    });
+
+                    // see if a connection should be selected or not
+                    d3.selectAll('g.connection').each(function (d: any) {
+                        // consider all points
+                        const points: Position[] = [d.start].concat(d.bends, [d.end]);
+
+                        // determine the bounding box
+                        const x: any = d3.extent(points, function (pt: Position) {
+                            return pt.x;
+                        });
+                        const y: any = d3.extent(points, function (pt: Position) {
+                            return pt.y;
+                        });
+
+                        // consider it selected if its already selected or enclosed in the bounding box
+                        if (
+                            d3.select(this).classed('selected') ||
+                            (x[0] >= selectionBoundingBox.x &&
+                                x[1] <= selectionBoundingBox.x + selectionBoundingBox.width &&
+                                y[0] >= selectionBoundingBox.y &&
+                                y[1] <= selectionBoundingBox.y + selectionBoundingBox.height)
+                        ) {
+                            selection.push(d.id);
+                        }
+                    });
+
+                    // dispatch the selected components
+                    self.store.dispatch(
+                        setSelectedComponents({
+                            ids: selection
+                        })
+                    );
+
+                    // remove the selection box
+                    selectionBox.remove();
+
+                    // TODO - update URL deep linking params
+                    // nfCanvasUtils.setURLParameters();
+                }
+            });
     }
 }
