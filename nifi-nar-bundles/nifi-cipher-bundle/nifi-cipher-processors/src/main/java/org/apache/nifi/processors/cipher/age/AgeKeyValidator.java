e6323d4b24dd7cd119f257f3cfb1f527a621f39f
exceptionfactory <exceptionfactory@apache.org>
2023-09-21 14:49:51 -0500

    NIFI-12033 Removed unused imports from AgeKeyValidator
    Signed-off-by: David Handermann <exceptionfactory@apache.org>

--- nifi-nar-bundles/nifi-cipher-bundle/nifi-cipher-processors/src/main/java/org/apache/nifi/processors/cipher/age/AgeKeyValidator.java
+++ nifi-nar-bundles/nifi-cipher-bundle/nifi-cipher-processors/src/main/java/org/apache/nifi/processors/cipher/age/AgeKeyValidator.java
@@ -32,8 +32,6 @@ import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.Optional;
 import java.util.Set;
-import java.util.function.Predicate;
-import java.util.regex.Matcher;
 import java.util.stream.Collectors;
 
 /**
