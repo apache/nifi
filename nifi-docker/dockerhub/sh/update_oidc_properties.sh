8a1f6d136f617685cac669c0afed877cae6146a1
Marcelo Vinícius de Sousa Campos <mr.225@hotmail.com>
2023-09-27 08:23:49 -0300

    Update update_oidc_properties.sh
    Update OIDC properties via update_oidc_properties.sh

--- nifi-docker/dockerhub/sh/update_oidc_properties.sh
+++ nifi-docker/dockerhub/sh/update_oidc_properties.sh
@@ -23,5 +23,7 @@ prop_replace 'nifi.security.user.oidc.client.secret'                    "${NIFI_
 prop_replace 'nifi.security.user.oidc.preferred.jwsalgorithm'           "${NIFI_SECURITY_USER_OIDC_PREFERRED_JWSALGORITHM}"
 prop_replace 'nifi.security.user.oidc.additional.scopes'                "${NIFI_SECURITY_USER_OIDC_ADDITIONAL_SCOPES}"
 prop_replace 'nifi.security.user.oidc.claim.identifying.user'           "${NIFI_SECURITY_USER_OIDC_CLAIM_IDENTIFYING_USER}"
+prop_replace 'nifi.security.user.oidc.claim.groups'                     "${NIFI_SECURITY_USER_OIDC_CLAIM_GROUPS}"
 prop_replace 'nifi.security.user.oidc.fallback.claims.identifying.user' "${NIFI_SECURITY_USER_OIDC_FALLBACK_CLAIMS_IDENTIFYING_USER}"
 prop_replace 'nifi.security.user.oidc.truststore.strategy'              "${NIFI_SECURITY_USER_OIDC_TRUSTSTORE_STRATEGY}"
+prop_replace 'nifi.security.user.oidc.token.refresh.window'             "${NIFI_SECURITY_USER_OIDC_TOKEN_REFRESH_WINDOW}"
