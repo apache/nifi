ed3a70c9859f7cccdf03c6133f05d42ab108cd25
Chris Sampson <chris.sampson82@gmail.com>
2023-10-01 18:42:02 +0100

    NIFI-12178 Add integration-tests and docker-tests GitHub Workflows
    Use Java Zulu distribution for all docker-tests and integration-tests Github Workflows
    
    This closes #7858
    
    Signed-off-by: David Handermann <exceptionfactory@apache.org>

--- nifi-toolkit/nifi-toolkit-assembly/docker/tests/exit-codes.sh
+++ nifi-toolkit/nifi-toolkit-assembly/docker/tests/exit-codes.sh
@@ -3,15 +3,16 @@
 set -xuo pipefail
 
 VERSION=${1:-}
-IMAGE=apache/nifi-toolkit:${VERSION}
+
+image_name=apache/nifi-toolkit:${VERSION}
 
 echo "Testing return values on missing input:"
-docker run --rm $IMAGE
+docker run --rm "${image_name}"
 test 0 -eq $? || exit 1
 
 echo "Testing return values on invalid input for all commands:"
-docker run --rm $IMAGE encrypt-config invalid 1>/dev/null 2>&1
+docker run --rm "${image_name}" encrypt-config invalid 1>/dev/null 2>&1
 test 2 -eq $? || exit 1
 
-docker run --rm $IMAGE cli invalid 1>/dev/null 2>&1
+docker run --rm "${image_name}" cli invalid 1>/dev/null 2>&1
 test 255 -eq $? || exit 1
