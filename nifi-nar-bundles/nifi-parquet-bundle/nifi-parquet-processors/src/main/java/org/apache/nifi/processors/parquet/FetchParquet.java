9a5ec83baa1b3593031f0917659a69e7a36bb0be
Rajmund Takacs <takraj@gmail.com>
2023-10-18 10:24:28 +0200

    NIFI-12241 Efficient Parquet Splitting
    This closes #7893.
    
    Signed-off-by: Tamas Palfy <tpalfy@apache.org>

--- nifi-nar-bundles/nifi-parquet-bundle/nifi-parquet-processors/src/main/java/org/apache/nifi/processors/parquet/FetchParquet.java
+++ nifi-nar-bundles/nifi-parquet-bundle/nifi-parquet-processors/src/main/java/org/apache/nifi/processors/parquet/FetchParquet.java
@@ -17,10 +17,13 @@
 package org.apache.nifi.processors.parquet;
 
 import java.io.IOException;
+import java.util.Optional;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.nifi.annotation.behavior.InputRequirement;
+import org.apache.nifi.annotation.behavior.ReadsAttribute;
+import org.apache.nifi.annotation.behavior.ReadsAttributes;
 import org.apache.nifi.annotation.behavior.Restricted;
 import org.apache.nifi.annotation.behavior.Restriction;
 import org.apache.nifi.annotation.behavior.SupportsBatching;
@@ -31,11 +34,14 @@ import org.apache.nifi.annotation.documentation.SeeAlso;
 import org.apache.nifi.annotation.documentation.Tags;
 import org.apache.nifi.components.RequiredPermission;
 import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.parquet.filter.OffsetRecordFilter;
 import org.apache.nifi.parquet.hadoop.AvroParquetHDFSRecordReader;
+import org.apache.nifi.parquet.utils.ParquetAttribute;
 import org.apache.nifi.processor.ProcessContext;
 import org.apache.nifi.processors.hadoop.AbstractFetchHDFSRecord;
 import org.apache.nifi.processors.hadoop.record.HDFSRecordReader;
 import org.apache.parquet.avro.AvroParquetReader;
+import org.apache.parquet.filter2.compat.FilterCompat;
 import org.apache.parquet.hadoop.ParquetReader;
 import org.apache.parquet.hadoop.util.HadoopInputFile;
 import org.apache.parquet.io.InputFile;
@@ -53,6 +59,16 @@ import org.apache.parquet.io.InputFile;
         @WritesAttribute(attribute = "record.count", description = "The number of records in the resulting flow file"),
         @WritesAttribute(attribute = "hadoop.file.url", description = "The hadoop url for the file is stored in this attribute.")
 })
+@ReadsAttributes({
+        @ReadsAttribute(
+                attribute = ParquetAttribute.RECORD_OFFSET,
+                description = "Gets the index of first record in the input."
+        ),
+        @ReadsAttribute(
+                attribute = ParquetAttribute.RECORD_COUNT,
+                description = "Gets the number of records in the input."
+        )
+})
 @SeeAlso({PutParquet.class})
 @Restricted(restrictions = {
     @Restriction(
@@ -63,9 +79,31 @@ public class FetchParquet extends AbstractFetchHDFSRecord {
 
     @Override
     public HDFSRecordReader createHDFSRecordReader(final ProcessContext context, final FlowFile flowFile, final Configuration conf, final Path path) throws IOException {
+        final Long offset = Optional.ofNullable(flowFile.getAttribute(ParquetAttribute.RECORD_OFFSET))
+                .map(Long::parseLong)
+                .orElse(null);
+
+        final Long count = Optional.ofNullable(flowFile.getAttribute(ParquetAttribute.RECORD_COUNT))
+                .map(Long::parseLong)
+                .orElse(null);
+
+        final long fileStartOffset = Optional.ofNullable(flowFile.getAttribute(ParquetAttribute.FILE_RANGE_START_OFFSET))
+                .map(Long::parseLong)
+                .orElse(0L);
+        final long fileEndOffset = Optional.ofNullable(flowFile.getAttribute(ParquetAttribute.FILE_RANGE_END_OFFSET))
+                .map(Long::parseLong)
+                .orElse(Long.MAX_VALUE);
+
         final InputFile inputFile = HadoopInputFile.fromPath(path, conf);
-        final ParquetReader.Builder<GenericRecord> readerBuilder = AvroParquetReader.<GenericRecord>builder(inputFile).withConf(conf);
-        return new AvroParquetHDFSRecordReader(readerBuilder.build());
+        final ParquetReader.Builder<GenericRecord> readerBuilder = AvroParquetReader.<GenericRecord>builder(inputFile)
+                .withConf(conf)
+                .withFileRange(fileStartOffset, fileEndOffset);
+
+        if (offset != null) {
+            readerBuilder.withFilter(FilterCompat.get(OffsetRecordFilter.offset(offset)));
+        }
+
+        return new AvroParquetHDFSRecordReader(readerBuilder.build(), count);
     }
 
 }
---
+++ nifi-nar-bundles/nifi-parquet-bundle/nifi-parquet-processors/src/test/java/org/apache/nifi/processors/parquet/CalculateParquetRowGroupOffsetsTest.java
@@ -0,0 +1,148 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.nifi.processors.parquet;
+
+import static org.apache.nifi.processors.parquet.CalculateParquetOffsets.PROP_ZERO_CONTENT_OUTPUT;
+import static org.apache.nifi.processors.parquet.CalculateParquetOffsets.REL_SUCCESS;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.nifi.parquet.ParquetTestUtils;
+import org.apache.nifi.parquet.utils.ParquetAttribute;
+import org.apache.nifi.util.MockFlowFile;
+import org.apache.nifi.util.TestRunner;
+import org.apache.nifi.util.TestRunners;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+@DisabledOnOs({ OS.WINDOWS })
+public class CalculateParquetRowGroupOffsetsTest {
+
+    private static final Path NOT_PARQUET_PATH = Paths.get("src/test/resources/core-site.xml");
+
+    private static final Map<String, String> PRESERVED_ATTRIBUTES = new HashMap<String, String>() {
+        {
+            put("foo", "bar");
+            put("example", "value");
+        }
+    };
+
+    private TestRunner runner;
+
+    @BeforeEach
+    public void setUp() {
+        runner = TestRunners.newTestRunner(new CalculateParquetRowGroupOffsets());
+    }
+
+    @Test
+    public void testSinglePartition() throws Exception {
+        File parquetFile = ParquetTestUtils.createUsersParquetFile(10);
+        runner.enqueue(parquetFile.toPath(), PRESERVED_ATTRIBUTES);
+        runner.run();
+        runner.assertAllFlowFilesTransferred(REL_SUCCESS, 1);
+
+        final List<MockFlowFile> results = runner.getFlowFilesForRelationship(REL_SUCCESS);
+
+        results.get(0).assertAttributeEquals(ParquetAttribute.RECORD_COUNT, "10");
+        results.get(0).assertAttributeEquals(ParquetAttribute.FILE_RANGE_START_OFFSET, "4");
+        results.get(0).assertAttributeEquals(ParquetAttribute.FILE_RANGE_END_OFFSET, "298");
+        results.get(0).assertContentEquals(parquetFile.toPath());
+
+        PRESERVED_ATTRIBUTES.forEach(results.get(0)::assertAttributeEquals);
+    }
+
+    @Test
+    public void testEachRowGroupGoesToSeparatePartition() throws Exception {
+        File parquetFile = ParquetTestUtils.createUsersParquetFile(1000);
+        runner.enqueue(parquetFile.toPath(), PRESERVED_ATTRIBUTES);
+        runner.run();
+        runner.assertAllFlowFilesTransferred(REL_SUCCESS, 4);
+
+        final List<MockFlowFile> results = runner.getFlowFilesForRelationship(REL_SUCCESS);
+
+        results.get(0).assertAttributeEquals(ParquetAttribute.RECORD_COUNT, "337");
+        results.get(0).assertAttributeEquals(ParquetAttribute.FILE_RANGE_START_OFFSET, "4");
+        results.get(0).assertAttributeEquals(ParquetAttribute.FILE_RANGE_END_OFFSET, "8301");
+        results.get(0).assertContentEquals(parquetFile.toPath());
+
+        results.get(1).assertAttributeEquals(ParquetAttribute.RECORD_COUNT, "326");
+        results.get(1).assertAttributeEquals(ParquetAttribute.FILE_RANGE_START_OFFSET, "8301");
+        results.get(1).assertAttributeEquals(ParquetAttribute.FILE_RANGE_END_OFFSET, "16543");
+        results.get(1).assertContentEquals(parquetFile.toPath());
+
+        results.get(2).assertAttributeEquals(ParquetAttribute.RECORD_COUNT, "326");
+        results.get(2).assertAttributeEquals(ParquetAttribute.FILE_RANGE_START_OFFSET, "16543");
+        results.get(2).assertAttributeEquals(ParquetAttribute.FILE_RANGE_END_OFFSET, "24784");
+        results.get(2).assertContentEquals(parquetFile.toPath());
+
+        results.get(3).assertAttributeEquals(ParquetAttribute.RECORD_COUNT, "11");
+        results.get(3).assertAttributeEquals(ParquetAttribute.FILE_RANGE_START_OFFSET, "24784");
+        results.get(3).assertAttributeEquals(ParquetAttribute.FILE_RANGE_END_OFFSET, "25143");
+        results.get(3).assertContentEquals(parquetFile.toPath());
+
+        results.forEach(flowFile -> PRESERVED_ATTRIBUTES.forEach(flowFile::assertAttributeEquals));
+    }
+
+    @Test
+    public void testZeroContentOutput() throws Exception {
+        File parquetFile = ParquetTestUtils.createUsersParquetFile(500);
+        runner.setProperty(PROP_ZERO_CONTENT_OUTPUT, "true");
+        runner.enqueue(parquetFile.toPath(), PRESERVED_ATTRIBUTES);
+        runner.run();
+        runner.assertAllFlowFilesTransferred(REL_SUCCESS, 2);
+
+        final List<MockFlowFile> results = runner.getFlowFilesForRelationship(REL_SUCCESS);
+
+        results.get(0).assertAttributeEquals(ParquetAttribute.RECORD_COUNT, "337");
+        results.get(0).assertAttributeEquals(ParquetAttribute.FILE_RANGE_START_OFFSET, "4");
+        results.get(0).assertAttributeEquals(ParquetAttribute.FILE_RANGE_END_OFFSET, "8301");
+        results.get(0).assertContentEquals("");
+
+        results.get(1).assertAttributeEquals(ParquetAttribute.RECORD_COUNT, "163");
+        results.get(1).assertAttributeEquals(ParquetAttribute.FILE_RANGE_START_OFFSET, "8301");
+        results.get(1).assertAttributeEquals(ParquetAttribute.FILE_RANGE_END_OFFSET, "12468");
+        results.get(1).assertContentEquals("");
+
+        results.forEach(flowFile -> PRESERVED_ATTRIBUTES.forEach(flowFile::assertAttributeEquals));
+    }
+
+    @Test
+    public void testEmptyInput() {
+        runner.enqueue("", PRESERVED_ATTRIBUTES);
+
+        final Throwable thrownException = assertThrows(Throwable.class, () -> runner.run());
+        assertTrue(thrownException.getMessage().contains("is not a Parquet file"));
+    }
+
+    @Test
+    public void testUnrecognizedInput() throws IOException {
+        runner.enqueue(NOT_PARQUET_PATH, PRESERVED_ATTRIBUTES);
+
+        final Throwable thrownException = assertThrows(Throwable.class, () -> runner.run());
+        assertTrue(thrownException.getMessage().contains("is not a Parquet file"));
+    }
+}
