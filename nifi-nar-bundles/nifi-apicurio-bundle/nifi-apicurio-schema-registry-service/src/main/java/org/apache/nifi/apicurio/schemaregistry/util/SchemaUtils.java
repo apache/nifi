21387316e3088034eeeb4cfbb8f75192e8dd7c99
lehelb <lehel44@hotmail.com>
2023-10-20 03:31:25 +0200

    NIFI-10222 Added Apicurio Schema Registry Service
    This closes #7913
    
    Signed-off-by: David Handermann <exceptionfactory@apache.org>

---
+++ nifi-nar-bundles/nifi-apicurio-bundle/nifi-apicurio-schema-registry-service/src/main/java/org/apache/nifi/apicurio/schemaregistry/util/SchemaUtils.java
@@ -0,0 +1,90 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.apicurio.schemaregistry.util;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaParseException;
+import org.apache.nifi.avro.AvroTypeUtil;
+import org.apache.nifi.schema.access.SchemaNotFoundException;
+import org.apache.nifi.serialization.record.RecordSchema;
+import org.apache.nifi.serialization.record.SchemaIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+
+public class SchemaUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(SchemaUtils.class);
+    private static final ObjectMapper OBJECT_MAPPER = setObjectMapper();
+
+    private SchemaUtils() {
+    }
+
+    public static RecordSchema createRecordSchema(final InputStream schemaStream, final String name, final int version) throws SchemaNotFoundException, IOException {
+        final JsonNode schemaNode = OBJECT_MAPPER.readTree(schemaStream);
+        final String schemaText = schemaNode.toString();
+
+        try {
+            final Schema avroSchema = new Schema.Parser().parse(schemaText);
+            final SchemaIdentifier schemaId = SchemaIdentifier.builder()
+                    .name(name)
+                    .version(version)
+                    .build();
+            return AvroTypeUtil.createSchema(avroSchema, schemaText, schemaId);
+        } catch (final SchemaParseException e) {
+            final String errorMessage = String.format("Obtained Schema with name [%s] from Apicurio Schema Registry but the Schema Text " +
+                    "that was returned is not a valid Avro Schema", name);
+            throw new SchemaNotFoundException(errorMessage, e);
+        }
+    }
+
+    public static int extractVersionAttributeFromStream(InputStream in) {
+        final JsonNode metadataNode;
+        try {
+            metadataNode = OBJECT_MAPPER.readTree(in);
+        } catch (IOException e) {
+            throw new UncheckedIOException("Failed to read version from HTTP response stream", e);
+        }
+        return Integer.parseInt(metadataNode.get("version").asText());
+    }
+
+    public static ResultAttributes getResultAttributes(InputStream in) {
+        final JsonNode jsonNode;
+        try {
+            jsonNode = OBJECT_MAPPER.readTree(in);
+        } catch (IOException e) {
+            throw new UncheckedIOException("Failed to read result attributes from HTTP response stream", e);
+        }
+        final JsonNode artifactNode = jsonNode.get("artifacts").get(0);
+        final String groupId = artifactNode.get("groupId").asText();
+        final String artifactId = artifactNode.get("id").asText();
+        final String name = artifactNode.get("name").asText();
+        return new ResultAttributes(groupId, artifactId, name);
+    }
+
+    public record ResultAttributes(String groupId, String artifactId, String name) {
+    }
+
+    static ObjectMapper setObjectMapper() {
+        return new ObjectMapper();
+    }
+}
---
+++ nifi-nar-bundles/nifi-apicurio-bundle/nifi-apicurio-schema-registry-service/src/test/java/org/apache/nifi/apicurio/schemaregistry/client/SchemaRegistryApiClientTest.java
@@ -0,0 +1,90 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.apicurio.schemaregistry.client;
+
+import org.apache.nifi.web.client.StandardHttpUriBuilder;
+import org.apache.nifi.web.client.api.HttpUriBuilder;
+import org.apache.nifi.web.client.provider.api.WebClientServiceProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.net.URI;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
+class SchemaRegistryApiClientTest {
+
+    private static final String BASE_URL = "http://test.apicurio-schema-registry.com:8888";
+    private static final String API_PATH = "/apis/registry/v2";
+    private static final String METADATA_PATH = "/meta";
+    private static final String GROUP_ID = "groupId1";
+    private static final String ARTIFACT_ID = "artifactId1";
+    private static final String SCHEMA_PATH = String.format("/groups/%s/artifacts/%s", GROUP_ID, ARTIFACT_ID);
+    private static final String SCHEMA_NAME = "schema1";
+    private static final String SEARCH_PATH = String.format("/search/artifacts?name=%s&limit=1", SCHEMA_NAME);
+
+    @Mock
+    private WebClientServiceProvider webClientServiceProvider;
+    private SchemaRegistryApiClient client;
+
+    @BeforeEach
+    void setup() {
+        doReturn(new StandardHttpUriBuilder()).when(webClientServiceProvider).getHttpUriBuilder();
+    }
+
+    @Test
+    void testBuildBaseUrl() {
+        client = new SchemaRegistryApiClient(webClientServiceProvider, BASE_URL);
+
+        final HttpUriBuilder httpUriBuilder = client.buildBaseUri();
+
+        assertEquals(BASE_URL + API_PATH, httpUriBuilder.build().toString());
+    }
+
+    @Test
+    void testBuildSearchUri() {
+        client = new SchemaRegistryApiClient(webClientServiceProvider, BASE_URL);
+
+        final URI uri = client.buildSearchUri(SCHEMA_NAME);
+
+        assertEquals(BASE_URL + API_PATH + SEARCH_PATH, uri.toString());
+    }
+
+    @Test
+    void testBuildMetadataUri() {
+        client = new SchemaRegistryApiClient(webClientServiceProvider, BASE_URL);
+
+        final URI uri = client.buildMetaDataUri(GROUP_ID, ARTIFACT_ID);
+
+        assertEquals(BASE_URL + API_PATH + SCHEMA_PATH + METADATA_PATH, uri.toString());
+    }
+
+    @Test
+    void testBuildSchemaUri() {
+        client = new SchemaRegistryApiClient(webClientServiceProvider, BASE_URL);
+
+        final URI uri = client.buildSchemaUri(GROUP_ID, ARTIFACT_ID);
+
+        assertEquals(BASE_URL + API_PATH + SCHEMA_PATH, uri.toString());
+    }
+
+}
