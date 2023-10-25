90498a352d059376b96630c1c3f1136313999d17
annanys23 <annanys@gmail.com>
2023-10-25 00:24:27 +0000

    NIFI-12249 FetchFTP and FetchSFTP set fetch.failure.reason on failures
    - Set fetch.failure.reason to relationship name when routing to failure relationships
    
    This closes #7929
    
    Signed-off-by: David Handermann <exceptionfactory@apache.org>

--- nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/test/java/org/apache/nifi/processors/standard/TestFetchFTP.java
+++ nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors/src/test/java/org/apache/nifi/processors/standard/TestFetchFTP.java
@@ -113,6 +113,9 @@ public class TestFetchFTP {
 
         runner.run(1, false, false);
         runner.assertAllFlowFilesTransferred(FetchFileTransfer.REL_NOT_FOUND, 1);
+        runner.assertAllFlowFilesContainAttribute(FetchFileTransfer.FAILURE_REASON_ATTRIBUTE);
+        MockFlowFile transferredFlowFile = runner.getPenalizedFlowFiles().get(0);
+        assertEquals(FetchFileTransfer.REL_NOT_FOUND.getName(), transferredFlowFile.getAttribute(FetchFileTransfer.FAILURE_REASON_ATTRIBUTE));
     }
 
     @Test
@@ -122,6 +125,9 @@ public class TestFetchFTP {
 
         runner.run(1, false, false);
         runner.assertAllFlowFilesTransferred(FetchFileTransfer.REL_PERMISSION_DENIED, 1);
+        runner.assertAllFlowFilesContainAttribute(FetchFileTransfer.FAILURE_REASON_ATTRIBUTE);
+        MockFlowFile transferredFlowFile = runner.getPenalizedFlowFiles().get(0);
+        assertEquals(FetchFileTransfer.REL_PERMISSION_DENIED.getName(), transferredFlowFile.getAttribute(FetchFileTransfer.FAILURE_REASON_ATTRIBUTE));
     }
 
     @Test
@@ -132,6 +138,7 @@ public class TestFetchFTP {
 
         runner.run(2, false, false);
         runner.assertAllFlowFilesTransferred(FetchFileTransfer.REL_PERMISSION_DENIED, 2);
+        runner.assertAllFlowFilesContainAttribute(FetchFileTransfer.FAILURE_REASON_ATTRIBUTE);
 
         assertEquals(1, proc.numberOfFileTransfers);
         assertFalse(proc.isClosed);
@@ -145,6 +152,7 @@ public class TestFetchFTP {
 
         runner.run(2, false, false);
         runner.assertAllFlowFilesTransferred(FetchFileTransfer.REL_NOT_FOUND, 2);
+        runner.assertAllFlowFilesContainAttribute(FetchFileTransfer.FAILURE_REASON_ATTRIBUTE);
 
         assertEquals(1, proc.numberOfFileTransfers);
         assertFalse(proc.isClosed);
@@ -157,6 +165,9 @@ public class TestFetchFTP {
 
         runner.run(1, false, false);
         runner.assertAllFlowFilesTransferred(FetchFileTransfer.REL_COMMS_FAILURE, 1);
+        runner.assertAllFlowFilesContainAttribute(FetchFileTransfer.FAILURE_REASON_ATTRIBUTE);
+        MockFlowFile transferredFlowFile = runner.getPenalizedFlowFiles().get(0);
+        assertEquals(FetchFileTransfer.REL_COMMS_FAILURE.getName(), transferredFlowFile.getAttribute(FetchFileTransfer.FAILURE_REASON_ATTRIBUTE));
 
         assertTrue(proc.isClosed);
     }
