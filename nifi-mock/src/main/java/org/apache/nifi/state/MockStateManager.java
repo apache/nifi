ff77ee51318c96a2ae630b1ddffc0c810edd1344
Peter Turcsanyi <turcsanyi@apache.org>
2023-11-12 21:55:25 +0100

    NIFI-11294 Support Component State Checkpoints in ConsumeAzureEventHub
    This closes #8013
    
    Signed-off-by: David Handermann <exceptionfactory@apache.org>
    Co-authored-by: Peter Turcsanyi <turcsanyip@apache.org>
    Co-authored-by: Malthe Borch <mborch@gmail.com>
    
    (cherry picked from commit 2a3a7d93795d0c7a7efbf91e195fc2a9a7c4633a)

--- nifi-mock/src/main/java/org/apache/nifi/state/MockStateManager.java
+++ nifi-mock/src/main/java/org/apache/nifi/state/MockStateManager.java
@@ -29,7 +29,18 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
+import static org.apache.nifi.state.MockStateManager.ExecutionMode.CLUSTERED;
+import static org.apache.nifi.state.MockStateManager.ExecutionMode.STANDALONE;
+
 public class MockStateManager implements StateManager {
+
+    public enum ExecutionMode {
+        // in CLUSTERED mode separate state maps are used for the CLUSTER and the LOCAL scopes
+        CLUSTERED,
+        // in STANDALONE mode the same state map (the local one) is used for both the CLUSTER and the LOCAL scopes
+        STANDALONE
+    }
+
     private final AtomicInteger versionIndex = new AtomicInteger(0);
 
     private StateMap localStateMap = new MockStateMap(null, -1L);
@@ -46,6 +57,8 @@ public class MockStateManager implements StateManager {
 
     private final boolean usesLocalState;
     private final boolean usesClusterState;
+
+    private ExecutionMode executionMode = CLUSTERED;
 
     public MockStateManager(final Object component) {
         final Stateful stateful = component.getClass().getAnnotation(Stateful.class);
@@ -75,13 +88,17 @@ public class MockStateManager implements StateManager {
         localStateMap = new MockStateMap(null, -1L);
     }
 
+    public void setExecutionMode(ExecutionMode executionMode) {
+        this.executionMode = executionMode;
+    }
+
     @Override
     public synchronized void setState(final Map<String, String> state, final Scope scope) throws IOException {
         verifyAnnotation(scope);
         verifyCanSet(scope);
         final StateMap stateMap = new MockStateMap(state, versionIndex.incrementAndGet());
 
-        if (scope == Scope.CLUSTER) {
+        if (scope == Scope.CLUSTER && executionMode == CLUSTERED) {
             clusterStateMap = stateMap;
         } else {
             localStateMap = stateMap;
@@ -99,7 +116,7 @@ public class MockStateManager implements StateManager {
         verifyAnnotation(scope);
         if (scope == Scope.CLUSTER) {
             clusterRetrievedCount.incrementAndGet();
-            return clusterStateMap;
+            return executionMode == CLUSTERED ? clusterStateMap : localStateMap;
         } else {
             localRetrievedCount.incrementAndGet();
             return localStateMap;
@@ -121,9 +138,13 @@ public class MockStateManager implements StateManager {
     public synchronized boolean replace(final StateMap oldValue, final Map<String, String> newValue, final Scope scope) throws IOException {
         verifyAnnotation(scope);
         if (scope == Scope.CLUSTER) {
-            if (oldValue == clusterStateMap) {
+            if (executionMode == CLUSTERED && oldValue == clusterStateMap) {
                 verifyCanSet(scope);
                 clusterStateMap = new MockStateMap(newValue, versionIndex.incrementAndGet());
+                return true;
+            } else if (executionMode == STANDALONE && oldValue == localStateMap) {
+                verifyCanSet(scope);
+                localStateMap = new MockStateMap(newValue, versionIndex.incrementAndGet());
                 return true;
             }
 
@@ -177,7 +198,7 @@ public class MockStateManager implements StateManager {
 
     private String getValue(final String key, final Scope scope) {
         final StateMap stateMap;
-        if (scope == Scope.CLUSTER) {
+        if (scope == Scope.CLUSTER && executionMode == CLUSTERED) {
             stateMap = clusterStateMap;
         } else {
             stateMap = localStateMap;
