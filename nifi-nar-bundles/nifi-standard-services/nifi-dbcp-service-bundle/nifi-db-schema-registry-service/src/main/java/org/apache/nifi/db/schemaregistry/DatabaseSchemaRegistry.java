392e94447e2b754cbe1be28db39fd0ef7365c250
Matt Burgess <mattyb149@apache.org>
2023-11-16 16:54:28 -0500

    NIFI-12382: Add DatabaseSchemaRegistry service

---
+++ nifi-nar-bundles/nifi-standard-services/nifi-dbcp-service-bundle/nifi-db-schema-registry-service/src/main/java/org/apache/nifi/db/schemaregistry/DatabaseSchemaRegistry.java
@@ -0,0 +1,172 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.db.schemaregistry;
+
+import org.apache.nifi.annotation.lifecycle.OnEnabled;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.controller.AbstractControllerService;
+import org.apache.nifi.controller.ConfigurationContext;
+import org.apache.nifi.dbcp.DBCPService;
+import org.apache.nifi.expression.ExpressionLanguageScope;
+import org.apache.nifi.processor.util.StandardValidators;
+import org.apache.nifi.schema.access.SchemaField;
+import org.apache.nifi.schema.access.SchemaNotFoundException;
+import org.apache.nifi.schemaregistry.services.SchemaRegistry;
+import org.apache.nifi.serialization.SimpleRecordSchema;
+import org.apache.nifi.serialization.record.RecordField;
+import org.apache.nifi.serialization.record.RecordSchema;
+import org.apache.nifi.serialization.record.SchemaIdentifier;
+import org.apache.nifi.serialization.record.util.DataTypeUtils;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+public class DatabaseSchemaRegistry extends AbstractControllerService implements SchemaRegistry {
+
+    private static final Set<SchemaField> schemaFields = EnumSet.of(SchemaField.SCHEMA_NAME);
+
+    static final PropertyDescriptor DBCP_SERVICE = new PropertyDescriptor.Builder()
+            .name("dbcp-service")
+            .displayName("Database Connection Pooling Service")
+            .description("The Controller Service that is used to obtain a connection to the database for retrieving table information.")
+            .required(true)
+            .identifiesControllerService(DBCPService.class)
+            .build();
+
+    static final PropertyDescriptor CATALOG_NAME = new PropertyDescriptor.Builder()
+            .name("catalog-name")
+            .displayName("Catalog Name")
+            .description("The name of the catalog used to locate the desired table. This may not apply for the database that you are querying. In this case, leave the field empty. Note that if the "
+                    + "property is set and the database is case-sensitive, the catalog name must match the database's catalog name exactly.")
+            .required(false)
+            .expressionLanguageSupported(ExpressionLanguageScope.ENVIRONMENT)
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
+
+    static final PropertyDescriptor SCHEMA_NAME = new PropertyDescriptor.Builder()
+            .name("schema-name")
+            .displayName("Schema Name")
+            .description("The name of the schema that the table belongs to. This may not apply for the database that you are updating. In this case, leave the field empty. Note that if the "
+                    + "property is set and the database is case-sensitive, the schema name must match the database's schema name exactly.")
+            .required(false)
+            .expressionLanguageSupported(ExpressionLanguageScope.ENVIRONMENT)
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
+
+    protected List<PropertyDescriptor> propDescriptors = Collections.unmodifiableList(Arrays.asList(
+            DBCP_SERVICE,
+            CATALOG_NAME,
+            SCHEMA_NAME
+    ));
+
+    private volatile DBCPService dbcpService;
+    private volatile String dbCatalogName;
+    private volatile String dbSchemaName;
+
+    @Override
+    protected List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        return propDescriptors;
+    }
+
+    @OnEnabled
+    public void onEnabled(final ConfigurationContext context) {
+        dbcpService = context.getProperty(DBCP_SERVICE).asControllerService(DBCPService.class);
+        dbCatalogName = context.getProperty(CATALOG_NAME).evaluateAttributeExpressions().getValue();
+        dbSchemaName = context.getProperty(SCHEMA_NAME).evaluateAttributeExpressions().getValue();
+    }
+
+    @Override
+    public RecordSchema retrieveSchema(SchemaIdentifier schemaIdentifier) throws IOException, SchemaNotFoundException {
+        if (schemaIdentifier.getName().isPresent()) {
+            return retrieveSchemaByName(schemaIdentifier);
+        } else {
+            throw new SchemaNotFoundException("This Schema Registry only supports retrieving a schema by name.");
+        }
+    }
+
+    @Override
+    public Set<SchemaField> getSuppliedSchemaFields() {
+        return schemaFields;
+    }
+
+    RecordSchema retrieveSchemaByName(final SchemaIdentifier schemaIdentifier) throws IOException, SchemaNotFoundException {
+        final Optional<String> schemaName = schemaIdentifier.getName();
+        if (!schemaName.isPresent()) {
+            throw new SchemaNotFoundException("Cannot retrieve schema because Schema Name is not present");
+        }
+
+        final String tableName = schemaName.get();
+        try {
+            try (final Connection conn = dbcpService.getConnection()) {
+                final DatabaseMetaData dmd = conn.getMetaData();
+                try (final ResultSet colrs = dmd.getColumns(dbCatalogName, dbSchemaName, tableName, "%")) {
+                    final List<RecordField> recordFields = new ArrayList<>();
+                    while (colrs.next()) {
+                        // COLUMN_DEF must be read first to work around Oracle bug, see NIFI-4279 for details
+                        final String defaultValue = colrs.getString("COLUMN_DEF");
+                        final String columnName = colrs.getString("COLUMN_NAME");
+                        final int dataType = colrs.getInt("DATA_TYPE");
+                        final String nullableValue = colrs.getString("IS_NULLABLE");
+                        final boolean isNullable = "YES".equalsIgnoreCase(nullableValue) || nullableValue.isEmpty();
+                        recordFields.add(new RecordField(
+                                columnName,
+                                DataTypeUtils.getDataTypeFromSQLTypeValue(dataType),
+                                defaultValue,
+                                isNullable));
+                    }
+
+                    // If no columns are found, check that the table exists
+                    if (recordFields.isEmpty()) {
+                        try (final ResultSet tblrs = dmd.getTables(dbCatalogName, dbSchemaName, tableName, null)) {
+                            List<String> qualifiedNameSegments = new ArrayList<>();
+                            if (dbCatalogName != null) {
+                                qualifiedNameSegments.add(dbCatalogName);
+                            }
+                            if (dbSchemaName != null) {
+                                qualifiedNameSegments.add(dbSchemaName);
+                            }
+                            qualifiedNameSegments.add(tableName);
+
+                            if (!tblrs.next()) {
+                                throw new SchemaNotFoundException("Table "
+                                        + String.join(".", qualifiedNameSegments)
+                                        + " not found");
+                            } else {
+                                getLogger().warn("Table "
+                                        + String.join(".", qualifiedNameSegments)
+                                        + " found but no columns were found, if this is not expected then check the user permissions for getting table metadata from the database");
+                            }
+                        }
+                    }
+                    return new SimpleRecordSchema(recordFields);
+                }
+            }
+        } catch (SQLException sqle) {
+            throw new IOException("Error retrieving schema for table " + schemaName.get(), sqle);
+        }
+    }
+}
---
+++ nifi-nar-bundles/nifi-standard-services/nifi-dbcp-service-bundle/nifi-db-schema-registry-service/src/test/java/org/apache/nifi/db/schemaregistry/DatabaseSchemaRegistryTest.java
@@ -0,0 +1,196 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.db.schemaregistry;
+
+import org.apache.nifi.controller.AbstractControllerService;
+import org.apache.nifi.dbcp.DBCPService;
+import org.apache.nifi.dbcp.utils.DBCPProperties;
+import org.apache.nifi.processor.exception.ProcessException;
+import org.apache.nifi.reporting.InitializationException;
+import org.apache.nifi.schema.access.SchemaNotFoundException;
+import org.apache.nifi.serialization.record.RecordField;
+import org.apache.nifi.serialization.record.RecordFieldType;
+import org.apache.nifi.serialization.record.RecordSchema;
+import org.apache.nifi.serialization.record.SchemaIdentifier;
+import org.apache.nifi.serialization.record.StandardSchemaIdentifier;
+import org.apache.nifi.util.NoOpProcessor;
+import org.apache.nifi.util.TestRunner;
+import org.apache.nifi.util.TestRunners;
+import org.apache.nifi.util.file.FileUtils;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.spy;
+
+public class DatabaseSchemaRegistryTest {
+
+    private static final List<String> CREATE_TABLE_STATEMENTS = Arrays.asList(
+            "CREATE TABLE PERSONS (id integer primary key, name varchar(100)," +
+                    " code integer CONSTRAINT CODE_RANGE CHECK (code >= 0 AND code < 1000), dt date)",
+            "CREATE TABLE SCHEMA1.PERSONS (id integer primary key, name varchar(100)," +
+                    " code integer CONSTRAINT CODE_RANGE CHECK (code >= 0 AND code < 1000), dt date)",
+            "CREATE TABLE SCHEMA2.PERSONS (id2 integer primary key, name varchar(100)," +
+                    " code integer CONSTRAINT CODE_RANGE CHECK (code >= 0 AND code < 1000), dt date)",
+            "CREATE TABLE UUID_TEST (id integer primary key, name VARCHAR(100))",
+            "CREATE TABLE LONGVARBINARY_TEST (id integer primary key, name LONG VARCHAR FOR BIT DATA)"
+    );
+
+    private static final String SERVICE_ID = DBCPServiceSimpleImpl.class.getName();
+
+    private final static String DB_LOCATION = "target/db_schema_reg";
+
+    private TestRunner runner;
+
+    @BeforeAll
+    public static void setupDatabase() throws Exception {
+        System.setProperty("derby.stream.error.file", "target/derby.log");
+
+        // remove previous test database, if any
+        final File dbLocation = new File(DB_LOCATION);
+        try {
+            FileUtils.deleteFile(dbLocation, true);
+        } catch (IOException ignore) {
+            // Do nothing, may not have existed
+        }
+
+        // Mock the DBCP Controller Service so we can control the Results
+        DBCPService setupDbService = spy(new DBCPServiceSimpleImpl(DB_LOCATION));
+
+        final Connection conn = setupDbService.getConnection();
+        final Statement stmt = conn.createStatement();
+        for (String createTableStatement : CREATE_TABLE_STATEMENTS) {
+            stmt.execute(createTableStatement);
+        }
+
+        stmt.close();
+        conn.close();
+    }
+
+    @AfterAll
+    public static void shutdownDatabase() throws Exception {
+        try {
+            DriverManager.getConnection("jdbc:derby:" + DB_LOCATION + ";shutdown=true");
+        } catch (Exception ignore) {
+            // Do nothing, this is what happens at Derby shutdown
+        }
+        // remove previous test database, if any
+        final File dbLocation = new File(DB_LOCATION);
+        try {
+            FileUtils.deleteFile(dbLocation, true);
+        } catch (IOException ignore) {
+            // Do nothing, may not have existed
+        }
+        System.clearProperty("derby.stream.error.file");
+    }
+
+    @BeforeEach
+    public void setService() throws InitializationException {
+        runner = TestRunners.newTestRunner(NoOpProcessor.class);
+        DBCPService dbcp = new DBCPServiceSimpleImpl(DB_LOCATION);
+        runner.addControllerService(SERVICE_ID, dbcp);
+
+        final String url = String.format("jdbc:derby:%s;create=false", DB_LOCATION);
+        runner.setProperty(dbcp, DBCPProperties.DATABASE_URL, url);
+        runner.setProperty(dbcp, DBCPProperties.DB_USER, String.class.getSimpleName());
+        runner.setProperty(dbcp, DBCPProperties.DB_PASSWORD, String.class.getName());
+        runner.setProperty(dbcp, DBCPProperties.DB_DRIVERNAME, "org.apache.derby.jdbc.EmbeddedDriver");
+        runner.enableControllerService(dbcp);
+    }
+
+    @Test
+    public void testGetSchemaExists() throws Exception {
+        DatabaseSchemaRegistry dbSchemaRegistry = new DatabaseSchemaRegistry();
+        runner.addControllerService("schemaRegistry", dbSchemaRegistry);
+        runner.setProperty(dbSchemaRegistry, DatabaseSchemaRegistry.DBCP_SERVICE, SERVICE_ID);
+        runner.setProperty(dbSchemaRegistry, DatabaseSchemaRegistry.SCHEMA_NAME, "SCHEMA1");
+        runner.enableControllerService(dbSchemaRegistry);
+        SchemaIdentifier schemaIdentifier = new StandardSchemaIdentifier.Builder()
+                .name("PERSONS")
+                .build();
+        RecordSchema recordSchema = dbSchemaRegistry.retrieveSchema(schemaIdentifier);
+        assertNotNull(recordSchema);
+        Optional<RecordField> recordField = recordSchema.getField("ID");
+        assertTrue(recordField.isPresent());
+        assertEquals(RecordFieldType.INT.getDataType(), recordField.get().getDataType());
+        recordField = recordSchema.getField("NAME");
+        assertTrue(recordField.isPresent());
+        assertEquals(RecordFieldType.STRING.getDataType(), recordField.get().getDataType());
+        recordField = recordSchema.getField("CODE");
+        assertTrue(recordField.isPresent());
+        assertEquals(RecordFieldType.INT.getDataType(), recordField.get().getDataType());
+        recordField = recordSchema.getField("DT");
+        assertTrue(recordField.isPresent());
+        assertEquals(RecordFieldType.DATE.getDataType(), recordField.get().getDataType());
+        // Get nonexistent field
+        recordField = recordSchema.getField("NOT_A_FIELD");
+        assertFalse(recordField.isPresent());
+    }
+
+    @Test
+    public void testGetSchemaNotExists() throws Exception {
+        DatabaseSchemaRegistry dbSchemaRegistry = new DatabaseSchemaRegistry();
+        runner.addControllerService("schemaRegistry", dbSchemaRegistry);
+        runner.setProperty(dbSchemaRegistry, DatabaseSchemaRegistry.DBCP_SERVICE, SERVICE_ID);
+        runner.setProperty(dbSchemaRegistry, DatabaseSchemaRegistry.SCHEMA_NAME, "SCHEMA1");
+        runner.enableControllerService(dbSchemaRegistry);
+        SchemaIdentifier schemaIdentifier = new StandardSchemaIdentifier.Builder()
+                .name("NOT_A_TABLE")
+                .build();
+        assertThrows(SchemaNotFoundException.class, () -> dbSchemaRegistry.retrieveSchema(schemaIdentifier));
+    }
+
+    private static class DBCPServiceSimpleImpl extends AbstractControllerService implements DBCPService {
+
+        private final String databaseLocation;
+
+        public DBCPServiceSimpleImpl(final String databaseLocation) {
+            this.databaseLocation = databaseLocation;
+        }
+
+        @Override
+        public String getIdentifier() {
+            return this.getClass().getName();
+        }
+
+        @Override
+        public Connection getConnection() throws ProcessException {
+            try {
+                Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+                return DriverManager.getConnection("jdbc:derby:" + databaseLocation + ";create=true");
+
+            } catch (final Exception e) {
+                throw new ProcessException("getConnection failed: " + e);
+            }
+        }
+    }
+}
