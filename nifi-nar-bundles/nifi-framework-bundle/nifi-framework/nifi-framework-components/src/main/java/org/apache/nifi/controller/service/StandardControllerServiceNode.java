6b310560f115fcfecf7699ac8a1ef0a39fdf3d0e
Nissim Shiman <nshiman@yahoo.com>
2024-01-19 15:40:06 +0000

    NIFI-12387 Initialize controller service comments
    This allows controller service configuration to be saved without flow
    configuration history recording a configuration change for comments.

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-components/src/main/java/org/apache/nifi/controller/service/StandardControllerServiceNode.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-components/src/main/java/org/apache/nifi/controller/service/StandardControllerServiceNode.java
@@ -136,6 +136,7 @@ public class StandardControllerServiceNode extends AbstractComponentNode impleme
         this.active = new AtomicBoolean();
         setControllerServiceAndProxy(implementation, proxiedControllerService, invocationHandler);
         stateTransition = new ServiceStateTransition(this);
+        this.comment = "";
     }
 
     @Override
