8530aa0974596e7617c769a6e17f3fd87f4f788e
James Scott Aslan <scottyaslan@gmail.com>
2024-01-30 15:50:54 -0500

    [NIFI-12698] dark mode, [NIFI-12687] status history axis label fix

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-frontend/src/main/nifi/src/app/ui/common/navigation/navigation.component.ts
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-frontend/src/main/nifi/src/app/ui/common/navigation/navigation.component.ts
@@ -15,8 +15,9 @@
  * limitations under the License.
  */
 
-import { Component } from '@angular/core';
-import { AsyncPipe, NgIf, NgOptimizedImage } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { AsyncPipe, DOCUMENT, NgIf, NgOptimizedImage } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatMenuModule } from '@angular/material/menu';
 import { getNodeStatusHistoryAndOpenDialog } from '../../../state/status-history/status-history.actions';
@@ -30,22 +31,27 @@ import { selectCurrentUser } from '../../../state/current-user/current-user.sele
 import { MatButtonModule } from '@angular/material/button';
 import { NiFiState } from '../../../state';
 import { selectFlowConfiguration } from '../../../state/flow-configuration/flow-configuration.selectors';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { environment } from '../../../environments/environment';
 
 @Component({
     selector: 'navigation',
     standalone: true,
-    imports: [NgOptimizedImage, AsyncPipe, MatDividerModule, MatMenuModule, NgIf, RouterLink, MatButtonModule],
+    imports: [NgOptimizedImage, AsyncPipe, MatDividerModule, MatMenuModule, NgIf, RouterLink, MatButtonModule, MatSlideToggleModule, FormsModule],
     templateUrl: './navigation.component.html',
     styleUrls: ['./navigation.component.scss']
 })
 export class Navigation {
     currentUser$ = this.store.select(selectCurrentUser);
     flowConfiguration$ = this.store.select(selectFlowConfiguration);
+    isDarkMode: boolean = false;
+    isDevMode: boolean = !environment.production;
 
     constructor(
         private store: Store<NiFiState>,
         private authStorage: AuthStorage,
-        private authService: AuthService
+        private authService: AuthService,
+        @Inject(DOCUMENT) private _document: Document
     ) {}
 
     allowLogin(user: CurrentUser): boolean {
@@ -78,5 +84,12 @@ export class Navigation {
                 }
             })
         );
+    }
+
+    toggleTheme(value = !this.isDarkMode) {
+        this.isDarkMode = value;
+        this._document.body.classList.toggle('dark-theme', value);
+
+        // TODO: save to local storage or read OS settings???
     }
 }
