88d434f9eca41b939e2eb7213f45b8bb8ae00ed3
Bence Simon <bsimon@apache.org>
2024-02-08 15:41:27 +0100

    NIFI-12735 Adding the option to execute flow analysis before committing snapshot to registry
    This closes #8377.
    
    Signed-off-by: Tamas Palfy <tpalfy@apache.org>

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-api/src/main/java/org/apache/nifi/web/StandardNiFiServiceFacade.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-api/src/main/java/org/apache/nifi/web/StandardNiFiServiceFacade.java
@@ -5026,21 +5026,13 @@ public class StandardNiFiServiceFacade implements NiFiServiceFacade {
             // If the flow has been created, but failed to add a snapshot,
             // then we need to capture the created versioned flow information as a partial successful result.
             if (registerNewFlow) {
-                logger.error("The flow has been created, but failed to add a snapshot. Returning the created flow information.", e);
-                final VersionControlInformationDTO vci = new VersionControlInformationDTO();
-                vci.setBucketId(registeredFlow.getBucketIdentifier());
-                vci.setBucketName(registeredFlow.getBucketName());
-                vci.setFlowId(registeredFlow.getIdentifier());
-                vci.setFlowName(registeredFlow.getName());
-                vci.setFlowDescription(registeredFlow.getDescription());
-                vci.setGroupId(groupId);
-                vci.setRegistryId(registryId);
-                vci.setRegistryName(getFlowRegistryName(registryId));
-                vci.setVersion(0);
-                vci.setState(VersionedFlowState.SYNC_FAILURE.name());
-                vci.setStateExplanation(e.getLocalizedMessage());
-
-                return createVersionControlComponentMappingEntity(groupId, versionedProcessGroup, vci);
+                try {
+                    flowRegistryDAO
+                        .getFlowRegistryClient(registryId)
+                        .deregisterFlow(FlowRegistryClientContextFactory.getContextForUser(NiFiUserUtils.getNiFiUser()), versionedFlowDto.getBucketId(), flowId);
+                } catch (final IOException | FlowRegistryException e2) {
+                    throw new NiFiCoreException("Failed to remove flow from Flow Registry due to " + e2.getMessage(), e2);
+                }
             }
 
             throw e;
