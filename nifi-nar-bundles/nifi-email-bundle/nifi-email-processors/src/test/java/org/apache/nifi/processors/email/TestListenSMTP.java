31ffdf0a67d89314734ee91ebc85e2781b224b89
exceptionfactory <exceptionfactory@apache.org>
2024-02-19 20:18:09 -0600

    NIFI-12820 Upgraded Email Processors to Jakarta Mail 2
    - Upgraded from Java Mail 1.4.7 to Jakarta Mail API 2.1.2
    - Upgraded Spring Integration from 5.5.20 to 6.2.1
    - Upgraded SubEtha SMTP from 3.1.7 to 7.0.1
    - Upgraded Greenmail from 1.6.15 to 2.0.1
    - Removed usage of Commons Lang3
    - Removed usage of Commons IO

--- nifi-nar-bundles/nifi-email-bundle/nifi-email-processors/src/test/java/org/apache/nifi/processors/email/TestListenSMTP.java
+++ nifi-nar-bundles/nifi-email-bundle/nifi-email-processors/src/test/java/org/apache/nifi/processors/email/TestListenSMTP.java
@@ -30,12 +30,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 import javax.net.ssl.SSLContext;
 import java.net.Socket;
 import java.security.GeneralSecurityException;
