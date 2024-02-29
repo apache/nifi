eeafa8354a4448020a6f936825fa47733a92bb71
ravisingh <ravisingh740@gmail.com>
2024-02-28 19:43:07 -0800

    NIFI-12828: Added Mapping for BIT type to return  INT and handled boolean case for postgres
    Signed-off-by: Matt Burgess <mattyb149@apache.org>

--- nifi-commons/nifi-record/src/main/java/org/apache/nifi/serialization/record/util/DataTypeUtils.java
+++ nifi-commons/nifi-record/src/main/java/org/apache/nifi/serialization/record/util/DataTypeUtils.java
@@ -2180,8 +2180,9 @@ public class DataTypeUtils {
         switch (sqlType) {
             case Types.BIGINT:
                 return RecordFieldType.BIGINT.getDataType();
-            case Types.BOOLEAN:
             case Types.BIT:
+                return RecordFieldType.INT.getDataType();
+            case Types.BOOLEAN:
                 return RecordFieldType.BOOLEAN.getDataType();
             case Types.TINYINT:
                 return RecordFieldType.BYTE.getDataType();
