53207a20a0d2692437368ffaee594ae6e796a647
EndzeitBegins <16666115+EndzeitBegins@users.noreply.github.com>
2024-03-02 00:34:07 +0100

    NIFI-12857 Simplified implementation of QueuePrioritizer and added tests
    This closes #8466
    
    Signed-off-by: David Handermann <exceptionfactory@apache.org>

--- nifi-framework-bundle/nifi-framework/nifi-framework-core/src/main/java/org/apache/nifi/controller/queue/QueuePrioritizer.java
+++ nifi-framework-bundle/nifi-framework/nifi-framework-core/src/main/java/org/apache/nifi/controller/queue/QueuePrioritizer.java
@@ -18,74 +18,47 @@
 package org.apache.nifi.controller.queue;
 
 import org.apache.nifi.controller.repository.FlowFileRecord;
-import org.apache.nifi.controller.repository.claim.ContentClaim;
 import org.apache.nifi.flowfile.FlowFilePrioritizer;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class QueuePrioritizer implements Comparator<FlowFileRecord>, Serializable {
     private static final long serialVersionUID = 1L;
-    private final transient List<FlowFilePrioritizer> prioritizers = new ArrayList<>();
+    private static final Comparator<FlowFileRecord> penaltyComparator = Comparator
+            .comparing(FlowFileRecord::isPenalized)
+            .thenComparingLong(record -> record.isPenalized() ? record.getPenaltyExpirationMillis() : 0);
+    private static final Comparator<FlowFileRecord> claimComparator = Comparator
+            .comparing(FlowFileRecord::getContentClaim, Comparator.nullsFirst(Comparator.naturalOrder()))
+            .thenComparingLong(FlowFileRecord::getContentClaimOffset);
+    private static final Comparator<FlowFileRecord> idComparator = Comparator.comparingLong(FlowFileRecord::getId);
+
+    private final transient List<FlowFilePrioritizer> prioritizers;
 
     public QueuePrioritizer(final List<FlowFilePrioritizer> priorities) {
-        if (null != priorities) {
-            prioritizers.addAll(priorities);
-        }
+        prioritizers = priorities == null ? List.of() : List.copyOf(priorities);
     }
 
     @Override
     public int compare(final FlowFileRecord f1, final FlowFileRecord f2) {
-        int returnVal = 0;
-        final boolean f1Penalized = f1.isPenalized();
-        final boolean f2Penalized = f2.isPenalized();
-
-        if (f1Penalized && !f2Penalized) {
-            return 1;
-        } else if (!f1Penalized && f2Penalized) {
-            return -1;
+        final int penaltyComparisonResult = penaltyComparator.compare(f1, f2);
+        if (penaltyComparisonResult != 0) {
+            return penaltyComparisonResult;
         }
 
-        if (f1Penalized && f2Penalized) {
-            if (f1.getPenaltyExpirationMillis() < f2.getPenaltyExpirationMillis()) {
-                return -1;
-            } else if (f1.getPenaltyExpirationMillis() > f2.getPenaltyExpirationMillis()) {
-                return 1;
+        for (FlowFilePrioritizer comparator : prioritizers) {
+            final int prioritizerComparisonResult = comparator.compare(f1, f2);
+            if (prioritizerComparisonResult != 0) {
+                return prioritizerComparisonResult;
             }
         }
 
-        if (!prioritizers.isEmpty()) {
-            for (final FlowFilePrioritizer prioritizer : prioritizers) {
-                returnVal = prioritizer.compare(f1, f2);
-                if (returnVal != 0) {
-                    return returnVal;
-                }
-            }
+        final int claimComparisionResult = claimComparator.compare(f1, f2);
+        if (claimComparisionResult != 0) {
+            return claimComparisionResult;
         }
 
-        final ContentClaim claim1 = f1.getContentClaim();
-        final ContentClaim claim2 = f2.getContentClaim();
-
-
-        // put the one without a claim first
-        if (claim1 == null && claim2 != null) {
-            return -1;
-        } else if (claim1 != null && claim2 == null) {
-            return 1;
-        } else if (claim1 != null && claim2 != null) {
-            final int claimComparison = claim1.compareTo(claim2);
-            if (claimComparison != 0) {
-                return claimComparison;
-            }
-
-            final int claimOffsetComparison = Long.compare(f1.getContentClaimOffset(), f2.getContentClaimOffset());
-            if (claimOffsetComparison != 0) {
-                return claimOffsetComparison;
-            }
-        }
-
-        return Long.compare(f1.getId(), f2.getId());
+        return idComparator.compare(f1, f2);
     }
 }
