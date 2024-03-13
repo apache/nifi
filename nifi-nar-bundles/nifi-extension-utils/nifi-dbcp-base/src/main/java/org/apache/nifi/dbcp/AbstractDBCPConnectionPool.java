7bfbae8190ab73cc47a9c9d9224bab35c94cec17
lehelb <lehel44@hotmail.com>
2024-03-13 14:22:19 -0500

    NIFI-12890: Refactor HadoopDBCPConnectionPool to extend AbstractDBCPConnectionPool

--- nifi-nar-bundles/nifi-extension-utils/nifi-dbcp-base/src/main/java/org/apache/nifi/dbcp/AbstractDBCPConnectionPool.java
+++ nifi-nar-bundles/nifi-extension-utils/nifi-dbcp-base/src/main/java/org/apache/nifi/dbcp/AbstractDBCPConnectionPool.java
@@ -160,7 +160,13 @@ public abstract class AbstractDBCPConnectionPool extends AbstractControllerServi
     protected void configureDataSource(final ConfigurationContext context, final BasicDataSource basicDataSource, final DataSourceConfiguration configuration) {
         final Driver driver = getDriver(configuration.getDriverName(), configuration.getUrl());
 
-        basicDataSource.setDriver(driver);
+        if (driver != null) {
+            basicDataSource.setDriver(driver);
+        } else {
+            basicDataSource.setDriverClassName(configuration.getDriverName());
+            basicDataSource.setDriverClassLoader(configuration.getDriverClassLoader());
+        }
+
         basicDataSource.setMaxWait(Duration.ofMillis(configuration.getMaxWaitMillis()));
         basicDataSource.setMaxTotal(configuration.getMaxTotal());
         basicDataSource.setMinIdle(configuration.getMinIdle());
