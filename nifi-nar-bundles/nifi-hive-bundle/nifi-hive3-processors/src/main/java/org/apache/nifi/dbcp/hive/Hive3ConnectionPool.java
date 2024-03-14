27fa595b25c848bc06579748c17be2b0bf6dcd2e
EndzeitBegins <16666115+EndzeitBegins@users.noreply.github.com>
2024-03-14 19:29:38 +0100

    NIFI-12901 Removed time units in description of time period properties
    This closes #8504
    
    Signed-off-by: David Handermann <exceptionfactory@apache.org>

--- nifi-nar-bundles/nifi-hive-bundle/nifi-hive3-processors/src/main/java/org/apache/nifi/dbcp/hive/Hive3ConnectionPool.java
+++ nifi-nar-bundles/nifi-hive-bundle/nifi-hive3-processors/src/main/java/org/apache/nifi/dbcp/hive/Hive3ConnectionPool.java
@@ -198,7 +198,7 @@ public class Hive3ConnectionPool extends AbstractControllerService implements Hi
     public static final PropertyDescriptor MAX_CONN_LIFETIME = new PropertyDescriptor.Builder()
             .displayName("Max Connection Lifetime")
             .name("dbcp-max-conn-lifetime")
-            .description("The maximum lifetime in milliseconds of a connection. After this time is exceeded the " +
+            .description("The maximum lifetime of a connection. After this time is exceeded the " +
                     "connection will fail the next activation, passivation or validation test. A value of zero or less " +
                     "means the connection has an infinite lifetime.")
             .defaultValue(DEFAULT_MAX_CONN_LIFETIME)
@@ -210,7 +210,7 @@ public class Hive3ConnectionPool extends AbstractControllerService implements Hi
     public static final PropertyDescriptor EVICTION_RUN_PERIOD = new PropertyDescriptor.Builder()
             .displayName("Time Between Eviction Runs")
             .name("dbcp-time-between-eviction-runs")
-            .description("The number of milliseconds to sleep between runs of the idle connection evictor thread. When " +
+            .description("The time period to sleep between runs of the idle connection evictor thread. When " +
                     "non-positive, no idle connection evictor thread will be run.")
             .defaultValue(DEFAULT_EVICTION_RUN_PERIOD)
             .required(false)
