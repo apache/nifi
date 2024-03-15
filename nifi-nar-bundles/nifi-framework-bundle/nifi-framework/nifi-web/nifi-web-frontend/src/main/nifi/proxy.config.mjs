d97c7946967ea57602b48b96d7e6312834bacf2e
Matt Gilman <matt.c.gilman@gmail.com>
2024-03-15 16:15:48 -0400

    NIFI-12902: Migrating to @angular-devkit/build-angular:application (#8507)
    * NIFI-12902:
    - Migrating to @angular-devkit/build-angular:application to avoid and avoid sporadic webpack build issues.
    - Updating roboto dependencies.
    
    * NIFI-12902:
    - Clean up.
    
    This closes #8507

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-frontend/src/main/nifi/proxy.config.mjs
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-frontend/src/main/nifi/proxy.config.mjs
@@ -5,9 +5,25 @@ const target = {
     changeOrigin: true,
     headers: {
         'X-ProxyPort': 4200
+    },
+    configure: (proxy, _options) => {
+        proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+        });
+        proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+        });
+        proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+        });
+    },
+    bypass: function(req, res, proxyOptions) {
+        if (req.url.startsWith('/nifi/')) {
+            return req.url;
+        }
     }
 };
 
 export default {
-    '/': target
+    '/**': target
 };
