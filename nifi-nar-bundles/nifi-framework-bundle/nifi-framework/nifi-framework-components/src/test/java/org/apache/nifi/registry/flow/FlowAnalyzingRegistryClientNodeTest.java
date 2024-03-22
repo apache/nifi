c0a01bd096c90e5502afe21349783485d6543486
Bence Simon <bsimon@apache.org>
2024-03-22 11:32:02 +0100

    NIFI-12735 Limiting the behaviour to enforcing rules; Smaller adjustments based on code review comments

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-components/src/test/java/org/apache/nifi/registry/flow/FlowAnalyzingRegistryClientNodeTest.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-components/src/test/java/org/apache/nifi/registry/flow/FlowAnalyzingRegistryClientNodeTest.java
@@ -20,6 +20,7 @@ import org.apache.nifi.controller.flow.FlowManager;
 import org.apache.nifi.controller.flowanalysis.FlowAnalyzer;
 import org.apache.nifi.controller.service.ControllerServiceProvider;
 import org.apache.nifi.flow.VersionedProcessGroup;
+import org.apache.nifi.flowanalysis.EnforcementPolicy;
 import org.apache.nifi.groups.ProcessGroup;
 import org.apache.nifi.registry.flow.mapping.InstantiatedVersionedProcessGroup;
 import org.apache.nifi.registry.flow.mapping.NiFiRegistryFlowMapper;
@@ -32,15 +33,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.UUID;
-import java.util.function.Supplier;
 
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class FlowAnalyzingRegistryClientNodeTest {
     private final static String INSTANCE_IDENTIFIER = UUID.randomUUID().toString();
     private final static String COMMENT_TEXT = "comment";
@@ -62,9 +65,6 @@ class FlowAnalyzingRegistryClientNodeTest {
     FlowManager flowManager;
 
     @Mock
-    Supplier<NiFiRegistryFlowMapper> flowMapperSupplier;
-
-    @Mock
     NiFiRegistryFlowMapper flowMapper;
 
     @Mock
@@ -76,6 +76,15 @@ class FlowAnalyzingRegistryClientNodeTest {
     @Mock
     VersionedProcessGroup versionedProcessGroup;
 
+    @Mock
+    RuleViolation ruleViolation1;
+
+    @Mock
+    RuleViolation ruleViolation2;
+
+    @Mock
+    RuleViolation ruleViolation3;
+
     private final FlowRegistryClientUserContext context = new StandardFlowRegistryClientUserContext();
     private final RegisteredFlow flow = new RegisteredFlow();
 
@@ -83,14 +92,16 @@ class FlowAnalyzingRegistryClientNodeTest {
     public void setUp() {
         Mockito.when(versionedProcessGroup.getInstanceIdentifier()).thenReturn(INSTANCE_IDENTIFIER);
         Mockito.when(flowManager.getGroup(Mockito.anyString())).thenReturn(processGroup);
-        Mockito.when(flowMapperSupplier.get()).thenReturn(flowMapper);
         Mockito.when(flowMapper.mapNonVersionedProcessGroup(Mockito.same(processGroup), Mockito.same(serviceProvider))).thenReturn(nonVersionedProcessGroup);
+        Mockito.when(ruleViolation1.getEnforcementPolicy()).thenReturn(EnforcementPolicy.ENFORCE);
+        Mockito.when(ruleViolation2.getEnforcementPolicy()).thenReturn(EnforcementPolicy.ENFORCE);
+        Mockito.when(ruleViolation3.getEnforcementPolicy()).thenReturn(EnforcementPolicy.WARN);
     }
 
     @Test
-    public void testWhenNoViolationFound() throws IOException, FlowRegistryException {
+    public void allowFlowRegistrationWhenNoEnforcingViolationFound() throws IOException, FlowRegistryException {
         Mockito.when(ruleViolationsManager.getRuleViolationsForGroup(Mockito.anyString())).thenReturn(Collections.emptyList());
-        final FlowAnalyzingRegistryClientNode testSubject = new FlowAnalyzingRegistryClientNode(node, serviceProvider, flowAnalyzer, ruleViolationsManager, flowManager, flowMapperSupplier);
+        final FlowAnalyzingRegistryClientNode testSubject = new FlowAnalyzingRegistryClientNode(node, serviceProvider, flowAnalyzer, ruleViolationsManager, flowManager, flowMapper);
 
         testSubject.registerFlowSnapshot(context, flow, versionedProcessGroup, Collections.emptyMap(), Collections.emptyMap(), Collections.emptyMap(), COMMENT_TEXT, EXPECTED_VERSION);
 
@@ -100,12 +111,24 @@ class FlowAnalyzingRegistryClientNodeTest {
     }
 
     @Test
-    public void testWhenViolationFound() throws IOException, FlowRegistryException {
-        Mockito.when(ruleViolationsManager.getRuleViolationsForGroup(Mockito.anyString())).thenReturn(Arrays.asList(Mockito.mock(RuleViolation.class), Mockito.mock(RuleViolation.class)));
-        final FlowAnalyzingRegistryClientNode testSubject = new FlowAnalyzingRegistryClientNode(node, serviceProvider, flowAnalyzer, ruleViolationsManager, flowManager, flowMapperSupplier);
+    public void allowFlowRegistrationWhenWarningViolationFound() throws IOException, FlowRegistryException {
+        Mockito.when(ruleViolationsManager.getRuleViolationsForGroup(Mockito.anyString())).thenReturn(Collections.singletonList(ruleViolation3));
+        final FlowAnalyzingRegistryClientNode testSubject = new FlowAnalyzingRegistryClientNode(node, serviceProvider, flowAnalyzer, ruleViolationsManager, flowManager, flowMapper);
+
+        testSubject.registerFlowSnapshot(context, flow, versionedProcessGroup, Collections.emptyMap(), Collections.emptyMap(), Collections.emptyMap(), COMMENT_TEXT, EXPECTED_VERSION);
+
+        Mockito
+            .verify(node, Mockito.only())
+            .registerFlowSnapshot(context, flow, versionedProcessGroup, Collections.emptyMap(), Collections.emptyMap(), Collections.emptyMap(), COMMENT_TEXT, EXPECTED_VERSION);
+    }
+
+    @Test
+    public void preventFlowRegistrationWhenEnforcingViolationFound() throws IOException, FlowRegistryException {
+        Mockito.when(ruleViolationsManager.getRuleViolationsForGroup(Mockito.anyString())).thenReturn(Arrays.asList(ruleViolation1, ruleViolation2));
+        final FlowAnalyzingRegistryClientNode testSubject = new FlowAnalyzingRegistryClientNode(node, serviceProvider, flowAnalyzer, ruleViolationsManager, flowManager, flowMapper);
 
         Assertions.assertThrows(
-            ViolatingFlowException.class,
+            FlowRegistryPreCommitException.class,
             () -> testSubject.registerFlowSnapshot(context, flow, versionedProcessGroup, Collections.emptyMap(), Collections.emptyMap(), Collections.emptyMap(), COMMENT_TEXT, EXPECTED_VERSION)
         );
 
