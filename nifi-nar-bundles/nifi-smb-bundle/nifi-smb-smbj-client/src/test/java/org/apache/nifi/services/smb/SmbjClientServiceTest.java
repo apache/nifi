d3a7549e8109774b06e79f13d570ef5da04c451f
Peter Turcsanyi <turcsanyi@apache.org>
2024-04-09 18:09:13 +0200

    NIFI-12231 FetchSmb supports Move and Delete Completion Strategies
    Signed-off-by: Pierre Villard <pierre.villard.fr@gmail.com>
    
    This closes #8617.

--- nifi-nar-bundles/nifi-smb-bundle/nifi-smb-smbj-client/src/test/java/org/apache/nifi/services/smb/SmbjClientServiceTest.java
+++ nifi-nar-bundles/nifi-smb-bundle/nifi-smb-smbj-client/src/test/java/org/apache/nifi/services/smb/SmbjClientServiceTest.java
@@ -52,7 +52,7 @@ class SmbjClientServiceTest {
         when(share.fileExists("to")).thenReturn(false);
         when(share.fileExists("create")).thenReturn(false);
 
-        underTest.createDirectory("directory/path/to/create");
+        underTest.ensureDirectory("directory/path/to/create");
 
         verify(share).mkdir("directory/path");
         verify(share).mkdir("directory/path/to");
