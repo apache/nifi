674e0ce86a06735645bf9ecf605fe0ed38c5793c
James Scott Aslan <scottyaslan@gmail.com>
2024-04-09 11:31:59 -0400

    review feedback

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-frontend/src/main/nifi/src/app/pages/bulletins/ui/bulletin-board/bulletin-board-list/bulletin-board-list.component.ts
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-frontend/src/main/nifi/src/app/pages/bulletins/ui/bulletin-board/bulletin-board-list/bulletin-board-list.component.ts
@@ -143,7 +143,7 @@ export class BulletinBoardList implements AfterViewInit {
     getSeverity(severity: string) {
         switch (severity.toLowerCase()) {
             case 'error':
-                return 'bulletin-error material-warn-darker';
+                return 'bulletin-error mat-warn-darker';
             case 'warn':
             case 'warning':
                 return 'bulletin-warn';
