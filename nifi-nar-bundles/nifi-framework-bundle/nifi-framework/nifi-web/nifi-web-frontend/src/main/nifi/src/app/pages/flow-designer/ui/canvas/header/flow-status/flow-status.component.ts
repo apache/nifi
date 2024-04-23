dac5086e2e36723bc7ea240c5e60862f336541d4
James Scott Aslan <scottyaslan@gmail.com>
2024-04-23 09:54:27 -0400

    fix flow status clustered icon warn color

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-frontend/src/main/nifi/src/app/pages/flow-designer/ui/canvas/header/flow-status/flow-status.component.ts
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-frontend/src/main/nifi/src/app/pages/flow-designer/ui/canvas/header/flow-status/flow-status.component.ts
@@ -69,7 +69,7 @@ export class FlowStatus {
             this.clusterSummary?.connectedToCluster === false ||
             this.clusterSummary?.connectedNodeCount != this.clusterSummary?.totalNodeCount
         ) {
-            return 'warning';
+            return 'nifi-warn-darker';
         }
 
         return 'primary-color';
