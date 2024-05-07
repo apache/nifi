4bbe08fb5523d03ef0f899898f86efa34f65c038
Matt Gilman <matt.c.gilman@gmail.com>
2024-05-07 11:33:58 -0400

    NIFI-13164: (#8760)
    - Moving from jsdom to happy dom for test environment execution.
    - Removing test for the Documentation component that was failing because it was attempting to load an iframe with the nifi documentation that wasn't available.
    
    This closes #8760

--- nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-frontend/src/main/nifi/jest.config.ts
+++ nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-frontend/src/main/nifi/jest.config.ts
@@ -155,7 +155,7 @@ const config: Config = {
     // snapshotSerializers: [],
 
     // The test environment that will be used for testing
-    testEnvironment: 'jsdom',
+    testEnvironment: '@happy-dom/jest-environment',
 
     // Options that will be passed to the testEnvironment
     // testEnvironmentOptions: {},
