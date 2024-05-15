0171f1b318a787e60b7301cce71aa9274fd6d6d7
James Scott Aslan <scottyaslan@gmail.com>
2024-05-15 16:06:42 -0400

    remove unused MatHint

--- nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-frontend/src/main/nifi/src/app/pages/cluster/ui/common/cluster-table-filter/cluster-table-filter.component.ts
+++ nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-frontend/src/main/nifi/src/app/pages/cluster/ui/common/cluster-table-filter/cluster-table-filter.component.ts
@@ -17,7 +17,7 @@
 
 import { AfterViewInit, Component, DestroyRef, EventEmitter, inject, Input, Output } from '@angular/core';
 import { MatCheckbox } from '@angular/material/checkbox';
-import { MatFormField, MatHint, MatLabel } from '@angular/material/form-field';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatOption } from '@angular/material/autocomplete';
 import { MatSelect } from '@angular/material/select';
@@ -42,7 +42,7 @@ export interface ClusterTableFilterContext extends ClusterTableFilterArgs {
 @Component({
     selector: 'cluster-table-filter',
     standalone: true,
-    imports: [MatCheckbox, MatFormField, MatInput, MatLabel, MatOption, MatSelect, ReactiveFormsModule, MatHint],
+    imports: [MatCheckbox, MatFormField, MatInput, MatLabel, MatOption, MatSelect, ReactiveFormsModule],
     templateUrl: './cluster-table-filter.component.html',
     styleUrl: './cluster-table-filter.component.scss'
 })
