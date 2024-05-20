a0ab203f58149380404e2b0ce53c809ce661673e
Jim Steinebrey <jrsteinebrey@gmail.com>
2024-05-20 16:40:28 -0400

    NIFI-13154 Remove unneeded import

--- nifi-api/src/main/java/org/apache/nifi/components/PropertyDescriptor.java
+++ nifi-api/src/main/java/org/apache/nifi/components/PropertyDescriptor.java
@@ -34,7 +34,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.regex.Pattern;
 
 /**
  * An immutable object for holding information about a type of component
