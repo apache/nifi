15a56a19026c093b7b838c04613195a77e90efdb
sujkm <suejkim12@gmail.com>
2024-06-10 18:22:26 -0400

    NIFI-13378: Adds Version State as filterable option in Process Groups tab in Summary (#8945)
    * NIFI-13378: Adds Version State as filterable option in Process Groups tab in Summary
    
    * updated value of selections to enum values
    
    This closes #8945

--- nifi-frontend/src/main/frontend/apps/nifi/src/app/pages/summary/ui/process-group-status-listing/process-group-status-table/process-group-status-table.component.ts
+++ nifi-frontend/src/main/frontend/apps/nifi/src/app/pages/summary/ui/process-group-status-listing/process-group-status-table/process-group-status-table.component.ts
@@ -83,7 +83,14 @@ export class ProcessGroupStatusTable extends ComponentStatusTable<ProcessGroupSt
     @Input() rootProcessGroup!: ProcessGroupStatusSnapshot;
 
     override filterPredicate(data: ProcessGroupStatusSnapshotEntity, filter: string): boolean {
-        const { filterTerm, filterColumn } = JSON.parse(filter);
+        const { filterTerm, filterColumn, filterVersionedFlowState } = JSON.parse(filter);
+        const matchOnVersionedFlowState: boolean = filterVersionedFlowState !== 'All';
+
+        if (matchOnVersionedFlowState) {
+            if (data.processGroupStatusSnapshot.versionedFlowState !== filterVersionedFlowState) {
+                return false;
+            }
+        }
 
         if (filterTerm === '') {
             return true;
