8ed8e617a6e8e58cb041f1210a28d857fb1ea5af
exceptionfactory <exceptionfactory@apache.org>
2024-06-20 10:16:29 -0500

    NIFI-13082 Corrected code formatting

--- nifi-extension-bundles/nifi-network-bundle/nifi-network-processors/src/test/java/org/apache/nifi/processors/network/pcap/TestPCAP.java
+++ nifi-extension-bundles/nifi-network-bundle/nifi-network-processors/src/test/java/org/apache/nifi/processors/network/pcap/TestPCAP.java
@@ -14,93 +14,91 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.apache.nifi.processors.network.pcap;
 
- package org.apache.nifi.processors.network.pcap;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
- import java.nio.ByteBuffer;
- import java.nio.ByteOrder;
- import java.util.ArrayList;
- import java.util.List;
- import java.util.Map;
+import org.junit.jupiter.api.Test;
 
- import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
- import static org.junit.jupiter.api.Assertions.assertArrayEquals;
- import static org.junit.jupiter.api.Assertions.assertEquals;
+public class TestPCAP {
+    private static final byte[] PACKET_DATA = new byte[]{
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+            10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+            20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+    };
 
- public class TestPCAP {
-     private static final byte[] PACKET_DATA = new byte[]{
-         0,  1,  2,  3,  4,  5,  6,  7,  8,  9,
-         10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-         20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-     };
+    private static final int[][] PCAP_HEADER_VALUES = new int[][]{
+            new int[]{2, 2},
+            new int[]{4, 2},
+            new int[]{0, 4},
+            new int[]{0, 4},
+            new int[]{40, 4},
+            new int[]{1, 4}
+    };
 
-     private static final int[][] PCAP_HEADER_VALUES = new int[][]{
-         new int[] {2, 2},
-         new int[] {4, 2},
-         new int[] {0, 4},
-         new int[] {0, 4},
-         new int[] {40, 4},
-         new int[] {1, 4}
-     };
+    private static final Map<String, Long> packetHeaderValues = Map.of(
+            "tsSec", 1713184965L,
+            "tsUsec", 1000L,
+            "inclLen", 30L,
+            "origLen", 30L
+    );
 
-     private static final Map<String, Long> packetHeaderValues = Map.of(
-         "tsSec", 1713184965L,
-         "tsUsec", 1000L,
-         "inclLen", 30L,
-         "origLen", 30L
-     );
+    @Test
+    void testReadBytesFull() {
 
-     @Test
-     void testReadBytesFull() {
+        // Create a header for the test PCAP
+        ByteBuffer headerBuffer = ByteBuffer.allocate(PCAPHeader.PCAP_HEADER_LENGTH);
+        headerBuffer.put(new byte[]{(byte) 0xa1, (byte) 0xb2, (byte) 0xc3, (byte) 0xd4});
+        for (int[] value : PCAP_HEADER_VALUES) {
+            headerBuffer.put(PCAP.readIntToNBytes(value[0], value[1]));
+        }
+        PCAPHeader hdr = new PCAPHeader(new ByteBufferReader(headerBuffer.array()));
+        // Create a sample packet
+        List<Packet> packets = new ArrayList<>();
+        packets.add(new Packet(
+                packetHeaderValues.get("tsSec"),
+                packetHeaderValues.get("tsUsec"),
+                packetHeaderValues.get("inclLen"),
+                packetHeaderValues.get("origLen"),
+                PACKET_DATA
+        ));
 
-         // Create a header for the test PCAP
-         ByteBuffer headerBuffer = ByteBuffer.allocate(PCAPHeader.PCAP_HEADER_LENGTH);
-         headerBuffer.put(new byte[]{(byte) 0xa1, (byte) 0xb2, (byte) 0xc3, (byte) 0xd4});
-         for (int[] value : PCAP_HEADER_VALUES) {
-             headerBuffer.put(PCAP.readIntToNBytes(value[0], value[1], false));
-         }
-         PCAPHeader hdr = new PCAPHeader(new ByteBufferReader(headerBuffer.array()));
-         // Create a sample packet
-         List<Packet> packets = new ArrayList<>();
-         packets.add(new Packet(
-             packetHeaderValues.get("tsSec"),
-             packetHeaderValues.get("tsUsec"),
-             packetHeaderValues.get("inclLen"),
-             packetHeaderValues.get("origLen"),
-             PACKET_DATA
-         ));
+        // create test PCAP
+        PCAP testPcap = new PCAP(hdr, packets);
 
-         // create test PCAP
-         PCAP testPcap = new PCAP(hdr, packets);
+        // Call the readBytesFull method
+        byte[] result = testPcap.toByteArray();
 
-         // Call the readBytesFull method
-         byte[] result = testPcap.toByteArray();
+        // Assert the expected byte array length
+        assertEquals(70, result.length);
 
-         // Assert the expected byte array length
-         assertEquals(70, result.length);
+        // Assert the expected byte array values
+        ByteBuffer buffer = ByteBuffer.wrap(result);
+        assertEquals(0xa1b2c3d4, buffer.getInt());
+        ByteBuffer litteEndianBuffer = ByteBuffer.wrap(result).order(ByteOrder.LITTLE_ENDIAN);
+        litteEndianBuffer.position(4);
 
-         // Assert the expected byte array values
-         ByteBuffer buffer = ByteBuffer.wrap(result);
-         assertEquals(0xa1b2c3d4, buffer.getInt());
-         ByteBuffer litteEndianBuffer = ByteBuffer.wrap(result).order(ByteOrder.LITTLE_ENDIAN);
-         litteEndianBuffer.position(4);
+        for (int[] value : PCAP_HEADER_VALUES) {
+            if (value[1] == 2) {
+                assertEquals(value[0], litteEndianBuffer.getShort());
+            } else {
+                assertEquals(value[0], litteEndianBuffer.getInt());
+            }
+        }
 
-         for (int[] value : PCAP_HEADER_VALUES) {
-             if (value[1] == 2) {
-                 assertEquals(value[0], litteEndianBuffer.getShort());
-             } else {
-                 assertEquals(value[0], litteEndianBuffer.getInt());
-             }
-         }
-
-         // Using getInt and then casting to long because we're reading 4 bytes, which is an 'int' in java.nio
-         assertEquals(packetHeaderValues.get("tsSec"), (long) litteEndianBuffer.getInt());
-         assertEquals(packetHeaderValues.get("tsUsec"), (long) litteEndianBuffer.getInt());
-         assertEquals(packetHeaderValues.get("inclLen"), (long) litteEndianBuffer.getInt());
-         assertEquals(packetHeaderValues.get("origLen"), (long) litteEndianBuffer.getInt());
-         byte[] body = new byte[30];
-         litteEndianBuffer.get(40, body, 0, 30).array();
-         assertArrayEquals(body, PACKET_DATA);
-     }
- }
+        assertEquals(packetHeaderValues.get("tsSec"), litteEndianBuffer.getInt());
+        assertEquals(packetHeaderValues.get("tsUsec"), litteEndianBuffer.getInt());
+        assertEquals(packetHeaderValues.get("inclLen"), litteEndianBuffer.getInt());
+        assertEquals(packetHeaderValues.get("origLen"), litteEndianBuffer.getInt());
+        byte[] body = new byte[30];
+        litteEndianBuffer.get(40, body, 0, 30);
+        assertArrayEquals(body, PACKET_DATA);
+    }
+}
--- nifi-extension-bundles/nifi-network-bundle/nifi-network-processors/src/test/java/org/apache/nifi/processors/network/pcap/TestSplitPCAP.java
+++ nifi-extension-bundles/nifi-network-bundle/nifi-network-processors/src/test/java/org/apache/nifi/processors/network/pcap/TestSplitPCAP.java
@@ -14,125 +14,121 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.apache.nifi.processors.network.pcap;
 
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.Map;
 
- package org.apache.nifi.processors.network.pcap;
+import org.apache.nifi.processor.Relationship;
+import org.apache.nifi.util.TestRunner;
+import org.apache.nifi.util.TestRunners;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
- import java.nio.ByteBuffer;
- import java.util.ArrayList;
- import java.util.List;
- import java.util.Collections;
- import java.util.Map;
+class TestSplitPCAP {
 
- import org.apache.nifi.processor.Relationship;
- import org.apache.nifi.util.TestRunner;
- import org.apache.nifi.util.TestRunners;
- import org.junit.jupiter.api.BeforeEach;
- import org.junit.jupiter.api.Test;
+    private PCAPHeader pcapHeader;
+    private Packet validPacket;
+    private Packet invalidPacket;
 
+    protected static final long PACKET_TIMESTAMP = 1713184965;
+    protected static final long PACKET_INTERVAL = 1000;
+    private static final byte[] PACKET_DATA = new byte[]{
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+            10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+            20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+    };
 
+    @BeforeEach
+    void init() {
 
- class TestSplitPCAP {
+        ByteBuffer headerBuffer = ByteBuffer.allocate(PCAPHeader.PCAP_HEADER_LENGTH);
+        headerBuffer.put(new byte[]{(byte) 0xa1, (byte) 0xb2, (byte) 0xc3, (byte) 0xd4});
+        headerBuffer.put(PCAP.readIntToNBytes(2, 2));
+        headerBuffer.put(PCAP.readIntToNBytes(4, 2));
+        headerBuffer.put(PCAP.readIntToNBytes(0, 4));
+        headerBuffer.put(PCAP.readIntToNBytes(0, 4));
+        headerBuffer.put(PCAP.readIntToNBytes(4000, 4));
+        headerBuffer.put(PCAP.readIntToNBytes(1, 4));
+        this.pcapHeader = new PCAPHeader(new ByteBufferReader(headerBuffer.array()));
 
-     private PCAPHeader pcapHeader;
-     private Packet validPacket;
-     private Packet invalidPacket;
+        this.validPacket = new Packet(
+                PACKET_TIMESTAMP,
+                PACKET_INTERVAL,
+                30,
+                30,
+                PACKET_DATA
+        );
 
-     protected static final long PACKET_TIMESTAMP = 1713184965;
-     protected static final long PACKET_INTERVAL = 1000;
-     private static final byte[] PACKET_DATA = new byte[]{
-         0,  1,  2,  3,  4,  5,  6,  7,  8,  9,
-         10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-         20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-     };
+        this.invalidPacket = new Packet(
+                PACKET_TIMESTAMP,
+                PACKET_INTERVAL,
+                50,
+                10,
+                PACKET_DATA
+        );
 
-     @BeforeEach
-     void init() {
+    }
 
-         ByteBuffer headerBuffer = ByteBuffer.allocate(PCAPHeader.PCAP_HEADER_LENGTH);
-         headerBuffer.put(new byte[]{(byte) 0xa1, (byte) 0xb2, (byte) 0xc3, (byte) 0xd4});
-         headerBuffer.put(PCAP.readIntToNBytes(2, 2, false));
-         headerBuffer.put(PCAP.readIntToNBytes(4, 2, false));
-         headerBuffer.put(PCAP.readIntToNBytes(0, 4, false));
-         headerBuffer.put(PCAP.readIntToNBytes(0, 4, false));
-         headerBuffer.put(PCAP.readIntToNBytes(4000, 4, false));
-         headerBuffer.put(PCAP.readIntToNBytes(1, 4, false));
-         this.pcapHeader = new PCAPHeader(new ByteBufferReader(headerBuffer.array()));
+    void executeTest(String pcapMaxSize, List<Packet> packets, Map<Relationship, Integer> expectedRelations) {
+        TestRunner runner = TestRunners.newTestRunner(SplitPCAP.class);
+        runner.setProperty(SplitPCAP.PCAP_MAX_SIZE, pcapMaxSize);
 
-         this.validPacket = new Packet(
-             PACKET_TIMESTAMP,
-             PACKET_INTERVAL,
-             (long) 30,
-             (long) 30,
-             PACKET_DATA
-         );
+        PCAP testPcap = new PCAP(this.pcapHeader, packets);
 
-         this.invalidPacket = new Packet(
-             PACKET_TIMESTAMP,
-             PACKET_INTERVAL,
-             (long) 50,
-             (long) 10,
-             PACKET_DATA
-         );
+        runner.enqueue(testPcap.toByteArray());
 
-     }
+        runner.run();
 
-     void executeTest(String pcapMaxSize, List<Packet> packets, Map<Relationship, Integer> expectedRelations) {
-         TestRunner runner = TestRunners.newTestRunner(SplitPCAP.class);
-         runner.setProperty(SplitPCAP.PCAP_MAX_SIZE, pcapMaxSize);
+        for (Map.Entry<Relationship, Integer> entry : expectedRelations.entrySet()) {
+            runner.assertTransferCount(entry.getKey(), entry.getValue());
+        }
 
-         PCAP testPcap = new PCAP(this.pcapHeader, packets);
+        runner.assertQueueEmpty();
+    }
 
-         runner.enqueue(testPcap.toByteArray());
+    @Test
+    void testSuccesses() {
+        executeTest(
+                "100B",
+                Collections.nCopies(3, this.validPacket),
+                Map.of(
+                        SplitPCAP.REL_SPLIT, 3,
+                        SplitPCAP.REL_ORIGINAL, 1
+                )
+        );
+        executeTest(
+                "50B",
+                Collections.nCopies(3, this.validPacket),
+                Map.of(
+                        SplitPCAP.REL_SPLIT, 4,
+                        SplitPCAP.REL_ORIGINAL, 1
+                )
+        );
+    }
 
-         runner.run();
+    @Test
+    void testFailures() {
+        executeTest(
+                "50B",
+                Collections.nCopies(3, this.invalidPacket),
+                Map.of(SplitPCAP.REL_FAILURE, 1)
+        );
+        executeTest(
+                "10B",
+                Collections.nCopies(3, this.validPacket),
+                Map.of(SplitPCAP.REL_FAILURE, 1)
+        );
 
-         for (Map.Entry<Relationship, Integer> entry : expectedRelations.entrySet()) {
-             runner.assertTransferCount(entry.getKey(), entry.getValue());
-         }
-
-         runner.assertQueueEmpty();
-     }
-
-     @Test
-     void testSuccesses()  {
-         executeTest(
-             "100B",
-             Collections.nCopies(3, this.validPacket),
-             Map.of(
-                 SplitPCAP.REL_SPLIT, 3,
-                 SplitPCAP.REL_ORIGINAL, 1
-             )
-         );
-         executeTest(
-             "50B",
-             Collections.nCopies(3, this.validPacket),
-             Map.of(
-                 SplitPCAP.REL_SPLIT, 4,
-                 SplitPCAP.REL_ORIGINAL, 1
-             )
-         );
-     }
-
-     @Test
-     void testFailures()  {
-         executeTest(
-             "50B",
-             Collections.nCopies(3, this.invalidPacket),
-             Map.of(SplitPCAP.REL_FAILURE, 1)
-         );
-         executeTest(
-             "10B",
-             Collections.nCopies(3, this.validPacket),
-             Map.of(SplitPCAP.REL_FAILURE, 1)
-         );
-
-         List<Packet> mixedValidityPackets = new ArrayList<>(Collections.nCopies(3, this.validPacket));
-         mixedValidityPackets.add(this.invalidPacket);
-         executeTest(
-             "50B",
-             mixedValidityPackets,
-             Map.of(SplitPCAP.REL_FAILURE, 1)
-         );
-     }
- }
+        List<Packet> mixedValidityPackets = new ArrayList<>(Collections.nCopies(3, this.validPacket));
+        mixedValidityPackets.add(this.invalidPacket);
+        executeTest(
+                "50B",
+                mixedValidityPackets,
+                Map.of(SplitPCAP.REL_FAILURE, 1)
+        );
+    }
+}
