0932c24a6ae6c55cc9b8143486b8cfcc88df4bca
sullis <github@seansullivan.com>
2024-06-20 10:15:06 -0700

    NIFI-13406: rename unit test

--- nifi-extension-bundles/nifi-extension-utils/nifi-event-transport/src/test/java/org/apache/nifi/event/transport/netty/NettyTransportTest.java
+++ nifi-extension-bundles/nifi-extension-utils/nifi-event-transport/src/test/java/org/apache/nifi/event/transport/netty/NettyTransportTest.java
@@ -27,7 +27,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-public class NettyTransportsTest {
+public class NettyTransportTest {
     @Test
     public void nioIsAvailable() {
         assertTrue(NettyTransport.NIO.isAvailable());
