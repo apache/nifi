b105b5bbef5df022b3df0c96bb6af39b19691fa1
Joe Gresock <jgresock@gmail.com>
2024-06-26 10:40:22 -0400

    NIFI-13454 Renamed FileParameterProvider to KubernetesSecretParameterProvider
    This closes #9008
    
    Signed-off-by: David Handermann <exceptionfactory@apache.org>

--- nifi-extension-bundles/nifi-standard-bundle/nifi-standard-parameter-providers/src/main/java/org/apache/nifi/parameter/KubernetesSecretParameterProvider.java
+++ nifi-extension-bundles/nifi-standard-bundle/nifi-standard-parameter-providers/src/main/java/org/apache/nifi/parameter/KubernetesSecretParameterProvider.java
@@ -53,8 +53,10 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 @Tags({"file"})
-@CapabilityDescription("Fetches parameters from files.  Parameter groups are indicated by a set of directories, and files within the directories map to parameter names. " +
-        "The content of the file becomes the parameter value.")
+@CapabilityDescription("Fetches parameters from files, in the format provided by Kubernetes mounted secrets.  " +
+        "Parameter groups are indicated by a set of directories, and files within the directories map to parameter names. " +
+        "The content of the file becomes the parameter value.  Since Kubernetes mounted Secrets are base64-encoded, the " +
+        "parameter provider defaults to Base64-decoding the value of the parameter from the file.")
 
 @Restricted(
         restrictions = {
@@ -63,7 +65,7 @@ import java.util.stream.Collectors;
                         explanation = "Provides operator the ability to read from any file that NiFi has access to.")
         }
 )
-public class FileParameterProvider extends AbstractParameterProvider implements VerifiableParameterProvider  {
+public class KubernetesSecretParameterProvider extends AbstractParameterProvider implements VerifiableParameterProvider  {
     private static final int MAX_SIZE_LIMIT = 8096;
     private static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
 
