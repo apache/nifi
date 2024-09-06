57a6fcba006dc395accf79764857567fbccabdbf
Mark Bathori <mbathori@apache.org>
2024-09-06 16:07:10 +0200

    NIFI-13720: Component is not reloaded when the isolation key depends on service property
    This closes #9236.
    
    Signed-off-by: Peter Turcsanyi <turcsanyi@apache.org>

---
+++ nifi-system-tests/nifi-system-test-extensions-bundle/nifi-system-test-extensions-services/src/main/java/org/apache/nifi/cs/tests/system/ClassloaderIsolationKeyProviderService.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.cs.tests.system;
+
+import org.apache.nifi.annotation.lifecycle.OnEnabled;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.controller.AbstractControllerService;
+import org.apache.nifi.controller.ConfigurationContext;
+import org.apache.nifi.processor.util.StandardValidators;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ClassloaderIsolationKeyProviderService extends AbstractControllerService implements KeyProviderService {
+
+    static final PropertyDescriptor KEY_FIELD = new PropertyDescriptor.Builder()
+            .name("Key Field")
+            .description("")
+            .required(true)
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
+
+    private volatile String keyField;
+
+    private static final List<PropertyDescriptor> PROPERTIES = Collections.singletonList(KEY_FIELD);
+
+    @Override
+    protected List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        return PROPERTIES;
+    }
+
+    @OnEnabled
+    public void onEnabled(final ConfigurationContext context) {
+        keyField = context.getProperty(KEY_FIELD).getValue();
+    }
+
+    @Override
+    public String getKeyField() {
+        return keyField;
+    }
+}
