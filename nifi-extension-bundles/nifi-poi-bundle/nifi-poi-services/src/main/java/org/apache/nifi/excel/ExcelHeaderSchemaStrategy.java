2d9b2fd20f687e97cf0c1132a14d4358beceeef0
exceptionfactory <exceptionfactory@apache.org>
2024-10-15 10:15:53 -0500

    NIFI-13744 Removed unused import in ExcelHeaderSchemaStrategy

--- nifi-extension-bundles/nifi-poi-bundle/nifi-poi-services/src/main/java/org/apache/nifi/excel/ExcelHeaderSchemaStrategy.java
+++ nifi-extension-bundles/nifi-poi-bundle/nifi-poi-services/src/main/java/org/apache/nifi/excel/ExcelHeaderSchemaStrategy.java
@@ -37,7 +37,6 @@ import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
