48a8b79b9930fe764183dbcd064e430127386b07
HunterG6700 <hgoller6700@gmail.com>
2024-11-14 15:41:17 +0000

    NIFI-14000: Fixed filename regex I forgot to change back after testing.

--- nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-api/src/main/java/org/apache/nifi/web/api/ProcessGroupResource.java
+++ nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-api/src/main/java/org/apache/nifi/web/api/ProcessGroupResource.java
@@ -326,7 +326,7 @@ public class ProcessGroupResource extends FlowUpdateResource<ProcessGroupImportE
             filenameSuffix = "_with_services";  // Appending "_with_services" if services are included
         }
         final String flowName = currentVersionedProcessGroup.getName() + filenameSuffix;
-        final String filename = flowName.replaceAll("[^a-zA-Z0-9._-]", "_") + ".json";
+        final String filename = flowName.replaceAll("\\s", "_") + ".json";
 
         return generateOkResponse(currentVersionedFlowSnapshot).header(HttpHeaders.CONTENT_DISPOSITION, String.format("attachment; filename=\"%s\"", filename)).build();
     }
