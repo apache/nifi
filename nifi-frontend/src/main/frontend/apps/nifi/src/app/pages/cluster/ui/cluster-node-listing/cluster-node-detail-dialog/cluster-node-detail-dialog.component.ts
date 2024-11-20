c12c5fbd53857c11224fa21c0bffb0755656a3a4
Rob Fellows <rob.fellows@gmail.com>
2024-11-20 15:27:44 -0500

    [NIFI-14038] - Add quick-copy to component-context component when it displays the id (#9546)
    * [NIFI-14038] - Add quick-copy to component-context component when it displays the id
    
    * npm audit fix - cross-spawn vulnerability
    
    This closes #9546

--- nifi-frontend/src/main/frontend/apps/nifi/src/app/pages/cluster/ui/cluster-node-listing/cluster-node-detail-dialog/cluster-node-detail-dialog.component.ts
+++ nifi-frontend/src/main/frontend/apps/nifi/src/app/pages/cluster/ui/cluster-node-listing/cluster-node-detail-dialog/cluster-node-detail-dialog.component.ts
@@ -19,12 +19,12 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { MatButton } from '@angular/material/button';
 import { ClusterNode } from '../../../state/cluster-listing';
-import { CloseOnEscapeDialog } from '@nifi/shared';
+import { CloseOnEscapeDialog, CopyDirective } from '@nifi/shared';
 
 @Component({
     selector: 'cluster-node-detail-dialog',
     standalone: true,
-    imports: [MatDialogModule, MatButton],
+    imports: [MatDialogModule, MatButton, CopyDirective],
     templateUrl: './cluster-node-detail-dialog.component.html',
     styleUrl: './cluster-node-detail-dialog.component.scss'
 })
