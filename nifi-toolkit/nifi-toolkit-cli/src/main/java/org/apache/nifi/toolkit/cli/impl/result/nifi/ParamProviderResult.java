305d2ea2dd5fc2d70fc8d57cd406ea72f83ac4fe
exceptionfactory <exceptionfactory@apache.org>
2024-12-26 22:58:14 -0600

    NIFI-14116 Added PMD Rule UnnecessaryVarargsArrayCreation
    - Removed unnecessary array creation from remaining classes
    - Suppressed warnings on CapturingLogger based on interface definition

--- nifi-toolkit/nifi-toolkit-cli/src/main/java/org/apache/nifi/toolkit/cli/impl/result/nifi/ParamProviderResult.java
+++ nifi-toolkit/nifi-toolkit-cli/src/main/java/org/apache/nifi/toolkit/cli/impl/result/nifi/ParamProviderResult.java
@@ -59,7 +59,7 @@ public class ParamProviderResult extends AbstractWritableResult<ParameterProvide
                 .column("Property Value", 20, 80, false)
                 .build();
         if (parameterProviderDTO.getProperties() != null && !parameterProviderDTO.getProperties().isEmpty()) {
-            parameterProviderDTO.getProperties().forEach((name, value) -> propertiesTable.addRow(new String[] {name, value}));
+            parameterProviderDTO.getProperties().forEach((name, value) -> propertiesTable.addRow(name, value));
         }
         final Table fetchedParametersTable = new Table.Builder()
                 .column("Parameter Group", 20, 60, false)
@@ -70,12 +70,12 @@ public class ParamProviderResult extends AbstractWritableResult<ParameterProvide
         if (!sortedParameterNameGroups.isEmpty()) {
             sortedParameterNameGroups.forEach(group -> {
                 group.getParameterSensitivities().keySet().stream().sorted()
-                        .forEach(param -> fetchedParametersTable.addRow(new String[] {
+                        .forEach(param -> fetchedParametersTable.addRow(
                                 group.getGroupName(),
                                 getParameterContextId(group.getParameterContextName()),
                                 group.getParameterContextName(),
                                 param
-                        }));
+                        ));
             });
         }
 
