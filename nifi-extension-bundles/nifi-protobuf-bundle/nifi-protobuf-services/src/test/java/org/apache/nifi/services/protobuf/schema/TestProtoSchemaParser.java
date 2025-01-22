9d835e078b567dd32797a31fcae64f26365cd646
Mark Bathori <mbathori@apache.org>
2025-01-22 10:59:09 +0100

    NIFI-14174: Protobuf Reader fails to generate schema for proto files containing circular reference
    This closes #9657.
    
    Signed-off-by: Tamas Palfy <tpalfy@apache.org>

--- nifi-extension-bundles/nifi-protobuf-bundle/nifi-protobuf-services/src/test/java/org/apache/nifi/services/protobuf/schema/TestProtoSchemaParser.java
+++ nifi-extension-bundles/nifi-protobuf-bundle/nifi-protobuf-services/src/test/java/org/apache/nifi/services/protobuf/schema/TestProtoSchemaParser.java
@@ -25,11 +25,14 @@ import org.apache.nifi.serialization.record.type.RecordDataType;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Optional;
 
+import static org.apache.nifi.services.protobuf.ProtoTestUtil.loadCircularReferenceTestSchema;
 import static org.apache.nifi.services.protobuf.ProtoTestUtil.loadProto2TestSchema;
 import static org.apache.nifi.services.protobuf.ProtoTestUtil.loadProto3TestSchema;
 import static org.apache.nifi.services.protobuf.ProtoTestUtil.loadRepeatedProto3TestSchema;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestProtoSchemaParser {
 
@@ -114,5 +117,35 @@ public class TestProtoSchemaParser {
 
         final RecordSchema actual = schemaParser.createSchema("Proto2Message");
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSchemaParserWithSelfCircularReference() {
+        final ProtoSchemaParser schemaParser = new ProtoSchemaParser(loadCircularReferenceTestSchema());
+        final RecordSchema recordCSchema = schemaParser.createSchema("C");
+
+        final Optional<RecordField> parentField = recordCSchema.getField("parent");
+        assertTrue(parentField.isPresent());
+        assertEquals(RecordFieldType.RECORD, parentField.get().getDataType().getFieldType());
+
+        assertEquals(recordCSchema, ((RecordDataType) parentField.get().getDataType()).getChildSchema());
+    }
+
+    @Test
+    public void testSchemaParserWithMutualCircularReference() {
+        final ProtoSchemaParser schemaParser = new ProtoSchemaParser(loadCircularReferenceTestSchema());
+        final RecordSchema recordASchema = schemaParser.createSchema("A");
+        final Optional<RecordField> bOfA = recordASchema.getField("b");
+
+        assertTrue(bOfA.isPresent());
+        assertEquals(RecordFieldType.RECORD, bOfA.get().getDataType().getFieldType());
+
+        final RecordSchema recordBSchema = schemaParser.createSchema("B");
+        final Optional<RecordField> aOfB = recordBSchema.getField("a");
+
+        assertTrue(aOfB.isPresent());
+        assertEquals(RecordFieldType.RECORD, aOfB.get().getDataType().getFieldType());
+        assertEquals(recordBSchema, ((RecordDataType) bOfA.get().getDataType()).getChildSchema());
+        assertEquals(recordASchema, ((RecordDataType) aOfB.get().getDataType()).getChildSchema());
     }
 }
