6dd45df313db849bd8f50a7a4d7cbc025e60bde0
Marcin Gemra <marcin.gemra@snowflake.com>
2025-02-24 15:59:50 +0100

    NIFI-14275 Standardized quoting identifiers in UpdateDatabaseTable for CREATE and ALTER (#9727)
    Signed-off-by: David Handermann <exceptionfactory@apache.org>

--- nifi-extension-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/db/impl/PhoenixDatabaseAdapter.java
+++ nifi-extension-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/db/impl/PhoenixDatabaseAdapter.java
@@ -144,14 +144,12 @@ public final class PhoenixDatabaseAdapter implements DatabaseAdapter {
     }
 
     @Override
-    public String getAlterTableStatement(final String tableName, final List<ColumnDescription> columnsToAdd, final boolean quoteTableName, final boolean quoteColumnNames) {
+    public String getAlterTableStatement(final String tableName, final List<ColumnDescription> columnsToAdd) {
         List<String> columnsAndDatatypes = new ArrayList<>(columnsToAdd.size());
         for (ColumnDescription column : columnsToAdd) {
             String dataType = getSQLForDataType(column.getDataType());
             StringBuilder sb = new StringBuilder("ADD COLUMN ")
-                    .append(quoteColumnNames ? getColumnQuoteString() : "")
                     .append(column.getColumnName())
-                    .append(quoteColumnNames ? getColumnQuoteString() : "")
                     .append(" ")
                     .append(dataType);
             columnsAndDatatypes.add(sb.toString());
@@ -159,9 +157,7 @@ public final class PhoenixDatabaseAdapter implements DatabaseAdapter {
 
         StringBuilder alterTableStatement = new StringBuilder();
         return alterTableStatement.append("ALTER TABLE ")
-                .append(quoteTableName ? getTableQuoteString() : "")
                 .append(tableName)
-                .append(quoteTableName ? getTableQuoteString() : "")
                 .append(" ")
                 .append(String.join(", ", columnsAndDatatypes))
                 .toString();
