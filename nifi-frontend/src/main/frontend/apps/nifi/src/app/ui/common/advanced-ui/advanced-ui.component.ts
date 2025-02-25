9f962de101d04a238967e3c1118b757e2c8a19cf
Scott Aslan <scottyaslan@gmail.com>
2025-02-25 18:17:14 -0500

    system-token service, extract all styles, add unit tests

--- nifi-frontend/src/main/frontend/apps/nifi/src/app/ui/common/advanced-ui/advanced-ui.component.ts
+++ nifi-frontend/src/main/frontend/apps/nifi/src/app/ui/common/advanced-ui/advanced-ui.component.ts
@@ -15,14 +15,14 @@
  * limitations under the License.
  */
 
-import { Component, ElementRef, SecurityContext, ViewChild } from '@angular/core';
+import { Component, SecurityContext } from '@angular/core';
 import { NiFiState } from '../../../state';
 import { Store } from '@ngrx/store';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { HttpParams } from '@angular/common/http';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Navigation } from '../navigation/navigation.component';
-import { isDefinedAndNotNull, selectRouteData } from '@nifi/shared';
+import { isDefinedAndNotNull, selectRouteData, SystemTokensService } from '@nifi/shared';
 import { AdvancedUiParams } from '../../../state/shared';
 import { selectDisconnectionAcknowledged } from '../../../state/cluster-summary/cluster-summary.selectors';
 
@@ -33,14 +33,14 @@ import { selectDisconnectionAcknowledged } from '../../../state/cluster-summary/
     styleUrls: ['./advanced-ui.component.scss']
 })
 export class AdvancedUi {
-    @ViewChild('iframeRef', { static: false }) iframeRef!: ElementRef<HTMLIFrameElement>;
     frameSource!: SafeResourceUrl | null;
 
     private params: AdvancedUiParams | null = null;
 
     constructor(
         private store: Store<NiFiState>,
-        private domSanitizer: DomSanitizer
+        private domSanitizer: DomSanitizer,
+        protected systemTokensService: SystemTokensService
     ) {
         this.store
             .select(selectRouteData)
@@ -70,70 +70,6 @@ export class AdvancedUi {
                     }
                 }
             });
-    }
-
-    onIframeLoad(): void {
-        if (this.iframeRef?.nativeElement) {
-            const iframe = this.iframeRef.nativeElement;
-
-            // Ensure the iframe's contentDocument is available
-            if (iframe.contentDocument) {
-                const iframeDoc = iframe.contentDocument;
-
-                // Get the CSS variables from the first stylesheet (adjust index if needed)
-                const styleSheet = document.styleSheets[0]; // First stylesheet
-                let cssVariables: string = '';
-
-                // Extract CSS variables from the :root selector
-                if (styleSheet && styleSheet.cssRules) {
-                    for (let i = 0; i < styleSheet.cssRules.length; i++) {
-                        const rule = styleSheet.cssRules[i] as CSSStyleRule;
-
-                        if (rule.selectorText === ':root') {
-                            const rootStyles = rule.style; // Get styles for :root
-                            for (let j = 0; j < rootStyles.length; j++) {
-                                const propertyName = rootStyles[j];
-                                if (propertyName.startsWith('--')) {
-                                    const value = rootStyles.getPropertyValue(propertyName).trim();
-                                    cssVariables += `${propertyName}: ${value}; `;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                let darkThemeCssVariables: string = '';
-
-                // Extract CSS variables from the :root selector
-                if (styleSheet && styleSheet.cssRules) {
-                    for (let i = 0; i < styleSheet.cssRules.length; i++) {
-                        const rule = styleSheet.cssRules[i] as CSSStyleRule;
-
-                        // Extract variables from .dark-theme if the parent document has the class
-                        if (rule.selectorText === '.dark-theme') {
-                            const darkThemeStyles = rule.style;
-                            for (let j = 0; j < darkThemeStyles.length; j++) {
-                                const propertyName = darkThemeStyles[j];
-                                if (propertyName.startsWith('--')) {
-                                    const value = darkThemeStyles.getPropertyValue(propertyName).trim();
-                                    darkThemeCssVariables += `${propertyName}: ${value}; `;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                // Append CSS variables as inline <style> to the end of the iframe's <head>
-                const styleElement = iframeDoc.createElement('style');
-                styleElement.type = 'text/css';
-                styleElement.appendChild(
-                    iframeDoc.createTextNode(`:root { ${cssVariables} } .dark-theme { ${darkThemeCssVariables} }`)
-                );
-
-                // Append to the end of the <head> to ensure it overrides existing styles
-                iframeDoc.head.appendChild(styleElement);
-            }
-        }
     }
 
     private getFrameSource(params: AdvancedUiParams): SafeResourceUrl | null {
