352979868f397a3e01b91e705e66e3fe8eca2a61
Peter Kedvessy <pkedvessy@gmail.com>
2025-03-06 09:42:30 +0100

    NIFI-14364 MiNiFi Nar auto unload in case of the extension is removed from the nar auto load directory

---
+++ minifi/minifi-nar-bundles/minifi-framework-bundle/minifi-framework/minifi-nar-unloader/src/main/java/org/apache/nifi/minifi/nar/NarAutoUnloaderTask.java
@@ -0,0 +1,95 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.nifi.minifi.nar;
+
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.util.Objects.requireNonNull;
+
+import java.nio.file.Path;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NarAutoUnloaderTask implements Runnable {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NarAutoUnloaderTask.class);
+    private static final long POLL_INTERVAL_MS = 5000;
+
+    private final Path autoLoadPath;
+    private final WatchService watchService;
+    private final NarAutoUnloadService narAutoUnloadService;
+
+    private volatile boolean stopped = false;
+
+    public NarAutoUnloaderTask(
+            Path autoLoadPath,
+            WatchService watchService,
+            NarAutoUnloadService narAutoUnloadService) {
+        this.autoLoadPath = requireNonNull(autoLoadPath);
+        this.watchService = requireNonNull(watchService);
+        this.narAutoUnloadService = requireNonNull(narAutoUnloadService);
+    }
+
+    @Override
+    public void run() {
+        while (!stopped) {
+            try {
+                WatchKey key;
+                try {
+                    LOGGER.debug("Polling for removed NARs at {}", autoLoadPath);
+                    key = watchService.poll(POLL_INTERVAL_MS, TimeUnit.MILLISECONDS);
+                } catch (InterruptedException x) {
+                    LOGGER.info("WatchService interrupted, returning...");
+                    return;
+                }
+                if (key != null) {
+                    for (WatchEvent<?> event : key.pollEvents()) {
+                        if (event.kind() == ENTRY_DELETE) {
+                            narAutoUnloadService.unloadNarFile(getFileName(event));
+                        }
+                    }
+                    if (!key.reset()) {
+                        LOGGER.error("NAR auto-load directory is no longer valid");
+                        stop();
+                    }
+                }
+            } catch (final Throwable t) {
+                LOGGER.error("Error un-loading NARs", t);
+            }
+        }
+    }
+
+    public void stop() {
+        LOGGER.info("Stopping NAR Auto-Unloader");
+        stopped = true;
+    }
+
+    public Path getAutoLoadPath() {
+        return autoLoadPath;
+    }
+
+    private String getFileName(WatchEvent<?> event) {
+        WatchEvent<Path> ev = (WatchEvent<Path>) event;
+        Path filename = ev.context();
+        Path autoUnLoadFile = autoLoadPath.resolve(filename);
+        return autoUnLoadFile.toFile().getName().toLowerCase();
+    }
+}
---
+++ minifi/minifi-nar-bundles/minifi-framework-bundle/minifi-framework/minifi-nar-unloader/src/test/java/org/apache/nifi/minifi/nar/NarAutoUnloadServiceTest.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.nifi.minifi.nar;
+
+import java.io.File;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import org.apache.nifi.bundle.Bundle;
+import org.apache.nifi.bundle.BundleDetails;
+import org.apache.nifi.nar.ExtensionManager;
+import org.apache.nifi.nar.NarLoader;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+public class NarAutoUnloadServiceTest {
+
+    private static final String SUPPORTED_FILENAME = "test.nar";
+    private static final String UNPACKED_POSTFIX = "-unpacked";
+
+    @Mock
+    private ExtensionManager extensionManager;
+    @Mock
+    private NarLoader narLoader;
+    private File extensionWorkDirectory;
+
+    private NarAutoUnloadService victim;
+
+    @BeforeEach
+    public void initTest() {
+        extensionWorkDirectory = new File(".");
+        victim = new NarAutoUnloadService(extensionManager, extensionWorkDirectory, narLoader);
+    }
+
+    @ParameterizedTest
+    @MethodSource("notSupportedFileNames")
+    public void testNotSupportedFileDoesNotTriggerUnload(String fileName) {
+
+        victim.unloadNarFile(fileName);
+
+        verifyNoInteractions(extensionManager, narLoader);
+    }
+
+    @Test
+    public void testUnload() {
+        Bundle bundle = mock(Bundle.class);
+        BundleDetails bundleDetails = mock(BundleDetails.class);
+        File workingDirectory = new File(extensionWorkDirectory, SUPPORTED_FILENAME + UNPACKED_POSTFIX);
+
+        when(extensionManager.getAllBundles()).thenReturn(Set.of(bundle));
+        when(bundle.getBundleDetails()).thenReturn(bundleDetails);
+        when(bundleDetails.getWorkingDirectory()).thenReturn(workingDirectory);
+
+        victim.unloadNarFile(SUPPORTED_FILENAME);
+
+        verify(narLoader).unload(bundle);
+    }
+
+    private static Stream<Arguments> notSupportedFileNames() {
+        return Stream.of(
+                Arguments.of("nar.jar"),
+                Arguments.of(".test.nar")
+        );
+    }
+}
