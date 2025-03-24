8ef2fc5d736880fe9fa25b7b99207199c9bcf8fa
Matt Burgess <mattyb149@apache.org>
2025-03-24 15:09:26 -0400

    NIFI-14381: Incorporate review comments, refactored SSL Context identification

---
+++ nifi-extension-bundles/nifi-standard-bundle/nifi-standard-rules/src/main/java/org/apache/nifi/flowanalysis/rules/RequireCustomSSLContext.java
@@ -0,0 +1,84 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.flowanalysis.rules;
+
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.annotation.documentation.UseCase;
+import org.apache.nifi.flow.VersionedComponent;
+import org.apache.nifi.flow.VersionedConfigurableExtension;
+import org.apache.nifi.flowanalysis.AbstractFlowAnalysisRule;
+import org.apache.nifi.flowanalysis.ComponentAnalysisResult;
+import org.apache.nifi.flowanalysis.FlowAnalysisRuleContext;
+import org.apache.nifi.util.StringUtils;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+
+@Tags({"component", "processor", "controller service", "type", "ssl", "tls", "listen"})
+@CapabilityDescription("Produces rule violations for each component (i.e. processors or controller services) having a property "
+        + "identifying an SSLContextService that is not set.")
+@UseCase(
+        description = "Ensure that an SSL Context has been configured for the specified components. This helps avoid ports being opened for insecure (plaintext, e.g.) communications.",
+        configuration = """
+                To avoid the violation, ensure that the "SSL Context Service" property is set for the specified component(s).
+                """
+)
+public class RequireCustomSSLContext extends AbstractFlowAnalysisRule {
+
+    private final List<String> componentTypes = List.of(
+            "ListenFTP",
+            "ListenHTTP",
+            "ListenTCP",
+            "ListenOLTP",
+            "ListenSyslog",
+            "HandleHttpRequest",
+            "JettyWebSocketServer"
+    );
+
+    @Override
+    public Collection<ComponentAnalysisResult> analyzeComponent(VersionedComponent component, FlowAnalysisRuleContext context) {
+        Collection<ComponentAnalysisResult> results = new HashSet<>();
+
+        if (component instanceof VersionedConfigurableExtension versionedConfigurableExtension) {
+
+            String encounteredComponentType = versionedConfigurableExtension.getType();
+            String encounteredSimpleComponentType = encounteredComponentType.substring(encounteredComponentType.lastIndexOf(".") + 1);
+
+            if (componentTypes.isEmpty() || componentTypes.contains(encounteredComponentType) || componentTypes.contains(encounteredSimpleComponentType)) {
+                // Loop over the properties for this component looking for an SSLContextService
+                versionedConfigurableExtension.getProperties().forEach((propertyName, propertyValue) -> {
+
+                    // If the SSL Context property exists and the value is not set, report a violation
+                    if (("SSL Context Service".equalsIgnoreCase(propertyName) || "ssl-context-service".equalsIgnoreCase(propertyName)) &&
+                            StringUtils.isEmpty(propertyValue)) {
+                        ComponentAnalysisResult result = new ComponentAnalysisResult(
+                                component.getInstanceIdentifier(),
+                                "'" + encounteredSimpleComponentType + "' must specify an SSL Context Service"
+                        );
+
+                        results.add(result);
+
+                    }
+                });
+            }
+        }
+        return results;
+    }
+}
