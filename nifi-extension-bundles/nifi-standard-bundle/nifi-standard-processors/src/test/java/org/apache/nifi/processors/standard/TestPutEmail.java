633652a5e96eae8f46c9c84ce919aa0d0b493587
EndzeitBegins <16666115+EndzeitBegins@users.noreply.github.com>
2025-04-07 21:29:43 +0200

    NIFI-14437 Refactor PutEmail to not rely on properties with unsatisfied dependencies

--- nifi-extension-bundles/nifi-standard-bundle/nifi-standard-processors/src/test/java/org/apache/nifi/processors/standard/TestPutEmail.java
+++ nifi-extension-bundles/nifi-standard-bundle/nifi-standard-processors/src/test/java/org/apache/nifi/processors/standard/TestPutEmail.java
@@ -91,9 +91,6 @@ public class TestPutEmail {
     public void setup() {
         processor = new PutEmailExtension();
         runner = TestRunners.newTestRunner(processor);
-
-        // implementation relies on default values of dependant properties; remove this once refactored
-        runner.setProhibitUseOfPropertiesWithUnsatisfiedDependencies(false);
     }
 
     @Test
