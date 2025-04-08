f99772551a9b29280a6c2188e63d466e9d48207e
Noah Cover <noahcover99@gmail.com>
2025-04-07 17:49:35 -0700

    NIFI-14445: Creating processor to fetch specific metadata instance for box file
    Signed-off-by: Pierre Villard <pierre.villard.fr@gmail.com>
    
    This closes #9856.

--- nifi-extension-bundles/nifi-box-bundle/nifi-box-processors/src/test/java/org/apache/nifi/processors/box/BoxParseJsonTest.java
+++ nifi-extension-bundles/nifi-box-bundle/nifi-box-processors/src/test/java/org/apache/nifi/processors/box/BoxParseJsonTest.java
@@ -20,21 +20,22 @@ import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonArray;
 import com.eclipsesource.json.JsonObject;
 import com.eclipsesource.json.JsonValue;
+import org.apache.nifi.processors.box.utils.BoxMetadataUtils;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ListBoxFileMetadataInstancesParseJsonTest {
+public class BoxParseJsonTest {
 
     @Test
     void testParseString() {
         String expected = "test string";
-        Object result = ListBoxFileMetadataInstances.parseJsonValue(Json.value(expected));
+        Object result = BoxMetadataUtils.parseJsonValue(Json.value(expected));
         assertEquals(expected, result);
 
         // Empty string
         expected = "";
-        result = ListBoxFileMetadataInstances.parseJsonValue(Json.value(expected));
+        result = BoxMetadataUtils.parseJsonValue(Json.value(expected));
         assertEquals(expected, result);
     }
 
@@ -42,12 +43,12 @@ public class ListBoxFileMetadataInstancesParseJsonTest {
     void testParseBoolean() {
         // Test true
         boolean expected = true;
-        Object result = ListBoxFileMetadataInstances.parseJsonValue(Json.value(expected));
+        Object result = BoxMetadataUtils.parseJsonValue(Json.value(expected));
         assertEquals(expected, result);
 
         // Test false
         expected = false;
-        result = ListBoxFileMetadataInstances.parseJsonValue(Json.value(expected));
+        result = BoxMetadataUtils.parseJsonValue(Json.value(expected));
         assertEquals(expected, result);
     }
 
@@ -55,17 +56,17 @@ public class ListBoxFileMetadataInstancesParseJsonTest {
     void testParseIntegerNumber() {
         // Integer value
         long expected = 42;
-        Object result = ListBoxFileMetadataInstances.parseJsonValue(Json.value(expected));
+        Object result = BoxMetadataUtils.parseJsonValue(Json.value(expected));
         assertEquals(expected, result);
 
         // Max long value
         expected = Long.MAX_VALUE;
-        result = ListBoxFileMetadataInstances.parseJsonValue(Json.value(expected));
+        result = BoxMetadataUtils.parseJsonValue(Json.value(expected));
         assertEquals(expected, result);
 
         // Min long value
         expected = Long.MIN_VALUE;
-        result = ListBoxFileMetadataInstances.parseJsonValue(Json.value(expected));
+        result = BoxMetadataUtils.parseJsonValue(Json.value(expected));
         assertEquals(expected, result);
     }
 
@@ -74,19 +75,19 @@ public class ListBoxFileMetadataInstancesParseJsonTest {
         // Double without exponent
         String input = "3.14159";
         JsonValue jsonValue = Json.parse(input);
-        Object result = ListBoxFileMetadataInstances.parseJsonValue(jsonValue);
+        Object result = BoxMetadataUtils.parseJsonValue(jsonValue);
         assertEquals(input, result);
 
         // Very small number that should be preserved
         input = "0.0000000001";
         jsonValue = Json.parse(input);
-        result = ListBoxFileMetadataInstances.parseJsonValue(jsonValue);
+        result = BoxMetadataUtils.parseJsonValue(jsonValue);
         assertEquals(input, result);
 
         // Very large number that should be preserved
         input = "9999999999999999.9999";
         jsonValue = Json.parse(input);
-        result = ListBoxFileMetadataInstances.parseJsonValue(jsonValue);
+        result = BoxMetadataUtils.parseJsonValue(jsonValue);
         assertEquals(input, result);
     }
 
@@ -95,19 +96,19 @@ public class ListBoxFileMetadataInstancesParseJsonTest {
         // Scientific notation is converted to plain string format
         String input = "1.234e5";
         JsonValue jsonValue = Json.parse(input);
-        Object result = ListBoxFileMetadataInstances.parseJsonValue(jsonValue);
+        Object result = BoxMetadataUtils.parseJsonValue(jsonValue);
         assertEquals("123400", result);
 
         // large exponent
         input = "1.234e20";
         jsonValue = Json.parse(input);
-        result = ListBoxFileMetadataInstances.parseJsonValue(jsonValue);
+        result = BoxMetadataUtils.parseJsonValue(jsonValue);
         assertEquals("123400000000000000000", result);
 
         // Negative exponent
         input = "1.234e-5";
         jsonValue = Json.parse(input);
-        result = ListBoxFileMetadataInstances.parseJsonValue(jsonValue);
+        result = BoxMetadataUtils.parseJsonValue(jsonValue);
         assertEquals("0.00001234", result);
     }
 
@@ -115,12 +116,12 @@ public class ListBoxFileMetadataInstancesParseJsonTest {
     void testParseObjectAndArray() {
         // JSON objects return their string representation
         JsonObject jsonObject = Json.object().add("key", "value");
-        Object result = ListBoxFileMetadataInstances.parseJsonValue(jsonObject);
+        Object result = BoxMetadataUtils.parseJsonValue(jsonObject);
         assertEquals(jsonObject.toString(), result);
 
         // JSON arrays return their string representation
         JsonArray jsonArray = Json.array().add("item1").add("item2");
-        result = ListBoxFileMetadataInstances.parseJsonValue(jsonArray);
+        result = BoxMetadataUtils.parseJsonValue(jsonArray);
         assertEquals(jsonArray.toString(), result);
     }
 
@@ -128,11 +129,11 @@ public class ListBoxFileMetadataInstancesParseJsonTest {
     void testParseNumberFormatException() {
         String largeIntegerString = "9999999999999999999"; // Beyond Long.MAX_VALUE
         JsonValue jsonValue = Json.parse(largeIntegerString);
-        Object result = ListBoxFileMetadataInstances.parseJsonValue(jsonValue);
+        Object result = BoxMetadataUtils.parseJsonValue(jsonValue);
         assertEquals(largeIntegerString, result);
 
         double doubleValue = 123.456;
-        result = ListBoxFileMetadataInstances.parseJsonValue(Json.value(doubleValue));
+        result = BoxMetadataUtils.parseJsonValue(Json.value(doubleValue));
         assertEquals(String.valueOf(doubleValue), result);
     }
 }
