8c2b6989ea53783b863133176c8ae75404271333
Shane Ardell <shane.m.ardell@gmail.com>
2025-04-29 15:06:22 -0500

    NIFI-14319: review feedback
    * fix error service

--- nifi-frontend/src/main/frontend/apps/nifi-registry/src/app/state/error/error.effects.ts
+++ nifi-frontend/src/main/frontend/apps/nifi-registry/src/app/state/error/error.effects.ts
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import * as ErrorActions from './error.actions';
 import { map, tap } from 'rxjs';
@@ -25,10 +25,11 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 @Injectable()
 export class ErrorEffects {
     constructor(
-        private actions$: Actions,
         private router: Router,
         private snackBar: MatSnackBar
     ) {}
+
+    actions$ = inject(Actions);
 
     fullScreenError$ = createEffect(
         () =>
