e7cbf38bf8e1f60a2064ea46971b77987e9f8a71
Mark Payne <markap14@hotmail.com>
2025-06-17 14:15:10 -0400

    NIFI-14839 Support different output strategies in ConsumeKinesis

--- nifi-extension-bundles/nifi-aws-bundle/nifi-aws-kinesis/src/main/resources/docs/org.apache.nifi.processors.aws.kinesis.ConsumeKinesis/additionalDetails.md
+++ nifi-extension-bundles/nifi-aws-bundle/nifi-aws-kinesis/src/main/resources/docs/org.apache.nifi.processors.aws.kinesis.ConsumeKinesis/additionalDetails.md
@@ -134,3 +134,142 @@ preserves record ordering for a single Shard, even with record schema changes.
 **Please note**, the processor relies on _Record Reader_ to provide correct schema for each record.
 Using a Reader with schema inference may produce a lot of different schemas, which may lead to excessive FlowFile creation.
 If performance is a concern, it is recommended to use a Reader with a predefined schema or schema registry.
+
+### Output Strategies
+
+This processor offers multiple strategies configured via processor property _Output Strategy_ for converting Kinesis records into FlowFiles.
+
+- _Use Content as Value_ (the default) writes only a Kinesis record value to a FlowFile.
+- _Use Wrapper_ writes a Kinesis Record value as well as metadata into separate fields of a FlowFile record.
+- _Inject Metadata_ writes a Kinesis Record value to a FlowFile record and adds a sub-record to it with metadata.
+
+The written metadata includes the following fields:
+- _stream_: The name of the Kinesis stream the record was received from.
+- _shardId_: The identifier of the shard the record was received from.
+- _sequenceNumber_: The sequence number of the record.
+- _subSequenceNumber_: The subsequence number of the record, used when multiple smaller records are aggregated into a single Kinesis record. If a record was not part of a batch, this value will be 0.
+- _shardedSequenceNumber_: A combination of the sequence number and subsequence number. This can be used to uniquely identify a record within a shard.
+- _partitionKey_: The partition key of the record.
+- _approximateArrival_: The approximate arrival timestamp of the record (in milliseconds since epoch).
+
+The record schema that is used when _Use Wrapper_ is selected is as follows (in Avro format):
+
+```json
+{
+  "type": "record",
+  "name": "nifiRecord",
+  "namespace": "org.apache.nifi",
+  "fields": [
+    {
+      "name": "value",
+      "type": [
+        {
+          < Fields as determined by the Record Reader for a Kinesis message >
+        },
+        "null"
+      ]
+    },
+    {
+      "name": "kinesisMetadata",
+      "type": [
+        {
+          "type": "record",
+          "name": "metadataType",
+          "fields": [
+            { "name": "stream", "type": ["string", "null"] },
+            { "name": "shardId", "type": ["string", "null"] },
+            { "name": "sequenceNumber", "type": ["string", "null"] },
+            { "name": "subSequenceNumber", "type": ["long", "null"] },
+            { "name": "shardedSequenceNumber", "type": ["string", "null"] },
+            { "name": "partitionKey", "type": ["string", "null"] },
+            { "name": "approximateArrival", "type": [ { "type": "long", "logicalType": "timestamp-millis" }, "null" ] }
+          ]
+        },
+        "null"
+      ]
+    }
+  ]
+}
+```
+
+The record schema that is used when "Inject Metadata" is selected is as follows (in Avro format):
+
+```json
+{
+  "type": "record",
+  "name": "nifiRecord",
+  "namespace": "org.apache.nifi",
+  "fields": [
+    < Fields as determined by the Record Reader for a Kinesis message >,
+    {
+      "name": "kinesisMetadata",
+      "type": [
+        {
+          "type": "record",
+          "name": "metadataType",
+          "fields": [
+            { "name": "stream", "type": ["string", "null"] },
+            { "name": "shardId", "type": ["string", "null"] },
+            { "name": "sequenceNumber", "type": ["string", "null"] },
+            { "name": "subSequenceNumber", "type": ["long", "null"] },
+            { "name": "shardedSequenceNumber", "type": ["string", "null"] },
+            { "name": "partitionKey", "type": ["string", "null"] },
+            { "name": "approximateArrival", "type": [ { "type": "long", "logicalType": "timestamp-millis" }, "null" ] }
+          ]
+        },
+        "null"
+      ]
+    }
+  ]
+}
+```
+
+Here is an example of FlowFile content that is emitted by JsonRecordSetWriter when strategy _Use Wrapper_ is selected:
+
+```json
+[
+  {
+    "value": {
+      "address": "1234 First Street",
+      "zip": "12345",
+      "account": {
+        "name": "Acme",
+        "number": "AC1234"
+      }
+    },
+    "kinesisMetadata" : {
+      "stream" : "stream-name",
+      "shardId" : "shardId-000000000000",
+      "sequenceNumber" : "123456789",
+      "subSequenceNumber" : 3,
+      "shardedSequenceNumber" : "12345678900000000000000000003",
+      "partitionKey" : "123",
+      "approximateArrival" : 1756459596788
+    }
+  }
+]
+```
+
+Here is an example of FlowFile content that is emitted by JsonRecordSetWriter when strategy _Inject Metadata_ is selected:
+
+```json
+[
+  {
+    "address": "1234 First Street",
+    "zip": "12345",
+    "account": {
+      "name": "Acme",
+      "number": "AC1234"
+    },
+    "kinesisMetadata" : {
+      "stream" : "stream-name",
+      "shardId" : "shardId-000000000000",
+      "sequenceNumber" : "123456789",
+      "subSequenceNumber" : 3,
+      "shardedSequenceNumber" : "12345678900000000000000000003",
+      "partitionKey" : "123",
+      "approximateArrival" : 1756459596788
+    }
+  }
+]
+```
---
+++ nifi-extension-bundles/nifi-aws-bundle/nifi-aws-kinesis/src/test/java/org/apache/nifi/processors/aws/kinesis/converter/WrapperRecordConverterTest.java
@@ -0,0 +1,127 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.processors.aws.kinesis.converter;
+
+import jakarta.annotation.Nullable;
+import org.apache.nifi.serialization.SimpleRecordSchema;
+import org.apache.nifi.serialization.record.MapRecord;
+import org.apache.nifi.serialization.record.Record;
+import org.apache.nifi.serialization.record.RecordField;
+import org.apache.nifi.serialization.record.RecordFieldType;
+import org.apache.nifi.serialization.record.RecordSchema;
+import org.junit.jupiter.api.Test;
+import software.amazon.kinesis.retrieval.KinesisClientRecord;
+
+import java.nio.ByteBuffer;
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class WrapperRecordConverterTest {
+
+    private static final String KINESIS_METADATA = "kinesisMetadata";
+
+    private static final String TEST_STREAM_NAME = "test-stream";
+    private static final String TEST_SHARD_ID = "shardId-000000000001";
+    private static final String TEST_SEQUENCE_NUMBER = "49590338271490256608559692538361571095921575989136588801";
+    private static final long TEST_SUB_SEQUENCE_NUMBER = 2;
+    private static final String TEST_PARTITION_KEY = "test-partition-key";
+    private static final Instant TEST_ARRIVAL_TIMESTAMP = Instant.ofEpochMilli(1640995200000L);
+
+    private static final String EXPECTED_SHARDED_SEQUENCE_NUMBER = "4959033827149025660855969253836157109592157598913658880100000000000000000002";
+
+    private static final RecordSchema INPUT_SCHEMA = new SimpleRecordSchema(List.of(
+            new RecordField("name", RecordFieldType.STRING.getDataType()),
+            new RecordField("age", RecordFieldType.INT.getDataType())
+    ));
+    private static final Record INPUT_RECORD = new MapRecord(INPUT_SCHEMA, Map.of(
+            "name", "John Doe",
+            "age", 30
+    ));
+
+    private static final RecordSchema SCHEMA_METADATA = new SimpleRecordSchema(List.of(
+            new RecordField("stream", RecordFieldType.STRING.getDataType()),
+            new RecordField("shardId", RecordFieldType.STRING.getDataType()),
+            new RecordField("sequenceNumber", RecordFieldType.STRING.getDataType()),
+            new RecordField("subSequenceNumber", RecordFieldType.LONG.getDataType()),
+            new RecordField("shardedSequenceNumber", RecordFieldType.STRING.getDataType()),
+            new RecordField("partitionKey", RecordFieldType.STRING.getDataType()),
+            new RecordField("approximateArrival", RecordFieldType.TIMESTAMP.getDataType())
+    ));
+    private static final RecordSchema EXPECTED_SCHEMA = new SimpleRecordSchema(List.of(
+            new RecordField(KINESIS_METADATA, RecordFieldType.RECORD.getRecordDataType(SCHEMA_METADATA)),
+            new RecordField("value", RecordFieldType.RECORD.getRecordDataType(INPUT_SCHEMA))
+    ));
+
+    private static final WrapperRecordConverter CONVERTER = new WrapperRecordConverter();
+
+    @Test
+    void testConvertWithApproximateArrivalTimestamp() {
+        final KinesisClientRecord kinesisRecord = createTestKinesisRecord(TEST_ARRIVAL_TIMESTAMP);
+
+        final Record record = CONVERTER.convert(INPUT_RECORD, kinesisRecord, TEST_STREAM_NAME, TEST_SHARD_ID);
+
+        assertEquals(EXPECTED_SCHEMA, record.getSchema());
+        assertEquals(INPUT_RECORD, record.getValue("value"));
+
+        final Record metadata = record.getAsRecord(KINESIS_METADATA, SCHEMA_METADATA);
+        final boolean expectTimestamp = true;
+        verifyMetadata(metadata, expectTimestamp);
+    }
+
+    @Test
+    void testConvertWithoutApproximateArrivalTimestamp() {
+        final KinesisClientRecord kinesisRecord = createTestKinesisRecord(null);
+
+        final Record record = CONVERTER.convert(INPUT_RECORD, kinesisRecord, TEST_STREAM_NAME, TEST_SHARD_ID);
+
+        assertEquals(EXPECTED_SCHEMA, record.getSchema());
+        assertEquals(INPUT_RECORD, record.getValue("value"));
+
+        final Record metadata = record.getAsRecord(KINESIS_METADATA, SCHEMA_METADATA);
+        final boolean expectTimestamp = false;
+        verifyMetadata(metadata, expectTimestamp);
+    }
+
+    private KinesisClientRecord createTestKinesisRecord(final @Nullable Instant arrivalTimestamp) {
+        return KinesisClientRecord.builder()
+                .data(ByteBuffer.allocate(0))
+                .sequenceNumber(TEST_SEQUENCE_NUMBER)
+                .subSequenceNumber(TEST_SUB_SEQUENCE_NUMBER)
+                .partitionKey(TEST_PARTITION_KEY)
+                .approximateArrivalTimestamp(arrivalTimestamp)
+                .build();
+    }
+
+    private static void verifyMetadata(final Record metadata, final boolean expectTimestamp) {
+        assertEquals(TEST_STREAM_NAME, metadata.getValue("stream"));
+        assertEquals(TEST_SHARD_ID, metadata.getValue("shardId"));
+        assertEquals(TEST_SEQUENCE_NUMBER, metadata.getValue("sequenceNumber"));
+        assertEquals(TEST_SUB_SEQUENCE_NUMBER, metadata.getValue("subSequenceNumber"));
+        assertEquals(EXPECTED_SHARDED_SEQUENCE_NUMBER, metadata.getValue("shardedSequenceNumber"));
+        assertEquals(TEST_PARTITION_KEY, metadata.getValue("partitionKey"));
+
+        if (expectTimestamp) {
+            assertEquals(TEST_ARRIVAL_TIMESTAMP.toEpochMilli(), metadata.getValue("approximateArrival"));
+        } else {
+            assertNull(metadata.getValue("approximateArrival"));
+        }
+    }
+}
