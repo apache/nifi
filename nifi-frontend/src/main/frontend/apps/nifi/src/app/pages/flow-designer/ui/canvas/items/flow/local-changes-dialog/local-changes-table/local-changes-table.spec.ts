dfd8376a7341d5bb56ebcec55be2fb6eec79b524
Matt Gilman <matt.c.gilman@gmail.com>
2025-06-25 13:53:26 -0400

    NIFI-14637: Preventing the go to action in the local changes dialog when the component was removed. (#10028)
    - Fixing routing to modified controller services.
    
    This closes #10028

--- nifi-frontend/src/main/frontend/apps/nifi/src/app/pages/flow-designer/ui/canvas/items/flow/local-changes-dialog/local-changes-table/local-changes-table.spec.ts
+++ nifi-frontend/src/main/frontend/apps/nifi/src/app/pages/flow-designer/ui/canvas/items/flow/local-changes-dialog/local-changes-table/local-changes-table.spec.ts
@@ -19,6 +19,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LocalChangesTable } from './local-changes-table';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ComponentType } from '@nifi/shared';
 
 describe('LocalChangesTable', () => {
     let component: LocalChangesTable;
@@ -36,5 +37,69 @@ describe('LocalChangesTable', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    describe('canGoTo', () => {
+        it('should return true if can go to', () => {
+            const localChange = {
+                componentType: 'Processor',
+                componentId: '123',
+                componentName: 'Test Processor',
+                processGroupId: '456',
+                differenceType: 'Component Added',
+                difference: 'Processor was added.'
+            };
+            expect(component.canGoTo(localChange)).toBe(true);
+        });
+
+        it('should return false if cannot go to', () => {
+            const localChange = {
+                componentType: 'Processor',
+                componentId: '123',
+                componentName: 'Test Processor',
+                processGroupId: '456',
+                differenceType: 'Component Removed',
+                difference: 'Processor was removed.'
+            };
+            expect(component.canGoTo(localChange)).toBe(false);
+        });
+    });
+
+    describe('goToClicked', () => {
+        it('should next goToChange event on Processor added', () => {
+            const localChange = {
+                componentType: 'Processor',
+                componentId: '123',
+                componentName: 'Test Processor',
+                processGroupId: '456',
+                differenceType: 'Component Added',
+                difference: 'Processor was added.'
+            };
+            jest.spyOn(component.goToChange, 'next');
+            component.goToClicked(localChange);
+            expect(component.goToChange.next).toHaveBeenCalledWith({
+                id: localChange.componentId,
+                type: ComponentType.Processor,
+                processGroupId: localChange.processGroupId
+            });
+        });
+
+        it('should next goToChange event on Controller Service added', () => {
+            const localChange = {
+                componentType: 'Controller Service',
+                componentId: '123',
+                componentName: 'Test Controller Service',
+                processGroupId: '456',
+                differenceType: 'Component Added',
+                difference: 'Controller Service was added.'
+            };
+            jest.spyOn(component.goToChange, 'next');
+            component.goToClicked(localChange);
+            expect(component.goToChange.next).toHaveBeenCalledWith({
+                id: localChange.componentId,
+                type: ComponentType.ControllerService,
+                processGroupId: localChange.processGroupId
+            });
+        });
     });
 });
