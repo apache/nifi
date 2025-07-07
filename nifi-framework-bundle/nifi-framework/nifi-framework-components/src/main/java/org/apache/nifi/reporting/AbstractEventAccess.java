8b1d625e72a07d90dffc761bed0bdb8885d3225f
Pierre Villard <pierre.villard.fr@gmail.com>
2025-07-07 14:23:34 +0200

    NIFI-14750 - Set registered flow snapshot metadata in AbstractEventAccess
    Signed-off-by: Pierre Villard <pierre.villard.fr@gmail.com>

--- nifi-framework-bundle/nifi-framework/nifi-framework-components/src/main/java/org/apache/nifi/reporting/AbstractEventAccess.java
+++ nifi-framework-bundle/nifi-framework/nifi-framework-components/src/main/java/org/apache/nifi/reporting/AbstractEventAccess.java
@@ -53,6 +53,7 @@ import org.apache.nifi.groups.ProcessGroup;
 import org.apache.nifi.groups.RemoteProcessGroup;
 import org.apache.nifi.processor.Relationship;
 import org.apache.nifi.provenance.ProvenanceEventRecord;
+import org.apache.nifi.registry.flow.RegisteredFlowSnapshotMetadata;
 import org.apache.nifi.registry.flow.VersionControlInformation;
 import org.apache.nifi.registry.flow.VersionedFlowState;
 import org.apache.nifi.registry.flow.VersionedFlowStatus;
@@ -540,6 +541,18 @@ public abstract class AbstractEventAccess implements EventAccess {
 
         final VersionControlInformation vci = group.getVersionControlInformation();
         if (vci != null) {
+            final RegisteredFlowSnapshotMetadata registeredFlowSnapshotMetadata = new RegisteredFlowSnapshotMetadata();
+            registeredFlowSnapshotMetadata.setBranch(vci.getBranch());
+            registeredFlowSnapshotMetadata.setBucketIdentifier(vci.getBucketIdentifier());
+            registeredFlowSnapshotMetadata.setFlowIdentifier(vci.getFlowIdentifier());
+            registeredFlowSnapshotMetadata.setVersion(vci.getVersion());
+            registeredFlowSnapshotMetadata.setComments(Map.of(
+                    "registryName", vci.getRegistryName(),
+                    "flowName", vci.getFlowName(),
+                    "flowDescription", vci.getFlowDescription(),
+                    "bucketName", vci.getBucketName(),
+                    "status", vci.getStatus().getState().name()).toString());
+            status.setRegisteredFlowSnapshotMetadata(registeredFlowSnapshotMetadata);
             try {
                 final VersionedFlowStatus flowStatus = vci.getStatus();
                 if (flowStatus != null && flowStatus.getState() != null) {
