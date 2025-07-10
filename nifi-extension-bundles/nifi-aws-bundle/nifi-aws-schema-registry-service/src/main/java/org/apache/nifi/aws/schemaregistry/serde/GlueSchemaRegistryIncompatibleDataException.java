ec90b4734a74ce139cf377d8814647539f91efdf
Dariusz Seweryn <dariusz.seweryn@gmail.com>
2025-07-10 10:17:59 +0200

    Move AWS Serde library files reference to NOTICE. Add final to param. Remove String qualifier.

--- nifi-extension-bundles/nifi-aws-bundle/nifi-aws-schema-registry-service/src/main/java/org/apache/nifi/aws/schemaregistry/serde/GlueSchemaRegistryIncompatibleDataException.java
+++ nifi-extension-bundles/nifi-aws-bundle/nifi-aws-schema-registry-service/src/main/java/org/apache/nifi/aws/schemaregistry/serde/GlueSchemaRegistryIncompatibleDataException.java
@@ -16,11 +16,11 @@
 package org.apache.nifi.aws.schemaregistry.serde;
 
 public class GlueSchemaRegistryIncompatibleDataException extends RuntimeException {
-    public static final java.lang.String UNKNOWN_DATA_ERROR_MESSAGE = "Data is not compatible with schema registry";
-    public static final java.lang.String UNKNOWN_HEADER_VERSION_BYTE_ERROR_MESSAGE = "Invalid schema registry header version byte in data";
-    public static final java.lang.String UNKNOWN_COMPRESSION_BYTE_ERROR_MESSAGE = "Invalid schema registry compression byte in data";
+    public static final String UNKNOWN_DATA_ERROR_MESSAGE = "Data is not compatible with schema registry";
+    public static final String UNKNOWN_HEADER_VERSION_BYTE_ERROR_MESSAGE = "Invalid schema registry header version byte in data";
+    public static final String UNKNOWN_COMPRESSION_BYTE_ERROR_MESSAGE = "Invalid schema registry compression byte in data";
 
-    public GlueSchemaRegistryIncompatibleDataException(java.lang.String message) {
+    public GlueSchemaRegistryIncompatibleDataException(final String message) {
         super(message);
     }
 }
