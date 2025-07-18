d6980550863f2a9fa5b89ae8e9977118c6f72237
dan-s1 <dstieg1@gmail.com>
2025-07-18 16:40:31 +0000

    NIFI-14766 Remove not null check.

--- minifi/minifi-bootstrap/src/main/java/org/apache/nifi/minifi/bootstrap/status/reporters/StatusLogger.java
+++ minifi/minifi-bootstrap/src/main/java/org/apache/nifi/minifi/bootstrap/status/reporters/StatusLogger.java
@@ -22,7 +22,6 @@ import static org.apache.nifi.minifi.commons.api.MiNiFiProperties.NIFI_MINIFI_ST
 import static org.apache.nifi.minifi.commons.api.MiNiFiProperties.NIFI_MINIFI_STATUS_REPORTER_LOG_QUERY;
 
 import java.io.IOException;
-import java.util.Objects;
 
 import org.apache.nifi.logging.LogLevel;
 import org.apache.nifi.minifi.bootstrap.QueryableStatusAggregator;
@@ -118,7 +117,7 @@ public class StatusLogger extends PeriodicStatusReporter {
                 }
 
             } catch (Exception e) {
-                if (Objects.requireNonNull(logLevel) == LogLevel.ERROR) {
+                if (logLevel == LogLevel.ERROR) {
                     logger.error("Unexpected exception when attempting to report the status", e);
                 } else {
                     logger.warn("Unexpected exception when attempting to report the status", e);
