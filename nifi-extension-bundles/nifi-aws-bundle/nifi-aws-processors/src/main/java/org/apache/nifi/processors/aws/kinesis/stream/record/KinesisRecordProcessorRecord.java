9e8c914630bbc04c6fc0e5ab1514dde05f535a30
Dariusz Seweryn <dariusz.seweryn@gmail.com>
2025-08-05 22:59:33 +0200

    NIFI-14696 Improved ConsumeKinesisStream handling of Record Schema Differences (#10053)
    Signed-off-by: David Handermann <exceptionfactory@apache.org>

--- nifi-extension-bundles/nifi-aws-bundle/nifi-aws-processors/src/main/java/org/apache/nifi/processors/aws/kinesis/stream/record/KinesisRecordProcessorRecord.java
+++ nifi-extension-bundles/nifi-aws-bundle/nifi-aws-processors/src/main/java/org/apache/nifi/processors/aws/kinesis/stream/record/KinesisRecordProcessorRecord.java
@@ -21,6 +21,7 @@ import org.apache.nifi.flowfile.attributes.CoreAttributes;
 import org.apache.nifi.logging.ComponentLog;
 import org.apache.nifi.processor.ProcessSession;
 import org.apache.nifi.processor.ProcessSessionFactory;
+import org.apache.nifi.processors.aws.kinesis.property.SchemaDifferenceHandlingStrategy;
 import org.apache.nifi.processors.aws.kinesis.stream.ConsumeKinesisStream;
 import org.apache.nifi.processors.aws.kinesis.stream.pause.RecordProcessorBlocker;
 import org.apache.nifi.processors.aws.kinesis.stream.record.converter.RecordConverter;
@@ -34,10 +35,10 @@ import org.apache.nifi.serialization.WriteResult;
 import org.apache.nifi.serialization.record.PushBackRecordSet;
 import org.apache.nifi.serialization.record.Record;
 import org.apache.nifi.serialization.record.RecordSchema;
-import org.apache.nifi.util.StopWatch;
 import software.amazon.kinesis.retrieval.KinesisClientRecord;
 
 import java.io.ByteArrayInputStream;
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -49,20 +50,19 @@ import java.util.List;
 import java.util.Map;
 
 public class KinesisRecordProcessorRecord extends AbstractKinesisRecordProcessor {
-    final RecordReaderFactory readerFactory;
-    final RecordSetWriterFactory writerFactory;
-    final Map<String, String> schemaRetrievalVariables;
-
-    private RecordSetWriter writer;
-    private OutputStream outputStream;
+    private final RecordReaderFactory readerFactory;
+    private final RecordSetWriterFactory writerFactory;
+    private final Map<String, String> schemaRetrievalVariables;
     private final RecordConverter recordConverter;
+    private final StateHandlerStrategy stateHandlerStrategy;
 
     public KinesisRecordProcessorRecord(final ProcessSessionFactory sessionFactory, final ComponentLog log, final String streamName,
                                         final String endpointPrefix, final String kinesisEndpoint,
                                         final long checkpointIntervalMillis, final long retryWaitMillis,
                                         final int numRetries, final DateTimeFormatter dateTimeFormatter,
                                         final RecordReaderFactory readerFactory, final RecordSetWriterFactory writerFactory,
-                                        final RecordConverter recordConverter, final RecordProcessorBlocker recordProcessorBlocker) {
+                                        final RecordConverter recordConverter, final RecordProcessorBlocker recordProcessorBlocker,
+                                        final SchemaDifferenceHandlingStrategy schemaDifferenceHandlingStrategy) {
         super(sessionFactory, log, streamName, endpointPrefix, kinesisEndpoint, checkpointIntervalMillis, retryWaitMillis,
                 numRetries, dateTimeFormatter, recordProcessorBlocker);
         this.readerFactory = readerFactory;
@@ -70,56 +70,63 @@ public class KinesisRecordProcessorRecord extends AbstractKinesisRecordProcessor
 
         schemaRetrievalVariables = Collections.singletonMap(KINESIS_RECORD_SCHEMA_KEY, streamName);
         this.recordConverter = recordConverter;
+        this.stateHandlerStrategy = new StateHandlerStrategy(schemaDifferenceHandlingStrategy, this::initializeFlowFileState, this::completeFlowFileState);
     }
 
     @Override
     void startProcessingRecords() {
         super.startProcessingRecords();
-        outputStream = null;
-        writer = null;
+        FlowFileState flowFileState;
+        while ((flowFileState = stateHandlerStrategy.pop()) != null) {
+            // this may happen if the previous processing has not been completed successfully, close the leftover state
+            closeSafe(flowFileState.asClosable(), "FlowFile State");
+        }
     }
 
     @Override
-    void processRecord(final List<FlowFile> flowFiles, final KinesisClientRecord kinesisRecord, final boolean lastRecord,
-                       final ProcessSession session, final StopWatch stopWatch) {
-        boolean firstOutputRecord = true;
-        int recordCount = 0;
-        final ByteBuffer dataBuffer = kinesisRecord.data();
-        byte[] data = dataBuffer != null ? new byte[dataBuffer.remaining()] : new byte[0];
-        if (dataBuffer != null) {
-            dataBuffer.get(data);
+    void finishProcessingRecords(final BatchProcessingContext batchProcessingContext) {
+        super.finishProcessingRecords(batchProcessingContext);
+        final List<FlowFile> flowFiles = batchProcessingContext.flowFiles();
+        FlowFileState flowFileState;
+        while ((flowFileState = stateHandlerStrategy.pop()) != null) {
+            if (!flowFiles.contains(flowFileState.flowFile)) {
+                // this is unexpected, flowFiles have been altered not in this class after the start of processing
+                throw new IllegalStateException("%s is not available in provided FlowFiles [%d]".formatted(flowFileState.flowFile, flowFiles.size()));
+            }
+            try {
+                completeFlowFileState(flowFileState, batchProcessingContext);
+            } catch (final FlowFileCompletionException e) {
+                handleFlowFileCompletionException(e, batchProcessingContext);
+            }
         }
+    }
 
-        FlowFile flowFile = null;
+    @Override
+    void processRecord(final KinesisClientRecord kinesisRecord, final BatchProcessingContext batchProcessingContext) {
+        final byte[] data = getData(kinesisRecord);
+
         try (final InputStream in = new ByteArrayInputStream(data);
              final RecordReader reader = readerFactory.createRecordReader(schemaRetrievalVariables, in, data.length, getLogger())
         ) {
             Record intermediateRecord;
             final PushBackRecordSet recordSet = new PushBackRecordSet(reader.createRecordSet());
             while ((intermediateRecord = recordSet.next()) != null) {
-                Record outputRecord = recordConverter.convert(intermediateRecord, kinesisRecord, getStreamName(), getKinesisShardId());
-                if (flowFiles.isEmpty()) {
-                    flowFile = session.create();
-                    flowFiles.add(flowFile);
-
-                    // initialize the writer when the first record is read.
-                    createWriter(flowFile, session, outputRecord);
+                FlowFileState flowFileState;
+                final Record outputRecord = recordConverter.convert(intermediateRecord, kinesisRecord, getStreamName(), getKinesisShardId());
+                try {
+                    flowFileState = stateHandlerStrategy.getOrCreate(outputRecord, batchProcessingContext);
+                } catch (final FlowFileCompletionException e) {
+                    stateHandlerStrategy.drop(e.flowFileState.recordSchema);
+                    handleFlowFileCompletionException(e, batchProcessingContext);
+                    flowFileState = stateHandlerStrategy.create(outputRecord, batchProcessingContext);
                 }
-
-                final WriteResult writeResult = writer.write(outputRecord);
-                recordCount += writeResult.getRecordCount();
-
-                // complete the FlowFile if there are no more incoming Kinesis Records and no more records in this RecordSet
-                if (lastRecord && !recordSet.isAnotherRecord()) {
-                    completeFlowFile(flowFiles, session, recordCount, writeResult, kinesisRecord, stopWatch);
-                }
-                firstOutputRecord = false;
+                flowFileState.write(outputRecord, kinesisRecord);
             }
         } catch (final MalformedRecordException | IOException | SchemaNotFoundException e) {
             // write raw Kinesis Record to the parse failure relationship
             getLogger().error("Failed to parse message from Kinesis Stream using configured Record Reader and Writer due to {}",
                     e.getLocalizedMessage(), e);
-            outputRawRecordOnException(firstOutputRecord, flowFile, flowFiles, session, data, kinesisRecord, e);
+            outputRawRecordOnException(batchProcessingContext.session(), data, kinesisRecord, e);
         }
 
         if (getLogger().isDebugEnabled()) {
@@ -128,63 +135,93 @@ public class KinesisRecordProcessorRecord extends AbstractKinesisRecordProcessor
         }
     }
 
-    private void createWriter(final FlowFile flowFile, final ProcessSession session, final Record outputRecord)
-            throws IOException, SchemaNotFoundException {
-
-        final RecordSchema readerSchema = outputRecord.getSchema();
-        final RecordSchema writeSchema = writerFactory.getSchema(schemaRetrievalVariables, readerSchema);
-        outputStream = session.write(flowFile);
-        writer = writerFactory.createWriter(getLogger(), writeSchema, outputStream, flowFile);
-        writer.beginRecordSet();
+    private void handleFlowFileCompletionException(final FlowFileCompletionException e, final BatchProcessingContext batchProcessingContext) {
+        if (!e.flowFileState.containsDataFromExactlyOneKinesisRecord()) {
+            throw new KinesisBatchUnrecoverableException("Not all KinesisClientRecords contained in FlowFile contents can be routed to Failure relationship", e);
+        }
+        dropFlowFileState(e.flowFileState, batchProcessingContext);
+        final KinesisClientRecord failedKinesisRecord = e.flowFileState.lastSuccessfulWriteInfo.kinesisRecord;
+        final byte[] failedRecordData = getData(failedKinesisRecord);
+        outputRawRecordOnException(batchProcessingContext.session(), failedRecordData, failedKinesisRecord, e);
     }
 
-    private void completeFlowFile(final List<FlowFile> flowFiles, final ProcessSession session, final int recordCount,
-                                  final WriteResult writeResult, final KinesisClientRecord lastRecord, final StopWatch stopWatch)
-            throws IOException {
+    private static byte[] getData(final KinesisClientRecord kinesisRecord) {
+        final ByteBuffer dataBuffer = kinesisRecord.data();
+        final byte[] data = dataBuffer != null ? new byte[dataBuffer.remaining()] : new byte[0];
+        if (dataBuffer != null) {
+            dataBuffer.get(data);
+        }
+        return data;
+    }
 
+    /**
+     * Initializes the FlowFile state for the current processing. This includes creating a new FlowFile, initializing the RecordSetWriter, and setting up the output stream.
+     * In case of an exception during initialization, the FlowFile is removed from the session and the resources are closed properly.
+     */
+    private FlowFileState initializeFlowFileState(final Record record, final BatchProcessingContext batchProcessingContext) throws IOException, SchemaNotFoundException {
+        final ProcessSession session = batchProcessingContext.session();
+        FlowFile flowFile = null;
+        OutputStream outputStream = null;
+        RecordSetWriter writer = null;
         try {
-            writer.finishRecordSet();
-        } catch (IOException e) {
-            getLogger().error("Failed to finish record output due to {}", e.getLocalizedMessage(), e);
-            session.remove(flowFiles.get(0));
-            flowFiles.remove(0);
-            throw e;
-        } finally {
-            try {
-                writer.close();
-                outputStream.close();
-            } catch (final IOException e) {
-                getLogger().warn("Failed to close Record Writer due to {}", e.getLocalizedMessage(), e);
+            flowFile = session.create();
+            final RecordSchema newReadSchema = record.getSchema();
+            final RecordSchema writeSchema = writerFactory.getSchema(schemaRetrievalVariables, newReadSchema);
+            outputStream = session.write(flowFile);
+            writer = writerFactory.createWriter(getLogger(), writeSchema, outputStream, flowFile);
+            writer.beginRecordSet();
+            batchProcessingContext.flowFiles().add(flowFile);
+        } catch (final Exception e) {
+            if (flowFile != null) {
+                session.remove(flowFile);
             }
+            closeSafe(writer, "Record Writer");
+            closeSafe(outputStream, "Output Stream");
+            throw e;
         }
-
-        reportProvenance(session, flowFiles.get(0), null, null, stopWatch);
-
-        final Map<String, String> attributes = getDefaultAttributes(lastRecord);
-        attributes.put("record.count", String.valueOf(recordCount));
-        attributes.put(CoreAttributes.MIME_TYPE.key(), writer.getMimeType());
-        attributes.putAll(writeResult.getAttributes());
-        flowFiles.set(0, session.putAllAttributes(flowFiles.get(0), attributes));
-
-        writer = null;
-        outputStream = null;
+        return new FlowFileState(flowFile, writer, outputStream, record.getSchema(), getLogger());
     }
 
-    private void outputRawRecordOnException(final boolean firstOutputRecord, final FlowFile flowFile,
-                                            final List<FlowFile> flowFiles, final ProcessSession session,
-                                            final byte[] data, final KinesisClientRecord kinesisRecord, final Exception e) {
-        if (firstOutputRecord && flowFile != null) {
-            session.remove(flowFile);
-            flowFiles.remove(0);
-            if (writer != null) {
-                try {
-                    writer.close();
-                    outputStream.close();
-                } catch (IOException ioe) {
-                    getLogger().warn("Failed to close Record Writer due to {}", ioe.getLocalizedMessage(), ioe);
-                }
-            }
+    private void completeFlowFileState(final FlowFileState flowFileState, final BatchProcessingContext batchProcessingContext)
+            throws FlowFileCompletionException {
+        final ProcessSession session = batchProcessingContext.session();
+        final List<FlowFile> flowFiles = batchProcessingContext.flowFiles();
+        if (flowFileState.isFlowFileEmpty()) {
+            dropFlowFileState(flowFileState, batchProcessingContext);
+            return;
         }
+        try {
+            flowFileState.writer.finishRecordSet();
+            closeSafe(flowFileState.asClosable(), "FlowFile State");
+            reportProvenance(session, flowFileState.flowFile, null, null, batchProcessingContext.stopWatch());
+
+            final Map<String, String> attributes = getDefaultAttributes(flowFileState.lastSuccessfulWriteInfo.kinesisRecord);
+            attributes.put("record.count", String.valueOf(flowFileState.lastSuccessfulWriteInfo.writeResult.getRecordCount()));
+            attributes.put(CoreAttributes.MIME_TYPE.key(), flowFileState.writer.getMimeType());
+            attributes.putAll(flowFileState.lastSuccessfulWriteInfo.writeResult.getAttributes());
+            final int flowFileIndex = flowFiles.indexOf(flowFileState.flowFile);
+            flowFiles.set(flowFileIndex, session.putAllAttributes(flowFileState.flowFile, attributes));
+        } catch (final IOException e) {
+            dropFlowFileState(flowFileState, batchProcessingContext);
+            final String message = "Failed to complete a FlowFile containing records from Stream Name: %s, Shard Id: %s, Sequence/Subsequence No range: [%s/%d, %s/%d)".formatted(
+                    getStreamName(),
+                    getKinesisShardId(),
+                    flowFileState.firstSuccessfulWriteInfo.kinesisRecord.sequenceNumber(),
+                    flowFileState.firstSuccessfulWriteInfo.kinesisRecord.subSequenceNumber(),
+                    flowFileState.lastSuccessfulWriteInfo.kinesisRecord.sequenceNumber(),
+                    flowFileState.lastSuccessfulWriteInfo.kinesisRecord.subSequenceNumber()
+            );
+            throw new FlowFileCompletionException(message, e, flowFileState);
+        }
+    }
+
+    private void dropFlowFileState(final FlowFileState flowFileState, final BatchProcessingContext batchProcessingContext) {
+        closeSafe(flowFileState.asClosable(), "FlowFile State");
+        batchProcessingContext.session().remove(flowFileState.flowFile);
+        batchProcessingContext.flowFiles().remove(flowFileState.flowFile);
+    }
+
+    private void outputRawRecordOnException(final ProcessSession session, final byte[] data, final KinesisClientRecord kinesisRecord, final Exception e) {
         FlowFile failed = session.create();
         session.write(failed, o -> o.write(data));
         final Map<String, String> attributes = getDefaultAttributes(kinesisRecord);
@@ -199,5 +236,72 @@ public class KinesisRecordProcessorRecord extends AbstractKinesisRecordProcessor
         final String sequenceNumber = kinesisRecord.sequenceNumber();
         final Instant approximateArrivalTimestamp = kinesisRecord.approximateArrivalTimestamp();
         return getDefaultAttributes(sequenceNumber, partitionKey, approximateArrivalTimestamp);
+    }
+
+    private void closeSafe(final Closeable closeable, final String closeableName) {
+        closeSafe(closeable, closeableName, getLogger());
+    }
+
+    private static void closeSafe(final Closeable closeable, final String closeableName, final ComponentLog logger) {
+        if (closeable != null) {
+            try {
+                closeable.close();
+            } catch (final IOException e) {
+                logger.warn("Failed to close {}", closeableName, e);
+            }
+        }
+    }
+
+    record SuccessfulWriteInfo(KinesisClientRecord kinesisRecord, WriteResult writeResult) { }
+
+    static class FlowFileState {
+        private final FlowFile flowFile;
+        private final RecordSetWriter writer;
+        private final OutputStream outputStream;
+        private final RecordSchema recordSchema;
+        private final ComponentLog componentLog;
+        private SuccessfulWriteInfo firstSuccessfulWriteInfo;
+        private SuccessfulWriteInfo lastSuccessfulWriteInfo;
+
+        private FlowFileState(final FlowFile flowFile, final RecordSetWriter writer, final OutputStream outputStream, final RecordSchema recordSchema, final ComponentLog componentLog) {
+            this.flowFile = flowFile;
+            this.writer = writer;
+            this.outputStream = outputStream;
+            this.recordSchema = recordSchema;
+            this.componentLog = componentLog;
+        }
+
+        private boolean isFlowFileEmpty() {
+            return lastSuccessfulWriteInfo == null;
+        }
+
+        @SuppressWarnings("BooleanMethodIsAlwaysInverted")
+        private boolean containsDataFromExactlyOneKinesisRecord() {
+            return !isFlowFileEmpty() && firstSuccessfulWriteInfo.kinesisRecord == lastSuccessfulWriteInfo.kinesisRecord;
+        }
+
+        private void write(final Record outputRecord, final KinesisClientRecord kinesisRecord) throws IOException {
+            final WriteResult writeResult = writer.write(outputRecord);
+            firstSuccessfulWriteInfo = firstSuccessfulWriteInfo == null
+                    ? new SuccessfulWriteInfo(kinesisRecord, writeResult)
+                    : firstSuccessfulWriteInfo;
+            lastSuccessfulWriteInfo = new SuccessfulWriteInfo(kinesisRecord, writeResult);
+        }
+
+        private Closeable asClosable() {
+            return () -> {
+                closeSafe(writer, "Record Writer", componentLog);
+                closeSafe(outputStream, "Output Stream", componentLog);
+            };
+        }
+    }
+
+    static class FlowFileCompletionException extends Exception {
+        private final FlowFileState flowFileState;
+
+        private FlowFileCompletionException(final String message, final Throwable cause, final FlowFileState flowFileState) {
+            super(message, cause);
+            this.flowFileState = flowFileState;
+        }
     }
 }
