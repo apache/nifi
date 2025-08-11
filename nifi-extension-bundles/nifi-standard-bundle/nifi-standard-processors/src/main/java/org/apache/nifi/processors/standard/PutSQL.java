0709235bfaf2bbd08ba1cc9031c551d4912af253
shubhrajain475 <shubhrajain380@gmail.com>
2025-08-11 15:58:25 +0530

    [NIFI-9653] Fix PutSQL to route duplicate key exceptions to failure

--- nifi-extension-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/PutSQL.java
+++ nifi-extension-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/PutSQL.java
@@ -659,14 +659,19 @@ public class PutSQL extends AbstractSessionFactoryProcessor {
 
         exceptionHandler = new ExceptionHandler<>();
         exceptionHandler.mapException(e -> {
-            if (e instanceof SQLNonTransientException) {
-                return ErrorTypes.InvalidInput;
-            } else if (e instanceof SQLException) {
-                return ErrorTypes.TemporalFailure;
-            } else {
-                return ErrorTypes.UnknownFailure;
+          if (e instanceof SQLException sqlEx) {
+            if (isDuplicateKeyException(sqlEx)) {
+            getLogger().debug("Detected duplicate key/integrity constraint violation: {}", sqlEx);
+            return ErrorTypes.InvalidInput; // Route to failure
             }
-        });
+            if (sqlEx instanceof SQLNonTransientException) {
+            return ErrorTypes.InvalidInput;
+          }
+        return ErrorTypes.TemporalFailure;
+        } else {
+            return ErrorTypes.UnknownFailure;
+        }
+    });
         adjustError = RollbackOnFailure.createAdjustError(getLogger());
         exceptionHandler.adjustError(adjustError);
     }
@@ -894,6 +899,26 @@ public class PutSQL extends AbstractSessionFactoryProcessor {
 
         return session.putAllAttributes(flowFile, attributes);
     }
+
+    
+    private boolean isDuplicateKeyException(SQLException e) {
+    // SQLState 23000 is integrity constraint violation (ANSI SQL)
+      if ("23000".equals(e.getSQLState())) {
+        return true;
+       }
+    // MySQL: 1062, PostgreSQL: 23505
+        int errorCode = e.getErrorCode();
+        if (errorCode == 1062 || errorCode == 23505) {
+        return true;
+        }
+    // Recursively check cause
+        Throwable cause = e.getCause();
+        if (cause instanceof SQLException) {
+         return isDuplicateKeyException((SQLException) cause);
+        }
+     return false;
+}
+// ...existing code...
 
     /**
      * A FlowFileFilter that is responsible for ensuring that the FlowFiles returned either belong
