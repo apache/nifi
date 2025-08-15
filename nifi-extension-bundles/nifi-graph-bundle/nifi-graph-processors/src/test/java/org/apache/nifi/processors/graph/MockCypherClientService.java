42ccc269938fcc7cd649358ae8ee55ef20277981
Matt Burgess <mattyb149@apache.org>
2025-08-15 16:50:15 -0400

    NIFI-14861: Add EnrichGraphRecord processor

--- nifi-extension-bundles/nifi-graph-bundle/nifi-graph-processors/src/test/java/org/apache/nifi/processors/graph/MockCypherClientService.java
+++ nifi-extension-bundles/nifi-graph-bundle/nifi-graph-processors/src/test/java/org/apache/nifi/processors/graph/MockCypherClientService.java
@@ -20,16 +20,20 @@ package org.apache.nifi.processors.graph;
 import org.apache.nifi.controller.AbstractControllerService;
 import org.apache.nifi.graph.GraphClientService;
 import org.apache.nifi.graph.GraphQueryResultCallback;
+import org.apache.nifi.processor.exception.ProcessException;
+import org.apache.nifi.util.Tuple;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MockCypherClientService extends AbstractControllerService implements GraphClientService {
 
     @Override
     public Map<String, String> executeQuery(String query, Map<String, Object> parameters, GraphQueryResultCallback handler) {
-        handler.process(Map.of("name", "John Smith", "age", 40), true);
-        handler.process(Map.of("name", "John Smith", "age", 40), false);
+        handler.process(Map.of("name", "John Smith", "age", 40, "relationship", "ASSOCIATED_WITH"), true);
+        handler.process(Map.of("name", "John Smith", "age", 40, "relationship", "ASSOCIATED_WITH"), false);
 
         Map<String, String> resultAttributes = new HashMap<>();
         resultAttributes.put(NODES_CREATED, String.valueOf(1));
@@ -46,5 +50,86 @@ public class MockCypherClientService extends AbstractControllerService implement
     @Override
     public String getTransitUrl() {
         return "mock://localhost:12345/fake_database";
+    }
+
+    @Override
+    public String generateSetPropertiesStatement(final String componentType,
+                                                 final List<Tuple<String, String>> identifiersAndValues,
+                                                 final String nodeType,
+                                                 final Map<String, Object> propertyMap) {
+
+        StringBuilder queryBuilder = switch (componentType) {
+            case GraphClientService.NODES_TYPE -> getNodeQueryBuilder(identifiersAndValues, nodeType);
+            case GraphClientService.EDGES_TYPE -> getEdgeQueryBuilder(identifiersAndValues, nodeType);
+            default -> throw new ProcessException("Unsupported component type: " + componentType);
+        };
+
+        queryBuilder.append(")\n")
+                .append("ON MATCH SET ");
+
+        List<String> setClauses = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : propertyMap.entrySet()) {
+            StringBuilder setClause = new StringBuilder("n.")
+                    .append(entry.getKey())
+                    .append(" = ");
+            if (entry.getValue() == null) {
+                setClause.append(" NULL");
+            } else {
+                setClause.append("'")
+                        .append(entry.getValue())
+                        .append("'");
+            }
+            setClauses.add(setClause.toString());
+        }
+        String setClauseString = String.join(", ", setClauses);
+        queryBuilder.append(setClauseString)
+                .append("\nON CREATE SET ")
+                .append(setClauseString);
+
+        return queryBuilder.toString();
+    }
+
+    private static StringBuilder getNodeQueryBuilder(List<Tuple<String, String>> identifiersAndValues, String nodeType) {
+        StringBuilder queryBuilder = new StringBuilder("MERGE (n");
+        if (nodeType != null && !nodeType.isEmpty()) {
+            queryBuilder.append(":").append(nodeType);
+        }
+
+        buildMatchClause(identifiersAndValues, queryBuilder);
+        return queryBuilder;
+    }
+
+    private static StringBuilder getEdgeQueryBuilder(List<Tuple<String, String>> identifiersAndValues, String edgeType) {
+        StringBuilder queryBuilder = new StringBuilder("MERGE (n)<-[e:");
+
+        if (edgeType == null || edgeType.isEmpty()) {
+            throw new ProcessException("Edge type must not be null or empty");
+        }
+        queryBuilder.append(edgeType);
+
+        buildMatchClause(identifiersAndValues, queryBuilder);
+        queryBuilder.append("]-> (x)");
+        return queryBuilder;
+    }
+
+    private static void buildMatchClause(List<Tuple<String, String>> identifiersAndValues, StringBuilder queryBuilder) {
+        if (!identifiersAndValues.isEmpty()) {
+            queryBuilder.append(" {");
+
+            List<String> identifierNamesAndValues = new ArrayList<>();
+            for (Tuple<String, String> identifierAndValue : identifiersAndValues) {
+                if (identifierAndValue == null || identifierAndValue.getKey() == null || identifierAndValue.getValue() == null) {
+                    throw new ProcessException("Identifiers and values must not be null");
+                }
+
+                final String identifierName = identifierAndValue.getKey();
+                final Object identifierObject = identifierAndValue.getValue();
+                if (identifierObject != null) {
+                    identifierNamesAndValues.add(identifierName + ": '" + identifierObject + "'");
+                }
+            }
+            queryBuilder.append(String.join(", ", identifierNamesAndValues))
+                    .append("}");
+        }
     }
 }
