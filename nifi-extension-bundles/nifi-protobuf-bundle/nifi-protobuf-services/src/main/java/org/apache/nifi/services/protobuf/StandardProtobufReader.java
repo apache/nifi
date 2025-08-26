a291971949f652aef89ab636d598349d5c30e31d
ForThoseWhoComeAfter <mbobowsk@gmail.com>
2025-08-26 09:48:47 +0200

    NIFI-14424 HexFormat

--- nifi-extension-bundles/nifi-protobuf-bundle/nifi-protobuf-services/src/main/java/org/apache/nifi/services/protobuf/StandardProtobufReader.java
+++ nifi-extension-bundles/nifi-protobuf-bundle/nifi-protobuf-services/src/main/java/org/apache/nifi/services/protobuf/StandardProtobufReader.java
@@ -50,6 +50,7 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.HexFormat;
 import java.util.List;
 import java.util.Map;
 
@@ -213,14 +214,7 @@ public class StandardProtobufReader extends SchemaRegistryService implements Rec
         }
         byte[] hash = digest.digest(
                 input.getBytes(StandardCharsets.UTF_8));
-        final StringBuilder hexString = new StringBuilder();
-        for (byte b : hash) {
-            final String hex = Integer.toHexString(0xff & b);
-            if (hex.length() == 1)
-                hexString.append('0');
-            hexString.append(hex);
-        }
-        return hexString.toString();
+        return HexFormat.of().formatHex(hash);
     }
 
     private SchemaDefinition createSchemaDefinitionFromRegistry(final Map<String, String> variables) throws SchemaNotFoundException, IOException {
