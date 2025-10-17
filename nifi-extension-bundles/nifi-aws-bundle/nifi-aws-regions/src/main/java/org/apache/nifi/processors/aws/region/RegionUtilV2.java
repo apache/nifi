939931fc2fc9245f31dbf69b8b8903f5eb31e17c
Alaksiej Ščarbaty <65507498+awelless@users.noreply.github.com>
2025-10-17 20:55:13 +0200

    NIFI-14727 Added new ConsumeKinesis Processor (#10077)
    Co-authored-by: ForThoseWhoComeAfter <mbobowsk@gmail.com>
    Co-authored-by: Mark Payne <markap14@hotmail.com>
    Signed-off-by: David Handermann <exceptionfactory@apache.org>

--- nifi-extension-bundles/nifi-aws-bundle/nifi-aws-regions/src/main/java/org/apache/nifi/processors/aws/region/RegionUtilV2.java
+++ nifi-extension-bundles/nifi-aws-bundle/nifi-aws-regions/src/main/java/org/apache/nifi/processors/aws/region/RegionUtilV2.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.nifi.processors.aws.v2;
+package org.apache.nifi.processors.aws.region;
 
 import org.apache.nifi.components.AllowableValue;
 import software.amazon.awssdk.regions.Region;
