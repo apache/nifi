a577453d50244e8aaac25d4aa5336804789324d9
Eric Secules <eric.secules@macrohealth.com>
2025-11-19 08:29:24 -0800

    NIFI-15162 change FlowMetricsReportingStrategy to a no-frills enum because we do not make use of any of the extra descriptions anymore.

--- nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-api/src/main/java/org/apache/nifi/web/api/request/FlowMetricsReportingStrategy.java
+++ nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-api/src/main/java/org/apache/nifi/web/api/request/FlowMetricsReportingStrategy.java
@@ -16,34 +16,7 @@
  */
 package org.apache.nifi.web.api.request;
 
-
-import org.apache.nifi.components.AllowableValue;
-import org.apache.nifi.components.DescribedValue;
-
-public enum FlowMetricsReportingStrategy implements DescribedValue {
-    ALL_PROCESS_GROUPS(new AllowableValue("All Process Groups", "All Process Groups",
-            "Send metrics for each process group")),
-    ALL_COMPONENTS(new AllowableValue("All Components", "All Components",
-            "Send metrics for each component in the system, to include processors, connections, controller services, etc."));
-
-    private final AllowableValue strategy;
-
-    FlowMetricsReportingStrategy(final AllowableValue strategy) {
-        this.strategy = strategy;
-    }
-
-    @Override
-    public String getValue() {
-        return this.strategy.getValue();
-    }
-
-    @Override
-    public String getDisplayName() {
-        return this.strategy.getDisplayName();
-    }
-
-    @Override
-    public String getDescription() {
-        return this.strategy.getDescription();
-    }
+public enum FlowMetricsReportingStrategy {
+    ALL_PROCESS_GROUPS,
+    ALL_COMPONENTS;
 }
