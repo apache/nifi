48291ba17afa98d6316715d1be19301bdd2ea580
Alaksiej Ščarbaty <hahaha.funny.yellow.dog@gmail.com>
2025-12-16 17:04:39 +0100

    NIFI-15307 ConsumeKinesis. Wait for long initialization in onTrigger

--- nifi-extension-bundles/nifi-aws-bundle/nifi-aws-kinesis/src/test/java/org/apache/nifi/processors/aws/kinesis/ConsumeKinesisTest.java
+++ nifi-extension-bundles/nifi-aws-bundle/nifi-aws-kinesis/src/test/java/org/apache/nifi/processors/aws/kinesis/ConsumeKinesisTest.java
@@ -16,17 +16,25 @@
  */
 package org.apache.nifi.processors.aws.kinesis;
 
+import org.apache.nifi.processor.ProcessContext;
+import org.apache.nifi.processor.ProcessSession;
 import org.apache.nifi.processor.Relationship;
 import org.apache.nifi.processor.exception.ProcessException;
 import org.apache.nifi.processors.aws.credentials.provider.service.AWSCredentialsProviderControllerService;
 import org.apache.nifi.processors.aws.region.RegionUtil;
 import org.apache.nifi.reporting.InitializationException;
+import org.apache.nifi.util.MockProcessSession;
+import org.apache.nifi.util.SharedSessionState;
 import org.apache.nifi.util.TestRunner;
 import org.apache.nifi.util.TestRunners;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
+import java.time.Duration;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 import static org.apache.nifi.processors.aws.kinesis.ConsumeKinesis.PROCESSING_STRATEGY;
 import static org.apache.nifi.processors.aws.kinesis.ConsumeKinesis.ProcessingStrategy.FLOW_FILE;
@@ -36,6 +44,7 @@ import static org.apache.nifi.processors.aws.kinesis.ConsumeKinesis.REL_SUCCESS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Timeout.ThreadMode.SEPARATE_THREAD;
 
 class ConsumeKinesisTest {
 
@@ -65,14 +74,28 @@ class ConsumeKinesisTest {
     }
 
     @Test
+    // It takes around 30 seconds for a scheduler to fail in this test.
+    @Timeout(value = 3, unit = TimeUnit.MINUTES, threadMode = SEPARATE_THREAD)
     void failInitializationWithInvalidValues() {
         // KCL Scheduler initialization will fail, as the runner is configured with placeholder credentials.
 
         // Using the processor object to avoid error wrapping by testRunner.
         final ConsumeKinesis consumeKinesis = (ConsumeKinesis) testRunner.getProcessor();
+
+        final ProcessContext context = testRunner.getProcessContext();
+        final ProcessSession session = new MockProcessSession(new SharedSessionState(consumeKinesis, new AtomicLong()), consumeKinesis);
+
         final ProcessException ex = assertThrows(
                 ProcessException.class,
-                () -> consumeKinesis.setup(testRunner.getProcessContext()));
+                () -> {
+                    // The error might occur either in @OnScheduled...
+                    consumeKinesis.setup(context);
+                    while (true) {
+                        // ... or in onTrigger, if the initialization takes longer.
+                        consumeKinesis.onTrigger(context, session);
+                        Thread.sleep(Duration.ofSeconds(1));
+                    }
+                });
 
         assertNotNull(ex.getCause(), "The initialization exception is expected to have a cause");
     }
