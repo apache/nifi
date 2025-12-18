0b0ce89afa084392eae727aa69f1107614d64273
Pierre Villard <pierre.villard.fr@gmail.com>
2025-12-18 12:38:15 +0100

    NIFI-15364 - Fix QUIC NoClassDefFoundError for Azure Parameter Provider

--- nifi-extension-bundles/nifi-azure-bundle/nifi-azure-parameter-providers/src/main/java/org/apache/nifi/parameter/azure/AzureKeyVaultSecretsParameterProvider.java
+++ nifi-extension-bundles/nifi-azure-bundle/nifi-azure-parameter-providers/src/main/java/org/apache/nifi/parameter/azure/AzureKeyVaultSecretsParameterProvider.java
@@ -16,6 +16,8 @@
  */
 package org.apache.nifi.parameter.azure;
 
+import com.azure.core.http.HttpClient;
+import com.azure.core.http.jdk.httpclient.JdkHttpClientBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
 import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
@@ -190,7 +192,9 @@ public class AzureKeyVaultSecretsParameterProvider extends AbstractParameterProv
         final AzureCredentialsService credentialsService =
                 context.getProperty(AZURE_CREDENTIALS_SERVICE).asControllerService(AzureCredentialsService.class);
         final String vaultUrl = context.getProperty(KEY_VAULT_URI).getValue();
+        final HttpClient httpClient = new JdkHttpClientBuilder().build();
         return new SecretClientBuilder()
+                .httpClient(httpClient)
                 .credential(credentialsService.getCredentials())
                 .vaultUrl(vaultUrl)
                 .buildClient();
