2c4f2a64bd989887c3216e7207d83bc9e4ace0d6
Freedom9339 <reynaldo.rea48@gmail.com>
2026-02-17 16:50:10 +0000

    NIFI-15182: Fixed adding/removing user to multiple groups not  saving to all groups.

--- nifi-frontend/src/main/frontend/apps/nifi/src/app/pages/users/state/user-listing/user-listing.effects.ts
+++ nifi-frontend/src/main/frontend/apps/nifi/src/app/pages/users/state/user-listing/user-listing.effects.ts
@@ -263,17 +263,23 @@ export class UserListingEffects {
             ofType(UserListingActions.createUserSuccess),
             map((action) => action.response),
             filter((response) => response.userGroupUpdate != null),
-            mergeMap((createUserResponse) =>
-                this.actions$.pipe(
+            mergeMap((createUserResponse) => {
+                // @ts-ignore
+                const expectedCount = createUserResponse.userGroupUpdate.userGroups?.length || 0;
+                if (expectedCount === 0) {
+                    return of(UserListingActions.createUserComplete({ response: createUserResponse }));
+                }
+                return this.actions$.pipe(
                     ofType(UserListingActions.updateUserGroupSuccess),
                     filter(
                         (updateSuccess) =>
                             // @ts-ignore
                             createUserResponse.userGroupUpdate.requestId === updateSuccess.response.requestId
                     ),
+                    take(expectedCount),
                     map(() => UserListingActions.createUserComplete({ response: createUserResponse }))
-                )
-            )
+                );
+            })
         )
     );
 
@@ -525,7 +531,7 @@ export class UserListingEffects {
                     if (userGroupUpdates.length === 0) {
                         return of(UserListingActions.updateUserComplete());
                     } else {
-                        return userGroupUpdates;
+                        return from(userGroupUpdates);
                     }
                 } else {
                     return of(UserListingActions.updateUserComplete());
@@ -539,17 +545,28 @@ export class UserListingEffects {
             ofType(UserListingActions.updateUserSuccess),
             map((action) => action.response),
             filter((response) => response.userGroupUpdate != null),
-            mergeMap((updateUserResponse) =>
-                this.actions$.pipe(
+            mergeMap((updateUserResponse) => {
+                // @ts-ignore
+                const addedCount = updateUserResponse.userGroupUpdate.userGroupsAdded?.length || 0;
+                // @ts-ignore
+                const removedCount = updateUserResponse.userGroupUpdate.userGroupsRemoved?.length || 0;
+                const expectedCount = addedCount + removedCount;
+
+                if (expectedCount === 0) {
+                    return of(UserListingActions.updateUserComplete());
+                }
+
+                return this.actions$.pipe(
                     ofType(UserListingActions.updateUserGroupSuccess),
                     filter(
                         (updateSuccess) =>
                             // @ts-ignore
                             updateUserResponse.userGroupUpdate.requestId === updateSuccess.response.requestId
                     ),
+                    take(expectedCount),
                     map(() => UserListingActions.updateUserComplete())
-                )
-            )
+                );
+            })
         )
     );
 
@@ -622,7 +639,7 @@ export class UserListingEffects {
         this.actions$.pipe(
             ofType(UserListingActions.updateUserGroup),
             map((action) => action.request),
-            switchMap((request) =>
+            mergeMap((request) =>
                 from(this.usersService.updateUserGroup(request)).pipe(
                     map((response) =>
                         UserListingActions.updateUserGroupSuccess({
