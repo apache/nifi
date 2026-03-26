9200763c847f5eeeb181bd4cac143826c54f808d
Pierre Villard <pierre.villard.fr@gmail.com>
2026-03-26 22:12:31 +0100

    missing file...

---
+++ nifi-extension-bundles/nifi-gcp-bundle/nifi-gcp-processors/src/main/java/org/apache/nifi/processors/gcp/drive/GoogleDriveApiScope.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.processors.gcp.drive;
+
+import org.apache.nifi.components.DescribedValue;
+
+public enum GoogleDriveApiScope implements DescribedValue {
+    DRIVE_SCOPES("Drive Scopes",
+            "Uses Drive-specific scopes. Recommended for most setups including service accounts with domain-wide delegation."),
+    CLOUD_PLATFORM("Cloud Platform",
+            "Uses the broader cloud-platform scope. Required when using Workload Identity Federation with service account impersonation.");
+
+    private final String displayName;
+    private final String description;
+
+    GoogleDriveApiScope(final String displayName, final String description) {
+        this.displayName = displayName;
+        this.description = description;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String getValue() {
+        return name();
+    }
+}
