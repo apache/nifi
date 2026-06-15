e746e5effa5e50f837178ec7a5a50ea2cdcde888
dan-s1 <dstieg1@gmail.com>
2026-06-15 14:34:18 +0000

    NIFI-15975 Removed unused import.

--- nifi-extension-bundles/nifi-jslt-bundle/nifi-jslt-processors/src/test/java/org/apache/nifi/processors/jslt/TestJSLTTransformJSON.java
+++ nifi-extension-bundles/nifi-jslt-bundle/nifi-jslt-processors/src/test/java/org/apache/nifi/processors/jslt/TestJSLTTransformJSON.java
@@ -45,7 +45,6 @@ import static org.apache.nifi.processors.jslt.JSLTTransformJSON.TransformationSt
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestJSLTTransformJSON {
 
