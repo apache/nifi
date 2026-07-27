d8aae33a981a85dcf96cc06eeec38b6cbd3e2768
David Handermann <exceptionfactory@apache.org>
2026-07-27 09:50:14 -0500

    NIFI-16146 Fixed FileUtils sanitization of UTF-16 in filenames (#11478)

--- nifi-commons/nifi-utils/src/main/java/org/apache/nifi/util/file/FileUtils.java
+++ nifi-commons/nifi-utils/src/main/java/org/apache/nifi/util/file/FileUtils.java
@@ -586,6 +586,8 @@ public class FileUtils {
     private static final int[] INVALID_CHARS = {34, 60, 62, 124, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
             16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 58, 42, 63, 92, 47};
 
+    private static final char REPLACEMENT_CHARACTER = '_';
+
     static {
         Arrays.sort(INVALID_CHARS);
     }
@@ -597,7 +599,7 @@ public class FileUtils {
      * @param filename The filename to clean
      * @return sanitized filename
      */
-    public static String getSanitizedFilename(String filename) {
+    public static String getSanitizedFilename(final String filename) {
         if (filename == null) {
             return null;
         }
@@ -605,15 +607,17 @@ public class FileUtils {
             return "";
         }
 
-        final int codePointCount = filename.codePointCount(0, filename.length());
+        final int filenameLength = filename.length();
         final StringBuilder cleanName = new StringBuilder();
-        for (int i = 0; i < codePointCount; i++) {
+        for (int i = 0; i < filenameLength;) {
             final int c = filename.codePointAt(i);
             if (Arrays.binarySearch(INVALID_CHARS, c) < 0) {
                 cleanName.appendCodePoint(c);
             } else {
-                cleanName.append('_');
+                cleanName.append(REPLACEMENT_CHARACTER);
             }
+            // Advance index based on character count to handle UTF-16
+            i += Character.charCount(c);
         }
         return cleanName.toString();
     }
