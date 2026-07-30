91293424dbcc36a6ce5608087283d994a69b928b
Shubham Sharma <shubh.luck@yahoo.in>
2026-07-30 15:39:18 -0400

    NIFI-16159 Add support for FileResourceService in PutFile

--- nifi-extension-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/PutFile.java
+++ nifi-extension-bundles/nifi-standard-bundle/nifi-standard-processors/src/main/java/org/apache/nifi/processors/standard/PutFile.java
@@ -27,6 +27,7 @@ import org.apache.nifi.components.PropertyDescriptor;
 import org.apache.nifi.components.ValidationResult;
 import org.apache.nifi.components.Validator;
 import org.apache.nifi.expression.ExpressionLanguageScope;
+import org.apache.nifi.fileresource.service.api.FileResource;
 import org.apache.nifi.flowfile.FlowFile;
 import org.apache.nifi.flowfile.attributes.CoreAttributes;
 import org.apache.nifi.logging.ComponentLog;
@@ -36,11 +37,14 @@ import org.apache.nifi.processor.ProcessSession;
 import org.apache.nifi.processor.Relationship;
 import org.apache.nifi.processor.exception.ProcessException;
 import org.apache.nifi.processor.util.StandardValidators;
+import org.apache.nifi.processors.transfer.ResourceTransferSource;
 import org.apache.nifi.util.StopWatch;
 
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.PosixFileAttributeView;
 import java.nio.file.attribute.PosixFilePermissions;
 import java.nio.file.attribute.UserPrincipalLookupService;
@@ -48,10 +52,15 @@ import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static org.apache.nifi.processors.transfer.ResourceTransferProperties.FILE_RESOURCE_SERVICE;
+import static org.apache.nifi.processors.transfer.ResourceTransferProperties.RESOURCE_TRANSFER_SOURCE;
+import static org.apache.nifi.processors.transfer.ResourceTransferUtils.getFileResource;
 
 @SupportsBatching
 @InputRequirement(Requirement.INPUT_REQUIRED)
@@ -158,7 +167,9 @@ public class PutFile extends AbstractProcessor {
             CHANGE_LAST_MODIFIED_TIME,
             CHANGE_PERMISSIONS,
             CHANGE_OWNER,
-            CHANGE_GROUP
+            CHANGE_GROUP,
+            RESOURCE_TRANSFER_SOURCE,
+            FILE_RESOURCE_SERVICE
     );
 
     public static final int MAX_FILE_LOCK_ATTEMPTS = 10;
@@ -304,7 +315,15 @@ public class PutFile extends AbstractProcessor {
                 }
             }
 
-            session.exportTo(flowFile, dotCopyFile, false);
+            final ResourceTransferSource resourceTransferSource = context.getProperty(RESOURCE_TRANSFER_SOURCE).asAllowableValue(ResourceTransferSource.class);
+            final Optional<FileResource> fileResource = getFileResource(resourceTransferSource, context, flowFile.getAttributes());
+            if (fileResource.isPresent()) {
+                try (InputStream in = fileResource.get().getInputStream()) {
+                    Files.copy(in, dotCopyFile, StandardCopyOption.REPLACE_EXISTING);
+                }
+            } else {
+                session.exportTo(flowFile, dotCopyFile, false);
+            }
 
             final String lastModifiedTime = context.getProperty(CHANGE_LAST_MODIFIED_TIME).evaluateAttributeExpressions(flowFile).getValue();
             if (lastModifiedTime != null && !lastModifiedTime.isBlank()) {
