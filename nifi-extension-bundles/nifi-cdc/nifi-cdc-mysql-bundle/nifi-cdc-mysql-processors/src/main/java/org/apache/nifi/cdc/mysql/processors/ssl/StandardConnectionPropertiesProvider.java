8ac3c9a84659a7f55a640f9866c979ea33fa4554
David Handermann <exceptionfactory@apache.org>
2026-08-15 07:20:04 -0500

    NIFI-16207 Switched CaptureChangeMySQL to SSLContextProvider (#11551)
    - Required MySQL Connector/J 8.1.0 as the minimum version
    - Added DelegatingSSLContextProvider for configuring supplied SSLContext from SSLContextProvider

--- nifi-extension-bundles/nifi-cdc/nifi-cdc-mysql-bundle/nifi-cdc-mysql-processors/src/main/java/org/apache/nifi/cdc/mysql/processors/ssl/StandardConnectionPropertiesProvider.java
+++ nifi-extension-bundles/nifi-cdc/nifi-cdc-mysql-bundle/nifi-cdc-mysql-processors/src/main/java/org/apache/nifi/cdc/mysql/processors/ssl/StandardConnectionPropertiesProvider.java
@@ -17,8 +17,6 @@
 package org.apache.nifi.cdc.mysql.processors.ssl;
 
 import com.github.shyiko.mysql.binlog.network.SSLMode;
-import org.apache.nifi.security.util.TlsConfiguration;
-import org.apache.nifi.security.util.TlsPlatform;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -28,77 +26,22 @@ import java.util.Objects;
  * Standard implementation of Connection Properties Provider
  */
 public class StandardConnectionPropertiesProvider implements ConnectionPropertiesProvider {
-    private static final String COMMA_SEPARATOR = ",";
 
     private final SSLMode sslMode;
 
-    private final TlsConfiguration tlsConfiguration;
-
-    public StandardConnectionPropertiesProvider(
-            final SSLMode sslMode,
-            final TlsConfiguration tlsConfiguration
-    ) {
+    public StandardConnectionPropertiesProvider(final SSLMode sslMode) {
         this.sslMode = Objects.requireNonNull(sslMode, "SSL Mode required");
-        this.tlsConfiguration = tlsConfiguration;
     }
 
     /**
-     * Get Connection Properties based on SSL Mode and TLS Configuration
+     * Get Connection Properties based on the configured SSL Mode
      *
      * @return JDBC Connection Properties
      */
     @Override
     public Map<String, String> getConnectionProperties() {
         final Map<String, String> properties = new LinkedHashMap<>();
-
-        if (SSLMode.DISABLED == sslMode) {
-            properties.put(SecurityProperty.USE_SSL.getProperty(), Boolean.FALSE.toString());
-        } else {
-            // Enable TLS negotiation for all modes
-            properties.put(SecurityProperty.USE_SSL.getProperty(), Boolean.TRUE.toString());
-
-            if (SSLMode.PREFERRED == sslMode) {
-                properties.put(SecurityProperty.REQUIRE_SSL.getProperty(), Boolean.FALSE.toString());
-            } else {
-                // Modes other than preferred require SSL
-                properties.put(SecurityProperty.REQUIRE_SSL.getProperty(), Boolean.TRUE.toString());
-            }
-
-            if (SSLMode.VERIFY_IDENTITY == sslMode) {
-                properties.put(SecurityProperty.VERIFY_SERVER_CERTIFICATE.getProperty(), Boolean.TRUE.toString());
-            }
-
-            if (tlsConfiguration == null) {
-                // Set preferred protocols based on Java platform configuration
-                final String protocols = String.join(COMMA_SEPARATOR, TlsPlatform.getPreferredProtocols());
-                properties.put(SecurityProperty.ENABLED_TLS_PROTOCOLS.getProperty(), protocols);
-            } else {
-                final Map<String, String> certificateProperties = getCertificateProperties();
-                properties.putAll(certificateProperties);
-            }
-        }
-
-        return properties;
-    }
-
-    private Map<String, String> getCertificateProperties() {
-        final Map<String, String> properties = new LinkedHashMap<>();
-
-        final String protocols = String.join(COMMA_SEPARATOR, tlsConfiguration.getEnabledProtocols());
-        properties.put(SecurityProperty.ENABLED_TLS_PROTOCOLS.getProperty(), protocols);
-
-        if (tlsConfiguration.isKeystorePopulated()) {
-            properties.put(SecurityProperty.CLIENT_CERTIFICATE_KEY_STORE_URL.getProperty(), tlsConfiguration.getKeystorePath());
-            properties.put(SecurityProperty.CLIENT_CERTIFICATE_KEY_STORE_TYPE.getProperty(), tlsConfiguration.getKeystoreType().getType());
-            properties.put(SecurityProperty.CLIENT_CERTIFICATE_KEY_STORE_PASSWORD.getProperty(), tlsConfiguration.getKeystorePassword());
-        }
-
-        if (tlsConfiguration.isTruststorePopulated()) {
-            properties.put(SecurityProperty.TRUST_CERTIFICATE_KEY_STORE_URL.getProperty(), tlsConfiguration.getTruststorePath());
-            properties.put(SecurityProperty.TRUST_CERTIFICATE_KEY_STORE_TYPE.getProperty(), tlsConfiguration.getTruststoreType().getType());
-            properties.put(SecurityProperty.TRUST_CERTIFICATE_KEY_STORE_PASSWORD.getProperty(), tlsConfiguration.getTruststorePassword());
-        }
-
+        properties.put(SecurityProperty.SSL_MODE.getProperty(), sslMode.toString());
         return properties;
     }
 }
